b1241d34173ec58ca2982ed5ee47c8e4f118b87f
jonikanerva <joni@devlab.fi>
2006-10-11 13:59:34 +0000

    Päivitetään vaan tarvittavat profiilit.
    profiilit != '' -> profiilit like '%$profiili%'"

--- kayttajaprofiilit.php
+++ kayttajaprofiilit.php
@@ -1,13 +1,13 @@
 <?php
 	require ("inc/parametrit.inc");
-	
+
 	echo "<font class='head'>".t("K‰ytt‰j‰profiilit").":</font><hr>";
-	
-	
+
+
 	//tehd‰‰n tsekki, ett‰ ei tehd‰ profiilia samannimiseksi kuin joku k‰ytt‰j‰
 	if ($profiili != '') {
 		$query = "	SELECT nimi
-					FROM kuka
+					FROM kuka use index (kuka_index)
 					WHERE kuka='$profiili' and yhtio='$kukarow[yhtio]'";
 		$result = mysql_query($query) or pupe_error($query);
 
@@ -17,28 +17,29 @@
 			echo "<br><font class='error'>".t("VIRHE: Profiilin nimi on jo k‰ytˆss‰. Valitse toinen nimi")."</font><br><br>";
 		}
 	}
-	
-	
-	if ($tee=='POISTA') {		
+
+
+	if ($tee=='POISTA') {
 		if ($profiili != '') {
 			$query = "	DELETE
 						FROM oikeu
 						WHERE yhtio='$kukarow[yhtio]' and profiili='$profiili' and lukittu = ''";
 			$result = mysql_query($query) or pupe_error($query);
-			
+
 			$maara = mysql_affected_rows();
-			
+
 			echo "<font class='message'>".t("Poistettiin")." $maara ".t("rivi‰")."</font><br>";
-		
+
 			$profiili = "";
 		}
-	
+
 		$tee = '';
 	}
-	
-	
+
+
 	// tehd‰‰n oikeuksien p‰ivitys
 	if ($tee == 'PAIVITA') {
+
 		// poistetaan ihan aluksi kaikki.. iiik.
 		$query = "	DELETE
 					FROM oikeu
@@ -55,21 +56,21 @@
 
 				//haetaan menu itemi
 				$query = "	SELECT nimi, nimitys, jarjestys, alanimi, sovellus, jarjestys2
-							FROM oikeu
+							FROM oikeu use index (sovellus_index)
 							WHERE kuka='' and nimi='$nimi' and alanimi='$alanimi' and sovellus='$sov' and yhtio='$kukarow[yhtio]'";
 				$result = mysql_query($query) or pupe_error($query);
 				$trow = mysql_fetch_array($result);
 
-				$query = "	INSERT into oikeu 
+				$query = "	INSERT into oikeu
 							SET
-							kuka		= '$profiili', 
-							profiili	= '$profiili', 
-							sovellus	= '$trow[sovellus]', 
-							nimi		= '$trow[nimi]', 
-							alanimi 	= '$trow[alanimi]', 
+							kuka		= '$profiili',
+							profiili	= '$profiili',
+							sovellus	= '$trow[sovellus]',
+							nimi		= '$trow[nimi]',
+							alanimi 	= '$trow[alanimi]',
 							paivitys	= '',
 							lukittu		= '',
-							nimitys		= '$trow[nimitys]', 
+							nimitys		= '$trow[nimitys]',
 							jarjestys 	= '$trow[jarjestys]',
 							jarjestys2	= '$trow[jarjestys2]',
 							yhtio		= '$kukarow[yhtio]'";
@@ -84,66 +85,67 @@
 				list ($nimi, $alanimi, $sov) = split("#", $rastit);
 
 				$query = "	SELECT nimi
-							FROM oikeu
-							WHERE yhtio		= '$kukarow[yhtio]' 
+							FROM oikeu use index (sovellus_index)
+							WHERE yhtio		= '$kukarow[yhtio]'
 							and kuka		= '$profiili'
-							and profiili	= '$profiili' 
-							and nimi		= '$nimi' 
-							and alanimi		= '$alanimi' 
+							and profiili	= '$profiili'
+							and nimi		= '$nimi'
+							and alanimi		= '$alanimi'
 							and sovellus	= '$sov'";
 				$result = mysql_query($query) or pupe_error($query);
 
 				if (mysql_num_rows($result) == 1) {
-					$query = "	UPDATE oikeu 
-								SET paivitys 	= '1' 
-								where yhtio		= '$kukarow[yhtio]' 
+					$query = "	UPDATE oikeu
+								SET paivitys 	= '1'
+								where yhtio		= '$kukarow[yhtio]'
 								and kuka		= '$profiili'
 								and profiili	= '$profiili'
-								and nimi		= '$nimi' 
-								and alanimi		= '$alanimi' 
+								and nimi		= '$nimi'
+								and alanimi		= '$alanimi'
 								and sovellus	= '$sov'";
 					$result = mysql_query($query) or pupe_error($query);
 				}
 			}
 		}
-				
-		//p‰ivitet‰‰n k‰ytt‰jien profiilit
+
+
+		//p‰ivitet‰‰n k‰ytt‰jien profiilit (joilla on k‰ytˆss‰ t‰m‰ profiili)
 		$query = "	SELECT *
 					FROM kuka
-			 		WHERE yhtio='$kukarow[yhtio]' and profiilit!=''";
+			 		WHERE yhtio='$kukarow[yhtio]' and profiilit like '%$profiili%'";
 		$kres = mysql_query($query) or pupe_error($query);
-		
+
 		while ($krow = mysql_fetch_array($kres)) {
 			$profiilit = explode(',', $krow["profiilit"]);
-			
-			if (count($profiilit) > 0) {											
+
+			if (count($profiilit) > 0) {
 				//k‰yd‰‰n l‰pi k‰ytt‰j‰n kaikki profiilit
 				$triggeri = "";
 				foreach($profiilit as $prof) {
-					//jos t‰m‰ kyseinen profiili on ollut k‰ytt‰j‰ll‰ aikaisemmin, niin joudumme p‰ivitt‰m‰‰n oikeudet		
+					//jos t‰m‰ kyseinen profiili on ollut k‰ytt‰j‰ll‰ aikaisemmin, niin joudumme p‰ivitt‰m‰‰n oikeudet
 					if (strtoupper($prof) == strtoupper($profiili)) {
 						$triggeri = "HAPPY";
-					}							
+					}
 				}
-				
-				if ($triggeri == "HAPPY") {					
-					//poistetaan k‰ytt‰j‰n vanhat 
+
+				if ($triggeri == "HAPPY") {
+					//poistetaan k‰ytt‰j‰n vanhat
 					$query = "	DELETE FROM oikeu
 								WHERE yhtio='$kukarow[yhtio]' and kuka='$krow[kuka]' and lukittu=''";
-					$pres = mysql_query($query) or pupe_error($query);	
-															
+					$pres = mysql_query($query) or pupe_error($query);
+
 					//k‰yd‰‰n uudestaan profiili l‰pi
 					foreach($profiilit as $prof) {
 						$query = "	SELECT *
-									FROM oikeu
+									FROM oikeu use index (oikeudet_index)
 									WHERE yhtio='$kukarow[yhtio]' and kuka='$prof' and profiili='$prof'";
-						$pres = mysql_query($query) or pupe_error($query);	
-											
+						$pres = mysql_query($query) or pupe_error($query);
+
 						while ($trow = mysql_fetch_array($pres)) {
-							//joudumme tarkistamaan ettei t‰t‰ oikeutta ole jo t‰ll‰ k‰ytt‰j‰ll‰. 
+							//joudumme tarkistamaan ettei t‰t‰ oikeutta ole jo t‰ll‰ k‰ytt‰j‰ll‰.
 							//voi olla esim jos se on lukittuna annettu
 							$query = "	SELECT yhtio
-										FROM oikeu
+										FROM oikeu use index (sovellus_index)
 										WHERE kuka		= '$krow[kuka]'
 										and sovellus	= '$trow[sovellus]'
 										and nimi		= '$trow[nimi]'
@@ -152,70 +154,74 @@
 										and nimitys		= '$trow[nimitys]'
 										and jarjestys 	= '$trow[jarjestys]'
 										and jarjestys2	= '$trow[jarjestys2]'
-										and yhtio		= '$kukarow[yhtio]'";			
+										and yhtio		= '$kukarow[yhtio]'";
 							$tarkesult = mysql_query($query) or pupe_error($query);
-							
+
 							if (mysql_num_rows($tarkesult) == 0) {
-								$query = "	INSERT into oikeu 
+
+								$query = "	INSERT into oikeu
 											SET
-											kuka		= '$krow[kuka]', 
-											sovellus	= '$trow[sovellus]', 
-											nimi		= '$trow[nimi]', 
-											alanimi 	= '$trow[alanimi]', 
+											kuka		= '$krow[kuka]',
+											sovellus	= '$trow[sovellus]',
+											nimi		= '$trow[nimi]',
+											alanimi 	= '$trow[alanimi]',
 											paivitys	= '$trow[paivitys]',
-											nimitys		= '$trow[nimitys]', 
+											nimitys		= '$trow[nimitys]',
 											jarjestys 	= '$trow[jarjestys]',
 											jarjestys2	= '$trow[jarjestys2]',
-											yhtio		= '$kukarow[yhtio]'";			
+											yhtio		= '$kukarow[yhtio]'";
 								$rresult = mysql_query($query) or pupe_error($query);
-							}																							
-						}												
+							}
+						}
 					}
 				}
-			}			
+			}
 		}
 	}
-	
+
+	// end if paivita
+
+	require ("inc/footer.inc");
 	echo "<SCRIPT LANGUAGE=JAVASCRIPT>
 				function verify(){
 						msg = '".t("Haluatko todella poistaa t‰m‰n profiilin ja k‰ytt‰jilt‰ oikeudet t‰h‰n profiiliin?")."';
 						return confirm(msg);
 				}
 		</SCRIPT>";
-	
+
 	echo "<table>
-			
+
 			<form action='$PHP_SELF' method='post'>
 			<tr>
 				<th>".t("Luo uusi profiili").":</th>
 				<td><input type='text' name='uusiprofiili' size='25'></td>
 				<td class='back'><input type='submit' value='".t("Luo uusi profiili")."'></td>
-			</tr>	
+			</tr>
 			</form>
 			<form action='$PHP_SELF' method='post'>
 			<input type='hidden' name='sovellus' value='$sovellus'>
 			<input type='hidden' name='vainval' value='$vainval'>
-			
+
 			<tr>
 				<th>".t("Valitse Profiili").":</th>
 				<td><select name='profiili' onchange='submit()'>";
-		
-	
-	
+
+
+
 	if ($uusiprofiili == "") {
 		$query = "	SELECT distinct profiili
 					FROM oikeu
 					WHERE yhtio='$kukarow[yhtio]' and profiili!=''
 					ORDER BY profiili";
 		$kukares = mysql_query($query) or pupe_error($query);
-	
+
 		while ($kurow=mysql_fetch_array($kukares)) {
-			$sel = "";		
-			
+			$sel = "";
+
 			if ($profiili == $kurow["profiili"]) {
 				$sel = "SELECTED";
 			}
-			
+
 			echo "<option value='$kurow[profiili]' $sel>$kurow[profiili]</option>";
 		}
 		echo "</select>";
@@ -224,39 +230,39 @@
 		echo "<option value='$uusiprofiili'>$uusiprofiili</option></select>";
 		echo "<input type='hidden' name='uusiprofiili' value='$uusiprofiili'>";
 	}
-	
-	
+
+
 	echo "</td><td class='back'><input type='submit' value='".t("Valitse profiili")."'></form></td>";
-			
+
 	if ($profiili != '') {
 		echo "<form method='post' action='$PHP_SELF' onSubmit = 'return verify()'>
 				<input type='hidden' name='tee' value='POISTA'>
 				<input type='hidden' name='profiili' value='$profiili'>
 				<td class='back'><input type='submit' value='".t("Poista t‰m‰ profiili")."'></td></form>";
-	}		
-	
-	
+	}
+
+
 	echo "</tr></table><br><br>";
 
 	if ($profiili != '') {
 
 		echo "<table>";
-		
+
 		$query = "	SELECT distinct sovellus
 					FROM oikeu
 					where yhtio='$kukarow[yhtio]'
 					order by sovellus";
 		$result = mysql_query($query) or pupe_error($query);
-	
+
 		if (mysql_num_rows($result) > 1) {
-			
+
 			echo "	<form action='$PHP_SELF' name='vaihdaSovellus' method='POST'>
 					<input type='hidden' name='profiili' value='$profiili'>
 					<input type='hidden' name='uusiprofiili' value='$uusiprofiili'>
 					<tr><th>".t("Valitse sovellus").":</th><td>
 					<select name='sovellus' onchange='submit()'>
 					<option value=''>".t("Nayta kaikki")."</option>";
-	
+
 			while ($orow = mysql_fetch_array($result)) {
 				$sel = '';
 				if ($sovellus == $orow["sovellus"]) {
@@ -265,10 +271,10 @@
 				echo "<option value='$orow[sovellus]' $sel>$orow[sovellus]</option>";
 			}
 		}
-		
+
 		echo "</select></td></tr>";
-		
-		$chk = "";		
+
+		$chk = "";
 		if ($vainval != "") {
 			$chk = "CHECKED";
 			$lisa = " kuka=profiili and profiili = '$profiili' ";
@@ -276,57 +282,57 @@
 		else {
 			$lisa = " kuka = '' and profiili = '' ";
 		}
-		
+
 		echo "<tr><th>".t("N‰yt‰ vain ruksatut")."</th><td><input type='checkbox' name='vainval' $chk onClick='submit();'></td></tr>";
-	
+
 		echo "</table></form>";
-	
+
 		// n‰ytet‰‰n oikeuslista
 		echo "<table>";
-	
+
 		$query = "	SELECT *
 					FROM oikeu
-					WHERE 
-					$lisa 
+					WHERE
+					$lisa
 					and yhtio = '$kukarow[yhtio]'";
-		
+
 		if ($sovellus != '') {
 			$query .= " and sovellus = '$sovellus'";
 		}
-		
+
 		$query .= "	ORDER BY sovellus, jarjestys, jarjestys2";
 		$result = mysql_query($query) or pupe_error($query);
-	
-		
+
+
 		print " <SCRIPT TYPE=\"text/javascript\" LANGUAGE=\"JavaScript\">
 			<!--
-	
+
 			function toggleAll(toggleBox) {
-	
+
 				var currForm = toggleBox.form;
 				var isChecked = toggleBox.checked;
 				var nimi = toggleBox.name;
-				
-				for (var elementIdx=0; elementIdx<currForm.elements.length; elementIdx++) {											
+
+				for (var elementIdx=0; elementIdx<currForm.elements.length; elementIdx++) {
 					if (currForm.elements[elementIdx].type == 'checkbox' && currForm.elements[elementIdx].name.substring(0,3) == nimi) {
 						currForm.elements[elementIdx].checked = isChecked;
 					}
 				}
 			}
-	
+
 			//-->
 			</script>";
-		
-		
-		
+
+
+
 		echo "<form action='$PHP_SELF' name='suojax' method='post'>
 				<input type='hidden' name='tee' value='PAIVITA'>
 				<input type='hidden' name='sovellus' value='$sovellus'>
 				<input type='hidden' name='profiili' value='$profiili'>";
-	
+
 		while ($orow=mysql_fetch_array($result)) {
-	
-	
+
+
 			if ($vsove != $orow['sovellus']) {
 				echo "<tr><td class='back colspan='5'><br></td></tr>";
 				echo "<tr><th>".t("Sovellus")."</th>
@@ -335,56 +341,56 @@
 					<th>".t("P‰ivitys")."</th>
 					</tr>";
 			}
-	
+
 			$checked	= '';
 			$paivit		= '';
-			
+
 			$oq = "	SELECT *
 					FROM oikeu
-					WHERE yhtio		= '$kukarow[yhtio]' 
-					and kuka		= '$profiili' 
-					and profiili	= '$profiili' 
-					and nimi		= '$orow[nimi]' 
-					and alanimi		= '$orow[alanimi]' 
+					WHERE yhtio		= '$kukarow[yhtio]'
+					and kuka		= '$profiili'
+					and profiili	= '$profiili'
+					and nimi		= '$orow[nimi]'
+					and alanimi		= '$orow[alanimi]'
 					and sovellus	= '$orow[sovellus]'";
 			$or = mysql_query($oq) or pupe_error($oq);
-						
-			if (mysql_num_rows($or) != 0) {				
+
+			if (mysql_num_rows($or) != 0) {
 				$checked = "CHECKED";
-				
+
 				$oikeurow=mysql_fetch_array($or);
-				
+
 				if ($oikeurow["paivitys"] == 1) {
 					$paivit = "CHECKED";
-				}		
+				}
 			}
-	
+
 			echo "<tr><td>".t("$orow[sovellus]")."</td>";
-	
+
 			if ($orow['jarjestys2']!='0') {
 				echo "<td class='back'>--></td><td>";
 			}
 			else {
 				echo "<td colspan='2'>";
 			}
-	
+
 			echo "	".t("$orow[nimitys]")."</td>
 					<td align='center'><input type='checkbox' $checked 	value='$orow[nimi]#$orow[alanimi]#$orow[sovellus]' name='valittu[]'></td>
 					<td align='center'><input type='checkbox' $paivit  	value='$orow[nimi]#$orow[alanimi]#$orow[sovellus]' name='paivitys[]'></td>
 					</tr>";
-	
+
 			$vsove = $orow['sovellus'];
 		}
 		echo "<tr>
 				<th colspan='3'>".t("Ruksaa kaikki")."</th>
 				<td align='center'><input type='checkbox' name='val' onclick='toggleAll(this);'></td>
 				<td align='center'><input type='checkbox' name='pai' onclick='toggleAll(this)'></td>
-				</tr>";			
-		echo "</table>";		
-	
+				</tr>";
+		echo "</table>";
+
 		echo "<input type='submit' value='".t("P‰ivit‰ tiedot")."'></form>";
 	}
-			
+
 	require("inc/footer.inc");
 
 ?>

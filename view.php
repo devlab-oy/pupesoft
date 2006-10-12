c1bc28819d24a99f73e949913d6f00aabd44ec33
jonikanerva <joni@devlab.fi>
2006-10-12 09:41:01 +0000

    Eka versio liitetiedostojen katseluun tietokannasta. Tarvitaan uusi tietokanta:
    CREATE TABLE liitetiedostot (
    yhtio varchar(5) not null,
    liitos varchar(50) not null,
    liitostunnus int not null,
    data longblob NOT NULL,
    selite text NOT NULL,
    filename varchar(50) NOT NULL,
    filesize varchar(50) NOT NULL,
    filetype varchar(50) NOT NULL,
    tunnus int NOT NULL auto_increment,
    PRIMARY KEY (tunnus)
    );

---
+++ view.php
@@ -0,0 +1,23 @@
+<?php
+
+require ("inc/connect.inc");
+
+$id = (int) $id;
+
+$query = "select * from liitetiedostot where tunnus='$id'";
+$liiteres = mysql_query($query) or pupe_error($query);
+
+if (mysql_num_rows($liiteres) > 0) {
+
+	$liiterow = mysql_fetch_array($liiteres);
+
+	header("Content-type: $liiterow[filetype]");
+	header("Content-length: $liiterow[filesize]");
+	header("Content-Disposition: inline; filename=$liiterow[filename]");
+	header("Content-Description: $liiterow[selite]");
+
+	echo $liiterow["data"];
+
+}
+
+?>

38453656fc5e69a7485f849be530974596271c52
jonikanerva <joni@devlab.fi>
2006-10-20 10:49:43 +0000

    Sampon pankkitilejä ei osattu tarkistaa. Ei osata vieläkään.

--- inc/pankkitilinoikeellisuus.php
+++ inc/pankkitilinoikeellisuus.php
@@ -1,51 +1,70 @@
 <?php
-// Onko kyseessä väliviivallinen tilino, jos on poistetaan se...
+
+	// onko kyseessä väliviivallinen tilino, jos on poistetaan se...
 	$tarkiste = 0;
+
+	// katsotaan mikä pankki
 	$pankki = substr($pankkitili,0,1);
-	$pos = strrpos($pankkitili, "-");
-	while(!($pos === false)) { // note: three equal signs
-		// Väliviiva pois....
-		$pankkitili = substr($pankkitili,0,$pos) . substr($pankkitili,$pos+1);
-		$pos = strrpos($pankkitili, "-");
-		//echo "Viivanpoisto: '$pankkitili'<br>";
-	}
+
+	// poistetaan väliviiva
+	$pankkitili = str_replace("-", "", $pankkitili);
+
+	// jos tilinumero on liian lyhyt
 	if (strlen($pankkitili != 14)) {
-		if (($pankki == "4") or ($pankki == "5")) { // Pankit 4 ja 5 hoitavat tämän näin
-		//	echo "Tapaus 4 ja 5<br>";
+ 		// Pankit 4 ja 5 hoitavat tämän näin
+		if ($pankki == "4" or $pankki == "5") {
 			$alku = substr($pankkitili,0,7);
-			$nollat = substr("000000000",0, 14 - strlen($pankkitili)); 
+			$nollat = substr("000000000",0, 14 - strlen($pankkitili));
 			$loppu = substr($pankkitili,7);
 		}
-		else { // ja muut
+		else {
 			$alku = substr($pankkitili,0,6);
 			$nollat = substr("000000000",0, 14 - strlen($pankkitili));
 			$loppu = substr($pankkitili,6);
 		}
 		$pankkitili = $alku . $nollat. $loppu;
 	}
-//	echo "Pankkiili = '$pankkitili' Pituus " . strlen($pankkitili) . "<br>";
-// Tarkistussumman laskenta
-	for ($ip=0;  $ip<13; $ip++) {
-		if (($ip % 2) == 0) { // hmm, tilinumero on aina 14 pitkä, joten eka kerroin on aina 2
-			$kerroin = 2;
+	//echo "Pankkiili = '$pankkitili' Pituus " . strlen($pankkitili) . "<br>";
+
+	// jos pankki ei ole sampo, niin lasketaan tarkiste: Luhnin moduli 10
+	if ($pankki != "8") {
+
+		for ($ip=0;  $ip<13; $ip++) {
+	 		// hmm, tilinumero on aina 14 pitkä, joten eka kerroin on aina 2
+			if ($ip % 2 == 0) {
+				$kerroin = 2;
+			}
+			else {
+				$kerroin = 1;
+			}
+			$tulo = $kerroin * substr($pankkitili, $ip, 1);
+	 		// jos > 10, lasketaan numerot yhteen....
+			if ($tulo > 9) {
+				$tulo = substr($tulo, 0, 1) + substr($tulo, 1, 1);
+			}
+			$tarkiste += $tulo;
+			//echo "$kerroin " . substr($pankkitili, $i, 1) . " --> $tulo --> $tarkiste <br>";
 		}
-		else {
-			$kerroin = 1;
+
+	 	//echo "laskettu tarkiste: 10 - ".substr($tarkiste, -1)." = ";
+		$tarkiste = 10 - substr($tarkiste, -1); // Viimeinen merkki
+		//echo "$tarkiste<br>";
+
+		if ($tarkiste == 10) {
+			$tarkiste = 0;
 		}
-		$tulo = $kerroin * substr($pankkitili, $ip, 1);
-		if ($tulo > 9) { // jos > 10, lasketaan numerot yhteen....
-			$tulo = substr($tulo, 0, 1) + substr($tulo, 1, 1);
+
+	 	//echo "tilinumeron tarkiste: ".substr($pankkitili, 13, 1)."<br>";
+
+		if (substr($pankkitili, 13, 1) != $tarkiste) {
+			$pankkitili = "";
 		}
-		$tarkiste += $tulo;
-//		echo "$kerroin " . substr($pankkitili, $i, 1) . " --> $tulo --> $tarkiste <br>";
 	}
 
-	$tarkiste = 10 - substr($tarkiste, -1); // Viimeinen merkki
-	if ($tarkiste == 10) {
-		$tarkiste = 0;
+	// sampon tilinumeroiden laskentamenetelmä on Kerroin 137
+	if ($pankki == "8") {
+		// en jaksa koodata sampon checkiä, älkää tehkö virhettä sampon tilien kanssa!
+		echo "<font class='error'>Syötit Sampon pankkitilin, tilinumeron tarkistusta ei tehty!</font><br><br>";
 	}
-	if (substr($pankkitili, 13, 1) != $tarkiste) {
-		$pankkitili = "";		
-	}
-//	echo "$pankkitili";
+
 ?>

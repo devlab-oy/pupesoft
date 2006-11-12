46271075bd71dcd9c4f8dcecf3541bec0a552573
juppe <juppe@devlab.fi>
2006-11-12 12:33:49 +0000

    Voidaan selata ja noutaa tallennettuja maksuaineistoja palvelimelta.

---
+++ nouda_maksuaineisto.php
@@ -0,0 +1,85 @@
+<?php
+
+	if($_POST["tee"] == 'lataa_tiedosto') $lataa_tiedosto=1;
+	if($_POST["filenimi"] != '') $kaunisnimi=$_POST["filenimi"];
+
+	require("inc/parametrit.inc");
+
+	if ($tee == "lataa_tiedosto") {
+		echo file_get_contents("dataout/".$filenimi);
+	}
+	else {
+	
+		if (strtoupper($yhtiorow['maakoodi']) == 'FI') {
+			$kotimaa = "FI";
+		}
+		elseif (strtoupper($yhtiorow['maakoodi']) == 'SE') {
+			$kotimaa = "SE";
+		}
+		if (strtoupper($yhtiorow['maakoodi']) != 'FI' and strtoupper($yhtiorow['maakoodi']) != 'SE') {
+			echo "<font class='error'>".t("Yrityksen maakoodi ei ole sallittu")." (FI, SE) '$yhtiorow[maakoodi]'</font><br>";
+			exit;
+		}
+
+		echo "<font class='head'>".t("Nouda tallennettu maksuaineisto")."</font><hr><br><br>";
+
+		if ($kotimaa == "FI") {
+			echo "<font class='message'>Kotimaan LM03-maksuaineisto:</font><br>";
+		}
+		else {
+			echo "<font class='message'>Betalningsuppdrang via Bankgirot - Inrikesbetalningar:</font><hr>";
+		}
+		
+		$handle = opendir("dataout");
+
+		echo "<form method='post' action='$PHP_SELF'>";
+		echo "<input type='hidden' name='tee' value='lataa_tiedosto'>";
+		echo "<select name='filenimi' multiple='FALSE' size='10'>";
+
+		echo "<option value=''></option>";
+
+		while ($file = readdir($handle)) {
+			
+			if (($kotimaa == "FI" and substr($file,0, 4) == "lm03") or ($kotimaa == "SE" and substr($file,0, 4+strlen($kukarow["yhtio"])) == "bg-$kukarow[yhtio]-")) {
+				echo "<option value='$file' $sel>".t($file)."</option>";
+			}
+		}
+		closedir($handle);
+
+		echo "</select>";
+		echo "<input type='submit' value='Tallenna'></form>";
+		echo "<br><br>";
+	
+	
+	
+		if ($kotimaa == "FI") {
+			echo "<font class='message'>Ulkomaan LUM3-maksuaineisto:</font><br>";
+		}
+		else {
+			echo "<font class='message'>Betalningsuppdrang via Bankgirot - Utlandsbetalningar:</font><hr>";
+		}
+		
+		$handle = opendir("dataout");
+
+		echo "<form method='post' action='$PHP_SELF'>";
+		echo "<input type='hidden' name='tee' value='lataa_tiedosto'>";
+		echo "<select name='filenimi' multiple='FALSE' size='10'>";
+
+		echo "<option value=''></option>";
+
+		while ($file = readdir($handle)) {
+			
+			if (($kotimaa == "FI" and substr($file,0, 4) == "lum3") or ($kotimaa == "SE" and substr($file,0, 4+strlen($kukarow["yhtio"])) == "bg-$kukarow[yhtio]-")) {
+				echo "<option value='$file' $sel>".t($file)."</option>";
+			}
+		}
+		closedir($handle);
+
+		echo "</select>";
+		echo "<input type='submit' value='Tallenna'></form>";
+		echo "<br><br>";
+	
+		require ("inc/footer.inc");
+	}
+
+?>

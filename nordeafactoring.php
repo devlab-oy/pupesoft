f4ae401212c7dbed8c6677f13f54b94381829c85
juppe <juppe@devlab.fi>
2006-11-12 12:19:06 +0000

    Uusinta develiä. Vaatii joitain tietokantamuutokisa toimiakseen.
    mysql> alter table lasku add column factoringsiirtonumero int not null after jaksotettu;
    mysql> create index factoring on lasku (yhtio,factoringsiirtonumero);

--- nordeafactoring.php
+++ nordeafactoring.php
@@ -1,7 +1,16 @@
 <?php
+	
+	if($_POST["tee"] == 'lataa_tiedosto') $lataa_tiedosto=1;
+	if($_POST["kaunisnimi"] != '') $kaunisnimi=$_POST["kaunisnimi"];
+	
 	require('inc/parametrit.inc');
 	
-	echo "<font class='head'>Nordea Factoring siirtotiedosto:</font><hr><br>";
+	if ($tee == "lataa_tiedosto") {
+		echo file_get_contents($filenimi);
+	}
+	else {
+		echo "<font class='head'>".t("Nordea Factoring siirtotiedosto").":</font><hr><br>";
+	}
 	
 	if ($tee == '') {
 		//Käyttöliittymä
@@ -10,38 +19,49 @@
 	
 		echo "	<input type='hidden' name='tee' value='TULOSTA'>";
 	
-		echo "<tr>
-				<th>Syötä laskuvälin alku</th>
-				<td><input type='text' name='ppa' value='$ppa' size='3'></td>
-				</tr>
-				<tr>
-				<th>Syötä laskuvälin loppu</th>
-				<td><input type='text' name='ppl' value='$ppl' size='3'>
-				</td>
-				</tr>";
-		
-		$query = "	SELECT max(factoringsiirtonumero) factoringsiirtonumero
+		$query = "	SELECT min(laskunro) eka, max(laskunro) vika
 					FROM lasku
-					WHERE  yhtio='$kukarow[yhtio]'";
-		$aresult = mysql_query ($query) or pupe_error("$query");
+					JOIN maksuehto ON lasku.yhtio=maksuehto.yhtio and lasku.maksuehto=maksuehto.tunnus and maksuehto.factoring='N'
+					WHERE lasku.yhtio	= '$kukarow[yhtio]' 
+					and lasku.tila	  	= 'U' 
+					and lasku.alatila	= 'X' 
+					and lasku.summa		!= 0
+					and lasku.factoringsiirtonumero = 0";
+		$aresult = mysql_query ($query) or pupe_error($query);
 		$arow = mysql_fetch_array($aresult);
 		
-		$arow["factoringsiirtonumero"]++;
+		echo "<tr>
+				<th>Syötä laskuvälin alku:</th>
+				<td><input type='text' name='ppa' value='$arow[eka]' size='10'></td>
+				</tr>
+				<tr>
+				<th>Syötä laskuvälin loppu:</th>
+				<td><input type='text' name='ppl' value='$arow[vika]' size='10'></td>
+				</tr>";
 		
+		$query = "	SELECT max(factoringsiirtonumero)+1 seuraava
+					FROM lasku
+					WHERE  yhtio		= '$kukarow[yhtio]'
+					and lasku.tila	  	= 'U' 
+					and lasku.alatila	= 'X' 
+					and lasku.summa		!= 0
+					and lasku.factoringsiirtonumero > 0";
+		$aresult = mysql_query ($query) or pupe_error($query);
+		$arow = mysql_fetch_array($aresult);
+				
 		echo "<tr><th>Siirtoluettelon numero:</th>
-				<td><input type='text' name='factoringsiirtonumero' value='$arow[0]' size='6'></td>";
+				<td><input type='text' name='factoringsiirtonumero' value='$arow[seuraava]' size='6'></td>";
 		
 		
-		echo "<td class='back'><input type='submit' value='Lähetä'></td></tr></table><br><br>";
+		echo "<td class='back'><input type='submit' value='Luo siirtoaineisto'></td></tr></table><br><br>";
 	}
 	
 	if ($tee == 'TULOSTA') {
 
 		$luontipvm	= date("Ymd");
 		$luontiaika	= date("Hi");
-		
-		//luodaan eratietue
-		
+				
+		//Luodaan erätietue
 		$ulos  = sprintf ('%-4.4s', 	"KRFL");									//sovellustunnus
 		$ulos .= sprintf ('%01d',	 	"0");										//tietuetunnus
 		$ulos .= sprintf ('%-17.17s', 	str_replace('-','',$yhtiorow["ytunnus"]));	//myyjän ytunnus
@@ -58,202 +78,227 @@
 		$ulos .= "\r\n";
 		
 		
-		if ($ppl == '')
+		if ($ppl == '') {
 			$ppl = $ppa;
+		}
 		
-		if ($ppa == '' or $ppl == '') {
+		if ($ppa == '' or $ppl == '' or $ppl < $ppa) {
 			echo "Huono laskunumeroväli!";
 			exit;		
-		}	
+		}
+		
+		$dquery = "	SELECT lasku.yhtio 
+					FROM lasku
+					JOIN maksuehto ON lasku.yhtio=maksuehto.yhtio and lasku.maksuehto=maksuehto.tunnus and maksuehto.factoring='N'
+					WHERE lasku.yhtio	  = '$kukarow[yhtio]' 
+					and lasku.tila	  = 'U' 
+					and lasku.alatila	  = 'X' 
+					and lasku.summa 	 != 0
+					and lasku.laskunro >= '$ppa'
+					and lasku.laskunro <= '$ppl'
+					and lasku.factoringsiirtonumero = 0";
+		$dresult = mysql_query ($dquery) or pupe_error($dquery);
+		
+		if (mysql_num_rows($dresult) == 0) {
+			echo "Huono laskunumeroväli! Yhtään Nordeaan siirettävää laskua ei löytynyt!";
+			exit;	
+		}
 	
-		$kentat1 = "if(arvo >= 0, '01', '02') tyyppi,
-					ytunnus,
-					asiakasnro,
-					nimi,
-					nimitark,
-					osoite,
-					postino,
-					postitp,
-					laskunro,
-					viivastyskorko,
-					round(abs(summa*100),0) summa,
-					toim_nimi,
-					toim_nimitark,
-					toim_osoite,
-					toim_postino,
-					toim_postitp,
-					viite,
-					DATE_FORMAT(tapvm, '%y%m%d') paivays,
-					DATE_FORMAT(erpcm, '%y%m%d') erapvm,
-					tunnus";
+		$query = "	SELECT if(lasku.summa >= 0, '01', '02') tyyppi,
+					lasku.ytunnus,
+					lasku.nimi,
+					lasku.nimitark,
+					lasku.osoite,
+					lasku.postino,
+					lasku.postitp,
+					lasku.maakoodi,
+					lasku.laskunro,
+					round(lasku.viikorkopros*100,0) viikorkopros,
+					round(abs(lasku.summa*100),0) summa,
+					lasku.toim_nimi,
+					lasku.toim_nimitark,
+					lasku.toim_osoite,
+					lasku.toim_postino,
+					lasku.toim_postitp,
+					lasku.toim_maa,
+					lasku.viite,
+					DATE_FORMAT(lasku.tapvm, '%y%m%d') tapvm,
+					DATE_FORMAT(lasku.erpcm, '%y%m%d') erpcm,
+					lasku.tunnus,
+					lasku.valkoodi
+					FROM lasku
+					JOIN maksuehto ON lasku.yhtio=maksuehto.yhtio and lasku.maksuehto=maksuehto.tunnus and maksuehto.factoring='N'
+					WHERE lasku.yhtio	= '$kukarow[yhtio]' 
+					and lasku.tila	  	= 'U' 
+					and lasku.alatila	= 'X' 
+					and lasku.summa		!= 0
+					and lasku.laskunro >= '$ppa'
+					and lasku.laskunro <= '$ppl'
+					and lasku.factoringsiirtonumero = 0";
+		$laskures = mysql_query ($query) or pupe_error($query);
 		
-		$taulu1 = "lasku";
-		$mitka1 = "yhtio='$kukarow[yhtio]' and tila='U' and alatila='X' 
-					and laskunro >= '$ppa'
-					and laskunro <= '$ppl'";
-		
-	
-		$query = "SELECT $kentat1
-				FROM   $taulu1
-				WHERE  $mitka1";
-		$bresult = mysql_query ($query) 
-			or die ("Kysely ei onnistu $query");
-		
-		if (mysql_num_rows($bresult) > 0) {
+		if (mysql_num_rows($laskures) > 0) {
 			
 			$laskukpl  = 0;
 			$vlaskukpl = 0;
 			$vlaskusum = 0;
 			$hlaskukpl = 0;
 			$hlaskusum = 0;
-			$ulos1 = "";
 			
-			while ($brow = mysql_fetch_array($bresult)) {
+			echo "<table>";
+			echo "<tr><th>Tyyppi</th><th>Laskunumero</th><th>Nimi</th><th>Summa</th><th>Valuutta</th></tr>";
+						
+			while ($laskurow = mysql_fetch_array($laskures)) {
 			
 				$dquery = "	UPDATE lasku
-							SET factoringsiirtonumero='$factoringsiirtonumero'
-							WHERE  yhtio='$kukarow[yhtio]' and tunnus='$brow[tunnus]'";
-				$dresult = mysql_query ($dquery) 
-					or die ("Kysely ei onnistu $dquery");
-				//$drow = mysql_fetch_array($dresult);
+							SET factoringsiirtonumero = '$factoringsiirtonumero'
+							WHERE  yhtio	= '$kukarow[yhtio]' 
+							and tunnus		= '$laskurow[tunnus]'";
+				$dresult = mysql_query ($dquery) or pupe_error($dquery);
 				
 				//luodaan ostajatietue
-				$ulos1 .= sprintf ('%-4.4s', 	"KRFL");								//sovellustunnus
-				$ulos1 .= sprintf ('%01d',	 	"1");									//tietuetunnus
-				$ulos1 .= sprintf ('%06d',	 	$arow["rahoitussopnro"]);				//sopimusnumero
-				$ulos1 .= sprintf ('%06d',	 	$brow["asiakasnro"]);					//ostajan numero aka asiakasnumero
-				$ulos1 .= sprintf ('%-4.4s', 	"");
-				$ulos1 .= sprintf ('%-10.10s', 	str_replace('-','',$brow["ytunnus"]));	//ostajan ytunnus
-				$ulos1 .= sprintf ('%-30.30s', 	$brow["nimi"]);							//ostajan nimi
-				$ulos1 .= sprintf ('%-30.30s',  $brow["nimitark"]);						//ostajan nimitark
-				$ulos1 .= sprintf ('%-20.20s', 	$brow["osoite"]);						//ostajan osoite
-				$ulos1 .= sprintf ('%-20.20s', 	$brow["postino"]." ".$brow["postitp"]);	//ostajan postino ja postitp
-				$ulos1 .= sprintf ('%-13.13s', 	"");
-				$ulos1 .= sprintf ('%-30.30s', 	"");
-				$ulos1 .= sprintf ('%-13.13s', 	"");
-				$ulos1 .= sprintf ('%-13.13s', 	"");
-				$ulos1 .= sprintf ('%-2.2s', 	"FI");									//kieli
-				$ulos1 .= sprintf ('%-3.3s', 	"EUR");									//valuutta
-				$ulos1 .= sprintf ('%04d', 		round($brow["viivastyskorko"]*100,0));	//viivastyskorko
-				$ulos1 .= sprintf ('%03d', 		"0");
-				$ulos1 .= sprintf ('%06d',   	"0");
-				$ulos1 .= sprintf ('%-10.10s', 	"");
-				$ulos1 .= sprintf ('%-172.172s',"");
-				$ulos1 .= "\r\n";
+				$ulos .= sprintf ('%-4.4s', 	"KRFL");										//sovellustunnus
+				$ulos .= sprintf ('%01d',	 	"1");											//tietuetunnus
+				$ulos .= sprintf ('%06d',	 	$yhtiorow["rahoitussopnro"]);					//sopimusnumero
+				$ulos .= sprintf ('%06d',	 	$laskurow["asiakasnro"]);						//ostajan numero aka asiakasnumero
+				$ulos .= sprintf ('%-4.4s', 	"");
+				$ulos .= sprintf ('%-10.10s', 	str_replace('-','',$laskurow["ytunnus"]));		//ostajan ytunnus
+				$ulos .= sprintf ('%-30.30s', 	$laskurow["nimi"]);								//ostajan nimi
+				$ulos .= sprintf ('%-30.30s',  	$laskurow["nimitark"]);							//ostajan nimitark
+				$ulos .= sprintf ('%-20.20s', 	$laskurow["osoite"]);							//ostajan osoite
+				$ulos .= sprintf ('%-20.20s', 	$laskurow["postino"]." ".$laskurow["postitp"]);	//ostajan postino ja postitp
+				$ulos .= sprintf ('%-13.13s', 	"");
+				$ulos .= sprintf ('%-30.30s', 	"");
+				$ulos .= sprintf ('%-13.13s', 	"");
+				$ulos .= sprintf ('%-13.13s', 	"");
+				$ulos .= sprintf ('%-2.2s', 	"FI");											//kieli
+				$ulos .= sprintf ('%-3.3s', 	$laskurow["valkoodi"]);							//valuutta
+				$ulos .= sprintf ('%04d', 		$laskurow["viikorkopros"]);						//viivastyskorko
+				$ulos .= sprintf ('%03d', 		"0");
+				$ulos .= sprintf ('%06d',   	"0");
+				
+				if ($laskurow["maa"] != $yhtiorow["maakoodi"] and $laskurow["maa"] != '') {
+					$ulos .= sprintf ('%-10.10s', $laskurow["maa"]);
+				}
+				else {
+					$ulos .= sprintf ('%-10.10s', 	"");
+				}
+				
+				$ulos .= sprintf ('%-172.172s',"");
+				$ulos .= "\r\n";
 				
 				//luodaan laskutietue
-				$ulos1 .= sprintf ('%-4.4s', 	"KRFL");								//sovellustunnus
-				$ulos1 .= sprintf ('%01d',	 	"3");									//tietuetunnus
-				$ulos1 .= sprintf ('%06d',	 	$arow["rahoitussopnro"]);				//sopimusnumero
-				$ulos1 .= sprintf ('%06d',	 	$brow["asiakasnro"]);					//ostajan numero aka asiakasnumero
-				$ulos1 .= sprintf ('%-4.4s',   	"");
-				$ulos1 .= sprintf ('%010d',	 	$brow["laskunro"]);
-				$ulos1 .= sprintf ('%06d',	 	$brow["paivays"]);
-				$ulos1 .= sprintf ('%-3.3s', 	"EUR");
-				$ulos1 .= sprintf ('%06d', 		$brow["paivays"]);
-				$ulos1 .= sprintf ('%02d', 		$brow["tyyppi"]);
-				$ulos1 .= sprintf ('%012d', 	$brow["summa"]);
-				$ulos1 .= sprintf ('%06d', 		$brow["erapvm"]);
-				$ulos1 .= sprintf ('%06d', 		"0");
-				$ulos1 .= sprintf ('%06d', 		"0");
-				$ulos1 .= sprintf ('%06d', 		"0");
-				$ulos1 .= sprintf ('%06d', 		"0");
-				$ulos1 .= sprintf ('%012d',		"0");
-				$ulos1 .= sprintf ('%012d', 	"0");
-				$ulos1 .= sprintf ('%012d',		"0");
-				$ulos1 .= sprintf ('%012d', 	"0");
-				$ulos1 .= sprintf ('%012d', 	"0");
-				$ulos1 .= sprintf ('%024d',  	"0");
-				$ulos1 .= sprintf ('%01d', 		"0");
-				$ulos1 .= sprintf ('%01d', 		"0");
-				$ulos1 .= sprintf ('%01d', 		"0");
-				$ulos1 .= sprintf ('%01d', 		"0");
-				$ulos1 .= sprintf ('%02d',   	"0");
-				$ulos1 .= sprintf ('%010d',  	"0");
-				$ulos1 .= sprintf ('%04d',   	"0");
-				$ulos1 .= sprintf ('%-30.30s', 	$brow["toim_nimi"]);								//toimituspaikan nimi
-				$ulos1 .= sprintf ('%06d',	 	$brow["asiakasnro"]);								//asiakasnro
-				$ulos1 .= sprintf ('%010d', 	str_replace('-','',$brow["toim_ytunnus"]));			//toim  ytunnus
-				$ulos1 .= sprintf ('%-20.20s', 	$brow["toim_osoite"]);								//toim osoite
-				$ulos1 .= sprintf ('%-20.20s', 	$brow["toim_postino"]." ".$brow["toim_postitp"]);	//toim postitp ja postino
-				$ulos1 .= sprintf ('%-30.30s', 	"");
-				$ulos1 .= sprintf ('%013d', 	"0");
-				$ulos1 .= sprintf ('%-30.30s', 	"");
-				$ulos1 .= sprintf ('%06d', 		"0");
-				$ulos1 .= sprintf ('%-10.10s', 	"");
-				$ulos1 .= sprintf ('%03d', 		"0");
-				$ulos1 .= sprintf ('%020d', 	$brow["viite"]);
-				$ulos1 .= sprintf ('%-8.8s', 	"");
-				$ulos1 .= "\r\n";
+				$ulos .= sprintf ('%-4.4s', 	"KRFL");													//sovellustunnus
+				$ulos .= sprintf ('%01d',	 	"3");														//tietuetunnus
+				$ulos .= sprintf ('%06d',	 	$yhtiorow["rahoitussopnro"]);								//sopimusnumero
+				$ulos .= sprintf ('%06d',	 	$laskurow["asiakasnro"]);									//ostajan numero aka asiakasnumero
+				$ulos .= sprintf ('%-4.4s',   	"");														//varalla
+				$ulos .= sprintf ('%010d',	 	$laskurow["laskunro"]);										//laskunro
+				$ulos .= sprintf ('%06d',	 	$laskurow["tapvm"]);										//laskun päiväys
+				$ulos .= sprintf ('%-3.3s', 	$laskurow["valkoodi"]);										//valuutta
+				$ulos .= sprintf ('%06d', 		$laskurow["tapvm"]);										//laskun arvopäivä
+				$ulos .= sprintf ('%02d', 		$laskurow["tyyppi"]);										//laskun tyyppi 01-veloitus 02-hyvitys 03-viivästyskorkolasku jne...
+				$ulos .= sprintf ('%012d', 		$laskurow["summa"]);										//summa etumerkitön, sentteinä
+				$ulos .= sprintf ('%06d', 		$laskurow["erpcm"]);										//eräpäivä
+				$ulos .= sprintf ('%06d', 		"0");														//kassa-ale1 pvm
+				$ulos .= sprintf ('%06d', 		"0");
+				$ulos .= sprintf ('%06d', 		"0");
+				$ulos .= sprintf ('%06d', 		"0");
+				$ulos .= sprintf ('%012d',		"0");
+				$ulos .= sprintf ('%012d', 		"0");														//kassa-ale1 valuutassa
+				$ulos .= sprintf ('%012d',		"0");
+				$ulos .= sprintf ('%012d', 		"0");
+				$ulos .= sprintf ('%012d', 		"0");
+				$ulos .= sprintf ('%024d',  	"0");
+				$ulos .= sprintf ('%01d', 		"0");														//kassa-ale1 koodi 01-ei alennus 1-alennus
+				$ulos .= sprintf ('%01d', 		"0");
+				$ulos .= sprintf ('%01d', 		"0");
+				$ulos .= sprintf ('%01d', 		"0");
+				$ulos .= sprintf ('%02d',   	"0");
+				$ulos .= sprintf ('%010d',  	"0");
+				$ulos .= sprintf ('%04d',   	"0");														//alv (ei välitetä)
+				$ulos .= sprintf ('%-30.30s', 	$laskurow["toim_nimi"]);									//toimituspaikan nimi
+				$ulos .= sprintf ('%06d',	 	$laskurow["asiakasnro"]);									//asiakasnro
+				$ulos .= sprintf ('%010d', 		str_replace('-','',$laskurow["ytunnus"]));					//toim  ytunnus
+				$ulos .= sprintf ('%-20.20s', 	$laskurow["toim_osoite"]);									//toim osoite
+				$ulos .= sprintf ('%-20.20s', 	$laskurow["toim_postino"]." ".$laskurow["toim_postitp"]);	//toim postitp ja postino
+				$ulos .= sprintf ('%-30.30s', 	"");
+				$ulos .= sprintf ('%013d', 		"0");
+				$ulos .= sprintf ('%-30.30s', 	"");
+				$ulos .= sprintf ('%06d', 		"0");
+				
+				if ($laskurow["toim_maa"] != $yhtiorow["maakoodi"] and $laskurow["toim_maa"] != '') {
+					$ulos .= sprintf ('%-10.10s', $laskurow["toim_maa"]);
+				}
+				else {
+					$ulos .= sprintf ('%-10.10s', 	"");
+				}
+				
+				$ulos .= sprintf ('%03d', 		"0");
+				$ulos .= sprintf ('%020d', 	$laskurow["viite"]);
+				$ulos .= sprintf ('%-8.8s', 	"");
+				$ulos .= "\r\n";
 			
 				$laskukpl++;
-				if ($brow["tyyppi"] == "01") {
+				if ($laskurow["tyyppi"] == "01") {
 					$vlaskukpl++;
-					$vlaskusum += $brow["summa"];
-					echo "Veloituslasku: Summa --> $brow[summa]<br>";
+					$vlaskusum += $laskurow["summa"];
 					
+					echo "<tr><td>Veloituslasku</td><td>$laskurow[laskunro]</td><td>$laskurow[nimi]</td><td align='right'>".($laskurow["summa"]/100)."</td><td>$laskurow[valkoodi]</td></tr>";
 				}
-				if ($brow["tyyppi"] == "02") {
+				if ($laskurow["tyyppi"] == "02") {
 					$hlaskukpl++;
-					$hlaskusum += $brow["summa"];
-					echo "Hyvityslasku: Summa ---> $brow[summa]<br>";
+					$hlaskusum += $laskurow["summa"];
+					
+					echo "<tr><td>Hyvityslasku:</td><td>$laskurow[laskunro]</td><td>$laskurow[nimi]</td><td align='right'>".($laskurow["summa"]/100)."</td><td>$laskurow[valkoodi]</td></tr>";
 				}
 			
 			}
-			
-			echo "Yhteensä $vlaskukpl veloituslaskua ".round($vlaskusum/100,2)." EUR.<br>";
-			echo "Yhteensä $hlaskukpl hyvityslaskua ".round($hlaskusum/100,2)." EUR.<br>";
 			
 			//luodaan summatietue
-			$ulos2  = sprintf ('%-4.4s', 	"KRFL");
-			$ulos2 .= sprintf ('%01d', 		"9");
-			$ulos2 .= sprintf ('%-17.17s', 	str_replace('-','',$arow["ytunnus"]));
-			$ulos2 .= sprintf ('%06d', 		$arow["luontipvm"]);
-			$ulos2 .= sprintf ('%04d',   	$arow["luontiaika"]);
-			$ulos2 .= sprintf ('%06d', 		$laskukpl);
-			$ulos2 .= sprintf ('%06d', 		$vlaskukpl);
-			$ulos2 .= sprintf ('%013d', 	$vlaskusum);
-			$ulos2 .= sprintf ('%06d', 		$hlaskukpl);
-			$ulos2 .= sprintf ('%013d', 	$hlaskusum);
-			$ulos2 .= sprintf ('%06d', 		"0");
-			$ulos2 .= sprintf ('%013d', 	"0");
-			$ulos2 .= sprintf ('%06d', 		"0");
-			$ulos2 .= sprintf ('%013d', 	"0");
-			$ulos2 .= sprintf ('%013d', 	"0");
-			$ulos2 .= sprintf ('%-273.273s',"");
-			$ulos2 .= "\n";
+			$ulos .= sprintf ('%-4.4s', 	"KRFL");
+			$ulos .= sprintf ('%01d', 		"9");
+			$ulos .= sprintf ('%-17.17s', 	str_replace('-','',$yhtiorow["ytunnus"]));
+			$ulos .= sprintf ('%06d', 		$luontipvm);
+			$ulos .= sprintf ('%04d',   	$luontiaika);
+			$ulos .= sprintf ('%06d', 		$laskukpl);
+			$ulos .= sprintf ('%06d', 		$vlaskukpl);
+			$ulos .= sprintf ('%013d', 		$vlaskusum);
+			$ulos .= sprintf ('%06d', 		$hlaskukpl);
+			$ulos .= sprintf ('%013d', 		$hlaskusum);
+			$ulos .= sprintf ('%06d', 		"0");
+			$ulos .= sprintf ('%013d', 		"0");
+			$ulos .= sprintf ('%06d', 		"0");
+			$ulos .= sprintf ('%013d', 		"0");
+			$ulos .= sprintf ('%013d', 		"0");
+			$ulos .= sprintf ('%-273.273s',	"");
+			$ulos .= "\r\n";
 			
-			if ($kukarow["eposti"] != '') {
-				$tietue = $ulos.$ulos1.$ulos2;
-				
-				$bound = uniqid(time()."_") ;
+			//keksitään uudelle failille joku varmasti uniikki nimi:
+			list($usec, $sec) = explode(' ', microtime());
+			mt_srand((float) $sec + ((float) $usec * 100000));
+			$filenimi = "dataout/Nordeasiirto-$factoringsiirtonumero-".md5(uniqid(mt_rand(), true)).".txt";
 			
-                require_once 'Mail.php';
-
-                $headers = array(
-                    'From' => '<siirto@victoria.fi>',
-                    'To'   => $kukarow['eposti'],
-                    'Subject' => 'Factoring siirto',
-                    'MIME-Version' => '1.0',
-                    //'Content-Type' => 'multipart/mixed; boundary="'.$bound.'"',
-                    'Content-Type' => 'text/plain',
-                    'Content-Transfer-Encoding' =>  '7bit',
-                    'Content-Disposition' => 'attachment; filename="siirto.txt"'
-                );
-
-                $params = array(
-                    'host' => 'smtp.eunet.fi'
-                );
-
-                $mail = Mail::factory('smtp', $params);
-
-				if(PEAR::isError($mail->send($kukarow['eposti'], $headers, $tietue))) {
-				    echo "<br>Tiedosto lähetettiin sähköpostiosoitteeseen {$kukarow['eposti']}.";
-                }
-			}
-			else {
-				echo "<br>Tiedostoa ei voitu lähettää! Sähköpostiosoitetta ei löydy!";
-			}
+			//kirjoitetaan faili levylle..
+			$fh = fopen($filenimi, "w");
+			if (fwrite($fh, $ulos) === FALSE) die("Kirjoitus epäonnistui $filenimi");
+			fclose($fh);
+			
+			echo "<tr><td class='back'><br></td></tr>";
+			
+			echo "<tr><td class='back' colspan='2'></td><th>Yhteensä $vlaskukpl veloituslaskua</th><td align='right'>".round($vlaskusum/100,2)."</td><td>$laskurow[valkoodi]</td></tr>";
+			echo "<tr><td class='back' colspan='2'></td><th>Yhteensä $hlaskukpl hyvityslaskua</th><td align='right'> ".round($hlaskusum/100,2)."</td><td>$laskurow[valkoodi]</td></tr>";
+			
+			echo "</table>";
+			echo "<br><br>";
+			echo "<table>";
+			echo "<tr><th>Tallenna siirtoaineisto levylle:</th>";
+			echo "<form method='post' action='$PHP_SELF'>";
+			echo "<input type='hidden' name='tee' value='lataa_tiedosto'>";
+			echo "<input type='hidden' name='kaunisnimi' value='".substr($filenimi,8)."'>";
+			echo "<input type='hidden' name='filenimi' value='$filenimi'>";
+			echo "<td><input type='submit' value='Tallenna'></td></form>";
+			echo "</tr></table>";
 		}
 		else {
 			echo "<br><br>Yhtään siirrettävää laskua ei ole!<br>";
@@ -261,5 +306,7 @@
 		}
 	}
 	
-	require ("inc/footer.inc");
+	if ($tee != "lataa_tiedosto") {
+		require ("inc/footer.inc");
+	}
 ?>

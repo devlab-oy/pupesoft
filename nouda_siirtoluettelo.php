c9257db83a0f2e7ed4a4ce9031b0578f153c01c4
juppe <juppe@devlab.fi>
2006-11-13 15:00:02 +0000

    Voidaan noutaa tallennetuja siirtoluetteloita

---
+++ nouda_siirtoluettelo.php
@@ -0,0 +1,37 @@
+<?php
+
+	if($_POST["tee"] == 'lataa_tiedosto') $lataa_tiedosto=1;
+	if($_POST["filenimi"] != '') $kaunisnimi=$_POST["filenimi"];
+
+	require("inc/parametrit.inc");
+
+	if ($tee == "lataa_tiedosto") {
+		echo file_get_contents("dataout/".$filenimi);
+	}
+	else {
+	
+
+
+		echo "<font class='head'>".t("Nouda tallennettu siirtoluettelo")."</font><hr><br><br>";
+
+		$handle = opendir("dataout");
+
+		echo "<form method='post' action='$PHP_SELF'>";
+		echo "<input type='hidden' name='tee' value='lataa_tiedosto'>";
+		echo "<select name='filenimi' multiple='FALSE' size='10'>";
+
+		while ($file = readdir($handle)) {
+			if (substr($file,0, 12) == "Nordeasiirto") {
+				echo "<option value='$file' $sel>".t($file)."</option>";
+			}
+		}
+		closedir($handle);
+
+		echo "</select>";
+		echo "<input type='submit' value='Tallenna'></form>";
+		echo "<br><br>";
+	
+		require ("inc/footer.inc");
+	}
+
+?>

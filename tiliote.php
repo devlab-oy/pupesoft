bd4f636207e248a06150b0b8256467424e465ce3
juppe <juppe@devlab.fi>
2006-12-13 13:05:24 +0000

    Tiliote.php käyttää nyttemmin uutta php-versiota viitemaksu.pl:stä ja viitemaksuille tehdään myös samata oikeellisuustarkistukset kuin tiliotteille.

--- tiliote.php
+++ tiliote.php
@@ -1,186 +1,189 @@
 <?php
-function dateconv ($date)
-{
-	//kääntää vvkkmm muodon muotoon vv-kk-mm
-	return substr($date,0,2). "-" . substr($date,2,2) . "-". substr($date,4,2);
-}
-$ok=0;
 
-// tehdään tällänen häkkyrä niin voidaan scriptiä kutsua vaikka perlistä..
+	$ok = 0;
 
-if ((trim($argv[1])=='perl') and (trim($argv[2])!='')) {
+	// tehdään tällänen häkkyrä niin voidaan scriptiä kutsua vaikka perlistä..
+	if ((trim($argv[1])=='perl') and (trim($argv[2])!='')) {
 
-	if ($argc == 0) die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
+		if ($argc == 0) die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
 
-	require ("inc/connect.inc");
-	require ("inc/functions.inc");
-	$userfile=trim($argv[2]);
-	$filenimi=$userfile;
-	$ok=1;
-}
-else
-	require "inc/parametrit.inc";
+		require ("inc/connect.inc");
+		require ("inc/functions.inc");
 
-// katotaan onko faili uploadttu
+		$userfile = trim($argv[2]);
+		$filenimi = $userfile;
 
-
-if (is_uploaded_file($userfile)==TRUE) {
-	$filenimi=$userfile_name;
-	$ok=1;
-}
-
-if ($ok=='1') {
-	$fd = fopen ($userfile, "r");
-	if (!($fd)) {
-		echo "<font class='message'>Tiedosto '$filenimi' ei auennut!</font>";
-		exit;
-	}
-	$tietue = fgets($fd, 4096);
-	
-// Jos tämä on viiteaineisto, se käsitellään erikseen (Historialliset syyt)
-
-	if($tietue{0} == '0') {		//meillä on kai viitemaksuaineistoa, vähän niukka tunnistus mutta..
-// Onko tämä aineisto jo käsitelty
-		$oliotsikko = '';
-		while (!feof($fd)) {
-			if ($tietue{0} == '0') { // Otsikolta aineiston luontipvm
-				$tarkistapvm = "20" . dateconv(substr($tietue,1,6));
-				$oliotsikko='X';
-			}
-			if ($tietue{0} == '3') { // Tapahtuma
-				if ($oliotsikko=='X') {// Tarkistus
-					$tilino = substr($tietue,1,14);
-					//echo "<font class='message'>Maksupvm = $tarkistapvm Tilino=$tilino</font><br>";
-					// Onko tämä aineisto jo ajettu?
-					$query= "SELECT * FROM tiliotedata 
-									WHERE tilino='$tilino' and alku = '$tarkistapvm' and tyyppi = 3";
-					$tiliotedataresult = mysql_query($query) or pupe_error($query);
-					if (mysql_num_rows($tiliotedataresult) > 0) {
-						die ("<font class='error'>Tämä aineisto on jo aiemmin käsitelty! Tili = $tilino Ajalta $tarkistapvm</font><br>");
-						
-					}
-				}
-				$oliotsikko='';
-			}
-			$tietue = fgets($fd, 4096);
-		}
-		fclose ($fd);			
-		$dirri = "datain/viitteet/";
-		$filenimi = $dirri . "viitedata-" . $filenimi; 
-		exec("mv $userfile $filenimi", $output, $returnval);
-		if($returnval != 0){
-			die("Tapahtui virhe tallentaessa tiedostoa $filenimi");
-		}
-		echo "<font class='message'>".t("Käsitellään saapuva viitemaksuaineisto")."</font><br>";
-		flush();
-		echo "<pre>";
-		passthru("/usr/bin/perl myyntires/viitemaksut.pl -user $dbuser -pass $dbpass -host $dbhost -db $dbkanta -file $filenimi", $returnval);
-		echo "</pre>";
-		if ($returnval=='0')
-			echo "<font class='message'>".t("Ajo onnistui")."</font><br>";
-		else
-			echo "<font class='error'>".t("Ajo epäonnistui")." $returnval</font><br>";
+		$ok = 1;
 	}
 	else {
+		require ("inc/parametrit.inc");
+
+		echo "<font class='head'>Tiliotteen, LMP:n ja viitemaksujen käsittely</font><hr><br><br>";
+	}
+
+	// katotaan onko faili uploadttu
+	if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+		$userfile = $_FILES['userfile']['name'];
+		$filenimi = $_FILES['userfile']['tmp_name'];
+		$ok = 1;
+	}
+
+	if ($ok == 1) {
+
+		$fd = fopen ($filenimi, "r");
+
+		if (!($fd)) {
+			echo "<font class='message'>Tiedosto '$filenimi' ei auennut!</font>";
+			exit;
+		}
+
 		$query= "LOCK TABLE tiliotedata WRITE, yriti READ";
 		$tiliotedataresult = mysql_query($query) or pupe_error($query);
-		
-		$query= "SELECT max(aineisto)+1 FROM tiliotedata";
-		$tiliotedataresult = mysql_query($query) or pupe_error($query); // Etsitään aineistonumero
-		$aineistorow=mysql_fetch_array ($tiliotedataresult);
-		
-		while (!feof ($fd)) {
-			if ((substr($tietue,0,3) == 'T00') or (substr($tietue,0,3) == 'T03')) {
-				if (substr($tietue,0,3) == 'T03') {
-					$xtyyppi = 2; // Tämä on LMP
-					$alkupvm=substr($tietue,38,6);
-					$loppupvm = $alkupvm;
-				}
-				if (substr($tietue,0,3) == 'T00') {
-					$xtyyppi = 1; // Tämä on tiliote
-					$alkupvm = dateconv(substr($tietue,26,6));
-					$loppupvm = dateconv(substr($tietue,32,6));
-				}
-				$tilino = substr($tietue, 9, 14);
 
-				$query = "SELECT *
+		// Etsitään aineistonumero
+		$query = "SELECT max(aineisto)+1 aineisto FROM tiliotedata";
+		$aineistores = mysql_query($query) or pupe_error($query);
+		$aineistorow = mysql_fetch_array($aineistores);
+
+		$tietue = fgets($fd);
+
+		while (!feof($fd)) {
+			$tietue = str_replace ("{", "ä", $tietue);
+			$tietue = str_replace ("|", "ö", $tietue);
+			$tietue = str_replace ("}", "å", $tietue);
+			$tietue = str_replace ("[", "Ä", $tietue);
+			$tietue = str_replace ("\\","Ö", $tietue);
+			$tietue = str_replace ("]", "Å", $tietue);
+			$tietue = str_replace ("'", " ", $tietue);
+
+			if (substr($tietue,0,3) == 'T00' or substr($tietue,0,3) == 'T03' or substr($tietue,0,1) == '0') {
+				// Konekielinen tiliote
+				if (substr($tietue,0,3) == 'T00') {
+					$xtyyppi 	= 1;
+					$alkupvm 	= dateconv(substr($tietue,26,6));
+					$loppupvm 	= dateconv(substr($tietue,32,6));
+					$tilino 	= substr($tietue, 9, 14);
+				}
+
+				// Laskujen maksupalvelu LMP
+				if (substr($tietue,0,3) == 'T03') {
+					$xtyyppi 	= 2;
+					$alkupvm	= substr($tietue,38,6);
+					$loppupvm 	= $alkupvm;
+					$tilino 	= substr($tietue, 9, 14);
+				}
+
+				// Saapuvat viitemaksut
+				if(substr($tietue,0,1) == '0') {
+					$xtyyppi 	= 3;
+					$alkupvm	= "20".dateconv(substr($tietue,1,6));
+					$loppupvm 	= $alkupvm;
+
+					//Luetaan tilinumero seuraavalta riviltä ja siirretään pointteri takaisin nykypaikkaan
+					$pointterin_paikka = ftell($fd);
+					$tilino 	= fgets($fd);
+					$tilino 	= substr($tilino,1,14);
+					fseek($fd, $pointterin_paikka);
+				}
+
+				$query = "	SELECT *
 							FROM yriti
 							WHERE tilino = '$tilino'";
 				$yritiresult = mysql_query($query) or pupe_error($query);
+
 				if (mysql_num_rows($yritiresult) != 1) {
 					echo "<font class='error'> Tiliä '$tilino' ei löytynyt!</font><br>";
-					$xtyyppi=0;
+					$xtyyppi = 0;
 				}
 				else {
-					$yritirow=mysql_fetch_array ($yritiresult);
+					$yritirow = mysql_fetch_array ($yritiresult);
 				}
-				
+
 				// Onko tämä aineisto jo ajettu?
-				$query= "SELECT * FROM tiliotedata 
-								WHERE tilino='$tilino' and alku = '$alkupvm' and loppu = '$loppupvm' and tyyppi = $xtyyppi";
+				$query= "	SELECT *
+							FROM tiliotedata
+							WHERE tilino = '$tilino'
+							and alku 	 = '$alkupvm'
+							and loppu 	 = '$loppupvm'
+							and tyyppi 	 = $xtyyppi";
 				$tiliotedataresult = mysql_query($query) or pupe_error($query);
+
 				if (mysql_num_rows($tiliotedataresult) > 0) {
 					echo "<font class='error'>Tämä aineisto on jo aiemmin käsitelty! Tili = $tilino Ajalta $alkupvm - $loppupvm Yritystunnus $yritirow[yhtio]</font><br>";
 					$xtyyppi=0;
 				}
-			}	
-			$tietue = str_replace ( "{", "ä", $tietue);
-			$tietue = str_replace ( "|", "Ö", $tietue);
-			$tietue = str_replace ( "}", "å", $tietue);
-			$tietue = str_replace ( "[", "Ä", $tietue);
-			$tietue = str_replace ( "\\", "Ö", $tietue);
-			$tietue = str_replace ( "]", "Å", $tietue);
-			if (($xtyyppi > 0) and ($xtyyppi < 3)) {
-				$tietue = str_replace ("'", " ", $tietue); // Poistaa SQL-virheen mahdollisuuden
-				$query= "INSERT into tiliotedata (yhtio, aineisto, tilino, alku, loppu, tyyppi, tieto)
-							values ('$yritirow[yhtio]', '$aineistorow[0]', '$tilino', '$alkupvm', '$loppupvm', '$xtyyppi', '$tietue')";
-				$tiliotedataresult = mysql_query($query) or pupe_error($query); // Kirjoitetaan tiedosto kantaan
 			}
+
+			if ($xtyyppi > 0 and $xtyyppi <= 3) {
+				// Kirjoitetaan tiedosto kantaan
+				$query = "INSERT into tiliotedata (yhtio, aineisto, tilino, alku, loppu, tyyppi, tieto) values ('$yritirow[yhtio]', '$aineistorow[aineisto]', '$tilino', '$alkupvm', '$loppupvm', '$xtyyppi', '$tietue')";
+				$tiliotedataresult = mysql_query($query) or pupe_error($query);
+			}
+
 			$tietue = fgets($fd, 4096);
 		}
 		$query= "UNLOCK TABLES";
 		$tiliotedataresult = mysql_query($query) or pupe_error($query);
-		
-		$query= "SELECT * FROM tiliotedata 
-						WHERE aineisto='$aineistorow[0]'
-						ORDER BY tunnus";
 
-		$tiliotedataresult = mysql_query($query) or pupe_error($query); // Käsitellään uudet tietueet
-		
-		while ($tiliotedatarow=mysql_fetch_array ($tiliotedataresult)) {
+
+		// Käsitellään uudet tietueet
+		$query = "	SELECT *
+					FROM tiliotedata
+					WHERE aineisto = '$aineistorow[0]'
+					ORDER BY tunnus";
+		$tiliotedataresult = mysql_query($query) or pupe_error($query);
+
+		while ($tiliotedatarow = mysql_fetch_array($tiliotedataresult)) {
 			$tietue = $tiliotedatarow['tieto'];
 
 			if ($tiliotedatarow['tyyppi'] == 1) {
-				require "inc/tiliote.inc";
+				require("inc/tiliote.inc");
 			}
 			if ($tiliotedatarow['tyyppi'] == 2) {
-				require "inc/LMP.inc";
+				require("inc/LMP.inc");
+			}
+			if ($tiliotedatarow['tyyppi'] == 3) {
+				require("inc/viitemaksut.inc");
 			}
 		}
-		//echo "tyyppi = $xtyyppi<br>";
+
 		if ($xtyyppi == 1) {
-			//echo "nyt PITÄISI syntiä vastavienti<br>";
+			//echo "nyt PITÄISI syntyä vastavienti<br>";
 			$tkesken = 0;
 			echo "<tr><td colspan = '6'>";
 			$maara = $vastavienti;
-			require "inc/teeselvittely.inc";
+
+			require("inc/teeselvittely.inc");
+
 			echo "</td></tr>";
 			echo "</table>";
 		}
+
 		if ($xtyyppi == 2) {
 			$tkesken = 0;
 			$maara = $vastavienti;
-			require "inc/teeselvittely.inc";
+
+			require("inc/teeselvittely.inc");
+
 			echo "</table>";
 		}
+
+		if ($xtyyppi == 3) {
+			require("inc/viitemaksut_kohdistus.inc");
+		}
 	}
-}
-else {
-	echo "<font class='head'>Tiliotteen, LMP:n ja viitemaksujen käsittely</font><hr><br><br>";
-	echo "<form enctype='multipart/form-data' name='sendfile' action='$PHP_SELF' method='post'>
-		pankin aineisto <input type='file' name='userfile'><br><br>
-		<input type='submit' value='Käsittele tiedosto'>
-		</form>";
-}
-require "inc/footer.inc";
+	else {
+		echo "<form enctype='multipart/form-data' name='sendfile' action='$PHP_SELF' method='post'>";
+
+		echo "<table>";
+		echo "<tr><th>Pankin aineisto:</th>
+				<td><input type='file' name='userfile'></td>
+				<td><input type='submit' value='Käsittele tiedosto'></td></tr>";
+		echo "</table>";
+
+		echo "</form>";
+	}
+
+	require("inc/footer.inc");
+
 ?>

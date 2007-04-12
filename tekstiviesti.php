ed9257d010d26973ac54816fef04bdce6f0da147
jonikanerva <joni@devlab.fi>
2007-04-12 12:44:09 +0000

    Jos salasanat.phpssä on sms_palvelin, sms_user, sms_pass ni voidaan lähetellä tekstareita..

--- tekstiviesti.php
+++ tekstiviesti.php
@@ -1,50 +1,56 @@
 <?php
+
 	require ("inc/parametrit.inc");
-	
+
 	echo "<font class='head'>Tekstiviestin l‰hetys</font><hr>";
 
-	if($tee == 'laheta') {
-		
-		$kotinum = ereg_replace ("-", "", $kotinum);
-		echo "<br>";
-		
-		if(strlen($kotinum) > 0 && is_numeric($kotinum) && strlen($teksti) > 0){
-			$pala = exec ("echo '".$teksti."'"."  | gammu --sendsms TEXT ".$kotinum);
-			echo "Viesti l‰hetetty!<br>$pala<br>"; 
-			
-			$kotinum = '';
-			$teksti = '';
-		
+	if ($tee == 'laheta') {
+
+		if (strlen($teksti) > 160) {
+			echo "<font class='error'>VIRHE: Tekstiviestin maksimipituus on 160 merkki‰!</font><br>";
 		}
-		else{
-			echo "<font class='error'>VIRHE: Viesti tai puhelinnumero puuttui!</font><br>";	
+
+		$kotinum = str_replace ("-", "", $kotinum);
+		$ok = 1;
+
+		if (is_numeric($kotinum) and strlen($teksti) > 0 and strlen($teksti) <= 160 and $sms_palvelin != "" and $sms_user != "" and $sms_pass != "") {
+			$retval = file_get_contents("$sms_palvelin?user=$sms_user&pass=$sms_pass&numero=$kotinum&viesti=$teksti");
+			if (trim($retval) == "0") $ok = 0;
 		}
-				
-		$tee = '';
+
+		if ($ok == 1) {
+			echo "<font class='error'>VIRHE: Tekstiviestin l‰hetys ep‰onnistui! $retval</font><br><br>";
+		}
+
+		if ($ok == 0) {
+			echo "<font class='message'>Tekstiviesti l‰hetetty!</font><br><br>";
+		}
+
+		$tee = "";
 	}
 
+	if ($tee == "") {
 
-	if($tee != 'laheta') {
-		echo "<script language=\"javascript\">
-				function TextMaxLength(evt, field) {
-					charCode = evt.keyCode;
-					if ((field.length >= 160) && (evt.keyCode == 0)) {
-						alert('Tekstiviestiin mahtuu enint‰‰n 160 merkki‰!' + evt.keyCode);
-						return false;
-					}
-				}
-				</script>";
-		
-		echo "	<br><br><table>";
-		echo "	<form name='form' action = '$PHP_SELF' method='post'>
-				<input type='hidden' name='tee' value = 'laheta'>
-				<tr><th>Puh.</th><td><input type='text' size='20' name='kotinum' value='$kotinum'></td></tr>
-				<tr><th>Viesti</th><td><textarea name='teksti' cols='45' rows='6' wrap='soft' 
-				onkeypress=\"return TextMaxLength(event, document.form.teksti.value);\">$teksti</textarea></td></tr>";
-		echo "	</table>";
-		
-		echo "<br><input type='submit' value = 'L‰het‰'>";
-		
-		echo "</form>";
+		echo "<form name='form' action = '$PHP_SELF' method='post' name='tekstari'>
+			<input type='hidden' name='tee' value = 'laheta'>
+
+			<table>
+				<tr>
+					<th>Puh.</th>
+					<td><input type='text' size='20' name='kotinum' value='$kotinum'></td>
+				</tr>
+				<tr>
+					<th>Viesti</th>
+					<td><textarea name='teksti' cols='45' rows='6' wrap='soft'>$teksti</textarea></td>
+				</tr>
+			</table>
+
+			<br><input type='submit' value = 'L‰het‰'>
+
+			</form>";
+
 	}
+
+	require ("inc/footer.inc");
+
 ?>

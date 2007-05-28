ba4635e2a669404dee40b04353b469260c315f3c
juppe <juppe@devlab.fi>
2007-05-28 07:13:44 +0000

    Pupesoftissa ei enää ole 'Virtuaaliraportointia'. Nyt ohjelman otsikko on ohjelman nimi pupe-standardin mukaisesti.

--- raportit.php
+++ raportit.php
@@ -4,8 +4,6 @@
 	if ((($toim != 'hyvaksynta') or ($tee != 'T')) and ($toim !='maksuvalmius')) $useslave = 1;
 	require ("inc/parametrit.inc");
 
-	echo "<font class='head'>".t("Virtuaaliraportointi")."</font><hr>";
-
 	require ("inc/".$toim.".inc");
 	require ("inc/footer.inc");
 ?>

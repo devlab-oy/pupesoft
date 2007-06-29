afa4cc20eef63d28fd397a2c4ba5286bcf4dc4c9
jonikanerva <joni@devlab.fi>
2007-06-29 12:33:35 +0000

    Tää oli päin perjantaita!

--- myyntires/karhuselaa.php
+++ myyntires/karhuselaa.php
@@ -15,67 +15,75 @@
 </form>
 
 <?php
-if (isset($_POST['tee']) && $_POST['tee'] == 'Hae') {
-	if (! empty($_POST['laskunro'])) {
+if (isset($_POST['tee']) and $_POST['tee'] == 'Hae') {
+	if (!empty($_POST['laskunro'])) {
 		$where = sprintf("lasku.laskunro = %d", (int) $_POST['laskunro']);
-	} else {
+	}
+	else {
 		$where = sprintf("lasku.ytunnus = '%s'", (int) $_POST['ytunnus']);
 	}
-	
+
 	// haetaan uusin karhukierros/karhukerta
-	$query = "SELECT MAX(karhu_lasku.ktunnus) as tunnus, lasku.ytunnus
-		FROM karhu_lasku
-		JOIN lasku ON lasku.tunnus=karhu_lasku.ltunnus
-		where {$where} AND lasku.yhtio = '{$kukarow['yhtio']}' GROUP BY tunnus";
+	$query = "SELECT karhu_lasku.ktunnus as tunnus, liitostunnus, ytunnus, concat_ws(' / ',lasku.nimi,lasku.toim_nimi) nimi
+				FROM karhu_lasku
+				JOIN lasku ON lasku.tunnus=karhu_lasku.ltunnus
+				WHERE {$where} AND lasku.yhtio = '{$kukarow['yhtio']}'
+				ORDER BY tunnus desc
+				LIMIT 1";
 	$res = mysql_query($query) or pupe_error($query);
-	$ktunnus = mysql_fetch_array($res);
 
-	$query = "SELECT lasku.laskunro, lasku.summa, lasku.saldo_maksettu, lasku.nimi, karhukierros.pvm, lasku.erpcm,
-				lasku.ytunnus, karhu_lasku.ltunnus
-				FROM lasku
-				JOIN karhu_lasku ON karhu_lasku.ltunnus=lasku.tunnus
-				JOIN karhukierros ON karhukierros.tunnus=karhu_lasku.ktunnus
-				WHERE karhu_lasku.ktunnus = '{$ktunnus['tunnus']}'
-				AND karhukierros.yhtio = '{$kukarow['yhtio']}'
-				AND karhu_lasku.ltunnus
-				AND lasku.ytunnus = '{$ktunnus['ytunnus']}'
-				GROUP BY karhu_lasku.ltunnus";
+	if (mysql_num_rows($res) > 0) {
 
-$res = mysql_query($query) or pupe_error($query);
+		$ktunnus = mysql_fetch_array($res);
 
-?>
-<p><?php echo t('Viimeisin karhukerta ytunnukselle ') . $ktunnus['ytunnus'] ?>:</p>
-<table>
-	<tr>
-		<th><?php echo t('Asiakas') ?></th>
-		<th><?php echo t('Laskunro') ?></th>
-		<th><?php echo t('Ytunnus') ?></th>
-		<th><?php echo t('Summa') ?></th>
-		<th><?php echo t('Maksettu') ?></th>
-		<th><?php echo t('Karhuamis pvm') ?></th>
-		<th><?php echo t('Eräpäivä') ?></th>
-		<th><?php echo t('Karhukertoja') ?></th>
-	</tr>
-	<?php while ($row = mysql_fetch_array($res)): ?>
-	<?php
-	$query = "select count(distinct ktunnus) as summa from karhu_lasku where ltunnus={$row['ltunnus']}";
-	$ka_res = mysql_query($query);
-	$karhuttu = mysql_fetch_array($ka_res);
+		echo "<br><font class='message'>Viimeinen karhukierros asiakkaalle ytunnus $ktunnus[ytunnus]:</font><br>";
+
+		$query = "SELECT lasku.laskunro, lasku.summa, lasku.saldo_maksettu, concat_ws('<br>',lasku.nimi,lasku.toim_nimi) nimi, karhukierros.pvm, lasku.erpcm, lasku.ytunnus, karhu_lasku.ltunnus
+					FROM karhu_lasku
+					JOIN lasku ON (lasku.tunnus = karhu_lasku.ltunnus and lasku.liitostunnus = {$ktunnus['liitostunnus']})
+					JOIN karhukierros ON (karhukierros.tunnus = karhu_lasku.ktunnus and karhukierros.yhtio = '{$kukarow['yhtio']}')
+					WHERE karhu_lasku.ktunnus = '{$ktunnus['tunnus']}'";
+
+	$res = mysql_query($query) or pupe_error($query);
+
 	?>
+
+	<br>
+	<table>
 		<tr>
-			<td><?php echo $row['nimi'] ?></td>
-			<td><?php echo $row['laskunro'] ?></td>
-			<td><?php echo $row['ytunnus'] ?></td>
-			<td><?php echo $row['summa'] ?></td>
-			<td><?php echo $row['saldo_maksettu'] ?></td>
-			<td><?php echo tv1dateconv($row['pvm']) ?></td>
-			<td><?php echo tv1dateconv($row['erpcm']) ?></td>
-			<td style="text-align: right;"><?php echo $karhuttu['summa'] ?></td>
+			<th><?php echo t('Asiakas') ?></th>
+			<th><?php echo t('Laskunro') ?></th>
+			<th><?php echo t('Ytunnus') ?></th>
+			<th><?php echo t('Summa') ?></th>
+			<th><?php echo t('Maksettu') ?></th>
+			<th><?php echo t('Karhuamis pvm') ?></th>
+			<th><?php echo t('Eräpäivä') ?></th>
+			<th><?php echo t('Karhukertoja') ?></th>
 		</tr>
-	<?php endwhile; ?>
-</table>
+		<?php while ($row = mysql_fetch_array($res)): ?>
+		<?php
+		$query = "select count(distinct ktunnus) as summa from karhu_lasku where ltunnus={$row['ltunnus']}";
+		$ka_res = mysql_query($query);
+		$karhuttu = mysql_fetch_array($ka_res);
+		?>
+			<tr>
+				<td><?php echo $row['nimi'] ?></td>
+				<td><?php echo $row['laskunro'] ?></td>
+				<td><?php echo $row['ytunnus'] ?></td>
+				<td><?php echo $row['summa'] ?></td>
+				<td><?php echo $row['saldo_maksettu'] ?></td>
+				<td><?php echo tv1dateconv($row['pvm']) ?></td>
+				<td><?php echo tv1dateconv($row['erpcm']) ?></td>
+				<td style="text-align: right;"><?php echo $karhuttu['summa'] ?></td>
+			</tr>
+		<?php endwhile; ?>
+	</table>
 
 <?php
+	}
+	else {
+		echo "<br><font class='message'>Yhtään karhua ei löytynyt!</font>";
+	}
 }
 ?>
 

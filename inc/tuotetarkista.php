e8602cfffaafc07be077538d2a5f2e4a2a1cf834
juppe <juppe@devlab.fi>
2007-08-02 11:46:56 +0000

    Deprekoitua ja kakkaa. (Kai?)

--- inc/tuotetarkista.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if ((mysql_field_name($result, $i) == "nimitys") or
-		(mysql_field_name($result, $i) == "try")) {
-		$pakko[$i]=1;
-		$virhe[$i] = "".t("Tieto puuttuu")."";
-		$errori = 1;
-	}
-?>

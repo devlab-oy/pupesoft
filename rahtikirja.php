9216783de7c5737116ab4d7d26a729a32392b2ea
juppe <juppe@devlab.fi>
2007-08-06 10:28:44 +0000

    JT-rivien tulostusfiilaus!

--- rahtikirja.php
+++ rahtikirja.php
@@ -442,14 +442,15 @@
 					$query = "  SELECT tilausrivi.*,							
 								round(if(tuote.myymalahinta != 0, tuote.myymalahinta, tilausrivi.hinta * if('$yhtiorow[alv_kasittely]' != '' and tilausrivi.alv < 500, (1+tilausrivi.alv/100), 1)),2) ovhhinta,
 								round(tilausrivi.hinta * (tilausrivi.varattu+tilausrivi.jt+tilausrivi.kpl) * if(tilausrivi.netto='N', (1-tilausrivi.ale/100), (1-(tilausrivi.ale+lasku.erikoisale-(tilausrivi.ale*lasku.erikoisale/100))/100)),2) rivihinta,
-								$sorttauskentta
+								$sorttauskentta,
+								if(tilausrivi.var='J', tilausrivi.tuoteno, 0) jtsort
 								FROM tilausrivi
 								JOIN tuote ON tilausrivi.yhtio = tuote.yhtio and tilausrivi.tuoteno = tuote.tuoteno
 								JOIN lasku ON tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus
 								WHERE tilausrivi.otunnus = '$otunnus'
 								and tilausrivi.yhtio = '$kukarow[yhtio]'
 								$tyyppilisa
-								ORDER BY sorttauskentta";
+								ORDER BY jtsort, sorttauskentta";
 					$riresult = mysql_query($query) or pupe_error($query);
 
 					//generoidaan rivinumerot

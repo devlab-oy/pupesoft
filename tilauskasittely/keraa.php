b31c50991391a338a5dcf698fd17dbe010d90302
juppe <juppe@devlab.fi>
2007-08-06 10:31:54 +0000

    SQL-fiilaus!

--- tilauskasittely/keraa.php
+++ tilauskasittely/keraa.php
@@ -785,7 +785,7 @@
 										round(if(tuote.myymalahinta != 0, tuote.myymalahinta, tilausrivi.hinta * if('$yhtiorow[alv_kasittely]' != '' and tilausrivi.alv < 500, (1+tilausrivi.alv/100), 1)),2) ovhhinta,
 										round(tilausrivi.hinta * (tilausrivi.varattu+tilausrivi.jt+tilausrivi.kpl) * if(tilausrivi.netto='N', (1-tilausrivi.ale/100), (1-(tilausrivi.ale+lasku.erikoisale-(tilausrivi.ale*lasku.erikoisale/100))/100)),2) rivihinta,
 										$sorttauskentta,
-										if(tilausrivi.var='J', tilausrivi.tuoteno, 0) jtsort
+										if(tilausrivi.var='J', 1, 0) jtsort
 										FROM tilausrivi
 										JOIN tuote ON tilausrivi.yhtio = tuote.yhtio and tilausrivi.tuoteno = tuote.tuoteno
 										JOIN lasku ON tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus

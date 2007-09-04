bce6052b95e6c401354d505dadc3de9fe785960c
juppe <juppe@devlab.fi>
2007-09-04 13:06:52 +0000

    Sarjanumeroiden jälkilaskenta bätchissä!

--- jalkikorjaasarjamyynnit.php
+++ jalkikorjaasarjamyynnit.php
@@ -5,19 +5,23 @@
 	echo "<font class='head'>".t("Korjaa sarjanumeromyyyntejä").":</font><hr><br>";
 	
 	if ($tee == "PAIVITA" and $paivamaara != '') {
-		$query = "	SELECT distinct tilausrivi.tuoteno, ostorivi.tunnus ostotunnus, round(ostorivi.rivihinta/ostorivi.kpl, 2) ostohinta
+		$query = "	SELECT distinct tilausrivi.tuoteno, sarjanumeroseuranta.ostorivitunnus
 					FROM tilausrivi 
-					JOIN tuote on tuote.yhtio=tilausrivi.yhtio and tuote.tuoteno=tilausrivi.tuoteno and tuote.sarjanumeroseuranta!=''
-					JOIN sarjanumeroseuranta ON tilausrivi.yhtio=sarjanumeroseuranta.yhtio and tilausrivi.tuoteno=sarjanumeroseuranta.tuoteno and tilausrivi.tunnus=sarjanumeroseuranta.myyntirivitunnus
-					JOIN tilausrivi ostorivi ON sarjanumeroseuranta.yhtio=ostorivi.yhtio and sarjanumeroseuranta.ostorivitunnus=ostorivi.tunnus
+					JOIN tuote on tuote.yhtio = tilausrivi.yhtio and tuote.tuoteno = tilausrivi.tuoteno and tuote.sarjanumeroseuranta = 'S'
+					JOIN sarjanumeroseuranta ON tilausrivi.yhtio = sarjanumeroseuranta.yhtio and tilausrivi.tuoteno = sarjanumeroseuranta.tuoteno and tilausrivi.tunnus = sarjanumeroseuranta.myyntirivitunnus
 					WHERE tilausrivi.yhtio			= '$kukarow[yhtio]'
 					and tilausrivi.tyyppi			= 'L'
 					and tilausrivi.laskutettuaika  >= '$paivamaara'
 					order by sarjanumeroseuranta.sarjanumero";
 		$vresult = mysql_query($query) or pupe_error($query);
 
-		while ($vrow = mysql_fetch_array($vresult)) {				
-			jalkilaskentafunktio($vrow["tuoteno"], "", $vrow["ostohinta"], $vrow["ostotunnus"]);
+		while ($vrow = mysql_fetch_array($vresult)) {
+			// Haetaan sarjanumeron ostohinta
+			$ostohinta = sarjanumeron_ostohinta("ostorivitunnus", $vrow["ostorivitunnus"]);	
+			
+			echo "jalkilaskentafunktio($vrow[tuoteno], , $ostohinta, $vrow[ostorivitunnus]);<br>";
+									
+			jalkilaskentafunktio($vrow["tuoteno"], "", $ostohinta, $vrow["ostorivitunnus"]);
 		}
 	
 		$tee = "";

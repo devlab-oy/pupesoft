4eb72115f2b7e124bbe1d5ebd08bc51dba9ebe78
juppe <juppe@devlab.fi>
2007-11-15 07:51:54 +0000

    Favicon fiilausta!

--- index.php
+++ index.php
@@ -19,28 +19,31 @@ if ($kukarow['resoluutio'] == 'P') {
 	$colwidth = '45';
 }
 
-echo "
-<html>
-	<head>
-		<title>$yhtiorow[nimi]</title>
-		<link rel='shortcut icon' href='http://www.pupesoft.com/pupeicon.gif'>
-		<meta http-equiv='Pragma' content='no-cache'>
+echo "<html>
+		<head>
+		<title>$yhtiorow[nimi]</title>";
+		
+if (file_exists("pics/pupeicon.gif")) {
+	echo "\n<link rel='shortcut icon' href='pics/pupeicon.gif'>\n";
+}
+else {
+	echo "\n<link rel='shortcut icon' href='http://www.pupesoft.com/pupeicon.gif'>\n";
+}
+
+echo "	<meta http-equiv='Pragma' content='no-cache'>
 		<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
-	</head>
-
-	<noscript>
+		</head>
+		<noscript>
 		Your browser does not support Javascript!
-	</noscript>
-
-	<frameset cols='$colwidth,*' border='0' frameborder='no'>
+		</noscript>
+		<frameset cols='$colwidth,*' border='0' frameborder='no'>
 		<frameset rows='*,1' border='0' frameborder='no'>
 		<frame noresize src='indexvas.php?go=$go2' name='menu'>
 		<frame noresize src='' name='alamenu' id='alamenuFrame'>
 		</frameset> 
 		<frame src='$go' name='main'>
 		<noframes>Your browser does not support frames!</noframes>
-	</frameset> 
-
-</html>"; 
+		</frameset> 
+		</html>"; 
 
 ?>

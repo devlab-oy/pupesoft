4350be87d5245add98367281ff3e46f5906a7cc9
samiluukkonen <sami@devlab.fi>
2008-02-05 13:40:47 +0000

    Eroteltu extranet-käyttöoikeudet ja normaalit käyttöoikeudet erikseen. Jatkossa extranet-käyttöoikeuksia muutetaan kohdasta "Extranet-käyttöoikeudet" joka sijaitsee "Käyttäjät ja valikot" -kohdassa.
    Normaaleja käyttöoikeuksia muutetaan kohdasta "Käyttöoikeudet".

--- suoja.php
+++ suoja.php
@@ -7,18 +7,20 @@
 		$query = "	SELECT nimi, kuka, tunnus
 					FROM kuka
 					WHERE tunnus='$selkuka'";
+		$result = mysql_query($query) or pupe_error($query);
+		$selkukarow = mysql_fetch_array($result);
 	}
-	else {
+	else if ($toim != "extranet") {
 		$query = "	SELECT nimi, kuka, tunnus
 					FROM kuka
 					WHERE tunnus='$kukarow[tunnus]'";
+		$result = mysql_query($query) or pupe_error($query);
+		$selkukarow = mysql_fetch_array($result);
 	}
-	$result = mysql_query($query) or pupe_error($query);
-	$selkukarow = mysql_fetch_array($result);
 
 
 	// tehdään oikeuksien päivitys
-	if ($update=='totta') {
+	if ($update=='totta' and $selkukarow["kuka"] != "") {
 		// poistetaan ihan aluksi kaikki.. iiik.
 		$query = "	DELETE
 					FROM oikeu
@@ -94,68 +96,102 @@
 
 	echo "<font class='head'>".t("Tietosuoja").":</font><hr>";
 
-
 	echo "<font class='message'>".t("Käyttäjän")." $selkukarow[nimi] ".t("käyttöoikeudet")." ($yhtiorow[nimi])</font><hr>";
 
 	echo "<table cellpadding='1' cellspacing='2'>
 			<form action='$PHP_SELF' method='post'>
+			<input type='hidden' name='toim' value='$toim'>
 			<input type='hidden' name='sovellus' value='$sovellus'>
 
 			<tr>
 				<th>Valitse käyttäjä:</th>
-				<td><select name='selkuka' onchange='submit()'>
-					<option selected value='$selkukarow[tunnus]'>$selkukarow[nimi] ($selkukarow[kuka])";
+				<td><select name='selkuka' onchange='submit()'>";
+		if ($toim == "" or $selkukarow["kuka"] != "") {	
+			echo "<option selected value='$selkukarow[tunnus]'>$selkukarow[nimi] ($selkukarow[kuka])</option>";
+		}
+		else if ($toim == "extranet" and $selkukarow["kuka"] == "") {
+			echo "<option selected value=''>".t("Valitse käyttäjä")."</option>";			
+		}
 
-	$query = "SELECT *
-			  FROM kuka
-			  WHERE tunnus!='$selkukarow[tunnus]' and yhtio='$kukarow[yhtio]'
-			  ORDER BY nimi";
+	if ($toim == "extranet" and $selkukarow["tunnus"] != "") {
+		$query = "SELECT *
+				  FROM kuka
+				  WHERE tunnus!='$selkukarow[tunnus]' and yhtio='$kukarow[yhtio]' and extranet != ''
+				  ORDER BY nimi";
+	}
+	else if ($toim == "extranet" and $selkukarow["tunnus"] == "") {
+		$query = "SELECT *
+				  FROM kuka
+				  WHERE yhtio='$kukarow[yhtio]' and extranet != ''
+				  ORDER BY nimi";
+	}
+	else {
+		$query = "SELECT *
+				  FROM kuka
+				  WHERE tunnus!='$selkukarow[tunnus]' and yhtio='$kukarow[yhtio]'
+				  ORDER BY nimi";
+	}
+
 	$kukares = mysql_query($query) or pupe_error($query);
 
 	while ($kurow=mysql_fetch_array($kukares)) {
-		echo "<option value='$kurow[tunnus]'>$kurow[nimi] ($kurow[kuka])";
+		echo "<option value='$kurow[tunnus]'>$kurow[nimi] ($kurow[kuka])</option>";
 	}
 
 	echo "</select></td></form>";
 
-	$query = "	SELECT distinct sovellus
-				FROM oikeu
-				where yhtio='$kukarow[yhtio]'
-				order by sovellus";
-	$result = mysql_query($query) or pupe_error($query);
-
-	if (mysql_num_rows($result) > 1) {
-		echo "	<form action='$PHP_SELF' name='vaihdaSovellus' method='POST'>
-				<input type='hidden' name='selkuka' value='$selkukarow[tunnus]'>
-				<tr><th>".t("Valitse sovellus").":</th><td>
-				<select name='sovellus' onchange='submit()'>
-				<option value=''>".t("Nayta kaikki")."</option>";
-
-		while ($orow = mysql_fetch_array($result)) {
-			$sel = '';
-			if ($sovellus == $orow[0]) {
-				$sel = "SELECTED";
-			}
-			echo "<option value='$orow[0]' $sel>".t("$orow[0]")."</option>";
-		}
+	if ($toim == "extranet") {
+		$sovellus = "Extranet";
 	}
-	echo "</select></td><td class='back'></td></tr></table></form>";
+	else {
+		$query = "	SELECT distinct sovellus
+					FROM oikeu
+					where yhtio='$kukarow[yhtio]'
+					order by sovellus";
+		$result = mysql_query($query) or pupe_error($query);
 
+		if (mysql_num_rows($result) > 1) {
+			echo "	<form action='$PHP_SELF' name='vaihdaSovellus' method='POST'>
+					<input type='hidden' name='selkuka' value='$selkukarow[tunnus]'>
+					<input type='hidden' name='toim' value='$toim'>
+					<tr><th>".t("Valitse sovellus").":</th><td>
+					<select name='sovellus' onchange='submit()'>
+					<option value=''>".t("Nayta kaikki")."</option>";
+
+			while ($orow = mysql_fetch_array($result)) {
+				$sel = '';
+				if ($sovellus == $orow[0] and $orow[0] != '') {
+					$sel = "SELECTED";
+				}
+				if ($orow[0] != '') {
+					echo "<option value='$orow[0]' $sel>".t("$orow[0]")."</option>";
+				}
+			}
+		}
+		echo "</select></td><td class='back'></td></tr></table></form>";
+	}
 
 	// näytetään oikeuslista
 	echo "<table>";
 
-	$query = "	SELECT *
-				FROM oikeu
-				WHERE kuka = ''	and yhtio='$kukarow[yhtio]'";
-	
+	if ($toim == "extranet") {
+		$query = "	SELECT *
+					FROM oikeu
+					WHERE kuka = ''	and yhtio='$kukarow[yhtio]' and sovellus='Extranet'";
+	}
+	else {
+		$query = "	SELECT *
+					FROM oikeu
+					WHERE kuka = ''	and yhtio='$kukarow[yhtio]'";
+	}
+
 	if ($sovellus != '') {
 		$query .= " and sovellus='$sovellus'";
 	}
-	
+
 	$query .= "	ORDER BY sovellus, jarjestys, jarjestys2";
 	$result = mysql_query($query) or pupe_error($query);
-	
+
 	print " <SCRIPT TYPE=\"text/javascript\" LANGUAGE=\"JavaScript\">
 		<!--
 
@@ -164,7 +200,7 @@
 			var currForm = toggleBox.form;
 			var isChecked = toggleBox.checked;
 			var nimi = toggleBox.name;
-			
+		
 			for (var elementIdx=0; elementIdx<currForm.elements.length; elementIdx++) {											
 				if (currForm.elements[elementIdx].type == 'checkbox' && currForm.elements[elementIdx].name.substring(0,3) == nimi) {
 					currForm.elements[elementIdx].checked = isChecked;
@@ -174,10 +210,11 @@
 
 		//-->
 		</script>";
-			
+		
 	echo "<form action='$PHP_SELF' name='suojax' method='post'>
 			<input type='hidden' name='update' value='totta'>
 			<input type='hidden' name='sovellus' value='$sovellus'>
+			<input type='hidden' name='toim' value='$toim'>
 			<input type='hidden' name='selkuka' value='$selkukarow[tunnus]'>";
 
 	while ($orow=mysql_fetch_array($result)) {
@@ -197,23 +234,25 @@
 		$paivit		= '';
 		$luk		= '';
 		
-		$oq = "	SELECT *
-				FROM oikeu
-				WHERE yhtio='$kukarow[yhtio]' and kuka='$selkukarow[kuka]' and nimi='$orow[nimi]' and alanimi='$orow[alanimi]' and sovellus='$orow[sovellus]'";
-		$or = mysql_query($oq) or pupe_error($oq);
+		if ($selkukarow["kuka"] != "") {
+			$oq = "	SELECT *
+					FROM oikeu
+					WHERE yhtio='$kukarow[yhtio]' and kuka='$selkukarow[kuka]' and nimi='$orow[nimi]' and alanimi='$orow[alanimi]' and sovellus='$orow[sovellus]'";
+			$or = mysql_query($oq) or pupe_error($oq);
 
-		if (mysql_num_rows($or) != 0) {
-			$checked = "CHECKED";
-			
-			$oikeurow=mysql_fetch_array($or);
-			
-			if ($oikeurow["paivitys"] == 1) {
-				$paivit = "CHECKED";
+			if (mysql_num_rows($or) != 0) {
+				$checked = "CHECKED";
+		
+				$oikeurow=mysql_fetch_array($or);
+		
+				if ($oikeurow["paivitys"] == 1) {
+					$paivit = "CHECKED";
+				}
+		
+				if ($oikeurow["lukittu"] == 1) {
+					$luk = "CHECKED";
+				}			
 			}
-			
-			if ($oikeurow["lukittu"] == 1) {
-				$luk = "CHECKED";
-			}			
 		}
 
 		echo "<tr><td>".t("$orow[sovellus]")."</td>";
@@ -241,7 +280,9 @@
 			</tr>";			
 	echo "</table>";		
 
-	echo "<input type='submit' value='".t("Päivitä tiedot")."'></form>";
+	if ($toim == "" or ($toim == "extranet" and $selkukarow["kuka"] != "")) {
+		echo "<input type='submit' value='".t("Päivitä tiedot")."'></form>";
+	}
 			
 	require("inc/footer.inc");
 

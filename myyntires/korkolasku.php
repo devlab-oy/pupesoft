8a3a7cb7f63b4a5f802b7a101a598881082378e2
jonikanerva <joni@devlab.fi>
2008-02-12 15:39:39 +0000

    Korkolaskut ei osannut factoringsaamisia.

--- myyntires/korkolasku.php
+++ myyntires/korkolasku.php
@@ -40,62 +40,65 @@ if (($yhtiorow["kasittelykulu_tuotenumero"] != '') and (!is_numeric($kasittelyku
 }
 
 if ($tee == "ALOITAKOROTUS") {
-	
+
 	$korkolisa = "";
 	if ($korkosumma > 0) {
 		$korkosumma = str_replace(',','.',$korkosumma);
 		$korkolisa = " and korkosumma > $korkosumma ";
 	}
-	
-	$query = "	SELECT GROUP_CONCAT(distinct ovttunnus) konsrernyhtiot 
+
+	$query = "	SELECT GROUP_CONCAT(distinct ovttunnus) konsrernyhtiot
 				FROM yhtio
 				WHERE (konserni = '$yhtiorow[konserni]' and konserni != '') or (yhtio = '$yhtiorow[yhtio]')";
-	$result = mysql_query($query) or pupe_error($query);			
-	
+	$result = mysql_query($query) or pupe_error($query);
+
 	$konslisa = "";
 	if (mysql_num_rows($result) > 0) {
 		$konsrow = mysql_fetch_array($result);
-		
-		$konslisa = " and lasku.ovttunnus not in ($konsrow[konsrernyhtiot])";				
+		$konslisa = " and lasku.ovttunnus not in ($konsrow[konsrernyhtiot])";
 	}
-	
+
 	$asiakaslisa = "";
 	if ($syot_ytunnus != '') {
 		$asiakaslisa = " and asiakas.ytunnus >= '$syot_ytunnus' ";
 	}
-		
-	$query = "	SELECT 
-				GROUP_CONCAT(distinct lasku.tunnus) korotettavat,				
-				round(sum(lasku.viikorkopros * tiliointi.summa * -1 * (to_days(tiliointi.tapvm)-to_days(lasku.erpcm)) / 36500),2) korkosumma																
+
+	$query = "	SELECT
+				GROUP_CONCAT(distinct lasku.tunnus) korotettavat,
+				round(sum(lasku.viikorkopros * tiliointi.summa * -1 * (to_days(tiliointi.tapvm)-to_days(lasku.erpcm)) / 36500),2) korkosumma
 				FROM lasku
-				JOIN (	SELECT lasku.tunnus, 
+				JOIN (	SELECT lasku.tunnus,
 						to_days(tiliointi.tapvm) - to_days(lasku.erpcm) ika,
-						round(lasku.viikorkopros * tiliointi.summa * -1 * (to_days(tiliointi.tapvm)-to_days(lasku.erpcm)) / 36500,2) korkosumma,
+						round(lasku.viikorkopros * tiliointi.summa * -1 * (to_days(tiliointi.tapvm)-to_days(lasku.erpcm)) / 36500,2) korkosumma2,
 						maksuehto.jv
 						FROM lasku use index (yhtio_tila_mapvm)
-						JOIN tiliointi use index (tositerivit_index) on (tiliointi.yhtio=lasku.yhtio and tiliointi.ltunnus=lasku.tunnus and tiliointi.tilino = '$yhtiorow[myyntisaamiset]' and tiliointi.tapvm > lasku.erpcm and tiliointi.korjattu = '')
-						LEFT JOIN maksuehto on (maksuehto.yhtio=lasku.yhtio and maksuehto.tunnus=lasku.maksuehto)						
+						JOIN asiakas ON (lasku.yhtio = asiakas.yhtio and lasku.liitostunnus = asiakas.tunnus)
+						JOIN tiliointi use index (tositerivit_index) on (tiliointi.yhtio = lasku.yhtio and tiliointi.ltunnus = lasku.tunnus and tiliointi.tilino in ('$yhtiorow[myyntisaamiset]', '$yhtiorow[factoringsaamiset]') and tiliointi.tapvm > lasku.erpcm and tiliointi.korjattu = '')
+						LEFT JOIN maksuehto on (maksuehto.yhtio = lasku.yhtio and maksuehto.tunnus = lasku.maksuehto)
 						WHERE lasku.yhtio 	= '$kukarow[yhtio]'
 						and lasku.tila 		= 'U'
 						and lasku.mapvm 	>='$vva-$kka-$ppa'
-						and lasku.mapvm 	<='$vvl-$kkl-$ppl'				
+						and lasku.mapvm 	<='$vvl-$kkl-$ppl'
 						and lasku.summa		!= 0
 						and lasku.olmapvm	= '0000-00-00'
-						HAVING ika > $min_myoh and korkosumma != 0 and (maksuehto.jv is null or maksuehto.jv = '')) as laskut
-				JOIN asiakas ON lasku.yhtio=asiakas.yhtio and lasku.liitostunnus=asiakas.tunnus		
-				JOIN tiliointi use index (tositerivit_index) on (tiliointi.yhtio=lasku.yhtio and tiliointi.ltunnus=lasku.tunnus and tiliointi.tilino = '$yhtiorow[myyntisaamiset]' and tiliointi.tapvm > lasku.erpcm and tiliointi.korjattu = '')
+						$asiakaslisa
+						$konslisa
+						HAVING ika > $min_myoh and korkosumma2 > 0 and (maksuehto.jv is null or maksuehto.jv = '')
+						ORDER BY asiakas.ytunnus) as laskut
+				JOIN asiakas ON (lasku.yhtio = asiakas.yhtio and lasku.liitostunnus = asiakas.tunnus)
+				JOIN tiliointi use index (tositerivit_index) on (tiliointi.yhtio = lasku.yhtio and tiliointi.ltunnus = lasku.tunnus and tiliointi.tilino in ('$yhtiorow[myyntisaamiset]', '$yhtiorow[factoringsaamiset]') and tiliointi.tapvm > lasku.erpcm and tiliointi.korjattu = '')
 				WHERE lasku.tunnus = laskut.tunnus
 				$konslisa
 				$asiakaslisa
 				GROUP BY asiakas.ytunnus, asiakas.nimi, asiakas.nimitark, asiakas.osoite, asiakas.postino, asiakas.postitp
-				HAVING korkosumma > 0 $korkolisa				
-				ORDER BY lasku.ytunnus";
+				HAVING korkosumma > 0 $korkolisa
+				ORDER BY asiakas.ytunnus";
 	$result = mysql_query($query) or pupe_error($query);
-		
+
+	$korotettavat = array();
+
 	if (mysql_num_rows($result) > 0) {
-		$korotettavat = array();
-		
-		while($karhuttavarow = mysql_fetch_array($result)) {		 
+		while ($karhuttavarow = mysql_fetch_array($result)) {
 			$korotettavat[] = $karhuttavarow["korotettavat"];
 		}
 		$tee = "KOROTA";
@@ -118,29 +121,29 @@ if ($tee == "KOROTA")  {
 				if (count(*) > 1, 'useita', to_days(tiliointi.tapvm) - to_days(lasku.erpcm)) as ika,
 				sum(round(lasku.viikorkopros * tiliointi.summa * -1 * (to_days(tiliointi.tapvm)-to_days(lasku.erpcm)) / 36500,2)) as korkosumma
 				FROM lasku
-				JOIN tiliointi use index (tositerivit_index) on (tiliointi.yhtio=lasku.yhtio and tiliointi.ltunnus=lasku.tunnus and tiliointi.tilino = '$yhtiorow[myyntisaamiset]' and tiliointi.tapvm > lasku.erpcm and tiliointi.korjattu = '')
-				LEFT JOIN maksuehto on (maksuehto.yhtio=lasku.yhtio and maksuehto.tunnus=lasku.maksuehto)
+				JOIN tiliointi use index (tositerivit_index) on (tiliointi.yhtio = lasku.yhtio and tiliointi.ltunnus = lasku.tunnus and tiliointi.tilino in ('$yhtiorow[myyntisaamiset]', '$yhtiorow[factoringsaamiset]') and tiliointi.tapvm > lasku.erpcm and tiliointi.korjattu = '')
+				LEFT JOIN maksuehto on (maksuehto.yhtio = lasku.yhtio and maksuehto.tunnus = lasku.maksuehto)
 				WHERE lasku.yhtio = '$kukarow[yhtio]'
 				and lasku.tunnus in ($korotettavat[0])
 				GROUP BY lasku.tunnus
 				ORDER BY lasku.erpcm";
 	$result = mysql_query($query) or pupe_error($query);
-	
+
 	//Poistetaan arraysta käytetyt tunnukset
-	unset($korotettavat[0]);	
-	
+	unset($korotettavat[0]);
+
 	//otetaan asiakastiedot ekalta laskulta
 	$asiakastiedot = mysql_fetch_array($result);
-	
+
 	$query = "	SELECT *
 				FROM asiakas
 				WHERE yhtio='$kukarow[yhtio]' and tunnus = '$asiakastiedot[liitostunnus]'";
 	$asiakasresult = mysql_query($query) or pupe_error($query);
 	$asiakastiedot = mysql_fetch_array($asiakasresult);
-	
+
 	//ja kelataan akuun
 	mysql_data_seek($result,0);
-	
+
 	echo "<table>
 	<tr><th>".t("Ytunnus")."</th><td>$asiakastiedot[ytunnus]</td></tr>
 	<tr><th>".t("Nimi")."</th><td>$asiakastiedot[nimi]</td></tr>
@@ -148,14 +151,14 @@ if ($tee == "KOROTA")  {
 	<tr><th>".t("Osoite")."</th><td>$asiakastiedot[osoite]</td></tr>
 	<tr><th>".t("Postinumero")."</th><td>$asiakastiedot[postino] $asiakastiedot[postitp]</td></tr>";
 	echo "</table><br>";
-	
+
 	echo "<table>";
 	echo "<tr>";
 	echo "<td><input type='button' onclick='javascript:document.lahetaformi.submit();' value='".t("Tee korkolasku")."'></td>";
 	echo "<td><input type='button' onclick='javascript:document.ohitaformi.submit();' value='".t("Ohita")."'></td>";
 	echo "</tr>";
 	echo "</table><br>";
-	
+
 	echo "<form name='lahetaformi' action='$PHP_SELF' method='post'>";
 	echo "<table><tr>";
 	echo "<th>".t("Laskunpvm")."</th>";
@@ -220,14 +223,14 @@ if ($tee == "KOROTA")  {
 
 	echo "<table>";
 	echo "<tr>";
-	
+
 	echo "<input name='tee' type='hidden' value='LAHETA'>";
 	echo "<input name='kasittelykulu' type='hidden' value='$kasittelykulu'>";
-	
+
 	foreach($korotettavat as $tunnukset) {
-		echo "\n<input type='hidden' name='korotettavat[]' value='$tunnukset'>";		
+		echo "\n<input type='hidden' name='korotettavat[]' value='$tunnukset'>";
 	}
-	
+
 	echo "<input name='korkosumma' type='hidden' value='$korkosumma'>";
 	echo "<input name='vmehto' type='hidden' value='$vmehto'>";
 	echo "<input name='yhteyshenkilo' type='hidden' value='$yhteyshenkilo'>";
@@ -237,21 +240,21 @@ if ($tee == "KOROTA")  {
 			<input type='hidden' name='ppl' value='$ppl'>
 			<input type='hidden' name='kkl' value='$kkl'>
 			<input type='hidden' name='vvl' value='$vvl'>";
-	
+
 	echo "<td><input type='submit' value='".t("Tee korkolasku")."'></td>";
 	echo "</form>";
 
 	echo "<form  name='ohitaformi' action='$PHP_SELF' method='post'>";
 	echo "<input name='tee' type='hidden' value='KOROTA'>";
-	
+
 	foreach($korotettavat as $tunnukset) {
-		echo "\n<input type='hidden' name='korotettavat[]' value='$tunnukset'>";		
+		echo "\n<input type='hidden' name='korotettavat[]' value='$tunnukset'>";
 	}
-	
+
 	echo "<input name='kasittelykulu' type='hidden' value='$kasittelykulu'>";
 	echo "<input name='korkosumma' type='hidden' value='$korkosumma'>";
 	echo "<input name='vmehto' type='hidden' value='$vmehto'>";
-	echo "<input name='yhteyshenkilo' type='hidden' value='$yhteyshenkilo'>";		
+	echo "<input name='yhteyshenkilo' type='hidden' value='$yhteyshenkilo'>";
 	echo "	<input type='hidden' name='ppa' value='$ppa'>
 			<input type='hidden' name='kka' value='$kka'>
 			<input type='hidden' name='vva' value='$vva'>
@@ -267,7 +270,7 @@ if ($tee == "KOROTA")  {
 if ($tee == "") {
 
 	echo "<form action='$PHP_SELF' method='post'>";
-	echo "<input name='tee' type='hidden' value='ALOITAKOROTUS'>";			
+	echo "<input name='tee' type='hidden' value='ALOITAKOROTUS'>";
 	echo t("Syötä ytunnus jos haluat lähettää korkolaskun tietylle asiakkaalle").".<br>";
 	echo t("Jätä kenttä tyhjäksi jos haluat aloittaa ensimmäisestä asiakkaasta").".<br>";
 	echo t("Minimi korkosumma on summa euroissa, jonka yli korkolaskun loppusumman on oltava, että sitä edes ehdotetaan. (tyhjä=kaikki laskut)")."<br>";
@@ -313,48 +316,48 @@ if ($tee == "") {
 			<td><input type='text' name='ppl' value='$ppl' size='3'></td>
 			<td><input type='text' name='kkl' value='$kkl' size='3'></td>
 			<td><input type='text' name='vvl' value='$vvl' size='5'></td>";
-	
-	echo "<tr><th>".t("Korkolaskun maksuehto").":</th>";	
+
+	echo "<tr><th>".t("Korkolaskun maksuehto").":</th>";
 	echo "<td colspan='3'>$ulos</td></tr>";
-	
+
 	echo "<tr><th>".t("Minimi korkosumma").":</th>";
 	echo "<td colspan='3'><input type='text' name='korkosumma' value='$korkosumma'></td></tr>";
-	
+
 	if ($yhtiorow["kasittelykulu_tuotenumero"] != '') {
-		
+
 		if ($laskurow["valkoodi"] != '' and trim(strtoupper($laskurow["valkoodi"])) != trim(strtoupper($yhtiorow["valkoodi"]))) {
 			$valuutta = $laskurow["valkoodi"];
 		}
 		else {
 			$valuutta = $yhtiorow["valkoodi"];
 		}
-		
+
 		echo "<tr><th>".t("Käsittelykulu").":</th>";
 		echo "<td colspan='3'><input type='text' name='kasittelykulu' value='$kasittelykulu'> $valuutta</td></tr>";
 	}
-	
-		
+
+
 	$apuqu = "	SELECT kuka, nimi, puhno, eposti, tunnus
-				from kuka 
+				from kuka
 				where yhtio='$kukarow[yhtio]' and nimi!='' and puhno!='' and eposti!='' and extranet=''";
 	$meapu = mysql_query($apuqu) or pupe_error($apuqu);
-	
-	echo "<tr><th>".t("Yhteyshenkilö").":</th>";		
+
+	echo "<tr><th>".t("Yhteyshenkilö").":</th>";
 	echo "<td colspan='3'><select name='yhteyshenkilo'>";
-	
+
 	while($row = mysql_fetch_array($meapu)) {
 		$sel = "";
-		
+
 		if ($row['kuka'] == $kukarow['kuka']) {
 			$sel = 'selected';
 		}
-				
+
 		echo "<option value='$row[tunnus]' $sel>$row[nimi]</option>";
 	}
-	
+
 	echo "</select></td></tr>";
-	
-	
+
+
 	echo "<tr><th>".t("Ytunnus").":</th>";
 	echo "<td colspan='3'>";
 	echo "<input name='syot_ytunnus' type='text' value='$syot_ytunnus'></td>";

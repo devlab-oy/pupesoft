33ff685239c8f8a627d5a37096939d0bf260477a
jonikanerva <joni@devlab.fi>
2008-03-04 12:30:35 +0000

    pikku css update

--- login.php
+++ login.php
@@ -155,10 +155,11 @@ echo "
 	BODY			{background:#fff;}
 	FONT.info		{font-size:8pt;  font-family:Lucida,Verdana,Helvetica,Arial; color: #c0c0c0;}
 	FONT.head		{font-size:15pt; font-family:Lucida,Verdana,Helvetica,Arial; color: #666699; font-weight:bold; letter-spacing: .05em;}
-	FONT.menu		{font-size:9pt;  font-family:Lucida,Verdana,Helvetica,Arial; color: #666;}
+	FONT.menu		{font-size:10pt; font-family:Lucida,Verdana,Helvetica,Arial; color: #666;}
 	FONT.error		{font-size:9pt;  font-family:Lucida,Verdana,Helvetica,Arial; color: #ff6666;}
-	TD				{padding:5px;}
-	TABLE.login		{border-width: 1px 1px 1px 1px; /* top right bottom left */ border-style: solid; border-color: #000; vertical-align: top; background: #eee;}
+	TD				{padding:3pt;}
+	TABLE.login		{padding:7pt; border-width: 1px 1px 1px 1px; /* top right bottom left */ border-style: solid; border-color: #a0a0a0; vertical-align: top; background: #eee; -moz-border-radius: 10pt; -webkit-border-radius: 10pt;}
+	INPUT			{font-size:10pt;}
 -->
 </style>
 

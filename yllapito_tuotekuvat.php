0e6253456a39f56b51edcda47ab794d35b55d1b4
samiluukkonen <sami@devlab.fi>
2008-08-27 10:21:31 +0000

    Osastojen ja tuoteryhmien haku uudella avainsana-funktiolla.

--- yllapito_tuotekuvat.php
+++ yllapito_tuotekuvat.php
@@ -54,8 +54,8 @@
 							echo "<td class='back'>";
 
 								// näytetään soveltuvat osastot
-								$query = "SELECT avainsana.selite, ".avain('select')." FROM avainsana ".avain('join','OSASTO_')." WHERE avainsana.yhtio='$kukarow[yhtio]' and avainsana.laji='OSASTO' order by avainsana.selite+0";
-								$res2  = mysql_query($query) or die($query);
+								// tehdään avainsana query
+								$res2 = avainsana("OSASTO", $kukarow['kieli']);
 
 								if (mysql_num_rows($res2) > 11) {
 									echo "<div style='height:320;overflow:auto;'>";
@@ -98,8 +98,8 @@
 							echo "<td valign='top' class='back'>";
 
 								// näytetään soveltuvat tryt
-								$query = "SELECT avainsana.selite, ".avain('select')." FROM avainsana ".avain('join','TRY_')." WHERE avainsana.yhtio='$kukarow[yhtio]' and avainsana.laji='TRY' order by avainsana.selite+0";
-								$res2  = mysql_query($query) or die($query);
+								// tehdään avainsana query
+								$res2 = avainsana("TRY", $kukarow['kieli']);
 
 								if (mysql_num_rows($res2) > 11) {
 									echo "<div style='height:320;overflow:auto;'>";

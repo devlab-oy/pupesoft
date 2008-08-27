f0f115f8ded813e68f5ee1288f7983fb4e2abb70
samiluukkonen <sami@devlab.fi>
2008-08-27 11:08:10 +0000

    Osastojen ja tuoteryhmien haku uudella avainsana-funktiolla.

--- varastopaikkojen_siivous.php
+++ varastopaikkojen_siivous.php
@@ -544,12 +544,8 @@
 
 		echo "<tr><th>".t("Osasto")."</th><td>";
 
-		$query = "	SELECT distinct avainsana.selite, ".avain('select')."
-					FROM avainsana
-					".avain('join','OSASTO_')."
-					WHERE avainsana.yhtio='$kukarow[yhtio]' and avainsana.laji='OSASTO'
-					ORDER BY avainsana.selite+0";
-		$sresult = mysql_query($query) or pupe_error($query);
+		// tehd‰‰n avainsana query
+		$sresult = avainsana("OSASTO", $kukarow['kieli']);
 
 		echo "<select name='osasto'>";
 		echo "<option value=''>".t("N‰yt‰ kaikki")."</option>";
@@ -568,12 +564,8 @@
 				<tr><th>".t("Tuoteryhm‰")."</th><td>";
 
 		//Tehd‰‰n osasto & tuoteryhm‰ pop-upit
-		$query = "	SELECT distinct avainsana.selite, ".avain('select')."
-					FROM avainsana
-					".avain('join','TRY_')."
-					WHERE avainsana.yhtio='$kukarow[yhtio]' and avainsana.laji='TRY'
-					ORDER BY avainsana.selite+0";
-		$sresult = mysql_query($query) or pupe_error($query);
+		// tehd‰‰n avainsana query
+		$sresult = avainsana("TRY", $kukarow['kieli']);
 
 		echo "<select name='tuoryh'>";
 		echo "<option value=''>".t("N‰yt‰ kaikki")."</option>";

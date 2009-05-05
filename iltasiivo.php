d442752c4136845129e92202ffad1440fe3e676d
juppe <juppe@devlab.fi>
2009-05-05 12:26:20 +0000

    Poistaa nyt myös tilausrivit jotka ei oo dellattu vaikka otsikot on dellattu!

--- iltasiivo.php
+++ iltasiivo.php
@@ -14,7 +14,7 @@
 		$kukarow['yhtio'] = $argv[1];
 		$kukarow['kuka'] = "crond";
 
-		$query    = "select * from yhtio where yhtio='$kukarow[yhtio]'";
+		$query    = "SELECT * from yhtio where yhtio='$kukarow[yhtio]'";
 		$yhtiores = mysql_query($query) or pupe_error($query);
 
 		if (mysql_num_rows($yhtiores) == 1) {
@@ -61,7 +61,7 @@
 		$laskuri   = 0;
 
 		// poistetaan kaikki tuotteen_toimittajat liitokset joiden toimittaja on poistettu
-		$query = "	select toimi.tunnus, tuotteen_toimittajat.tunnus toimtunnus
+		$query = "	SELECT toimi.tunnus, tuotteen_toimittajat.tunnus toimtunnus
 					from tuotteen_toimittajat
 					left join toimi on toimi.yhtio = tuotteen_toimittajat.yhtio and toimi.tunnus = tuotteen_toimittajat.liitostunnus
 					where tuotteen_toimittajat.yhtio = '$kukarow[yhtio]'
@@ -69,7 +69,7 @@
 		$result = mysql_query($query) or die($query);
 
 		while ($row = mysql_fetch_array($result)) {
-			$query = "delete from tuotteen_toimittajat where tunnus = '$row[toimtunnus]'";
+			$query = "DELETE from tuotteen_toimittajat where tunnus = '$row[toimtunnus]'";
 			$deler = mysql_query($query) or die($query);
 			$laskuri ++;
 		}
@@ -78,7 +78,7 @@
 		$laskuri = 0;
 
 		// poistetaan kaikki tuotteen_toimittajat liitokset joiden tuote on poistettu
-		$query = "	select tuote.tunnus, tuotteen_toimittajat.tunnus toimtunnus
+		$query = "	SELECT tuote.tunnus, tuotteen_toimittajat.tunnus toimtunnus
 					from tuotteen_toimittajat
 					left join tuote on tuote.yhtio = tuotteen_toimittajat.yhtio and tuote.tuoteno = tuotteen_toimittajat.tuoteno
 					where tuotteen_toimittajat.yhtio = '$kukarow[yhtio]'
@@ -86,7 +86,7 @@
 		$result = mysql_query($query) or die($query);
 
 		while ($row = mysql_fetch_array($result)) {
-			$query = "delete from tuotteen_toimittajat where tunnus = '$row[toimtunnus]'";
+			$query = "DELETE from tuotteen_toimittajat where tunnus = '$row[toimtunnus]'";
 			$deler = mysql_query($query) or die($query);
 			$laskuri ++;
 		}
@@ -95,8 +95,9 @@
 		
 		$laskuri = 0;
 		$laskuri2 = 0;
+		
 		// poistetaan kaikki JT-otsikot jolla ei ole enää rivejä ja extranet tilaukset joilla ei ole rivejä ja tietenkin myös ennakkootsikot joilla ei ole rivejä.
-		$query = "	select tilausrivi.tunnus, lasku.tunnus laskutunnus, lasku.tila, lasku.tunnusnippu
+		$query = "	SELECT tilausrivi.tunnus, lasku.tunnus laskutunnus, lasku.tila, lasku.tunnusnippu
 					from lasku
 					left join tilausrivi on tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus
 					where lasku.yhtio = '$kukarow[yhtio]' and
@@ -109,18 +110,18 @@
 
 			//	Jos kyseessä on tunnusnippupaketti, halutaan säilyttää linkki tästä tehtyihin tilauksiin, tilaus merkataan vain toimitetuksi
 			if($row["tunnusnippu"] > 0) {
-				$query = "update lasku set tila = 'L', alatila='X' where yhtio = '$kukarow[yhtio]' and tunnus = '$row[laskutunnus]'";
+				$query = "UPDATE lasku set tila = 'L', alatila='X' where yhtio = '$kukarow[yhtio]' and tunnus = '$row[laskutunnus]'";
 				$deler = mysql_query($query) or die($query);
 				$laskuri2 ++;
 			}
 			else {
-				$query = "update lasku set alatila='$row[tila]', tila='D',  comments = '$komm' where yhtio = '$kukarow[yhtio]' and tunnus = '$row[laskutunnus]'";
+				$query = "UPDATE lasku set alatila='$row[tila]', tila='D',  comments = '$komm' where yhtio = '$kukarow[yhtio]' and tunnus = '$row[laskutunnus]'";
 				$deler = mysql_query($query) or die($query);
 				$laskuri ++;
 			}
 
 			//poistetaan TIETENKIN kukarow[kesken] ettei voi syöttää extranetissä rivejä tälle
-			$query = "update kuka set kesken = '' where yhtio = '$kukarow[yhtio]' and kesken = '$row[laskutunnus]'";
+			$query = "UPDATE kuka set kesken = '' where yhtio = '$kukarow[yhtio]' and kesken = '$row[laskutunnus]'";
 			$deler = mysql_query($query) or die($query);
 
 		}
@@ -129,8 +130,9 @@
 		if ($laskuri2 > 0) $iltasiivo .= "Merkattiin toimitetuksi $laskuri2 rivitöntä tilausta.\n";
 
 		$laskuri = 0;
+		
 		// Merkitään laskut mitätöidyksi joilla on pelkästään mitätöityjä rivejä.
-		$query = "	select lasku.tunnus laskutunnus, lasku.tila, count(*) kaikki, sum(if(tilausrivi.tyyppi='D',1,0)) dellatut
+		$query = "	SELECT lasku.tunnus laskutunnus, lasku.tila, count(*) kaikki, sum(if(tilausrivi.tyyppi='D',1,0)) dellatut
 					from lasku
 					join tilausrivi on tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus
 					where lasku.yhtio = '$kukarow[yhtio]' 
@@ -143,33 +145,57 @@
 		while ($row = mysql_fetch_array($result)) {
 			$komm = "(" . $kukarow['kuka'] . "@" . date('Y-m-d') .") ".t("Mitätöi ohjelmassa iltasiivo.php (1.5)")."<br>";
 
-			$query = "update lasku set alatila='$row[tila]', tila='D',  comments = '$komm' where yhtio = '$kukarow[yhtio]' and tunnus = '$row[laskutunnus]'";
+			$query = "UPDATE lasku set alatila='$row[tila]', tila='D',  comments = '$komm' where yhtio = '$kukarow[yhtio]' and tunnus = '$row[laskutunnus]'";
 			$deler = mysql_query($query) or die($query);
 			$laskuri ++;
 
 			//poistetaan TIETENKIN kukarow[kesken] ettei voi syöttää extranetissä rivejä tälle
-			$query = "update kuka set kesken = '' where yhtio = '$kukarow[yhtio]' and kesken = '$row[laskutunnus]'";
+			$query = "UPDATE kuka set kesken = '' where yhtio = '$kukarow[yhtio]' and kesken = '$row[laskutunnus]'";
 			$deler = mysql_query($query) or die($query);
 
 		}
 
 		if ($laskuri > 0) $iltasiivo .= "Mitätöitiin $laskuri tilausta joilla oli pelkkiä mitätöityjä rivejä.\n";
 
+
+		$laskuri = 0;
+		
+		// Merkitään rivit mitätöidyksi joiden otsikot on mitätöity
+		$query = "	SELECT lasku.tunnus laskutunnus
+					from lasku
+					join tilausrivi on tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus and tilausrivi.tyyppi!='D'
+					where lasku.yhtio = '$kukarow[yhtio]' 
+					and lasku.tila = 'D'
+					GROUP BY 1";
+		$result = mysql_query($query) or die($query);
+
+		while ($row = mysql_fetch_array($result)) {
+			$komm = "(" . $kukarow['kuka'] . "@" . date('Y-m-d') .") ".t("Mitätöi ohjelmassa iltasiivo.php (1.5)")."<br>";
+
+			$query = "UPDATE tilausrivi set tyyppi='D'  where yhtio = '$kukarow[yhtio]' and otunnus = '$row[laskutunnus]'";
+			$deler = mysql_query($query) or die($query);
+			$laskuri ++;
+		}
+
+		if ($laskuri > 0) $iltasiivo .= "Mitätöitiin $laskuri mitätöidyn tilauksen rivit. (Rivit jostain syystä ei dellattuja)\n";
+
+
 		// tässä tehdään isittömistä perheistä ei-perheitä ja myös perheistä joissa ei ole lapsia eli nollataan perheid
 		$lask = 0;
 		$lask2 = 0;
 
-		$query =	"SELECT perheid, count(*) koko
+		$query = "	SELECT perheid, count(*) koko
 					FROM tilausrivi
 					WHERE yhtio = '$kukarow[yhtio]' and tyyppi = 'L' and laskutettuaika = '0000-00-00' and perheid != '0'
 					GROUP BY perheid";
 		$result = mysql_query($query) or pupe_error($query);
 
 		while ($row = mysql_fetch_array($result)) {
-			$query =	"SELECT perheid
+			$query = "	SELECT perheid
 						FROM tilausrivi
 						WHERE yhtio = '$kukarow[yhtio]' and tyyppi = 'L' and laskutettuaika = '0000-00-00' and tunnus = '$row[perheid]'";
 			$result2 = mysql_query($query) or pupe_error($query);
+			
 			if (mysql_num_rows($result2) == 0) {
 				$lask++;
 				$query = "UPDATE tilausrivi SET perheid = 0 WHERE yhtio = '$kukarow[yhtio]' and tyyppi = 'L' and laskutettuaika = '0000-00-00' and perheid = '$row[perheid]' order by tunnus";
@@ -192,22 +218,23 @@
 		$laskpois = 0;
 		$poistetaankpl = 0;
 
-		$query = 	"SELECT tuoteno, liitostunnus, count(tunnus) countti
+		$query = "	SELECT tuoteno, liitostunnus, count(tunnus) countti
 					FROM tuotteen_toimittajat
 					WHERE yhtio = '$kukarow[yhtio]'
 					GROUP BY 1,2
 					HAVING countti > 1";
 		$result = mysql_query($query) or pupe_error($query);
+		
 		while ($row = mysql_fetch_array($result)) {
 			$lasktuote++;
 			$poistetaankpl = $row['countti']-1;
 
-			$poisquery =	"DELETE FROM tuotteen_toimittajat
+			$poisquery = "	DELETE FROM tuotteen_toimittajat
 							WHERE yhtio = '$kukarow[yhtio]'
 							AND tuoteno = '$row[tuoteno]'
 							AND liitostunnus = '$row[liitostunnus]'
 							ORDER BY tunnus DESC
-							LIMIT $poistetaankpl";
+						 	LIMIT $poistetaankpl";
 			$poisresult = mysql_query($poisquery) or pupe_error($poisquery);
 			$laskpois += mysql_affected_rows();
 		}
@@ -216,22 +243,7 @@
 			$iltasiivo .= "Poistettiin $lasktuote tuotteelta yhteensä $laskpois duplikaatti tuotteen_toimittajaa\n";
 		}
 
-		// Korjataan rikkinäiset käyttöoikeudet (jos samassa sovelluksessa on duplikaatti linkkejä)
-		$query = "	SELECT kuka, sovellus, nimi, alanimi, group_concat(tunnus) tunnukset,  count(*) kpl
-					FROM oikeu
-					WHERE yhtio = '$kukarow[yhtio]'
-					and kuka   != ''
-					group by 1,2,3,4
-					having kpl > 1";
-		$result = mysql_query($query) or pupe_error($query);
-
-		while ($row = mysql_fetch_array($result)) {
-			$query = "DELETE FROM oikeu WHERE yhtio = '$kukarow[yhtio]' and tunnus in ($row[tunnukset]) LIMIT ".($row["kpl"]-1);
-			$delresult = mysql_query($query) or pupe_error($query);
-
-			$iltasiivo .= "Poistettiin käyttäjältä $row[kuka] duplikaatti käyttöoikeus: $row[sovellus] $row[nimi] $row[alanimi]\n";
-		}
-
+		
 		if ($iltasiivo != "") {
 
 			echo "Iltasiivo ".date("d.m.Y")." - $yhtiorow[nimi]\n\n";

9482af0c8ab35929d6be090cd98b65f5b47b7349
jonikanerva <joni@devlab.fi>
2009-06-09 07:18:07 +0000

    Ei näytetä saldottomien tuotteiden keräysaikoja.

--- pakkaamojen_selaus.php
+++ pakkaamojen_selaus.php
@@ -1,36 +1,36 @@
 <?php
 	require ("inc/parametrit.inc");
-	
+
 	if ($tee == 'NAYTATILAUS') {
 		echo "<font class='head'>".t("Tilaus")." $tunnus:</font><hr>";
 		require ("raportit/naytatilaus.inc");
 	}
-	
+
 	echo "<font class='head'>".t("Pakkaamojen selaus")."</font><hr>";
-	
+
 	if ($tee == "nollaa") {
 		if (isset($checktunnukset) and is_array($checktunnukset)) {
 			$tunnukset = implode(',', $checktunnukset);
 		}
-		
+
 		if ($tunnukset != "") {
 			$query = " 	UPDATE lasku
 						SET pakkaamo = 0
 						WHERE yhtio = '$kukarow[yhtio]'
 						AND tunnus in($tunnukset)";
 			$nollausres = mysql_query($query) or pupe_error($query);
-			
-			echo t("Lokerot nollattiin tilauksista")." : ".$tunnukset; 
+
+			echo t("Lokerot nollattiin tilauksista")." : ".$tunnukset;
 		}
 		else {
 			echo "<font class='error'>".t("VIRHE: Et valinnut yht‰‰n tilausta")."!</font>";
 		}
-		
+
 	}
-	
+
 	echo "<table>";
-	echo "<form action='$PHP_SELF' name='find' method='post'>";	
-	
+	echo "<form action='$PHP_SELF' name='find' method='post'>";
+
 	echo "<tr><td>".t("Valitse pakkaamo:")."</td><td><select name='tupakkaamo' onchange='submit()'>";
 
 	$query = "	SELECT distinct nimi
@@ -59,9 +59,9 @@
 		echo "<option value='$row[nimi]' $sel>".$row["nimi"]."</option>";
 	}
 
-	echo "</select></td></tr>";	
+	echo "</select></td></tr>";
 	echo "</form></table>";
-	
+
 	if ($tupakkaamo == '' and $kukarow['oletus_pakkaamo'] != '') {
 		$query = "	SELECT group_concat(tunnus SEPARATOR ',') tunnukset
 		  			FROM pakkaamo
@@ -69,9 +69,9 @@
 					AND nimi = '$kukarow[oletus_pakkaamo]'";
 		$etsire = mysql_query($query) or pupe_error($query);
 		$etsirow = mysql_fetch_array($etsire);
-		
+
 		$haku .= " and lasku.pakkaamo in($etsirow[tunnukset])";
-		
+
 	}
 	elseif ($tupakkaamo != '' and $tupakkaamo != 'KAIKKI') {
 		$query = "	SELECT group_concat(tunnus SEPARATOR ',') tunnukset
@@ -80,47 +80,45 @@
 					AND nimi = '$tupakkaamo'";
 		$etsire = mysql_query($query) or pupe_error($query);
 		$etsirow = mysql_fetch_array($etsire);
-		
+
 		$haku .= " and lasku.pakkaamo in($etsirow[tunnukset])";
 	}
-		
+
 	$query = "	SELECT 	pakkaamo.nimi pakkaamo,
 	 					pakkaamo.lokero,
 						lasku.ytunnus,
 						lasku.tunnus,
 						lasku.lahetepvm,
-						if(lasku.tila = 'L', concat_ws(' ', lasku.toim_nimi, lasku.toim_nimitark), lasku.nimi) asnimi,
-						ifnull(min(tilausrivi.kerattyaika),'0000-00-00 00:00:00') kerayspvm
+						if(lasku.tila = 'L', concat_ws(' ', lasku.toim_nimi, lasku.toim_nimitark), lasku.nimi) asnimi
 						FROM lasku
-						JOIN pakkaamo ON pakkaamo.yhtio = lasku.yhtio and pakkaamo.tunnus = lasku.pakkaamo
-						LEFT JOIN tilausrivi ON tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus and tilausrivi.kerattyaika != '0000-00-00 00:00:00'
+						JOIN pakkaamo ON (pakkaamo.yhtio = lasku.yhtio and pakkaamo.tunnus = lasku.pakkaamo)
 						WHERE lasku.yhtio = '$kukarow[yhtio]'
-						AND lasku.tila in('L','G') 
-						AND lasku.alatila in('A','C')
+						AND lasku.tila in ('L','G')
+						AND lasku.alatila in ('A','C')
 						AND lasku.pakkaamo > 0
 						$haku
 						GROUP BY pakkaamo.nimi, pakkaamo.lokero, lasku.ytunnus, lasku.tunnus, lasku.lahetepvm, asnimi
 						ORDER BY pakkaamo.nimi, pakkaamo.lokero, lasku.ytunnus, lasku.lahetepvm, kerayspvm";
 	$pakkaamore = mysql_query($query) or pupe_error($query);
-	
+
 	if (mysql_num_rows($pakkaamore) != 0) {
-				
+
 		$pakkaamo = $lokero = "";
-		
-		
+
+
 		//echo "<table><tr><td>";
 		echo "<table>";
 		echo "<form method='post' action='$PHP_SELF'>";
 		echo "<input type='hidden' name='tee' value='nollaa'>";
 		echo "<input type='hidden' name='tupakkaamo' value='$tupakkaamo'>";
 		while ($row = mysql_fetch_array($pakkaamore)) {
-			
+
 			if ($pakkaamo == "" or $pakkaamo != $row['pakkaamo']) {
-				echo "<tr><td colspan ='7' class='back'><br><font class='head'>".$row['pakkaamo']."</font></td></tr>";				
+				echo "<tr><td colspan ='7' class='back'><br><font class='head'>".$row['pakkaamo']."</font></td></tr>";
 			}
-			
+
 			if ($lokero == "" or $lokero != $row['lokero']) {
-			
+
 				echo "<tr>";
 				echo "<th valign='top'>".t("Lokero")."</th>";
 				echo "<th valign='top'>".t("Asiakas")."</th>";
@@ -129,26 +127,36 @@
 				echo "<th valign='top'>".t("Tulostettu")."</th>";
 				echo "<th valign='top'>".t("Ker‰tty")."</th>";
 				echo "<th valign='top'><input type='submit' value='".t("Nollaa")."'></th>";
-												
+
 				echo "<tr><td>".$row['lokero']."</td>";
 			}
 			else {
 				echo "<tr><td></td>";
 			}
-			
+
+			$query = "	SELECT min(tilausrivi.kerattyaika) kerayspvm
+						FROM tilausrivi USE INDEX (yhtio_otunnus)
+						JOIN tuote ON (tuote.yhtio = tilausrivi.yhtio and tuote.tuoteno = tilausrivi.tuoteno and tuote.ei_saldoa = '')
+						WHERE tilausrivi.yhtio = '$kukarow[yhtio] '
+						and tilausrivi.otunnus = '$row[tunnus]'
+						and tilausrivi.kerattyaika != '0000-00-00 00:00:00'
+						and tilausrivi.var NOT IN ('P','J')";
+			$kerayspvmres = mysql_query($query) or pupe_error($query);
+			$kerayspvmrow = mysql_fetch_array($kerayspvmres);
+
 			echo "<td>".$row['ytunnus']."</td>";
 			echo "<td>".$row['asnimi']."</td>";
 			echo "<td><a href='?tupakkaamo=$tupakkaamo&tee=NAYTATILAUS&tunnus=$row[tunnus]'>".$row['tunnus']."</a></td>";
 			echo "<td>".tv1dateconv($row['lahetepvm'],"P","LYHYT")."</td>";
-			echo "<td>".tv1dateconv($row['kerayspvm'],"P","LYHYT")."</td>";
+			echo "<td>".tv1dateconv($kerayspvmrow['kerayspvm'],"P","LYHYT")."</td>";
 			echo "<td><input type='checkbox' name='checktunnukset[]' value='$row[tunnus]'></td></tr>";
-			
+
 			$pakkaamo = $row['pakkaamo'];
 			$lokero = $row['lokero'];
 		}
 		echo "</form></table>";
-		
+
 	}
-	
+
 	require ("inc/footer.inc");
 ?>

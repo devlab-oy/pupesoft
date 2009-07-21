65208b32d166ee45bb35c1dfb112b50807f269d5
juppe <juppe@devlab.fi>
2009-07-21 11:59:06 +0000

    t_avainsanattu!

--- yllapito_tuotekuvat.php
+++ yllapito_tuotekuvat.php
@@ -55,7 +55,7 @@
 
 								// näytetään soveltuvat osastot
 								// tehdään avainsana query
-								$res2 = avainsana("OSASTO", $kukarow['kieli']);
+								$res2 = t_avainsana("OSASTO");
 
 								if (mysql_num_rows($res2) > 11) {
 									echo "<div style='height:320;overflow:auto;'>";
@@ -99,7 +99,7 @@
 
 								// näytetään soveltuvat tryt
 								// tehdään avainsana query
-								$res2 = avainsana("TRY", $kukarow['kieli']);
+								$res2 = t_avainsana("TRY");
 
 								if (mysql_num_rows($res2) > 11) {
 									echo "<div style='height:320;overflow:auto;'>";

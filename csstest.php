8a49e5305d92eeff7de9fae7840b3e05d9fa3808
jonikanerva <joni@devlab.fi>
2009-07-24 12:44:24 +0000

    CSS viilauksia, alkudatassa on nyt viimeistelty CSS

--- csstest.php
+++ csstest.php
@@ -20,7 +20,7 @@
 	echo "
 	<font class='head'>CSS-testing:</font><hr><br>";
 
-	preg_match_all("/.*?\/\*((FONT|TAUSTA)VÄRI.*?)\*\//", $yhtiorow['css'], $varitmatch);
+	preg_match_all("/.*?\/\*(.*?(_COLOR|_BACKGROUND))\*\//", $yhtiorow['css'], $varitmatch);
 
 	$varit = array();
 
@@ -32,6 +32,71 @@
 
 	ksort($varit);
 
+	echo "
+	Tässä näkee miten formit käyttäytyy:<hr>
+	<form action='#'>
+	<select>
+	<option>1 - ensimmäinen</option>
+	<option>2 - toinen</option>
+	<option>3 - kolmas</option>
+	</select>
+	<input type='text'>
+	<input type='checkbox'	name='1'>
+	<input type='radio' 	name='2'>
+	<input type='radio' 	name='2'>
+	<input type='submit' value='Normaali submit-nappula'>
+	</form> pitäisi pysyä nipussa ilman suurempia aukkoja ja rivinvaihtoja.
+
+	<br>
+	<br>
+
+	Muutama nappula:<hr>
+	<input type='button' value='input type=button'>
+	<button class='valinta'>valinta:class button-nappula</button>
+	<button class='valinta'>normaali button-nappula</button>
+
+	<br>
+	<br>
+
+	Taulukko:<hr>
+	<table>
+	<tr><th>TH</th><th>Normi headeri</th></tr>
+	<tr><th><a href='#'>TH linkki</a></th><th><a href='#'>normi linkki th solussa</th></tr>
+	<tr><td>TD</td><td>Normi solu</td></tr>
+	<tr><td><a href='#'>TD linkki</a></td><td><a href='#'>normi linkki td solusssa</a></td></tr>
+	<tr><td><a class='td' href='#'>TD linkki class='td'</a></td><td><a class='td' href='#'>td-luokan linkki td solusssa</a></td></tr>
+	<tr class='aktiivi'><td>TD (TR class='aktiivi')</td><td>normi TD mutta TR:ssä on hover toiminto</td></tr>
+	<tr><td class='back'>TD class='back'</td><td class='back'>tausta sama kuin BODY:ssä</td></tr>
+	<tr><td class='ok'>TD class='ok'</td><td class='ok'>ok tekstiä normi td solussa</td></tr>
+	<tr><td class='error'>TD class='error'</td><td class='error'>error tekstiä normi td solussa</td></tr>
+	<tr><td class='spec'>TD class='spec'</td><td class='spec'>speciaali TD, esim. tausta sama kuin TD:ssä mutta fontinväri sama ku TH:ssa</td></tr>
+	<tr><td class='tumma'>TD class='tumma'</td><td class='tumma'>speciaali TD esim. fontti ja tausta samanväriset kuin TH:ssa</td></tr>
+	<tr><td class='liveSearch'>TD class='liveSearch'</td><td class='liveSearch'>käytetään jossain searchissa</td></tr>
+	</table>
+
+	<br>
+	Linkit:<hr>
+	<div style='width:300px'>
+	<p><a href='#'>oletus-linkki</a></p>
+	<p><a class='kale' href='#'>class kale linkki</a></p>
+	<p><a class='menu' href='#'>class menu linkki</a></p>
+	<p><a class='td' href='#'>class td linkki</a></p>
+	</div>
+
+	<br>
+	Fontit:<hr>
+	Default tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+	<font class='head'>HEAD tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</font><br>
+	<font class='message'>MESSAGE tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</font><br>
+	<font class='error'>ERROR tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit. (Esim. Punainen)</font><br>
+	<font class='ok'>OK tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit. (Esim. Kirkkaanvihreä)</font><br>
+	<font class='info'>INFO tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit. </font><br>
+	<font class='kaleinfo'>KALEINFO tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</font><br>
+	<pre>PRE-tekstiä: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tämä on monospace fontti.</pre>
+	<div class='popup' style='visibility:visible'>div class='popup' tulee yleensä taulukon päälle, pitää olla hyvän näköinen suhteessa muihin väreihin</div>
+
+	<br><br><br>";
+	
 	echo "	<script language='javascript'>
 				function variupdate (vari_index) {
 					document.getElementById(\"2_\"+vari_index).style.backgroundColor = document.getElementById(\"1_\"+vari_index).value.substring(1);
@@ -53,96 +118,7 @@
 	}
 
 	echo "</table><input type='submit' value='Päivitä'></form><br><br><br><br>";
-
-
-	echo "
-	Tässä näkee miten logon linkki käyttäytyy (Linkki A.puhdas):<hr><br>
-	<a class='puhdas' href='#'><img border='0' src='http://www.pupesoft.com/pupesoft.gif' alt='logo' height='50'></a>
-
-	<br>
-	<br>
-	<br>
-
-	Tässä näkee miten formit käyttäytyy:<hr>
-	<form action='#'>
-	<select>
-	<option>1 - ensimmäinen</option>
-	<option>2 - toinen</option>
-	<option>3 - kolmas</option>
-	</select>
-	<input type='text'>
-	<input type='checkbox'	name='1'>
-	<input type='radio' 	name='2'>
-	<input type='radio' 	name='2'>
-	<input type='submit' value='Normaali submit-nappula'>
-	</form> pitäisi pysyä nipussa ilman suurempia aukkoja ja rivinvaihtoja.
-
-	<br>
-	<br>
-	<br>
-
-	Muutama nappula:<hr>
-	<input type='button' value='input type=button'>
-	<button class='valinta'>valinta:class button-nappula</button>
-	<button class='valinta'>normaali button-nappula</button>
-
-	<br>
-	<br>
-	<br>
-
-	Taulukko:<hr>
-	<table>
-	<tr><th>TH</th><th>TH</th><th>TH</th></tr>
-	<tr><th><a href='#'>TH-linkki, fontinväri sama kuin TH:ssa</a></th><th><a href='#'>TH-linkki, fontinväri sama kuin TH:ssa</th><th><a href='#'>TH-linkki, fontinväri sama kuin TH:ssa</th></tr>
-	<tr><td>TD</td><td>TD</td><td>TD</td></tr>
-	<tr><td><a class='td' href='#'>td:class linkki</a></td><td><a class='td' href='#'>td:class linkki</a></td><td><a class='td' href='#'>td:class linkki</a></td></tr>
-	<tr class='aktiivi'><td>TD (TR.aktiivi) (Tässä on hover toiminto)</td><td>TD (tr aktiivi)</td><td>TD (tr aktiivi)</td></tr>
-	<tr><td class='back'>TD.back: Fontinväri sama kuin TD:ssä mutta tausta sama kuin BODY:ssä</td><td class='back'>TD.back</td><td class='back'>TD.back</td></tr>
-	<tr><td class='green'>TD.green: Fontti vihreä. Tausta sama kuin TD:ssä</td><td class='green'>TD.green</td><td class='green'>TD.green</td></tr>
-	<tr><td class='liveSearch'>TD.liveSearch</td><td class='liveSearch'></td><td class='liveSearch'></td></tr>
-	<tr><td class='red'>TD.red: Fontti punainen. Tausta sama kuin TD:ssä</td><td class='red'>TD.red</td><td class='red'>TD.red</td></tr>
-	<tr><td class='spec'>TD.spec: Tausta sama kuin TD:ssä mutta fontinväri sama ku TH:ssa</td><td class='spec'>TD.spec</td><td class='spec'>TD.spec</td></tr>
-	<tr><td class='tumma'>TD.tumma: Fontti ja tausta samanväriset kuin TH:ssa</td><td class='tumma'>TD.tumma</td><td class='tumma'>TD.tumma</td></tr>
-	</table>
-
-	<br>
-	<br>
-	<br>
-	Linkit:<hr>
-	<a href='#'>oletus-linkki</a><br><br>
-	<a class='kale' href='#'>kale:class linkki</a><br><br>
-	<a class='menu' href='#'>menu:class linkki</a><br>
-	<a class='puhdas' href='#'>puhdas:class linkki</a><br><br>
-	<a class='td' href='#'>td:class linkki</a><br>
-
-	<br>
-	<br>
-	<br>
-	Fontit:<hr>
-	Default tekstiä: bla bla bla bla!!!<br>
-	<font class='error'>ERROR tekstiä: bla bla bla bla!!! (Esim. Punainen)</font><br>
-	<font class='head'>HEAD tekstiä: bla bla bla bla!!!</font><br>
-	<font class='info'>INFO tekstiä: bla bla bla bla!!!</font><br>
-	<font class='kaleinfo'>KALEINFO tekstiä: bla bla bla bla!!!</font><br>
-	<font class='menu'>MENU tekstiä: bla bla bla bla!!!</font><br>
-	<font class='message'>MESSAGE tekstiä: bla bla bla bla!!!</font><br>
-	<font class='ok'>OK tekstiä: bla bla bla bla!!! (Esim. Kirkkaanvihreä)</font><br>
-
-
-
-	<br>
-	<br>
-
-	<pre>PRE-tekstiä: bla bla bla bla!!! Tämä on monospace fontti.</pre>
-
-	<br>
-	<br>
-
-	<div class='popup' style='visibility:visible'>DIV:POPUP kannataa vaan kattoa, että on hyvän näköinen suhteessa muihin väreihin</div>
-
-	<br>
-	<br>";
-
+	
 	require("inc/footer.inc");
 
 ?>

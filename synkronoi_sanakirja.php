1f0313681589f330a7998c1912a2b53e046b5271
juppe <juppe@devlab.fi>
2009-09-17 11:57:18 +0000

    Urli oli väärin!

--- synkronoi_sanakirja.php
+++ synkronoi_sanakirja.php
@@ -24,22 +24,22 @@ if ($oikeurow['paivitys'] != '1') { // Saako päivittää
 
 if ($tee == "TEE" or $tee == "UPDATE") {
 
-	$file	 = fopen("http://www.pupesoft.com/softa/referenssisanakirja.sql","r") or die (t("Tiedoston avaus epäonnistui")."!");
+	$file	 = fopen("http://www.devlab.fi/softa/referenssisanakirja.sql","r") or die (t("Tiedoston avaus epäonnistui")."!");
 	$rivi    = fgets($file);
 	$otsikot = explode("\t", strtoupper(trim($rivi)));
 
 	if (count($otsikot) > 1) {
 		$sync_otsikot = array();
-		
+
 		for ($i = 0; $i < count($otsikot); $i++) {
-			
+
 			$a = strtolower($otsikot[$i]);
-			
+
 			$sync_otsikot[$a] = $i;
 		}
-				
+
 		if (isset($sync_otsikot["fi"])) {
-			
+
 			echo "<table>";
 			echo "<tr><th>".t("Kysytty")."</td>
 			<th>".t("Me")." FI</td><th>".t("Ref")." FI</td>
@@ -47,11 +47,11 @@ if ($tee == "TEE" or $tee == "UPDATE") {
 			<th>".t("Me")." EN</td><th>".t("Ref")." EN</td>
 			<th>".t("Me")." DE</td><th>".t("Ref")." DE</td>
 			</tr>";
-			
-			
+
+
 			$sanakirjaquery  = "UPDATE sanakirja SET synkronoi = ''";
 			$sanakirjaresult = mysql_query($sanakirjaquery) or pupe_error($sanakirjaquery);
-			
+
 			$rivi = fgets($file);
 
 			while (!feof($file)) {
@@ -61,27 +61,27 @@ if ($tee == "TEE" or $tee == "UPDATE") {
 				$rivi	 = explode("\t", trim($rivi));
 
 				if($rivi[$sync_otsikot["fi"]] != "") {
-					
+
 					$sanakirjaquery  = "SELECT kysytty,fi,se,en,de,dk FROM sanakirja WHERE fi = BINARY '".$rivi[$sync_otsikot["fi"]]."'";
 					$sanakirjaresult = mysql_query($sanakirjaquery) or pupe_error($sanakirjaquery);
-					
+
 					if (mysql_num_rows($sanakirjaresult) > 0) {
 						$sanakirjarow = mysql_fetch_array($sanakirjaresult);
-						
+
 						$sanakirjaquery  = "UPDATE sanakirja SET synkronoi = 'X' where fi = BINARY '$sanakirjarow[fi]'";
 						$sanakirjaresult = mysql_query($sanakirjaquery) or pupe_error($sanakirjaquery);
-						
+
 						echo "<tr><td>".$rivi[$sync_otsikot["kysytty"]]."</td>";
-							
+
 						echo "<td>".$sanakirjarow["fi"]."</td><td>".$rivi[$sync_otsikot["fi"]]."</td>";
-						
+
 						if ($sanakirjarow["se"] != $rivi[$sync_otsikot["se"]]) {
-							
+
 							if ($tee == "UPDATE") {
 								$sanakirjaquery  = "UPDATE sanakirja SET se = '".$rivi[$sync_otsikot["se"]]."' where fi = BINARY '$sanakirjarow[fi]'";
 								$sanakirjaresult = mysql_query($sanakirjaquery) or pupe_error($sanakirjaquery);
 							}
-							
+
 							$e = "<font class='error'>";
 							$t = "</font>";
 						}
@@ -89,17 +89,17 @@ if ($tee == "TEE" or $tee == "UPDATE") {
 							$e = "";
 							$t = "";
 						}
-						
+
 						echo "<td>$e".$sanakirjarow["se"]."$t</td><td>".$rivi[$sync_otsikot["se"]]."</td>";
-						
+
 						if ($sanakirjarow["en"] != $rivi[$sync_otsikot["en"]]) {
-							
+
 							if ($tee == "UPDATE") {
 								$sanakirjaquery  = "UPDATE sanakirja SET en = '".$rivi[$sync_otsikot["en"]]."' where fi = BINARY '$sanakirjarow[fi]'";
 								$sanakirjaresult = mysql_query($sanakirjaquery) or pupe_error($sanakirjaquery);
 							}
-							
-							
+
+
 							$e = "<font class='error'>";
 							$t = "</font>";
 						}
@@ -107,17 +107,17 @@ if ($tee == "TEE" or $tee == "UPDATE") {
 							$e = "";
 							$t = "";
 						}
-						
+
 						echo "<td>$e".$sanakirjarow["en"]."$t</td><td>".$rivi[$sync_otsikot["en"]]."</td>";
-						
+
 						if ($sanakirjarow["de"] != $rivi[$sync_otsikot["de"]]) {
-							
+
 							if ($tee == "UPDATE") {
 								$sanakirjaquery  = "UPDATE sanakirja SET de = '".$rivi[$sync_otsikot["de"]]."' where fi = BINARY '$sanakirjarow[fi]'";
 								$sanakirjaresult = mysql_query($sanakirjaquery) or pupe_error($sanakirjaquery);
 							}
-							
-							
+
+
 							$e = "<font class='error'>";
 							$t = "</font>";
 						}
@@ -125,19 +125,19 @@ if ($tee == "TEE" or $tee == "UPDATE") {
 							$e = "";
 							$t = "";
 						}
-						
+
 						echo "<td>$e".$sanakirjarow["de"]."$t</td><td>".$rivi[$sync_otsikot["de"]]."</td>";
-						
-						
+
+
 						echo "</tr>";
 					}
 					else {
-						
+
 						if ($tee == "UPDATE") {
 							$sanakirjaquery  = "INSERT INTO sanakirja SET fi = '".$rivi[$sync_otsikot["fi"]]."', aikaleima=now(), kysytty=1, laatija='$kukarow[kuka]', luontiaika=now()";
 							$sanakirjaresult = mysql_query($sanakirjaquery, $link) or pupe_error($sanakirjaquery);
 						}
-						
+
 						echo "<tr>
 								<td>".$rivi[$sync_otsikot["kysytty"]]."</td>
 								<td><font class='error'>".t("Sanaa puuttuu")."!</font></td><td>".$rivi[$sync_otsikot["fi"]]."</td>
@@ -151,9 +151,9 @@ if ($tee == "TEE" or $tee == "UPDATE") {
 				// luetaan seuraava rivi failista
 				$rivi = fgets($file);
 			}
-			
+
 			fclose($file);
-			
+
 			$sanakirjaquery  = "SELECT kysytty,fi,se,en,de,dk FROM sanakirja WHERE synkronoi='' and (se!='' or en!='') and kysytty>1 ORDER BY kysytty desc";
 			$sanakirjaresult = mysql_query($sanakirjaquery) or pupe_error($sanakirjaquery);
 
@@ -165,10 +165,10 @@ if ($tee == "TEE" or $tee == "UPDATE") {
 				echo "<td>".$sanakirjarow["de"]."</td><td><font class='error'>".t("Puuttuu referenssistä")."</font></td>";
 				echo "</tr>";
 			}
-			
+
 			echo "</table><br><br>";
-			
-			
+
+
 			echo "	<form method='post' action='$PHP_SELF'>
 					<input type='hidden' name='tee' value='UPDATE'>
 					<input type='submit' value='".t("Synkronoi")."'>
@@ -184,7 +184,7 @@ else {
 			<input type='submit' value='".t("Vertaa sanakirjoja")."'>
 			</form>";
 }
-	
+
 require ("inc/footer.inc");
-	
+
 ?>

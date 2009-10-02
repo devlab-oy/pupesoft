92cc702d0f8437906d29cdc8516b330846f14e39
jonikanerva <joni@devlab.fi>
2009-10-02 13:59:16 +0000

    Koodi 2000 luvulle.

--- tilauskasittely/korjaa_kerays.php
+++ tilauskasittely/korjaa_kerays.php
@@ -21,26 +21,28 @@
 
 	echo "<font class='head'>".t("Korjaa keräys").":</font><hr>";
 
-	if($tee=='KORJAA') {
-	
+	if ($tee == 'KORJAA') {
+
 		$query = "	UPDATE tilausrivi
-					SET keratty='',
-					kerattyaika=''
-					WHERE otunnus='$tunnus' and yhtio='$kukarow[yhtio]'";
+					SET keratty = '',
+					kerattyaika = ''
+					WHERE otunnus = '$tunnus'
+					and yhtio = '$kukarow[yhtio]'";
 		$result = mysql_query($query) or pupe_error($query);
-		
-		$query  = "	UPDATE lasku 
-					set alatila='A' 
-					where yhtio='$kukarow[yhtio]' 
-					and tunnus='$tunnus' 
-					and tila='L' and alatila='C'";
+
+		$query  = "	UPDATE lasku
+					set alatila = 'A'
+					where yhtio = '$kukarow[yhtio]'
+					and tunnus = '$tunnus'
+					and tila = 'L'
+					and alatila = 'C'";
 		$result = mysql_query($query) or pupe_error($query);
-		
+
 		if (mysql_affected_rows() != 1) {
 			pupe_error("".t("Keräämättömäksi merkkaaminen ei onnistu")."! $query");
-			exit;	
+			exit;
 		}
-		
+
 		$tee = '';
 
 		if ($logistiikka_yhtio != '' and $konserni_yhtiot != '') {
@@ -50,98 +52,74 @@
 
 	// meillä ei ole valittua tilausta
 	if ($tee == '') {
-		$formi="find";
-		$kentta="etsi";
+
+		$formi = "find";
+		$kentta = "etsi";
 
 		// tehdään etsi valinta
-		echo "<form action='$PHP_SELF' name='find' method='post'>".t("Etsi tilausta").": <input type='text' name='etsi'><input type='Submit' value='".t("Etsi")."'></form>";
+		echo "<form action='$PHP_SELF' name='find' method='post'>".t("Etsi tilausta").": <input type='text' name='etsi'><input type='Submit' value='".t("Etsi")."'></form><br><br>";
 
-		$haku='';
-		if (is_string($etsi))  $haku="and nimi LIKE '%$etsi%'";
-		if (is_numeric($etsi)) $haku="and tunnus='$etsi'";
-		
-		$kka = date("m",mktime(0, 0, 0, date("m"), date("d")-1, date("Y")));	
+		$haku = '';
+		if (is_string($etsi)) $haku = "and lasku.nimi LIKE '%".mysql_real_escape_string($etsi)."%'";
+		if (is_numeric($etsi)) $haku = "and lasku.tunnus = '".mysql_real_escape_string($etsi)."'";
+
+		$kka = date("m",mktime(0, 0, 0, date("m"), date("d")-1, date("Y")));
 		$vva = date("Y",mktime(0, 0, 0, date("m"), date("d")-1, date("Y")));
 		$ppa = date("d",mktime(0, 0, 0, date("m"), date("d")-1, date("Y")));
-	
+
 		$kkl = date("m");
 		$vvl = date("Y");
 		$ppl = date("d");
-		
-		$query = "	SELECT distinct otunnus 
-					from tilausrivi, lasku 
-					where tilausrivi.$logistiikka_yhtiolisa
-					and var!='J' 
-					and kerattyaika>='$vva-$kka-$ppa 00:00:00'
-					and kerattyaika<='$vvl-$kkl-$ppl 23:59:59'
-					and lasku.$logistiikka_yhtiolisa
-					and lasku.tunnus=tilausrivi.otunnus 
-					and lasku.tila='L' 
-					and lasku.alatila='C'";
+
+		$query = "	SELECT distinct lasku.yhtio yhtio, lasku.tunnus, concat_ws(' ', lasku.nimi, lasku.nimitark) asiakas, date_format(lasku.luontiaika, '%Y-%m-%d') laadittu, lasku.laatija
+					from lasku
+					join tilausrivi on (tilausrivi.yhtio = lasku.yhtio
+										and tilausrivi.otunnus = lasku.tunnus
+										and tilausrivi.var != 'J'
+										and tilausrivi.kerattyaika >= '$vva-$kka-$ppa 00:00:00'
+										and tilausrivi.kerattyaika <= '$vvl-$kkl-$ppl 23:59:59')
+					where lasku.$logistiikka_yhtiolisa
+					and lasku.tila = 'L'
+					and lasku.alatila = 'C'
+					$haku";
 		$tilre = mysql_query($query) or pupe_error($query);
 
-		while ($tilrow = mysql_fetch_array($tilre))
-		{
-			// etsitään sopivia tilauksia
-			$query = "	SELECT lasku.yhtio yhtio, tunnus 'tilaus', concat_ws(' ', nimi, nimitark) asiakas, date_format(lasku.luontiaika, '%Y-%m-%d') laadittu, laatija
-						FROM lasku
-						WHERE tunnus='$tilrow[otunnus]' 
-						and tila='L' $haku 
-						and $logistiikka_yhtiolisa
-						and alatila='C' 
-						ORDER by laadittu desc";
-			$result = mysql_query($query) or pupe_error($query);
-
-			//piirretään taulukko...
-			if (mysql_num_rows($result)!=0)
-			{
-				while ($row = mysql_fetch_array($result))
-				{
-					if ($boob=='') // piirretään vaan kerran taulukko-otsikot
-					{
-						$boob='kala';
-						echo "<table>";
-						echo "<tr>";
-						for ($i=0; $i<mysql_num_fields($result); $i++) {
-							if (mysql_field_name($result, $i) == 'yhtio') {
-								if ($logistiikka_yhtio != '') {
-									echo "<th align='left'>",t("Yhtiö"),"</th>";
-								}
-							}
-							else {
-								echo "<th align='left'>".t(mysql_field_name($result,$i))."</th>";
-							}
-						}
-						echo "</tr>";
-					}
-
-					echo "<tr>";
-
-					for ($i=0; $i<mysql_num_fields($result); $i++) {
-						if (mysql_field_name($result, $i) == 'yhtio') {
-							if ($logistiikka_yhtio != '') {
-								echo "<td>$row[yhtio]</td>";
-							}
-						}
-						else {
-							echo "<td>$row[$i]</td>";
-						}
-					}
-
-					echo "	<form method='post' action='$PHP_SELF'><td class='back'>
-							<input type='hidden' name='tee' value='KORJAA'>	
-							<input type='hidden' name='lasku_yhtio' value='$row[yhtio]'>
-						  	<input type='hidden' name='tunnus' value='$row[tilaus]'>
-						  	<input type='submit' name='tila' value='".t("Korjaa")."'></td></tr></form>";
-				}
-			}
+		if (mysql_num_rows($tilre) != 0) {
+			echo "<table>";
+			echo "<tr>";
+			echo "<th>Yhtio</th>";
+			echo "<th>Tilaus</th>";
+			echo "<th>Asiakas</th>";
+			echo "<th>Laadittu</th>";
+			echo "<th>Laatija</th>";
+			echo "<tr>";
 		}
 
-		if ($boob!='')
+		while ($tilrow = mysql_fetch_array($tilre)) {
+			echo "<tr>";
+			echo "<td>$tilrow[yhtio]</td>";
+			echo "<td>$tilrow[tunnus]</td>";
+			echo "<td>$tilrow[asiakas]</td>";
+			echo "<td>$tilrow[laadittu]</td>";
+			echo "<td>$tilrow[laatija]</td>";
+			echo "<td class='back'><form method='post' action='$PHP_SELF'>
+					<input type='hidden' name='tee' value='KORJAA'>
+					<input type='hidden' name='lasku_yhtio' value='$tilrow[yhtio]'>
+				  	<input type='hidden' name='tunnus' value='$tilrow[tunnus]'>
+				  	<input type='submit' name='tila' value='".t("Korjaa")."'>
+					</form></td>";
+			echo "</tr>";
+		}
+
+		if (mysql_num_rows($tilre) != 0) {
 			echo "</table>";
-		else
+		}
+		else {
 			echo "<font class='message'>".t("Yhtään korjattavaa tilausta ei löytynyt")."...</font>";
+		}
+
 	}
-	
-	require ("../inc/footer.inc");
+
+	require ("inc/footer.inc");
+
 ?>

77f55fd27271d27b49bd4425ee3d9244d7a01f00
juppe <juppe@devlab.fi>
2009-10-06 07:58:46 +0000

    #OHJE Kaksi uutta valintaa: Kehahinnat näytetään vain jos halutaan. Osaamme myös näyttää vain myytävissäolevia tuotteita.

--- raportit/hinnastoraportti.php
+++ raportit/hinnastoraportti.php
@@ -14,104 +14,148 @@
 
 	echo "<font class='head'>".t("Hinnastoajo").":</font><hr>";
 
-	if (include('Spreadsheet/Excel/Writer.php')) {
-	    $excelnimi = "$kukarow[yhtio]-".date("YmdHis").".xls";
-	    $workbook = new Spreadsheet_Excel_Writer('/tmp/'.$excelnimi);
-	    $workbook->setVersion(8);
-	    $worksheet = $workbook->addWorksheet('Hinnasto');
-	    $format_bold = $workbook->addFormat();
-	    $format_bold->setBold();
-	    $excelrivi = 0;
-	    $i = 0;
+	if ($raptee == "AJA") {
+		if (include('Spreadsheet/Excel/Writer.php')) {
+		    $excelnimi = "$kukarow[yhtio]-".date("YmdHis").".xls";
+		    $workbook = new Spreadsheet_Excel_Writer('/tmp/'.$excelnimi);
+		    $workbook->setVersion(8);
+		    $worksheet = $workbook->addWorksheet('Hinnasto');
+		    $format_bold = $workbook->addFormat();
+		    $format_bold->setBold();
+		    $excelrivi = 0;
+		    $i = 0;
 
-		if (isset($workbook)) {
-			echo "<font class='message'>".t("Tallenna raportti (xls)").": </font>";
-			echo "<form method='post' action='$PHP_SELF'>";
-			echo "<input type='hidden' name='tee' value='lataa_tiedosto'>";
-			echo "<input type='hidden' name='kaunisnimi' value='hinnastoraportti.xls'>";
-			echo "<input type='hidden' name='tmpfilenimi' value='$excelnimi'>";
-			echo "<input type='submit' value='".t("Tallenna")."'>";
-			echo "</form>";
-			echo "<br><br>";
-		}
-	}
-
-	$productquery = "	SELECT tuoteno, try, nimitys, kehahin, myyntihinta, eankoodi
-						FROM tuote
-						WHERE yhtio = '$kukarow[yhtio]'
-						AND hinnastoon != 'E'";
-	$productqueryresult = mysql_query($productquery) or pupe_error($productquery);
-
-	echo "<table>";
-	echo "<tr>";
-	echo "<th>".t("Tuoteno")."</th>";
-	echo "<th>".t("Nimitys")."</th>";
-	echo "<th>".t("Kehahin")."</th>";
-	echo "<th>".t("Myyntihinta")."</th>";
-	echo "<th>".t("Saldo")."</th>";
-	echo "<th>".t("Try")."</th>";
-	echo "<th>".t("Ean")."</th>";
-	echo "</tr>";
-
-	if (isset($workbook)) {
-	    $worksheet->write($excelrivi, $i, t('Tuoteno'), $format_bold);
-	    $i++;
-	    $worksheet->write($excelrivi, $i, t('Nimitys'), $format_bold);
-	    $i++;
-	    $worksheet->write($excelrivi, $i, t('Kehahin'), $format_bold);
-	    $i++;
-	    $worksheet->write($excelrivi, $i, t('Myyntihinta'), $format_bold);
-	    $i++;
-	    $worksheet->write($excelrivi, $i, t('Saldo'), $format_bold);
-	    $i++;
-	    $worksheet->write($excelrivi, $i, t('TRY'), $format_bold);
-	    $i++;
-	    $worksheet->write($excelrivi, $i, t('EAN'), $format_bold);
-	    $i=0;
-	    $excelrivi++;
-	}
-
-    while ($productrow = mysql_fetch_array($productqueryresult)) {
-
-		list(,,$apu_myytavissa) = saldo_myytavissa($productrow["tuoteno"]);
-		$sresult = t_avainsana("TRY", "", "and avainsana.selite  = '$productrow[try]'");
-		$srow = mysql_fetch_array($sresult);
-
-		if (isset($workbook)) {
-			$worksheet->writeString($excelrivi, $i, $productrow['tuoteno']);
-			$i++;
-			$worksheet->writeString($excelrivi, $i, $productrow['nimitys']);
-			$i++;
-			$worksheet->writeString($excelrivi, $i, $productrow['kehahin']);
-			$i++;
-			$worksheet->writeString($excelrivi, $i, $productrow['myyntihinta']);
-			$i++;
-			$worksheet->writeString($excelrivi, $i, $apu_myytavissa);
-			$i++;
-			$worksheet->writeString($excelrivi, $i, $srow["selitetark"]);
-			$i++;
-			$worksheet->writeString($excelrivi, $i, $productrow['eankoodi']);
-			$i=0;
-			$excelrivi++;
+			if (isset($workbook)) {
+				echo "<font class='message'>".t("Tallenna raportti (xls)").": </font>";
+				echo "<form method='post' action='$PHP_SELF'>";
+				echo "<input type='hidden' name='tee' value='lataa_tiedosto'>";
+				echo "<input type='hidden' name='kaunisnimi' value='hinnastoraportti.xls'>";
+				echo "<input type='hidden' name='tmpfilenimi' value='$excelnimi'>";
+				echo "<input type='submit' value='".t("Tallenna")."'>";
+				echo "</form>";
+				echo "<br><br>";
+			}
 		}
 
-		echo "<tr class='aktiivi'>";
-		echo "<td>$productrow[tuoteno]</td>";
-		echo "<td>$productrow[nimitys]</td>";
-		echo "<td>$productrow[kehahin]</td>";
-		echo "<td>$productrow[myyntihinta]</td>";
-		echo "<td>$apu_myytavissa</td>";
-		echo "<td>$srow[selitetark]</td>";
-		echo "<td>$productrow[eankoodi]</td>";
+		$productquery = "	SELECT tuoteno, try, nimitys, kehahin, myyntihinta, eankoodi
+							FROM tuote
+							WHERE yhtio = '$kukarow[yhtio]'
+							AND hinnastoon != 'E'
+							and tuotetyyppi not in ('A','B')";
+		$productqueryresult = mysql_query($productquery) or pupe_error($productquery);
+
+		echo "<table>";
+		echo "<tr>";
+		echo "<th>".t("Tuoteno")."</th>";
+		echo "<th>".t("Nimitys")."</th>";
+		if ($kehahinnat != "") echo "<th>".t("Kehahin")."</th>";
+		echo "<th>".t("Myyntihinta")."</th>";
+		echo "<th>".t("Saldo")."</th>";
+		echo "<th>".t("Try")."</th>";
+		echo "<th>".t("Ean")."</th>";
 		echo "</tr>";
 
-    }
+		if (isset($workbook)) {
+		    $worksheet->write($excelrivi, $i, t('Tuoteno'), $format_bold);
+		    $i++;
+		    $worksheet->write($excelrivi, $i, t('Nimitys'), $format_bold);
+		    $i++;
 
-	echo "</table>";
+			if ($kehahinnat != "") {
+				$worksheet->write($excelrivi, $i, t('Kehahin'), $format_bold);
+		    	$i++;
+	    	}
 
-	if (isset($workbook)) {
-	    $workbook->close();
+			$worksheet->write($excelrivi, $i, t('Myyntihinta'), $format_bold);
+		    $i++;
+		    $worksheet->write($excelrivi, $i, t('Saldo'), $format_bold);
+		    $i++;
+		    $worksheet->write($excelrivi, $i, t('TRY'), $format_bold);
+		    $i++;
+		    $worksheet->write($excelrivi, $i, t('EAN'), $format_bold);
+		    $i=0;
+		    $excelrivi++;
+		}
+
+	    while ($productrow = mysql_fetch_array($productqueryresult)) {
+
+			list(,,$apu_myytavissa) = saldo_myytavissa($productrow["tuoteno"]);
+			$sresult = t_avainsana("TRY", "", "and avainsana.selite  = '$productrow[try]'");
+			$srow = mysql_fetch_array($sresult);
+
+
+			if ($myytavissao == "" or $apu_myytavissa > 0) {
+				if (isset($workbook)) {
+					$worksheet->writeString($excelrivi, $i, $productrow['tuoteno']);
+					$i++;
+					$worksheet->writeString($excelrivi, $i, $productrow['nimitys']);
+					$i++;
+
+					if ($kehahinnat != "") {
+						$worksheet->writeNumber($excelrivi, $i, $productrow['kehahin']);
+						$i++;
+					}
+
+					$worksheet->writeNumber($excelrivi, $i, $productrow['myyntihinta']);
+					$i++;
+					$worksheet->writeNumber($excelrivi, $i, $apu_myytavissa);
+					$i++;
+					$worksheet->writeString($excelrivi, $i, $srow["selitetark"]);
+					$i++;
+					$worksheet->writeString($excelrivi, $i, $productrow['eankoodi']);
+					$i=0;
+					$excelrivi++;
+				}
+
+				echo "<tr class='aktiivi'>";
+				echo "<td>$productrow[tuoteno]</td>";
+				echo "<td>$productrow[nimitys]</td>";
+				if ($kehahinnat != "") echo "<td align='right'>$productrow[kehahin]</td>";
+				echo "<td align='right'>$productrow[myyntihinta]</td>";
+				echo "<td align='right'>$apu_myytavissa</td>";
+				echo "<td>$srow[selitetark]</td>";
+				echo "<td>$productrow[eankoodi]</td>";
+				echo "</tr>";
+			}
+	    }
+
+		echo "</table>";
+
+		if (isset($workbook)) {
+		    $workbook->close();
+		}
 	}
+
+	if (isset($kehahinnat) and $kehahinnat != "") {
+		$chk = "CHECKED";
+	}
+	else {
+		$chk = "";
+	}
+
+	if (isset($myytavissao) and $myytavissao != "") {
+		$chk2 = "CHECKED";
+	}
+	else {
+		$chk2 = "";
+	}
+
+	echo "<br><form method='post' action='$PHP_SELF'>";
+	echo "<input type='hidden' name='raptee' value='AJA'>";
+	echo "<table>";
+	echo "<tr>
+		<th>".t("Näytä myös keskihankintahinnat")."</th>
+		<td><input type='checkbox' name='kehahinnat' $chk></td>
+		</tr>\n";
+
+	echo "<tr>
+		<th>".t("Näytä vain myytävissä olevat tuotteet")."</th>
+		<td><input type='checkbox' name='myytavissao' $chk2></td>
+		</tr>\n";
+
+	echo "</table><br>";
+	echo "<br><input type='submit' value='".t("Tee hinnasto")."'>";
+	echo "</form>";
 
 	require ("inc/footer.inc");
 

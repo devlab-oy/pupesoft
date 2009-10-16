e6a6475f093a344ec1315c57562837321453998c
jonikanerva <joni@devlab.fi>
2009-10-16 06:46:26 +0000

    Käytetään pbzip2 pakkausta. Muista asentaa pbzip2 paketti:
    yum install pbzip2

--- pupe-backup.php
+++ pupe-backup.php
@@ -6,38 +6,39 @@
 	$dbkanta  = "pupesoft";
 	$dbuser   = "kayttajanimi";
 	$dbpass   = "salasana";
-          
+
 	if ($argc == 0) die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
-            
+
 	echo date("d.m.Y @ G:i:s")." - Backup $dbkanta.\n";
-              
+
 	$filename = "$dbkanta-backup-".date("Y-m-d").".bz2";
-                
+
 	// backupataan kaikki failit
 	passthru("/usr/bin/mysqlhotcopy -q -u $dbuser --password=$dbpass $dbkanta /tmp");
-                    
+
 	echo date("d.m.Y @ G:i:s")." - Copy done.\n";
-                      
+
 	// siirrytään temppidirriin
 	chdir("/tmp/$dbkanta");
-                          
+
 	// pakataan failit
-	system("/bin/tar -cjf $filename *");
-                              
+	system("/bin/tar -cf $filename --use-compress-prog=pbzip2 *");
+
 	echo date("d.m.Y @ G:i:s")." - Bzip2 done.\n";
-                                
+
 	// kopsataan faili
 	$scpma = "cp $filename $cp_dir";
 	system($scpma);
-                                      
+
 	echo date("d.m.Y @ G:i:s")." - Copy done.\n";
-                                        
+
 	// dellataan pois tempit
 	system("rm -rf /tmp/$dbkanta");
-	
+
 	// Siivotaan yli 30pv vanhat backupit pois
 	chdir($cp_dir);
 	system("find $cp_dir -mtime +30 -exec rm -f {} \;");
-	                                           
-	echo date("d.m.Y @ G:i:s")." - All done.\n";                                              
+
+	echo date("d.m.Y @ G:i:s")." - All done.\n\n";
+
 ?>

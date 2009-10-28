fe1966208f4eb4df92f3533f2169d7e598fac47a
jonikanerva <joni@devlab.fi>
2009-10-28 12:27:42 +0000

    Pvm vertailuja.

--- varauskalenteri/varauskalenteri_lisaa.php
+++ varauskalenteri/varauskalenteri_lisaa.php
@@ -2,49 +2,52 @@
 
 	$date		= $year."-".sprintf('%02d',$month)."-".sprintf('%02d',$day);
 	$dateloppu 	= $lyear."-".sprintf('%02d',$lmonth)."-".sprintf('%02d',$lday);
-	
+
 	$date2 		= $year.sprintf('%02d',$month).sprintf('%02d',$day);
 	$dateloppu2 = $lyear.sprintf('%02d',$lmonth).sprintf('%02d',$lday);
-	
+
 	$mylmonth 	= sprintf('%02d',$lmonth);
-	$mylday 	= sprintf('%02d',$lday); 
-	
+	$mylday 	= sprintf('%02d',$lday);
+
+	// en jaksa muuttaa idiootteja formeja, joten vertailu nyt näillä muuttujulla uusiks
+	list($vertailu_alkutunti, $vertailu_alkuminuutti) = explode(":", $kello);
+	list($vertailu_lopputunti, $vertailu_loppuminuutti) = explode(":", $lkello);
+
+	$vertailu_alkuaika = mktime($vertailu_alkutunti, $vertailu_alkuminuutti, 0, $month, $day, $year);
+	$vertailu_loppuaika = mktime($vertailu_lopputunti, $vertailu_loppuminuutti, 0, $lmonth, $lday, $lyear);
+
 	//tarkistetaan, etta alku ja loppu ovat eri..
-	if ($kello == $lkello && $date == $dateloppu) {
-		echo "<br><br>".t("VIRHE: Alku- ja päättymisajankohta ovat samat")."!";
-		exit;
-	}
-	if ($date2 > $dateloppu2) {
+	if ($vertailu_alkuaika >= $vertailu_loppuaika) {
 		echo "<br><br>".t("VIRHE: Päättymisjankohta on aikaisempi kuin alkamisajankohta")."!";
 		exit;
 	}
-	
+
 	//Tarkisetetaan päällekkäisyys konsernikohtaisesti
 	$query = "SELECT distinct yhtio FROM yhtio WHERE (konserni = '$yhtiorow[konserni]' and konserni != '') or (yhtio = '$yhtiorow[yhtio]')";
 	$result = mysql_query($query) or pupe_error($query);
 	$konsernit = "";
-	
-	while ($row = mysql_fetch_array($result)) {	
+
+	while ($row = mysql_fetch_array($result)) {
 		$konsernit .= " '".$row["yhtio"]."' ,";
-	}		
+	}
 	$konsernit = " and yhtio in (".substr($konsernit, 0, -1).") ";
-	
-	$query = "	select tunnus 
+
+	$query = "	select tunnus
 				from kalenteri
-				where tapa    = '$toim' 
-				and ((pvmloppu > '$year-$month-$day $kello' and pvmloppu < '$lyear-$mylmonth-$mylday $lkello') 
+				where tapa    = '$toim'
+				and ((pvmloppu > '$year-$month-$day $kello' and pvmloppu < '$lyear-$mylmonth-$mylday $lkello')
 				or (pvmalku > '$year-$month-$day $kello' and pvmalku < '$lyear-$mylmonth-$mylday $lkello')
 				or (pvmalku < '$year-$month-$day $kello' and pvmloppu > '$year-$month-$day $kello'))
-				$konsernit";		
+				$konsernit";
 	$result = mysql_query($query) or pupe_error($query);
-	
+
 	if (mysql_num_rows($result) > 0) {
 		echo "<br><br>".t("VIRHE: Päällekkäisiä tapahtumia")."!";
 		exit;
 	}
-	
+
 	if ($toim == "Sauna") {
-		if ($kentta03 != '') {			
+		if ($kentta03 != '') {
 			$meili = "\n\n\n$kukarow[nimi] on varannut saunan ajalle:\n\n##################################################\n$day-$month-$year   Klo: $kello --> Klo: $lkello\n##################################################\n\n\n";
 			$meili .= "Yhtiö:\n$kentta01\n";
 			$meili .= "Osasto:\n$kentta02\n\n";
@@ -54,7 +57,7 @@
 			$meili .= "Vieraat:\n$kentta07\n\n";
 			$meili .= "Vieraslukumäärä:\n$kentta08\n\n\n";
 			$meili .= "Juomatoivomus:\n$kentta10\n";
-			
+
 			$tulos = mail("$yhtiorow[varauskalenteri_email]", "Saunavaraus", $meili,"From: ".$kukarow["nimi"]."<".$kukarow["eposti"].">\nReply-To: ".$kukarow["nimi"]."<".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
 		}
 		else{
@@ -62,20 +65,20 @@
 			exit;
 		}
 	}
-	if($toim == "Starcraft" || $toim == "Perämoottori 1" || $toim == "Perämoottori 2") {		
+	if($toim == "Starcraft" || $toim == "Perämoottori 1" || $toim == "Perämoottori 2") {
 		$meili = "$kukarow[nimi] on varannut kohteen'".$toim."' ajalle:\n\n##################################################\n$day-$month-$year   Klo: $kello --> $lday-$lmonth-$lyear  Klo: $lkello\n##################################################\n\n\nViesti:\n$kentta05";
-		$tulos = mail("$yhtiorow[varauskalenteri_email]", $toim, $meili,"From: ".$kukarow["nimi"]."<".$kukarow["eposti"].">\nReply-To: ".$kukarow["nimi"]."<".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");	
-	}
-	
-	if($toim == "Kuorma-auto") {
-		$meili = "$kukarow[nimi] on varannut kohteen '".$toim."' ajalle:\n\n##################################################\n$day-$month-$year   Klo: $kello --> $lday-$lmonth-$lyear  Klo: $lkello\n##################################################\n\n\nViesti:\n$kentta05";		
 		$tulos = mail("$yhtiorow[varauskalenteri_email]", $toim, $meili,"From: ".$kukarow["nimi"]."<".$kukarow["eposti"].">\nReply-To: ".$kukarow["nimi"]."<".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
-	}	
+	}
 
-	
-	$query = "	INSERT into kalenteri SET 
+	if($toim == "Kuorma-auto") {
+		$meili = "$kukarow[nimi] on varannut kohteen '".$toim."' ajalle:\n\n##################################################\n$day-$month-$year   Klo: $kello --> $lday-$lmonth-$lyear  Klo: $lkello\n##################################################\n\n\nViesti:\n$kentta05";
+		$tulos = mail("$yhtiorow[varauskalenteri_email]", $toim, $meili,"From: ".$kukarow["nimi"]."<".$kukarow["eposti"].">\nReply-To: ".$kukarow["nimi"]."<".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
+	}
+
+
+	$query = "	INSERT into kalenteri SET
 				kuka 		= '$kukarow[kuka]',
-				yhtio		= '$kukarow[yhtio]',		
+				yhtio		= '$kukarow[yhtio]',
 				pvmalku		= '$year-$month-$day $kello',
 				pvmloppu	= '$lyear-$lmonth-$lday $lkello',
 				tapa 		= '$toim',
@@ -91,9 +94,9 @@
 				kentta10	= '$kentta10',
 				tyyppi		= 'varauskalenteri'";
 	$result = mysql_query($query) or pupe_error($query);
-	
+
 	echo "<br><br>".t("Tapahtuma lisätty varauskalenteriin")."!";
-	
+
 	if($lopetus != "") {
 		lopetus($lopetus, "META");
 	}

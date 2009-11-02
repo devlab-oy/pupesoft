707510e8ebe5f3c9dc0d6511390676347d6aca3f
jonikanerva <joni@devlab.fi>
2009-11-02 14:33:41 +0000

    Lisätään doctype XHTML 1.0 transitional.. :/

--- index.php
+++ index.php
@@ -1,26 +1,20 @@
 <?php
 
-require ("inc/parametrit.inc"); 
+require ("inc/parametrit.inc");
 
-if(isset($_POST['go'])) 		$go		= $_POST['go'];
-else 							$go		= "";
+$go = $goso = '';
+
+if (isset($_REQUEST['go'])) {
+	$go = $_REQUEST['go'];
+}
+
+if (isset($_REQUEST['goso'])) {
+	$goso = $_REQUEST['goso'];
+}
 
 if ($go == '') {
-	if(isset($_GET['go'])) 		$go		= $_GET['go'];
-	else 						$go		= "";	
-}
-
-if(isset($_POST['goso'])) 		$goso	= $_POST['goso'];
-else 							$goso	= "";
-
-if ($goso == '') {
-	if(isset($_GET['goso'])) 	$goso	= $_GET['goso'];
-	else 						$goso	= "";	
-}
-
-if ($go == '') { 
-	$go		= 'tervetuloa.php';
-	$goso	= "";
+	$go = 'tervetuloa.php';
+	$goso = '';
 }
 
 $colwidth = '175';
@@ -29,31 +23,34 @@ if ($kukarow['resoluutio'] == 'P') {
 	$colwidth = '45';
 }
 
-echo "<html>
-		<head>
-		<title>$yhtiorow[nimi]</title>";
-		
+echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
+\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+<html>
+<head>
+<title>$yhtiorow[nimi]</title>
+";
+
 if (file_exists("pics/pupeicon.gif")) {
-	echo "\n<link rel='shortcut icon' href='pics/pupeicon.gif'>\n";
+	echo "<link rel='shortcut icon' href='pics/pupeicon.gif'>\n";
 }
 else {
-	echo "\n<link rel='shortcut icon' href='http://www.pupesoft.com/pupeicon.gif'>\n";
+	echo "<link rel='shortcut icon' href='http://www.devlab.fi/devlab-shortcut.png'>\n";
 }
 
-echo "	<meta http-equiv='Pragma' content='no-cache'>
-		<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
-		</head>
-		<noscript>
-		Your browser does not support Javascript!
-		</noscript>
-		<frameset cols='$colwidth,*' border='0' frameborder='no'>
-		<frameset rows='*,0' border='0' frameborder='no'>
-		<frame noresize src='indexvas.php?goso=$goso' name='menu'>
-		<frame noresize src='' name='alamenu' id='alamenuFrame'>
-		</frameset> 
-		<frame src='$go' name='main'>
-		<noframes>Your browser does not support frames!</noframes>
-		</frameset> 
-		</html>"; 
+echo "<meta http-equiv='Pragma' content='no-cache'>
+<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
+</head>
+<noscript>
+Your browser does not support Javascript!
+</noscript>
+<frameset cols='$colwidth,*' border='0' frameborder='no'>
+<frameset rows='*,0' border='0' frameborder='no'>
+<frame noresize src='indexvas.php?goso=$goso' name='menu'>
+<frame noresize src='' name='alamenu' id='alamenuFrame'>
+</frameset>
+<frame src='$go' name='main'>
+<noframes>Your browser does not support frames!</noframes>
+</frameset>
+</html>";
 
 ?>

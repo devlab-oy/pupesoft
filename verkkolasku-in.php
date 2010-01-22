3685dcb668d865ae14fa86ee9e752a440495004d
juppe <juppe@devlab.fi>
2010-01-22 14:15:49 +0000

    Verkkoalskuja voidaan sisäänlukea myös tiliote.php:llä
    Laittakaa siinä tapauksessa nämä muuttujat kuntoon salasanat.php:ssä
    
    $verkkolaskut_in	= "/Users/juppe/Desktop/verkkolaskut";
    $verkkolaskut_ok	= "/Users/juppe/Desktop/verkkolaskut/ok";
    $verkkolaskut_orig	= "/Users/juppe/Desktop/verkkolaskut/orig";
    $verkkolaskut_error	= "/Users/juppe/Desktop/verkkolaskut/error";

--- verkkolasku-in.php
+++ verkkolasku-in.php
@@ -1,9 +1,8 @@
-#!/usr/bin/php
 <?php
 
-	if ($argc == 0) die ("T‰t‰ scripti‰ voi ajaa vain komentorivilt‰!");
-
-	// pit‰‰ siirty‰ www roottiin
+	if (isset($argc) and $argc > 0) {
+		//Komentorivilt‰
+		// pit‰‰ siirty‰ www roottiin
 	chdir("/var/www/html/pupesoft");
 
 	// m‰‰ritell‰‰n polut
@@ -12,12 +11,39 @@
     $origlaskut = "/home/verkkolaskut/orig";
     $errlaskut  = "/home/verkkolaskut/error";
 
-	// otetaan includepath aina rootista
-	ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(dirname(__FILE__)).PATH_SEPARATOR."/usr/share/pear");
-	error_reporting(E_ALL ^E_WARNING ^E_NOTICE);
-	ini_set("display_errors", 0);
+		$komentorivilta = TRUE;
 
-	require ("inc/connect.inc"); // otetaan tietokantayhteys
+		// otetaan includepath aina rootista
+		ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(dirname(__FILE__)).PATH_SEPARATOR."/usr/share/pear");
+		error_reporting(E_ALL ^E_WARNING ^E_NOTICE);
+		ini_set("display_errors", 0);
+
+		require ("inc/connect.inc"); // otetaan tietokantayhteys
+	}
+	elseif (strpos($_SERVER['SCRIPT_NAME'], "tiliote.php") !== FALSE and $verkkolaskut_in != "" and $verkkolaskut_ok != "" and $verkkolaskut_orig != "" and $verkkolaskut_error != "") {
+		//Pupesoftista
+		echo "Aloitetaan verkkolaskun sis‰‰nluku...<br><br>";
+		
+		$laskut     = $verkkolaskut_in;
+		$oklaskut   = $verkkolaskut_ok;
+		$origlaskut = $verkkolaskut_orig;
+		$errlaskut  = $verkkolaskut_error;	
+		
+		$komentorivilta = FALSE;
+		
+		// Kopsataan uploadatta faili verkkoalskudirikkaan
+		$copy_boob = copy($filenimi, $laskut."/".$userfile);
+		
+		if ($copy_boob === FALSE) {
+		    echo "Kopiointi ep‰onnistui $filenimi $laskut/$userfile<br>";
+			exit;
+		}		
+	}
+	else {
+		echo "N‰ill‰ ehdoilla emme voi ajaa verkkolaskujen sis‰‰nlukua!";
+		exit;
+	}
+	
     require ("inc/verkkolasku-in.inc"); // t‰‰ll‰ on itse koodi
     require ("inc/verkkolasku-in-erittele-laskut.inc"); // t‰‰ll‰ pilkotaan Finvoiceaineiston laskut omiksi tiedostoikseen
 
@@ -53,9 +79,17 @@
 				$laskuvirhe = verkkolasku_in($nimi);
 
 			    if ($laskuvirhe == "") {
+					if (!$komentorivilta)  {
+						echo "Verkkolasku vastaanotettu onnistuneesti!<br><br>";
+					}				
+				
 			    	system("mv -f $nimi $oklaskut/$file");
 			    }
 			    else {
+					if (!$komentorivilta)  {
+						echo "<font class='error'>Verkkolaskun vastaanotossa virhe:</font><br><pre>$laskuvirhe</pre><br>";
+					}
+					
 			    	system("mv -f $nimi $errlaskut/$file");
 				}
 			}

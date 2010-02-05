f746d6ba4762b280a2a85cbd3b032f4336e8fbf2
jonikanerva <joni@devlab.fi>
2010-02-05 15:24:11 +0000

    KARMEA ratkaisu.

--- raportit/sql-query.php
+++ raportit/sql-query.php
@@ -13,6 +13,11 @@
 
 	require("../inc/parametrit.inc");
 
+	if (in_array($kukarow["yhtio"], array("allr","arja","arse","artr","arwi","atarv","bamb","demo","edil","hsr","katj","kolo","mast","rose","savt","yama"))) {
+		echo "<font class='error'>SQL-haku poistettu käytöstä</font><br>";
+		exit;
+	}
+
 	//Ja tässä laitetaan ne takas
 	$sqlhaku = $sqlapu;
 

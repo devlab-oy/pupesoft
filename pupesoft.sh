f8490e48b2c8dc821a19060ca8f3f9d9c133c379
jonikanerva <joni@devlab.fi>
2010-04-19 08:02:13 +0000

    Cron.daily scripti

---
+++ pupesoft.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pupesoft asennuksen tiedot
+POLKU="/var/www/html/pupesoft/"
+YHTIO="demo"
+
+# Pupesoft varmistuksen tiedot
+BACKUPDIR="/backup/pupe-backup"
+BACKUPDB="pupesoft"
+BACKUPUSER="pupe"
+BACKUPPASS="pupe1"
+BACKUPSAVEDAYS="30"
+
+# Komennot
+sh ${POLKU}pupe-backup.sh $BACKUPDIR $BACKUPDB $BACKUPUSER $BACKUPPASS $BACKUPSAVEDAYS
+sh ${POLKU}pupe-cron.sh $YHTIO
+sh ${POLKU}pupe-cron-server.sh

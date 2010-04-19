8694dd427e2cc93b17925a191e7367166b1aaea9
jonikanerva <joni@devlab.fi>
2010-04-19 08:06:33 +0000

    Pupesoft päivityscripti.

---
+++ svn-pupeen.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo
+echo "Tervetuloa Pupesoft-narupalveluun"
+echo "---------------------------------"
+echo
+echo "Haetaan tietokantamuutokset..."
+echo
+
+pupedir=`dirname $0`
+
+php ${pupedir}dumppaa_mysqlkuvaus.php komentorivilta
+
+echo
+echo "HUOM: Tee tarvittavat tietokantamuutoket ennen kuin jatkat!"
+echo
+echo -n "Jatketaanko (k/e)? "
+
+read jatketaanko
+
+echo
+
+if [ $jatketaanko = "k" ]; then
+       echo "Paivitetaan Pupesoft..."
+       echo
+       svn update ${pupedir}
+else
+       echo "Pupesoftia ei paivitetty!"
+fi
+
+echo
+echo "Valmis!"
+echo
+
+###################################################################################
+# Nain luodaan pupeinstallaatio:
+# svn co svn://svn.devlab.fi/var/svn/devlab/devlab/pupesoft /var/www/html/pupesoft/
+###################################################################################

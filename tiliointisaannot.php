d7709b97b31ff98e6d65fead1baa6e42c79c2ab2
juppe <juppe@devlab.fi>
2010-04-21 07:56:39 +0000

    Uusia ominaisuuksia tiliöintisääntöihin! Voidaan tehdä sääntö laskun toimitusasiakkaan tietojen perusteella tai sääntö sillä persusteella onko kauttalskutusta vai ei.

--- tiliointisaannot.php
+++ tiliointisaannot.php
@@ -1,9 +1,12 @@
 <?php
-	require "inc/parametrit.inc";
+
+	require ("inc/parametrit.inc");
+
 	if (!isset($tyyppi)) $tyyppi='t';
+
 	echo "<font class='head'>".t("Toimittajan tiliöintisäännöt")."</font><hr>";
 
-	if (($tee == 'S') or ($tee == 'N') or ($tee == 'Y')) {
+	if ($tee == 'S' or $tee == 'N' or $tee == 'Y') {
 
 		if ($tee == 'S') { // S = selaussanahaku
 			$lisat = "and selaus like '%" . $nimi . "%'";
@@ -17,12 +20,12 @@
 			$lisat = "and ytunnus = '$nimi'";
 		}
 
-		$query = "SELECT tunnus, ytunnus, nimi, postitp
+		$query = "	SELECT tunnus, ytunnus, nimi, postitp
 					FROM toimi
 					WHERE yhtio = '$kukarow[yhtio]' $lisat
 					ORDER BY nimi";
-
 		$result = mysql_query($query) or pupe_error($query);
+
 		if (mysql_num_rows($result) == 0) {
 			echo "<font class='error'>".t("Haulla ei löytynyt yhtään toimittajaa")."</font>";
 		}
@@ -52,52 +55,76 @@
 	}
 
 	if ($tee == 'P') {
-// Olemassaolevaa sääntöä muutetaan, joten poistetaan rivi ja annetaan perustettavaksi
-
-		$query = "SELECT *
+		// Olemassaolevaa sääntöä muutetaan, joten poistetaan rivi ja annetaan perustettavaksi
+		$query = "	SELECT *
 					FROM tiliointisaanto
 					WHERE tunnus = '$rtunnus' and yhtio = '$kukarow[yhtio]'";
 		$result = mysql_query($query) or pupe_error($query);
+
 		if (mysql_num_rows($result) == 0) {
-			echo "".t("Tiliöintisääntöä ei löydy")."! $query";
+			echo t("Tiliöintisääntöä ei löydy")."! $query";
 			exit;
 		}
 
-		$tiliointirow=mysql_fetch_array($result);
-		$mintuote = $tiliointirow['mintuote'];
-		$maxtuote= $tiliointirow['maxtuote'];
-		$kuvaus = $tiliointirow['kuvaus'];
-		$tilino = $tiliointirow['tilino'];
-		$kustp = $tiliointirow['kustp'];
-		$ok = 1;
+		$tiliointirow = mysql_fetch_array($result);
+		$mintuote	= $tiliointirow['mintuote'];
+		$maxtuote	= $tiliointirow['maxtuote'];
+		$kuvaus		= $tiliointirow['kuvaus'];
+		$kuvaus2	= $tiliointirow['kuvaus2'];
+		$tilino		= $tiliointirow['tilino'];
+		$kustp		= $tiliointirow['kustp'];
+		$ok 		= 1;
 
 		$query = "DELETE from tiliointisaanto WHERE tunnus = '$rtunnus' and yhtio = '$kukarow[yhtio]'";
 		$result = mysql_query($query) or pupe_error($query);
 	}
+
 	if ($tee == 'U') {
-// Tarkistetaan sääntö
-		if ($tyyppi=='t') {
 
-			$query = "SELECT tunnus	FROM toimi WHERE yhtio = '$kukarow[yhtio]' $lisat ORDER BY selaus";
+		$query = "	SELECT tunnus
+					FROM toimi
+					WHERE yhtio = '$kukarow[yhtio]'
+					$lisat
+					ORDER BY selaus";
+		$result = mysql_query($query) or pupe_error($query);
 
+		if (mysql_num_rows($result) == 0) {
+			$virhe	= "<font class='error'>".t("Haulla ei löytynyt yhtään toimittajaa")."</font>";
+			$ok		= 1;
+			$tee	= '';
+		}
+
+		if ($kustp != 0) {
+			$query = "	SELECT tunnus
+						FROM kustannuspaikka
+						WHERE tunnus = '$kustp'
+						and yhtio = '$kukarow[yhtio]'
+						and tyyppi = 'K'
+						and kaytossa != 'E'";
 			$result = mysql_query($query) or pupe_error($query);
 
 			if (mysql_num_rows($result) == 0) {
-				echo "<font class='error'>".t("Haulla ei löytynyt yhtään toimittajaa")."</font>";
+				$virhe = "<font class='error'>".t("Kustannuspaikkaa ei löydy")."!</font>";
+				$ok = 1;
+				$tee = '';
 			}
+		}
+
+		// Tarkistetaan sääntö
+		if ($tyyppi == 't') {
 
 			if ($mintuote != '' and $maxtuote == '') $maxtuote = $mintuote;
 			if ($maxtuote != '' and $mintuote == '') $mintuote = $maxtuote;
 
 			if ($mintuote != '') {
 				if ($mintuote > $maxtuote) {
-					$virhe = "<font class='error'>".t("Minimituote on pienempi kuin maksimituote")."!</font>";
-					$ok = 1;
-					$tee = '';
+					$virhe	= "<font class='error'>".t("Minimituote on pienempi kuin maksimituote")."!</font>";
+					$ok		= 1;
+					$tee	= '';
 				}
 			}
 
-			$query = "SELECT tilino
+			$query = "	SELECT tilino
 						FROM tili
 						WHERE tilino = '$tilino' and yhtio = '$kukarow[yhtio]'";
 			$result = mysql_query($query) or pupe_error($query);
@@ -108,29 +135,27 @@
 				$tee = '';
 			}
 
-			if ($kustp != 0) {
-				$query = "SELECT tunnus
-							FROM kustannuspaikka
-							WHERE tunnus = '$kustp' and yhtio = '$kukarow[yhtio]' and tyyppi='K'";
-				$result = mysql_query($query) or pupe_error($query);
-				if (mysql_num_rows($result) == 0) {
-					$virhe = "<font class='error'>".t("Kustannuspaikkaa ei löydy")."!</font>";
-					$ok = 1;
-					$tee = '';
-				}
-			}
-
 			//Onko tälle välille jo sääntö?
 			if ($mintuote != '') {
-				$query = "SELECT mintuote, maxtuote FROM tiliointisaanto
-									WHERE ttunnus = '$tunnus' and yhtio = '$kukarow[yhtio]' and
-									mintuote <= '$mintuote' and maxtuote >= '$mintuote' and tilino != 0";
+				$query = "	SELECT mintuote, maxtuote
+							FROM tiliointisaanto
+							WHERE ttunnus = '$tunnus'
+							and yhtio = '$kukarow[yhtio]'
+							and mintuote <= '$mintuote'
+							and maxtuote >= '$mintuote'
+							and tilino != 0";
 				$result = mysql_query($query) or pupe_error($query);
+
 				if (mysql_num_rows($result) == 0) {
-					$query = "SELECT mintuote, maxtuote FROM tiliointisaanto
-									WHERE ttunnus = '$tunnus' and yhtio = '$kukarow[yhtio]' and
-									mintuote <= '$maxtuote' and maxtuote >= '$maxtuote' and tilino != 0";
+					$query = "	SELECT mintuote, maxtuote
+								FROM tiliointisaanto
+								WHERE ttunnus = '$tunnus'
+								and yhtio = '$kukarow[yhtio]'
+								and mintuote <= '$maxtuote'
+								and maxtuote >= '$maxtuote'
+								and tilino != 0";
 					$result = mysql_query($query) or pupe_error($query);
+
 					if (mysql_num_rows($result) != 0) {
 						$virhe = "<font class='error'>".t("Tälle välille on jo sääntö")." 1</font>";
 						$ok = 1;
@@ -144,184 +169,205 @@
 				}
 			}
 		}
-		else {
-			if (($mintuote!='') or ($maxtuote!='') or ($tilino != '')) {
+		elseif ($tyyppi == 'a') {
+			if ($mintuote != '' or $maxtuote != '' or $tilino != '') {
 				$virhe = t("Sisäinen virhe")."!";
 				$ok = 1;
 				$tee = '';
 			}
-			else {
-				if ($kuvaus == '') {
-					$virhe = t("Asiakastunnnus on pakollinen tieto")."!";
-					$ok = 1;
-					$tee = '';
-				}
+			elseif ($kuvaus == '') {
+				$virhe = t("Asiakastunnnus on pakollinen tieto")."!";
+				$ok = 1;
+				$tee = '';
 			}
 		}
-		//Onko tälle välille jo sääntö?
 	}
 
 	if ($tee == 'U') {
-// Lisätään sääntö
+		// Lisätään sääntö
 		$query = "INSERT into tiliointisaanto VALUES (
 				'$kukarow[yhtio]',
+				'$tyyppi',
 				'$tunnus',
 				'$mintuote',
 				'$maxtuote',
 				'$kuvaus',
+				'$kuvaus2',
 				'$tilino',
 				'$kustp',
 				'')";
 		$result = mysql_query($query) or pupe_error($query);
 	}
 
-	if (isset($tunnus)) {
-// Toimittaja on valittu ja sille annetaan sääntöjä
-		$query = "SELECT ytunnus, concat_ws(' ', nimi, nimitark) nimi, concat_ws(' ', postino, postitp) osoite
+	if (isset($tunnus) and $tunnus > 0) {
+		// Toimittaja on valittu ja sille annetaan sääntöjä
+		$query = "	SELECT ytunnus, concat_ws(' ', nimi, nimitark) nimi, concat_ws(' ', postino, postitp) osoite
 					FROM toimi
-					WHERE tunnus='$tunnus' and yhtio = '$kukarow[yhtio]'";
-
+					WHERE tunnus = '$tunnus'
+					and yhtio = '$kukarow[yhtio]'";
 		$result = mysql_query($query) or pupe_error($query);
 
 		if (mysql_num_rows($result) == 0) {
 			echo "<font class='error'>".t("Toimittaja katosi")."</font><br>";
 			exit;
 		}
+
 		echo "<table><tr>";
 		for ($i = 0; $i < mysql_num_fields($result); $i++) {
 			echo "<th>" . t(mysql_field_name($result,$i))."</th>";
 		}
 		echo "</tr>";
 
-		while ($toimittajarow=mysql_fetch_array ($result)) {
+		while ($toimittajarow = mysql_fetch_array($result)) {
 			for ($i=0; $i<mysql_num_fields($result); $i++) {
 				echo "<td>$toimittajarow[$i]</td>";
 			}
 		}
 		echo "</tr></table><br>";
 
-		$sel1='checked';
-		$sel2='';
-		if ($tyyppi=='a') {
-			$sel1='';
-			$sel2='checked';
-		}
+		$sel[] = array();
+		$sel[$tyyppi] = "SELECTED";
 
 		echo "<font class='head'>".t("Säännöt")."</font><hr>
 				<form action = '$PHP_SELF' method='post'>
 				<input type='hidden' name='tunnus' value='$tunnus'>
-				<input type='radio' name='tyyppi' value='t' onchange='submit()' $sel1> Tuotesäännöt
-				<input type='radio' name='tyyppi' value='a' onchange='submit()' $sel2> Asiakastunnukset
-				<input type='submit' value='Päivitä'></form><table>";
-	// Näytetään vanhat säännöt muutosta varten
-		if ($tyyppi=='t')
-			$query = "SELECT tiliointisaanto.tunnus, mintuote, maxtuote, kuvaus, concat(tili.tilino,'/',tili.nimi) tilinumero, kustp
-					  FROM tiliointisaanto
-					  LEFT JOIN tili ON tili.yhtio=tiliointisaanto.yhtio and tili.tilino = tiliointisaanto.tilino
-					  WHERE ttunnus = '$tunnus' and tiliointisaanto.yhtio = '$kukarow[yhtio]' and tiliointisaanto.tilino != 0
-					  order by mintuote";
-		else
-			$query = "SELECT tunnus, kuvaus, kustp
-					  FROM tiliointisaanto
-					  WHERE ttunnus = '$tunnus' and yhtio = '$kukarow[yhtio]' and tilino = 0";
+				<table>
+				<tr>
+				<th>".t("Valitse tiliöintisäännön tyyppi").":</th>
+				<td><select name='tyyppi' onchange='submit();'>
+				<option value='t' $sel[t]>1 ".t("Tuotesäännöt")."</option>
+				<option value='o' $sel[o]>2 ".t("Toimitusosoiteet")."</option>
+				<option value='a' $sel[a]>3 ".t("Asiakastunnukset")."</option>
+				<option value='k' $sel[k]>4 ".t("Kauttalaskutukset")."</option>
+				</select></td>
+				<td class='back'><input type='submit' value='".t("Näytä")."'></td>
+				</tr>
+				</table>
+				</form><br><br>
+				<table>";
+
+		// Näytetään vanhat säännöt muutosta varten
+		if ($tyyppi == 't') {
+			$query = "	SELECT tiliointisaanto.tunnus, tiliointisaanto.mintuote, tiliointisaanto.maxtuote, tiliointisaanto.kuvaus, concat(tili.tilino,'/',tili.nimi) tilinumero, kustannuspaikka.nimi Kustannuspaikka, tiliointisaanto.kustp
+						FROM tiliointisaanto
+						LEFT JOIN tili ON tili.yhtio = tiliointisaanto.yhtio and tili.tilino = tiliointisaanto.tilino
+						LEFT JOIN kustannuspaikka ON tiliointisaanto.yhtio = kustannuspaikka.yhtio and tiliointisaanto.kustp = kustannuspaikka.tunnus
+						WHERE tiliointisaanto.ttunnus	= '$tunnus'
+						and tiliointisaanto.tyyppi 		= 't'
+						and tiliointisaanto.yhtio 		= '$kukarow[yhtio]'
+						order by tiliointisaanto.mintuote";
+		}
+		elseif ($tyyppi == 'o') {
+			$query = "	SELECT tiliointisaanto.tunnus, tiliointisaanto.kuvaus Nimi, tiliointisaanto.kuvaus2 Osoite, tiliointisaanto.mintuote Postino, tiliointisaanto.maxtuote Postitp, kustannuspaikka.nimi Kustannuspaikka, tiliointisaanto.kustp
+						FROM tiliointisaanto
+						LEFT JOIN kustannuspaikka ON tiliointisaanto.yhtio = kustannuspaikka.yhtio and tiliointisaanto.kustp = kustannuspaikka.tunnus
+						WHERE tiliointisaanto.ttunnus 	= '$tunnus'
+						and tiliointisaanto.tyyppi 		= 'o'
+						and tiliointisaanto.yhtio 		= '$kukarow[yhtio]'
+						order by tiliointisaanto.kuvaus";
+		}
+		elseif ($tyyppi == 'a') {
+			$query = "	SELECT tiliointisaanto.tunnus, tiliointisaanto.kuvaus Asiakastunnus, kustannuspaikka.nimi Kustannuspaikka, tiliointisaanto.kustp
+						FROM tiliointisaanto
+						LEFT JOIN kustannuspaikka ON tiliointisaanto.yhtio = kustannuspaikka.yhtio and tiliointisaanto.kustp = kustannuspaikka.tunnus
+						WHERE tiliointisaanto.ttunnus 	= '$tunnus'
+						and tiliointisaanto.tyyppi 		= 'a'
+						and tiliointisaanto.yhtio 		= '$kukarow[yhtio]'
+						and tiliointisaanto.tilino 		= 0";
+		}
+		elseif ($tyyppi == 'k') {
+			$query = "	SELECT tiliointisaanto.tunnus, tiliointisaanto.kuvaus Kauttalaskutus, kustannuspaikka.nimi Kustannuspaikka, tiliointisaanto.kustp
+						FROM tiliointisaanto
+						LEFT JOIN kustannuspaikka ON tiliointisaanto.yhtio = kustannuspaikka.yhtio and tiliointisaanto.kustp = kustannuspaikka.tunnus
+						WHERE tiliointisaanto.ttunnus 	= '$tunnus'
+						and tiliointisaanto.tyyppi 		= 'k'
+						and tiliointisaanto.yhtio 		= '$kukarow[yhtio]'
+						order by tiliointisaanto.kuvaus";
+		}
+
 		$result = mysql_query($query) or pupe_error($query);
 
 		echo "<tr>";
-		for ($i = 1; $i < mysql_num_fields($result); $i++) {
+		for ($i = 1; $i < mysql_num_fields($result)-1; $i++) {
 			echo "<th>" . t(mysql_field_name($result,$i))."</th>";
 		}
 		echo "</tr>";
 
-		while ($tiliointirow=mysql_fetch_array ($result)) {
+		while ($tiliointirow = mysql_fetch_array($result)) {
 			echo "<tr>";
-			for ($i = 1; $i<mysql_num_fields($result); $i++) {
-				if (mysql_field_name($result,$i) == 'kustp') {
-					echo "<td>";
-					if (strlen($tiliointirow[$i]) > 0) { // Meillä on kustannuspaikka
-						$query = "SELECT nimi FROM kustannuspaikka
-									WHERE yhtio = '$kukarow[yhtio]' and
-									tunnus = '$tiliointirow[$i]' and tyyppi = 'K'";
-						$xresult = mysql_query($query) or pupe_error($query);
-						$xrow=mysql_fetch_array ($xresult);
-						echo "$xrow[0]";
-					}
-					echo "</td>";
-				}
-				else {
-					echo "<td>$tiliointirow[$i]</td>";
-				}
+			for ($i = 1; $i<mysql_num_fields($result)-1; $i++) {
+				echo "<td>$tiliointirow[$i]</td>";
 			}
-			echo "<td align='center'>
+			echo "<td class='back'>
 					<form action = '$PHP_SELF' method='post'>
 					<input type='hidden' name='tunnus' value = '$tunnus'>
 					<input type='hidden' name='rtunnus' value = '$tiliointirow[0]'>
 					<input type='hidden' name='tee' value = 'P'>
 					<input type='hidden' name='tyyppi' value = '$tyyppi'>
-					<input type='Submit' value = '".t("Muuta")."'>
+					<input type='Submit' value = '".t("Muuta tiliöintisääntöä")."'>
 				</td></tr></form>";
 		}
 
 		// Annetaan mahdollisuus tehdä uusi tiliöinti
 		if ($ok != 1) {
 			// Annetaan tyhjät tiedot, jos rivi oli virheetön
-			$maxtuote = '';
-			$mintuote = '';
-			$kuvaus = '';
-			$kustp = '';
-			$tilino = '';
+			$maxtuote	= '';
+			$mintuote	= '';
+			$kuvaus		= '';
+			$kuvaus2	= '';
+			$kustp		= '';
+			$tilino		= '';
 		}
 
 		$query = "	SELECT tunnus, nimi
 					FROM kustannuspaikka
-					WHERE yhtio = '$kukarow[yhtio]' and
-					tyyppi = 'K' and
-					kaytossa <> 'E'
+					WHERE yhtio = '$kukarow[yhtio]'
+					and tyyppi = 'K'
+					and kaytossa <> 'E'
 					ORDER BY nimi";
 		$result = mysql_query($query) or pupe_error($query);
 
-		$ulos = "<select name = 'kustp'><option value = ' '>Ei kustannuspaikkaa";
+		$ulos = "<select name = 'kustp'><option value = ' '>".t("Ei kustannuspaikkaa")."</option>";
 
 		while ($kustannuspaikkarow=mysql_fetch_array ($result)) {
 			$valittu = "";
 			if ($kustannuspaikkarow[0] == $kustp) {
 				$valittu = "selected";
 			}
-			$ulos .= "<option value = '$kustannuspaikkarow[0]' $valittu>$kustannuspaikkarow[1]";
+			$ulos .= "<option value = '$kustannuspaikkarow[0]' $valittu>$kustannuspaikkarow[1]</option>";
 		}
 		$ulos .= "</select><br>";
 
-		echo "<tr>";
-		if ($tyyppi=='t') echo "<td><form action = '$PHP_SELF' method='post'>
-					<input type='hidden' name='tee' value = 'U'>
-					<input type='hidden' name='tunnus' value = '$tunnus'>
-					<input type='hidden' name='tyyppi' value = '$tyyppi'>
-				    <input type='text' name='mintuote' size='15' value = '$mintuote'></td>
-				<td><input type='text' name='maxtuote' size='15' value = '$maxtuote'></td>
-				<td><input type='text' name='kuvaus' size='15' value = '$kuvaus'></td>
-				<td><input type='text' name='tilino' size='5' value = '$tilino'></td>
-				<td>
-					$ulos
-				</td>
-				<td align='center'>
-					$virhe <input type='Submit' value = '".t("Lisää")."'>
-				</td>";
-		else echo "<td><form action = '$PHP_SELF' method='post'>
-					<input type='hidden' name='tee' value = 'U'>
-					<input type='hidden' name='tunnus' value = '$tunnus'>
-					<input type='hidden' name='tyyppi' value = '$tyyppi'>
-				    <input type='text' name='kuvaus' size='15' value = '$kuvaus'></td>
-				<td>
-					$ulos
-				</td>
-				<td align='center'>
-					$virhe <input type='Submit' value = '".t("Lisää")."'>
-				</td>
-			</tr></form></table>";
+		echo "<tr>
+				<form action = '$PHP_SELF' method='post'>
+				<input type='hidden' name='tee' value = 'U'>
+				<input type='hidden' name='tunnus' value = '$tunnus'>
+				<input type='hidden' name='tyyppi' value = '$tyyppi'>";
 
+		if ($tyyppi == 't') {
+			echo "	<td><input type='text' name='mintuote' size='15' value = '$mintuote'></td>
+					<td><input type='text' name='maxtuote' size='15' value = '$maxtuote'></td>
+					<td><input type='text' name='kuvaus' size='15' value = '$kuvaus'></td>
+					<td><input type='text' name='tilino' size='5' value = '$tilino'></td>";
+		}
+		elseif ($tyyppi == 'o') {
+			echo "	<td><input type='text' name='kuvaus' size='35' value = '$kuvaus'></td>
+					<td><input type='text' name='kuvaus2' size='35' value = '$kuvaus2'></td>
+					<td><input type='text' name='mintuote' size='15' value = '$mintuote'></td>
+					<td><input type='text' name='maxtuote' size='15' value = '$maxtuote'></td>";
+		}
+		elseif ($tyyppi == 'a') {
+			echo "	<td><input type='text' name='kuvaus' size='15' value = '$kuvaus'></td>";
+		}
+		elseif ($tyyppi == 'k') {
+			echo "	<td><input type='text' name='kuvaus' size='15' value = '$kuvaus'></td>";
+		}
+
+		echo "<td>$ulos</td><td class='back'><input type='Submit' value = '".t("Lisää tiliöintisääntö")."'>$virhe</td>";
+		echo "</tr></form></table>";
 	}
 	else {
-
-// Tällä ollaan, jos olemme vasta valitsemassa toimittajaa
+		// Tällä ollaan, jos olemme vasta valitsemassa toimittajaa
 		echo "<form name = 'valinta' action = '$PHP_SELF' method='post'>
 				<table>
 				<td>".t("Valitse toimittaja")."</td>
@@ -332,9 +378,14 @@
 				</select>
 				</td>
 				<td><input type = 'submit' value = '".t("Valitse")."'></td>
-				</tr></table></form>";
+				</tr>
+				</table>
+				</form>";
+
+		$formi = 'valinta';
 		$kentta = 'nimi';
-		require "inc/footer.inc";
-		exit;
 	}
+
+	require ("inc/footer.inc");
+
 ?>

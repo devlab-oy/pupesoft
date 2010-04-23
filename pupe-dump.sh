28eb284ab3dfbf4c9184b993c2c169c294b25a2a
jonikanerva <joni@devlab.fi>
2010-04-23 08:03:48 +0000

    Pupebackup mysql dumpilla. Uusi ja improved versio.

---
+++ pupe-dump.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+BACKUPDIR=$1
+DBKANTA=$2
+DBKAYTTAJA=$3
+DBSALASANA=$4
+BACKUPPAIVAT=$5
+
+# Katsotaan, etta parametrit on annettu
+if [ -z $BACKUPDIR ] || [ -z $DBKANTA ] || [ -z $DBKAYTTAJA ] || [ -z $DBSALASANA ]; then
+	echo
+	echo "ERROR! Pakollisia parametreja ei annettu!"
+	echo
+	echo "Ohje: pupe-dump.sh backup.kansio tietokanta kantakayttaja kantasalasana backuplukumaara"
+	echo "Esim: pupe-dump.sh /backup/pupesoft-backup pupesoft kayttajanimi salasana 30"
+	echo
+	exit
+fi
+
+# Katsotaan, etta hakemisto loytyy
+if [ ! -d $BACKUPDIR ]; then
+	echo
+	echo "ERROR! Hakemistoa $BACKUPDIR ei loydy!"
+	echo
+	exit
+fi
+
+# Oletuksena saastetaan 30 backuppia
+if [ -z $BACKUPPAIVAT ]; then
+	BACKUPPAIVAT=30
+fi
+
+echo -n `date "+%Y-%m-%d %H:%M:%S"`
+echo " - Backup $DBKANTA."
+
+FILEDATE=`date "+%Y-%m-%d"`
+FILENAME="${DBKANTA}-backup-${FILEDATE}.sql.bz2"
+
+# tehdaan mysqldump ja pakataan se
+mysqldump -u ${DBKAYTTAJA} --password=${DBSALASANA} ${DBKANTA} | pbzip2 > ${BACKUPDIR}/${FILENAME}
+
+# siivotaan yli 30pv vanhat pois
+find ${BACKUPDIR} -mtime +${BACKUPPAIVAT} -delete
+
+echo -n `date "+%Y-%m-%d %H:%M:%S"`
+echo " - Backup done."

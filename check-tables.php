003d726c9be6fb13c4442373cc515aa3eb2c9874
juppe <juppe@devlab.fi>
2010-05-14 08:14:44 +0000

    Sisennys väärin...

--- check-tables.php
+++ check-tables.php
@@ -1,45 +1,45 @@
 #!/usr/bin/php
 <?php
 
-if ($argc == 0) die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
+	if ($argc == 0) die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
 
-require ("inc/connect.inc");
+	require ("inc/connect.inc");
 
-$query  = "show tables from $dbkanta";
-$result =  mysql_query($query);
+	$query  = "show tables from $dbkanta";
+	$result =  mysql_query($query);
 
-echo date("H:i:s").": Checking tables from $dbkanta.\n";
+	echo date("H:i:s").": Checking tables from $dbkanta.\n";
 
-while ($row = mysql_fetch_array($result)) {
+	while ($row = mysql_fetch_array($result)) {
 
-	$table = $row[0];
+		$table = $row[0];
 
-	// check table for errors
-	$query = "check table $table";
-	$chkre = mysql_query($query);
-	$chkro = mysql_fetch_array($chkre);
-
-	if ($chkro["Msg_text"] != "OK") {
-		echo "$query -> $chkro[Msg_text]\n";
-
-		// repair table for errors
-		$query = "repair table $table";
+		// check table for errors
+		$query = "check table $table";
 		$chkre = mysql_query($query);
 		$chkro = mysql_fetch_array($chkre);
-		echo "$query -> $chkro[Msg_text]\n";
+
+		if ($chkro["Msg_text"] != "OK") {
+			echo "$query -> $chkro[Msg_text]\n";
+
+			// repair table for errors
+			$query = "repair table $table";
+			$chkre = mysql_query($query);
+			$chkro = mysql_fetch_array($chkre);
+			echo "$query -> $chkro[Msg_text]\n";
+		}
+
+		// optimize table
+		$query = "optimize table $table";
+		$chkre = mysql_query($query);
+		$chkro = mysql_fetch_array($chkre);
+
+		if ($chkro["Msg_text"] != "OK" and $chkro["Msg_text"] != "Table is already up to date") {
+			echo "$query -> $chkro[Msg_text]\n";
+		}
+
 	}
 
-	// optimize table
-	$query = "optimize table $table";
-	$chkre = mysql_query($query);
-	$chkro = mysql_fetch_array($chkre);
-
-	if ($chkro["Msg_text"] != "OK" and $chkro["Msg_text"] != "Table is already up to date") {
-		echo "$query -> $chkro[Msg_text]\n";
-	}
-
-}
-
-echo date("H:i:s").": Done.\n\n";
+	echo date("H:i:s").": Done.\n\n";
 
 ?>

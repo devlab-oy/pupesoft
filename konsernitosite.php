8ffacf407f7911817adee33b4cd810bf12c4067c
jonikanerva <joni@devlab.fi>
2010-06-28 11:23:16 +0000

    Konsernitosite koodi ja UI siistitty.

--- konsernitosite.php
+++ konsernitosite.php
@@ -1,23 +1,36 @@
 <?php
-	require "inc/parametrit.inc";
-	
+
+	if (!isset($link)) require "inc/parametrit.inc";
+
+	enable_ajax();
+
+	if ($livesearch_tee == "TILIHAKU") {
+		livesearch_tilihaku();
+		exit;
+	}
+
 	echo "<font class='head'>".t("Uusi konsernitosite")."</font><hr>";
 
-	if ($tee == 'I') { // Tarkistetetaan syötteet perustusta varten
+	// Tarkistetetaan syötteet perustusta varten
+	if ($tee == 'I') {
+
 		$summa = str_replace ( ",", ".", $summa);
 		$tpk += 0;
 		$tpp += 0;
 		$tpv += 0;
+
 		if ($tpv < 1000) $tpv += 2000;
 
 		$val = checkdate($tpk, $tpp, $tpv);
+
 		if (!$val) {
 			echo "<font class='error'>" . t('Virheellinen tapahtumapvm') . "</font><br>";
 			$gok = 1; //  Tositetta ei kirjoiteta kantaan vielä
 		}
 
 		$kuva = false;
-		if(is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+		if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+
 			$filetype = $_FILES['userfile']['type'];
 			$filesize = $_FILES['userfile']['size'];
 			$filename = $_FILES['userfile']['name'];
@@ -25,7 +38,7 @@
 			$data = mysql_real_escape_string(file_get_contents($_FILES['userfile']['tmp_name']));
 
 			// lisätään kuva
-			$query = "	insert into liitetiedostot set
+			$query = "	INSERT INTO liitetiedostot set
 						yhtio      = '{$kukarow['yhtio']}',
 						liitos     = 'lasku',
 						laatija    = '{$kukarow['kuka']}',
@@ -34,64 +47,81 @@
 						filename   = '$filename',
 						filesize   = '$filesize',
 						filetype   = '$filetype'";
-
 			$result = mysql_query($query) or pupe_error($query);
 			$liitostunnus = mysql_insert_id();
 			$fnimi = $liitostunnus;
 		}
 
-
 		$turvasumma = $summa;
 		$totsumma[$kukarow['yhtio']] = 0;
 		$totmaara = $omaara+$vmaara; // Yhteensä tarvittavien tiliöintirivien määrä
+
 		for ($i=0; $i<$totmaara; $i++) {
-			if ($i == $omaara) { //Vaihdetaan yritystä!
-				$turvayhtio=$kukarow['yhtio'];
-				$kukarow['yhtio']=$vastaanottaja;
+
+			//Vaihdetaan yritystä!
+			if ($i == $omaara) {
+				$turvayhtio = $kukarow['yhtio'];
+				$kukarow['yhtio'] = $vastaanottaja;
 				$totsumma[$kukarow['yhtio']] = 0;
 			}
-			if ((strlen($itili[$i]) > 0) or (strlen($isumma[$i]) > 0)) { // Käsitelläänkö rivi??
-				$isumma[$i] = str_replace ( ",", ".", $isumma[$i]);
-				if ((strlen($selite) > 0) && (strlen($iselite[$i]) == 0)) { // Siirretään oletusselite tiliöinneille
+
+			// Käsitelläänkö rivi??
+			if (strlen($itili[$i]) > 0 or strlen($isumma[$i]) > 0) {
+
+				$isumma[$i] = str_replace (",", ".", $isumma[$i]);
+
+				// Siirretään oletusselite tiliöinneille
+				if (strlen($selite) > 0 and strlen($iselite[$i]) == 0) {
 					$iselite[$i] = $selite;
 				}
-				if (strlen($iselite[$i]) == 0) { // Selite puuttuu
+
+				// Selite puuttuu
+				if (strlen($iselite[$i]) == 0) {
 					$ivirhe[$i] = t('Riviltä puuttuu selite').'<br>';
 					$gok = 1;
 				}
 
-				if ($turvasumma > 0) {  // Oletussummalla korvaaminen mahdollista
-					if ($isumma[$i] == '-') { // Summan vastaluku käyttöön
+				// Oletussummalla korvaaminen mahdollista
+				if ($turvasumma > 0) {
+					if ($isumma[$i] == '-') {
+						// Summan vastaluku käyttöön
 						$isumma[$i] = -1 * $turvasumma;
 					}
-					else {
-						if ($isumma[$i] == 0)  { // Kopioidaan summa
-							$isumma[$i] = $turvasumma;
-						}
+					elseif ($isumma[$i] == 0) {
+						// Kopioidaan summa
+						$isumma[$i] = $turvasumma;
 					}
 				}
-				if ($isumma[$i] == 0) { // Summa puuttuu
+
+				// Summa puuttuu
+				if ($isumma[$i] == 0) {
 					$ivirhe[$i] .= t('Riviltä puuttuu summa').'<br>';
 					$gok = 1;
 				}
+
 				$totsumma[$kukarow['yhtio']] += $isumma[$i];
 
-				if ((strlen($itili[$i]) != 5) and (strlen($itili[$i]) != 0)) { // Onko tämä konsernitili
-					$ivirhe[$i] .= t('Tili ei ole konsernitili (Ne ovat 5 merkkiä pitkiä)') . '<br>';
-					$gok = 1;
+				// Onko tämä konsernitili
+				if (strlen($itili[$i]) != 5 and strlen($itili[$i]) != 0) {
+//					$ivirhe[$i] .= t('Tili ei ole konsernitili (Ne ovat 5 merkkiä pitkiä)') . '<br>';
+//					$gok = 1;
 				}
 
+				$ulos		= "";
+				$virhe		= "";
+				$tili		= $itili[$i];
+				$summa		= $isumma[$i];
+				$selausnimi	= "itili[$i]"; // Minka niminen mahdollinen popup on?
 
-				$ulos='';
-				$virhe = '';
-				$tili = $itili[$i];
-				$summa = $isumma[$i];
-				$selausnimi = 'itili[' . $i .']'; // Minka niminen mahdollinen popup on?
-				require "inc/tarkistatiliointi.inc";
+				require ("inc/tarkistatiliointi.inc");
+
 				$ivirhe[$i] .= $virhe;
 				$iulos[$i] = $ulos;
-				if ($ok==0) { // Sieltä kenties tuli päivitys tilinumeroon
-					if ($itili[$i] != $tili) { // Annetaan käyttäjän päättää onko ok
+
+				// Sieltä kenties tuli päivitys tilinumeroon
+				if ($ok == 0) {
+					// Annetaan käyttäjän päättää onko ok
+					if ($itili[$i] != $tili) {
 						$itili[$i] = $tili;
 						$gok = 1; // Tositetta ei kirjoiteta kantaan vielä
 					}
@@ -104,71 +134,80 @@
 		}
 
 		if ($totsumma[$kukarow['yhtio']] != -1 * $totsumma[$turvayhtio]) {
-			echo "<font class='error'>".t("Yrityksille menevät tiliöinnit eivät täsmää")."</font><br>";
+			echo "<font class='error'>".t("Yrityksille menevät tiliöinnit eivät täsmää")."!</font><br><br>";
 			$tee = '';
 		}
+
 		if ($gok == 1) { // Jossain tapahtui virhe
-			echo "<font class='error'>".t("Jossain oli virheitä/muutoksia!")."</font><br>";
+			echo "<font class='error'>".t("Jossain oli virheitä/muutoksia!")."</font><br><br>";
 			$tee = '';
 		}
+
 		$summa = $turvasumma;
 		$kukarow['yhtio']=$turvayhtio; //Palautetaan oletusyhtio
 	}
-// Kirjoitetaan tosite jos tiedot ok!
 
+	// Kirjoitetaan tosite jos tiedot ok!
 	if ($tee == 'I') {
 
 		$query = "	INSERT into lasku set
-						yhtio = '$kukarow[yhtio]',
-						tapvm = '$tpv-$tpk-$tpp',
-						tila = 'X',
-						laatija = '$kukarow[kuka]',
-						luontiaika = now()";
+					yhtio = '$kukarow[yhtio]',
+					tapvm = '$tpv-$tpk-$tpp',
+					tila = 'X',
+					laatija = '$kukarow[kuka]',
+					luontiaika = now()";
 		$result = mysql_query($query) or pupe_error($query);
 		$tunnus = mysql_insert_id($link);
 
 		if ($fnimi) {
 			// päivitetään kuvalle vielä linkki toiseensuuntaa
-			$query = "update liitetiedostot set liitostunnus='$tunnus', selite='$selite $summa' where tunnus='$fnimi'";
+			$query = "	UPDATE liitetiedostot SET
+						liitostunnus = '$tunnus',
+						selite = '$selite $summa'
+						WHERE tunnus = '$fnimi'";
 			$result = mysql_query($query) or pupe_error($query);
 		}
 
 		$totsumma = 0;
 		$totmaara = $omaara+$vmaara; // Yhteensä tarvittavien tiliöintirivien määrä
+
 		for ($i=0; $i<$totmaara; $i++) {
-	//Vaihtuuko yritys?
+
+			// Vaihtuuko yritys?
 			if ($i == $omaara) {
-			//Vastavienti
-				if ($totsumma < 0) $tili = $yhtiorow['konsernisaamiset']; else $tili = $yhtiorow['konsernivelat'];
+				// Vastavienti
+
+				if ($totsumma < 0) {
+					$tili = $yhtiorow['konsernisaamiset'];
+				}
+				else {
+					$tili = $yhtiorow['konsernivelat'];
+				}
+
 				$selite = t('Konsernitositteen vastavienti');
 				$summa = $totsumma * -1;
 				$vero = 0;
-				require "inc/teetiliointi.inc";
-	// Aloitetaan vieraan yrityksen tiliöinnit
-				$totsumma = 0;
-				$turvayhtio=$kukarow['yhtio'];
-				$kukarow['yhtio'] = $vastaanottaja;
-				$query = "SELECT *
-							  FROM yhtio
-							  WHERE  yhtio = '$kukarow[yhtio]'";
-				$result = mysql_query($query) or pupe_error($query);
-				if (mysql_num_rows($result) != 1) {
-					echo "".t("Yritystä")." $kukarow[yhtio] ".t("ei löytynytkään")."!";
 
-					require "inc/footer.inc";
-					exit;
-				}
-				$yhtiorow = mysql_fetch_array ($result);
+				require ("inc/teetiliointi.inc");
+
+				// Aloitetaan vieraan yrityksen tiliöinnit
+				$totsumma = 0;
+				$turvayhtio = $kukarow['yhtio'];
+				$kukarow['yhtio'] = $vastaanottaja;
+
+				$yhtiorow = hae_yhtion_parametrit($kukarow['yhtio']);
+
 				$query = "	INSERT into lasku set
-								yhtio = '$kukarow[yhtio]',
-								tapvm = '$tpv-$tpk-$tpp',
-								tila = 'X',
-								laatija = '$kukarow[kuka]',
-								luontiaika = now()";
+							yhtio = '$kukarow[yhtio]',
+							tapvm = '$tpv-$tpk-$tpp',
+							tila = 'X',
+							laatija = '$kukarow[kuka]',
+							luontiaika = now()";
 				$result = mysql_query($query) or pupe_error($query);
 				$tunnus = mysql_insert_id ($link);
 
-				if (! empty($fnimi)) {
+				if (!empty($fnimi)) {
+
 					// kopioidaan liitetiedosto toiselta riviltä
 					$query = "SELECT * from liitetiedostot where tunnus='$fnimi'";
 					$res = mysql_query($query) or pupe_error($query);
@@ -191,8 +230,9 @@
 				}
 
 			}
+
 			if (strlen($itili[$i]) > 0) {
-		// Tehdään tiliöinnit
+				// Tehdään tiliöinnit
 				$tili = $itili[$i];
 				$kustp = $ikustp[$i];
 				$kohde = $ikohde[$i];
@@ -201,70 +241,85 @@
 				$totsumma += $summa; // Vastavietiä varten
 				$vero = $ivero[$i];
 				$selite = $iselite[$i];
-				require "inc/teetiliointi.inc";
+				require ("inc/teetiliointi.inc");
 			}
-			$itili[$i]='';
-			$ikustp[$i]='';
-			$ikohde[$i]='';
-			$iprojekti[$i]='';
-			$isumma[$i]='';
-			$ivero[$i]='';
-			$iselite[$i]='';
+
+			$itili[$i] = '';
+			$ikustp[$i] = '';
+			$ikohde[$i] = '';
+			$iprojekti[$i] = '';
+			$isumma[$i] = '';
+			$ivero[$i] = '';
+			$iselite[$i] = '';
 		}
-	//Vastavienti
-		if ($totsumma < 0) $tili = $yhtiorow['konsernisaamiset']; else $tili = $yhtiorow['konsernivelat'];
+
+		// Vastavienti
+		if ($totsumma < 0) {
+			$tili = $yhtiorow['konsernisaamiset'];
+		}
+		else {
+			$tili = $yhtiorow['konsernivelat'];
+		}
+
 		$summa = $totsumma * -1;
 		$vero = 0;
 		$selite = t('Konsernitositteen vastavienti');
-		require "inc/teetiliointi.inc";
 
-		$kukarow['yhtio']=$turvayhtio;
-		$query = "SELECT *
-					  FROM yhtio
-					  WHERE  yhtio = '$kukarow[yhtio]'";
-		$result = mysql_query($query) or pupe_error($query);
-		if (mysql_num_rows($result) != 1) {
-			echo "Yritystä $kukarow[yhtio] ei löytynytkään!";
+		require ("inc/teetiliointi.inc");
 
-			require "inc/footer.inc";
-			exit;
-		}
-		$yhtiorow=mysql_fetch_array ($result);
-		$tee='';
-		$selite='';
-		$summa=0;
+		$kukarow['yhtio'] = $turvayhtio;
+		$yhtiorow = hae_yhtion_parametrit($kukarow['yhtio']);
+
+		$tee = '';
+		$selite = '';
+		$summa = 0;
+
+		echo "<font class='message'>".t("Konsernitosite luotu")."!</font>";
+
+		echo "	<form action = 'muutosite.php' method='post'>
+				<input type='hidden' name='tee' value='E'>
+				<input type='hidden' name='tunnus' value='$tunnus'>
+				<input type='Submit' value='".t("Näytä tosite")."'>
+				</form><br><hr><br>";
 	}
 
-	if ($tee == '') { // Uusi tosite
+	// Uusi tosite
+	if ($tee == '') {
+
 		if ($vastaanottaja == '') {
 			$query = "	SELECT yhtio, nimi
-							FROM yhtio
-							WHERE konserni='$yhtiorow[konserni]' and trim(konserni) != ''
-									and yhtio!='$kukarow[yhtio]'";
+						FROM yhtio
+						WHERE konserni = '$yhtiorow[konserni]'
+						AND trim(konserni) != ''
+						AND yhtio != '$kukarow[yhtio]'";
 			$yresult = mysql_query($query) or pupe_error($query);
+
 			if (mysql_num_rows($yresult) < 2) {
 				echo "<font class='error'>".t("Konsernissasi ei ole yhtään yritystä. Näin ollen et voi käyttää tätä toimintoa")."</font>";
-
-				require "inc/footer.inc";
+				require ("inc/footer.inc");
 				exit;
 			}
-			echo "<form name = 'tosite' action = '$PHP_SELF' method='post'>
-					<font class='message'>" . t("Valitse vastaanottava yritys") . "</font>";
+
+			echo "<form name = 'tosite' action = '$PHP_SELF' method='post'>";
+			echo "<font class='message'>" . t("Valitse vastaanottava yritys") . " </font> ";
 			echo "<select name='vastaanottaja'>";
+
 			while ($yrow = mysql_fetch_array($yresult)) {
 				$sel = '';
 				if ($yrow['yhtio'] == $vastaanottaja) {
 					$sel = "selected";
 				}
-				echo "<option value='$yrow[0]' $sel>$yrow[1] ($yrow[0])</option>";
+				echo "<option value='$yrow[0]' $sel>$yrow[1]</option>";
 			}
-			echo "</select><input type='Submit' value = '" . t("Valitse") ."'></form>";
+			echo "</select>";
+			echo " <input type='Submit' value = '" . t("Valitse") ."'></form>";
 		}
 		else {
-			if (($omaara == 0) or ($vmaara == 0)) {
+			if ($omaara == 0 or $vmaara == 0) {
 				$omaara = 1;
 				$vmaara = 1;
 			}
+
 			echo "<form action = '$PHP_SELF' method='post'>
 					<input type='hidden' name='vastaanottaja' value='$vastaanottaja'>
 					<input type='hidden' name='tpp' maxlength='2' size=2 value='$tpp'>
@@ -281,6 +336,7 @@
 					<option value ='8'>8
 					</select>
 					</td>
+					</tr><tr>
 					<td>".t("Konserniyrityksen tiliöintirivien määrä")."</td>
 					<td>
 					<select name='vmaara'>
@@ -289,60 +345,88 @@
 					<option value ='4'>4
 					<option value ='8'>8
 					</select>
-					</td><td><input type = 'submit' value = '".t("Perusta") ."'></td>
-					</tr></table></form>";
+					</td><td class='back'><input type = 'submit' value = '".t("Perusta") ."'></td>
+					</tr></table></form><br>";
 
 			$formi = 'tosite';
 			$kentta = 'tpp';
+
+			echo "<font class='message'>Syötä tositteen otsikkotiedot:</font>";
+
 			echo "<form name = 'tosite' action = '$PHP_SELF' method='post' enctype='multipart/form-data'>
 					<input type='hidden' name='tee' value='I'>
 					<input type='hidden' name='omaara' value='$omaara'>
 					<input type='hidden' name='vmaara' value='$vmaara'>
 					<input type='hidden' name='vastaanottaja' value='$vastaanottaja'>
-			      <table><tr>
-			      <td>".t("Tositteen päiväys")."</td>
-			      <td><input type='text' name='tpp' maxlength='2' size=2 value='$tpp'>
-						<input type='text' name='tpk' maxlength='2' size=2 value='$tpk'>
-						<input type='text' name='tpv' maxlength='4' size=4 value='$tpv'> ".t("ppkkvvvv")."</td>
-			      </tr>";
-			echo "<td>".t("Summa")."</td>";
+					<table>";
 
-			echo "<td><input type='text' name='summa' value='$summa'></td></tr>";
-			echo "<td colspan = '2'>".t("Selite"). " <input type='text' name='selite' value='$selite' maxlength='150' size=60></td></tr>
-			      <tr><td>".t("Mahdollinen tositteen kuva/liite")."</td>";
-			if (! empty($fnimi)) {
+			echo "<tr>";
+			echo "<th>".t("Tositteen päiväys")."</th>";
+			echo "<td><input type='text' name='tpp' maxlength='2' size=2 value='$tpp'>";
+			echo "<input type='text' name='tpk' maxlength='2' size=2 value='$tpk'>";
+			echo "<input type='text' name='tpv' maxlength='4' size=4 value='$tpv'> ".t("ppkkvvvv")."</td>";
+			echo "</tr>";
+
+			echo "<tr>";
+			echo "<th>".t("Summa")."</th>";
+			echo "<td><input type='text' name='summa' value='$summa'></td>";
+			echo "</tr>";
+
+			echo "<th>".t("Selite"). "</th>";
+			echo "<td><input type='text' name='selite' value='$selite' maxlength='150' size=60></td>";
+			echo "</tr>";
+			echo "<tr>";
+
+			echo "<th>".t("Tositteen kuva/liite")."</th>";
+
+			if (!empty($fnimi)) {
 				echo "<td>".t("Liite jo tallessa")."!<input name='fnimi' type='hidden' value = '$fnimi'></td></tr></table>";
 			}
 			else {
 				echo "<td><input name='userfile' type='file' value = '$userfile_name'></td></tr></table>";
 			}
 
-			echo "<br><br><table>
-			      <tr><th>".t("Tili")."</th><th>".t("Tarkenne")."</th>
-			      <th>".t("Summa")."</th><th>".t("Vero")."</th></tr>";
+			echo "<br><font class='message'>".t("Oma yritys").": $yhtiorow[nimi]</font>";
+
 			$totmaara = $omaara+$vmaara; // Yhteensä tarvittavien tiliöintirivien määrä
 
 			for ($i=0; $i<$totmaara; $i++) {
+
 				// Valitaan vastaanottava konsernin jäsen
 				if ($i == $omaara) {
 					$turvayhtio=$kukarow['yhtio'];
-					$kukarow['yhtio']=$vastaanottaja;
-					$query = "SELECT *
-								  FROM yhtio
-								  WHERE  yhtio = '$kukarow[yhtio]'";
-					$result = mysql_query($query) or pupe_error($query);
-					if (mysql_num_rows($result) != 1) {
-						echo "".t("Yritystä")." $kukarow[yhtio] ".t("ei löytynytkään")."!";
 
-						require "inc/footer.inc";
-						exit;
-					}
-					$yhtiorow=mysql_fetch_array ($result);
-					echo "<tr><th colspan = '5'>".t("Vastaanottava yritys")." $yhtiorow[nimi] ($yhtiorow[yhtio])</th></tr>";
+					$kukarow['yhtio'] = $vastaanottaja;
+					$yhtiorow = hae_yhtion_parametrit($kukarow['yhtio']);
+					echo "<font class='message'>".t("Vastaanottava yritys").": $yhtiorow[nimi]</font>";
 				}
+
+				echo "<table>";
+				echo "<tr>";
+				echo "<th width='200'>".t("Tili")."</th>";
+				echo "<th>".t("Tarkenne")."</th>";
+				echo "<th>".t("Summa")."</th>";
+				echo "<th>".t("Vero")."</th>";
+				echo "</tr>";
+
 				echo "<tr>";
 				if ($iulos[$i] == '') {
-					echo "<td><input type='text' name='itili[$i]' value='$itili[$i]''></td>";
+					//Annetaan selväkielinen nimi
+					$tilinimi = '';
+
+					if ($itili[$i] != '') {
+						$query = "	SELECT nimi
+									FROM tili
+									WHERE yhtio = '$kukarow[yhtio]'
+									AND tilino = '$itili[$i]'";
+						$vresult = mysql_query($query) or pupe_error($query);
+
+						if (mysql_num_rows($vresult) == 1) {
+							$vrow = mysql_fetch_array($vresult);
+							$tilinimi = "<br>".$vrow['nimi'];
+						}
+					}
+					echo "<td width='200' valign='top'>".livesearch_kentta("tosite", "TILIHAKU", "itili[$i]", 170, $itili[$i], "EISUBMIT")." $tilinimi</td>";
 				}
 				else {
 					echo "<td>$iulos[$i]</td>";
@@ -360,8 +444,8 @@
 				echo "<td><select name='ikustp[$i]'>";
 				echo "<option value =' '>".t("Ei kustannuspaikkaa");
 
-				while ($vrow=mysql_fetch_array($vresult)) {
-					$sel="";
+				while ($vrow = mysql_fetch_array($vresult)) {
+					$sel = "";
 					if ($ikustp[$i] == $vrow[0]) {
 						$sel = "selected";
 					}
@@ -381,8 +465,8 @@
 				echo "<select name='ikohde[$i]'>";
 				echo "<option value =' '>".t("Ei kohdetta");
 
-				while ($vrow=mysql_fetch_array($vresult)) {
-					$sel="";
+				while ($vrow = mysql_fetch_array($vresult)) {
+					$sel = "";
 					if ($ikohde[$i] == $vrow[0]) {
 						$sel = "selected";
 					}
@@ -402,8 +486,8 @@
 				echo "<select name='iprojekti[$i]'>";
 				echo "<option value =' '>".t("Ei projektia");
 
-				while ($vrow=mysql_fetch_array($vresult)) {
-					$sel="";
+				while ($vrow = mysql_fetch_array($vresult)) {
+					$sel = "";
 					if ($iprojekti[$i] == $vrow[0]) {
 						$sel = "selected";
 					}
@@ -415,16 +499,20 @@
 
 				echo "<td>" . alv_popup('ivero['.$i.']', $ivero[$i]) . "</td>";
 
-				echo  "<td>$ivirhe[$i]</td>";
+				echo  "<td class='back'>$ivirhe[$i]</td>";
 
 				echo "</tr>";
 
-				echo "<tr><td colspan = '5'>
-				      ".t("Selite").":<input type='text' name='iselite[$i]' value='$iselite[$i]' maxlength='150' size=60></td></tr>";
+				echo "<tr><td colspan = '4'>".t("Selite").":<input type='text' name='iselite[$i]' value='$iselite[$i]' maxlength='150' size=60></td></tr>";
+				echo "</table><br>";
 			}
-			echo "</table><input type='Submit' value = '".t("Tee tosite")."'></form>";
+
+			echo "</table>";
+			echo "<br>";
+			echo "<input type='Submit' value = '".t("Tee tosite")."'></form>";
 		}
 	}
 
-	require "inc/footer.inc";
+	require ("inc/footer.inc");
+
 ?>

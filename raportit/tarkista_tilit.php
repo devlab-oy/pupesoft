184ae250c082be798ec4a318271c118939fa0a17
juppe <juppe@devlab.fi>
2010-08-11 11:59:50 +0000

    Pikkuviilaus!

--- raportit/tarkista_tilit.php
+++ raportit/tarkista_tilit.php
@@ -2,56 +2,68 @@
 
 require("../inc/parametrit.inc");
 
+echo "<br><font class='head'>".t("Tiliöintien tilit")."</font><hr><br>";
 
-echo "<br><font class='message'>Tiliöintien tilit</font><br>";
 $query = "	SELECT distinct tili.tilino t, tiliointi.tilino
 			FROM tiliointi
 			LEFT JOIN tili ON tili.yhtio=tiliointi.yhtio and tili.tilino=tiliointi.tilino
-			WHERE tiliointi.yhtio='$kukarow[yhtio]'  and korjattu='' and tiliointi.tilino > 0
+			WHERE tiliointi.yhtio = '$kukarow[yhtio]'
+			and tiliointi.korjattu = ''
+			and tiliointi.tilino > 0
 			HAVING t IS NULL";
-$result = mysql_query($query) or pupe_error($query);	
-while($tili = mysql_fetch_array($result)) {
+$result = mysql_query($query) or pupe_error($query);
+
+while ($tili = mysql_fetch_array($result)) {
 	$query = "	SELECT tapvm viimeisin, selite
 				FROM tiliointi
-				WHERE tiliointi.yhtio='$kukarow[yhtio]' and tiliointi.tilino = $tili[tilino] and korjattu=''
+				WHERE yhtio		= '$kukarow[yhtio]'
+				and tilino 		= $tili[tilino]
+				and korjattu	= ''
 				ORDER BY tapvm asc
 				LIMIT 1";
-	$res = mysql_query($query) or pupe_error($query);	
+	$res = mysql_query($query) or pupe_error($query);
 	$viimrow = mysql_fetch_array($res);
-	echo "<br><font class='error'>Tiliä $tili[tilino] ei ole enään olemassa! viimeisin tiliöinti $viimrow[viimeisin], $viimrow[selite]</font><br>";
+
+	echo "<br><font class='error'>Tiliä $tili[tilino] ei ole enää olemassa! Viimeisin tiliöinti $viimrow[viimeisin], $viimrow[selite]</font><br>";
 }
 
 $tables	= array("asiakas", "tuote", "toimi");
-
 $columnit = array("tilino", "tilino_eu", "tilino_ei_eu");
-$query = "	SHOW TABLES";
+
+$query = "SHOW TABLES";
 $result = mysql_query($query) or pupe_error($query);
+
 while($table = mysql_fetch_array($result)) {
-	
-	if(in_array($table[0], $tables)) {
+
+	if (in_array($table[0], $tables)) {
+
 		echo "<br><font class='message'>Tarkastetaan taulu $table[0]</font><br>";
+
 		$query = "	SHOW columns FROM $table[0]";
 		$res = mysql_query($query) or pupe_error($query);
+
 		while($col = mysql_fetch_array($res)) {
-			if(in_array($col[0], $columnit)) {
-				foreach($columnit as $c) {
+			if (in_array($col[0], $columnit)) {
+				foreach ($columnit as $c) {
 					$query = "	SELECT $c
 								FROM $table[0]
-								WHERE yhtio = '$kukarow[yhtio]' and $c != ''";
+								WHERE yhtio = '$kukarow[yhtio]'
+								and $c != ''";
 					$haku = mysql_query($query);
 
-					while($row = mysql_fetch_array($haku)) {
+					while ($row = mysql_fetch_array($haku)) {
 						$query = "	SELECT tunnus
 									FROM tili
 									WHERE yhtio = '$kukarow[yhtio]' and tilino = '".$row[$c]."'";
 						$tarkresr = mysql_query($query) or pupe_error($query);
+
 						if(mysql_num_rows($tarkresr) == 0) {
 							echo "<font class='error'>[$c] Tiliä ei löydy '".$row[$c]."'</font><br>";
 						}
 					}
 				}
 			}
-		}	
+		}
 	}
 }
 

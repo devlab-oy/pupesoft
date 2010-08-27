5e629e4fadace0a1d169acf5bfce250bdc9a9e9a
jonikanerva <joni@devlab.fi>
2010-08-27 06:16:31 +0000

    Backupsavedays

--- pupe-cron-server.sh
+++ pupe-cron-server.sh
@@ -4,6 +4,12 @@
 #"Esim: /var/www/html/pupesoft/pupe-cron-server.sh"
 
 POLKU=`dirname $0`
+BACKUPSAVEDAYS=$1
+
+# Oletuksena s‰‰stet‰‰n 30 backuppia
+if [ -z $BACKUPSAVEDAYS ]; then
+	BACKUPSAVEDAYS=30
+fi
 
 # Tarkistetaan mysql-tietokannan taulut
 cd ${POLKU};php check-tables.php

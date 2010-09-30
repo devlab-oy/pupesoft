b8167abcc57216861ddb8b71bd9cee4254cc057c
juppe <juppe@devlab.fi>
2010-09-30 10:52:09 +0000

    From: ".mb_encode_mimeheader($yhtiorow["nimi"], "ISO-8859-1", "Q")." <$yhtiorow[postittaja_email]>

--- varauskalenteri/varauskalenteri_lisaa.php
+++ varauskalenteri/varauskalenteri_lisaa.php
@@ -58,7 +58,7 @@
 			$meili .= "Vieraslukum‰‰r‰:\n$kentta08\n\n\n";
 			$meili .= "Juomatoivomus:\n$kentta10\n";
 
-			$tulos = mail("$yhtiorow[varauskalenteri_email]", "Saunavaraus", $meili,"From: ".$kukarow["nimi"]."<".$kukarow["eposti"].">\nReply-To: ".$kukarow["nimi"]."<".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
+			$tulos = mail("$yhtiorow[varauskalenteri_email]", "Saunavaraus", $meili,"From: ".mb_encode_mimeheader($kukarow["nimi"])." <".$kukarow["eposti"].">\nReply-To: ".mb_encode_mimeheader($kukarow["nimi"])." <".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
 		}
 		else{
 			echo "<br><br>Tarkista, ett&auml; kaikki tiedot on sy&ouml;tetty!";
@@ -67,12 +67,12 @@
 	}
 	if($toim == "Starcraft" || $toim == "Per‰moottori 1" || $toim == "Per‰moottori 2") {
 		$meili = "$kukarow[nimi] on varannut kohteen'".$toim."' ajalle:\n\n##################################################\n$day-$month-$year   Klo: $kello --> $lday-$lmonth-$lyear  Klo: $lkello\n##################################################\n\n\nViesti:\n$kentta05";
-		$tulos = mail("$yhtiorow[varauskalenteri_email]", $toim, $meili,"From: ".$kukarow["nimi"]."<".$kukarow["eposti"].">\nReply-To: ".$kukarow["nimi"]."<".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
+		$tulos = mail("$yhtiorow[varauskalenteri_email]", $toim, $meili,"From: ".mb_encode_mimeheader($kukarow["nimi"])." <".$kukarow["eposti"].">\nReply-To: ".mb_encode_mimeheader($kukarow["nimi"])." <".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
 	}
 
 	if($toim == "Kuorma-auto") {
 		$meili = "$kukarow[nimi] on varannut kohteen '".$toim."' ajalle:\n\n##################################################\n$day-$month-$year   Klo: $kello --> $lday-$lmonth-$lyear  Klo: $lkello\n##################################################\n\n\nViesti:\n$kentta05";
-		$tulos = mail("$yhtiorow[varauskalenteri_email]", $toim, $meili,"From: ".$kukarow["nimi"]."<".$kukarow["eposti"].">\nReply-To: ".$kukarow["nimi"]."<".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
+		$tulos = mail("$yhtiorow[varauskalenteri_email]", $toim, $meili,"From: ".mb_encode_mimeheader($kukarow["nimi"])." <".$kukarow["eposti"].">\nReply-To: ".mb_encode_mimeheader($kukarow["nimi"])." <".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
 	}
 
 

a68065d82b5ad539faf3e8fe48aeff057b686589
juppe <juppe@devlab.fi>
2010-10-04 09:47:40 +0000

    mb_encode_mimeheader subjectiin!

--- crm/asiakaslista.php
+++ crm/asiakaslista.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 	// käsittämätön juttu, mutta ei voi muuta
 	if ($_POST["voipcall"] != "") $_GET["voipcall"]  = "";
 
@@ -12,7 +12,7 @@
 		ob_end_clean();
 		if ($retval != "OK") echo "<font class='error'>Soitto $o -&gt; $d epäonnistui!</font><br><br>";
 	}
-	
+
 	$otsikko   = 'Asiakaslista';
 	if ($yhtiorow['viikkosuunnitelma'] == '') {
 		$kentat    = "tunnus::nimi::asiakasnro::ytunnus::if(toim_postitp!='',toim_postitp,postitp)::postino::yhtio::myyjanro::email";
@@ -20,11 +20,11 @@
 	else {
 		$kentat    = "tunnus::nimi::myyjanro::ytunnus::asiakasnro::if(toim_postitp!='',toim_postitp,postitp)::yhtio";
 	}
-	
+
 	$jarjestys = 'selaus, nimi';
 
 	echo "<font class='head'>".t("$otsikko")."</font><hr>";
-	
+
 	$array = explode("::", $kentat);
 	$count = count($array);
 	for ($i=0; $i<=$count; $i++) {
@@ -36,92 +36,92 @@
 	if (strlen($ojarj) > 0) {
 		$jarjestys = $ojarj;
 	}
-	
+
 	if ($asos != '') {
 		$lisa .= " and osasto='$asos' ";
 	}
-	
+
 	if ($aspiiri != '') {
 		$lisa .= " and piiri='$aspiiri' ";
 	}
-	
+
 	if ($asryhma != '') {
 		$lisa .= " and ryhma='$asryhma' ";
 	}
 	if ($astila != '') {
 		$lisa .= " and tila='$astila' ";
 	}
-	
+
 	if ($asmyyja != '') {
 		$lisa .= " and myyjanro='$asmyyja' ";
 	}
 
 	$lisa .= " and laji != 'P' ";
-	
+
 	if (trim($konserni) != '') {
 		$query = "SELECT distinct yhtio FROM yhtio WHERE (konserni = '$yhtiorow[konserni]' and konserni != '') or (yhtio = '$yhtiorow[yhtio]')";
 		$result = mysql_query($query) or pupe_error($query);
 		$konsernit = "";
-		
-		while ($row = mysql_fetch_array($result)) {	
+
+		while ($row = mysql_fetch_array($result)) {
 			$konsernit .= " '".$row["yhtio"]."' ,";
-		}		
-		$konsernit = " yhtio in (".substr($konsernit, 0, -1).") ";			
+		}
+		$konsernit = " yhtio in (".substr($konsernit, 0, -1).") ";
 	}
 	else {
 		$konsernit = " yhtio = '$kukarow[yhtio]' ";
 	}
-	
+
 	if ($yhtiorow['viikkosuunnitelma'] == '') {
-		if ($tee == "lahetalista") {		
+		if ($tee == "lahetalista") {
 			$query = "	SELECT tunnus, nimi, postitp, ytunnus, yhtio, asiakasnro, nimitark, osoite, postino, postitp, maa, toim_nimi, toim_nimitark, toim_osoite, toim_postino, toim_postitp, toim_maa,
 						puhelin, fax, myyjanro, email, osasto, piiri, ryhma, fakta, toimitustapa, yhtio
-						FROM asiakas 
-						WHERE $konsernit 
+						FROM asiakas
+						WHERE $konsernit
 						$lisa";
 			$tiednimi = "asiakaslista.xls";
 		}
 		else {
 			$query = "	SELECT tunnus, IF(nimi != toim_nimi, CONCAT(nimi, '<br />', toim_nimi), nimi) nimi, asiakasnro, ytunnus,  if(toim_postitp!='',toim_postitp,postitp) postitp, if(toim_postino!=00000,toim_postino,postino) postino, yhtio, myyjanro, email, puhelin
-						FROM asiakas 
-						WHERE $konsernit 
+						FROM asiakas
+						WHERE $konsernit
 						$lisa";
 			$tiednimi = "viikkosuunnitelma.xls";
 		}
 	}
 	else {
 		$query = "	SELECT tunnus, nimi, (SELECT concat_ws(' ',myyja,nimi) from kuka where yhtio='$kukarow[yhtio]' and myyja=myyjanro and myyja > 0 limit 1) myyja, ytunnus, asiakasnro, if(toim_postitp!='',toim_postitp,postitp) postitp, yhtio, puhelin
-					FROM asiakas 
-					WHERE $konsernit 
+					FROM asiakas
+					WHERE $konsernit
 					$lisa";
 	}
-	
 
-				
+
+
 	if ($lisa == "" and ($tee != 'laheta' or $tee != 'lahetalista')) {
 		$limit = " LIMIT 200 ";
-	} 
+	}
 	else {
 		$limit = " ";
 	}
-				
+
 	$query .= "$ryhma ORDER BY $jarjestys $limit";
 	$result = mysql_query($query) or pupe_error($query);
 
 	if ($oper == t("Vaihda listan kaikkien asiakkaiden tila")) {
 		// Käydään lista läpi kertaalleen
 		while ($trow = mysql_fetch_array ($result)) {
-			$query_update = "	UPDATE asiakas 
-								SET tila = '$astila_vaihto' 
-								WHERE tunnus = '$trow[tunnus]' 
+			$query_update = "	UPDATE asiakas
+								SET tila = '$astila_vaihto'
+								WHERE tunnus = '$trow[tunnus]'
 								AND yhtio = '$yhtiorow[yhtio]'";
 			$result_update = mysql_query($query_update) or pupe_error($query_update);
 		}
 		$result = mysql_query($query) or pupe_error($query);
 	}
-	
+
 	if ($tee == 'laheta' or $tee == 'lahetalista') {
-		
+
 		if ($tee == "lahetalista") {
 			if (@include('Spreadsheet/Excel/Writer.php')) {
 				//keksitään failille joku varmasti uniikki nimi:
@@ -144,14 +144,14 @@
 					for ($i=1; $i<mysql_num_fields($result)-1; $i++) {
 						if(isset($workbook)) {
 							$worksheet->write($excelrivi, $excelsarake, t(mysql_field_name($result,$i)) , $format_bold);
-							$excelsarake++;					
+							$excelsarake++;
 						}
 					}
-					
-					
+
+
 					$excelsarake = 0;
-					$excelrivi++;					
-					
+					$excelrivi++;
+
 				}
 			}
 		}
@@ -173,15 +173,15 @@
 
 				if(isset($workbook)) {
 					$excelsarake = 0;
-					
+
 					for ($i=1; $i<mysql_num_fields($result); $i++) {
 						//$liite .= $trow[$i]."\t";
 						if(isset($workbook)) {
 							$worksheet->write($excelrivi, $excelsarake, t(mysql_field_name($result,$i)) , $format_bold);
-							$excelsarake++;					
+							$excelsarake++;
 						}
-					}					
-					
+					}
+
 					$worksheet->write($excelrivi, $excelsarake, t("pvm") , $format_bold);
 					$excelsarake++;
 					$worksheet->write($excelrivi, $excelsarake, t("kampanjat") , $format_bold);
@@ -197,11 +197,11 @@
 					$worksheet->write($excelrivi, $excelsarake, t("pvraha") , $format_bold);
 					$excelsarake++;
 					$worksheet->write($excelrivi, $excelsarake, t("kommentti") , $format_bold);
-					
-					
+
+
 					$excelsarake = 0;
-					$excelrivi++;					
-					
+					$excelrivi++;
+
 				}
 			}
 		}
@@ -210,25 +210,25 @@
 			for ($i=1; $i<mysql_num_fields($result)-1; $i++) {
 				if(isset($workbook)) {
 					$worksheet->writeString($excelrivi, $excelsarake, $trow[$i], $format_bold);
-					$excelsarake++;					
+					$excelsarake++;
 				}
 			}
 			$excelrivi++;
 		}
-		
+
 		if(isset($workbook)) {
 			// We need to explicitly close the workbook
 			$workbook->close();
 		}
-		
+
 		$liite = "/tmp/".$excelnimi;
-		
+
 		$bound = uniqid(time()."_") ;
 
 		$header  = "From: ".mb_encode_mimeheader($yhtiorow["nimi"], "ISO-8859-1", "Q")." <$yhtiorow[postittaja_email]>\n";
 		$header .= "MIME-Version: 1.0\n" ;
 		$header .= "Content-Type: multipart/mixed; boundary=\"$bound\"\n" ;
-		
+
 		$content = "--$bound\n" ;
 
 		$content .= "Content-Type: application/excel; name=\"".basename($liite)."\"\n" ;
@@ -241,37 +241,35 @@
 
 		$content .= chunk_split(base64_encode($sisalto));
 		$content .= "\n" ;
-		
-				
-		
+
+
+
 		if ($tee == "lahetalista") {
-			mail($kukarow['eposti'], "Asiakkaiden tiedot", $content, $header, "-f $yhtiorow[postittaja_email]");			
+			mail($kukarow['eposti'], mb_encode_mimeheader("Asiakkaiden tiedot", "ISO-8859-1", "Q"), $content, $header, "-f $yhtiorow[postittaja_email]");
 			echo "<br><br><font class='message'>".t("Asiakkaiden tiedot sähköpostiisi")."!</font><br><br><br>";
 		}
 		else {
-			mail($kukarow['eposti'], "Viikkosunnitelmapohja", $content, $header, "-f $yhtiorow[postittaja_email]");
+			mail($kukarow['eposti'], mb_encode_mimeheader("Viikkosunnitelmapohja", "ISO-8859-1", "Q"), $content, $header, "-f $yhtiorow[postittaja_email]");
 			echo "<br><br><font class='message'>".t("Suunnitelmapohja lähetetty sähköpostiisi")."!</font><br><br><br>";
 		}
-		
-		
+
 		mysql_data_seek($result,0);
-		
 	}
-	
+
 	if ($yhtiorow['viikkosuunnitelma'] == '') {
 		echo "<li><a href='$PHP_SELF?tee=laheta&asos=$asos&asryhma=$asryhma&astila=$astila&aspiiri=$aspiiri&konserni=$konserni&asmyyja=$asmyyja".$ulisa."'>".t("Lähetä viikkosuunnitelmapohja sähköpostiisi")."</a><br>";
 		echo "<li><a href='$PHP_SELF?tee=lahetalista&asos=$asos&asryhma=$asryhma&astila=$astila&aspiiri=$aspiiri&konserni=$konserni&asmyyja=$asmyyja".$ulisa."'>".t("Lähetä asiakaslista sähköpostiisi")."</a><br>";
 	}
-	
+
 	echo "<br><table>
 			<form action='$PHP_SELF' method='post'>
 			<input type='hidden' name='voipcall' value='kala'>";
-	
+
 	$asosresult = t_avainsana("ASIAKASOSASTO");
-	
+
 	echo "<tr><th>".t("Valitse asiakkaan osasto").":</th><td><select name='asos' onchange='submit();'>";
 	echo "<option value=''>".t("Kaikki osastot")."</option>";
-	
+
 	while ($asosrow = mysql_fetch_array($asosresult)) {
 		$sel2 = '';
 		if ($asos == $asosrow["selite"]) {
@@ -280,21 +278,21 @@
 		echo "<option value='$asosrow[selite]' $sel2>$asosrow[selite] - $asosrow[selitetark]</option>";
 	}
 	echo "</select></td>";
-	
-	
+
+
 	$chk = "";
 	if (trim($konserni) != '') {
 		$chk = "CHECKED";
 	}
-	
+
 	echo "<th>".t("Näytä konsernin kaikki asiakkaat").":</th><td><input type='checkbox' name='konserni' $chk onclick='submit();'></td>";
 	echo "</tr>\n\n";
-	
+
 	$asosresult = t_avainsana("PIIRI");
-	
+
 	echo "<tr><th>".t("Valitse asiakkaan piiri").":</th><td><select name='aspiiri' onchange='submit();'>";
 	echo "<option value=''>".t("Kaikki piirit")."</option>";
-	
+
 	while ($asosrow = mysql_fetch_array($asosresult)) {
 		$sel2 = '';
 		if ($aspiiri == $asosrow["selite"]) {
@@ -303,13 +301,13 @@
 		echo "<option value='$asosrow[selite]' $sel2>$asosrow[selite] - $asosrow[selitetark]</option>";
 	}
 	echo "</select></td>";
-	
+
 
 	$asosresult = t_avainsana("ASIAKASRYHMA");
-	
+
 	echo "<th>".t("Valitse asiakkaan ryhmä").":</th><td><select name='asryhma' onchange='submit();'>";
 	echo "<option value=''>".t("Kaikki ryhmät")."</option>";
-	
+
 	while ($asosrow = mysql_fetch_array($asosresult)) {
 		$sel2 = '';
 		if ($asryhma == $asosrow["selite"]) {
@@ -317,21 +315,21 @@
 		}
 		echo "<option value='$asosrow[selite]' $sel2>$asosrow[selite] - $asosrow[selitetark]</option>";
 	}
-	
+
 	echo "</select></td></tr>\n\n";
-					
+
 	$query = "	SELECT distinct asiakas.myyjanro, kuka.nimi
 				FROM asiakas
 				LEFT JOIN kuka ON kuka.yhtio = asiakas.yhtio and kuka.myyja=asiakas.myyjanro and kuka.myyja > 0
-				WHERE asiakas.yhtio = '$kukarow[yhtio]' 
+				WHERE asiakas.yhtio = '$kukarow[yhtio]'
 				and asiakas.myyjanro > 0
 				order by myyjanro";
 	$asosresult = mysql_query($query) or pupe_error($query);
-	
+
 	echo "<tr>";
 	echo "<th>".t("Valitse asiakkaan myyjä").":</th><td><select name='asmyyja' onchange='submit();'>";
 	echo "<option value=''>".t("Kaikki myyjät")."</option>";
-	
+
 	while ($asosrow = mysql_fetch_array($asosresult)) {
 		$sel2 = '';
 		if ($asmyyja == $asosrow["myyjanro"]) {
@@ -339,14 +337,14 @@
 		}
 		echo "<option value='$asosrow[myyjanro]' $sel2>$asosrow[myyjanro] - $asosrow[nimi]</option>";
 	}
-	
-	echo "</select></td>\n\n";				
+
+	echo "</select></td>\n\n";
 
 	$asosresult = t_avainsana("ASIAKASTILA");
 
 	echo "<th>".t("Valitse asiakkaan tila").":</th><td><select name='astila' onchange='submit();'>";
 	echo "<option value=''>".t("Kaikki tilat")."</option>";
-	
+
 	while ($asosrow = mysql_fetch_array($asosresult)) {
 		$sel2 = '';
 		if ($astila == $asosrow["selite"]) {
@@ -354,12 +352,12 @@
 		}
 		echo "<option value='$asosrow[selite]' $sel2>$asosrow[selite] - $asosrow[selitetark]</option>";
 	}
-	
-	echo "</select></td></tr>\n\n";			
+
+	echo "</select></td></tr>\n\n";
 	echo "</table><br><table>";
 
 	echo "<tr>";
-		
+
 	for ($i = 1; $i < mysql_num_fields($result)-1; $i++) {
 		echo "<th><a href='$PHP_SELF?asos=$asos&asryhma=$asryhma&astila=$astila&aspiiri=$aspiiri&konserni=$konserni&asmyyja=$asmyyja&ojarj=".mysql_field_name($result,$i).$ulisa."'>" . t(mysql_field_name($result,$i)) . "</a>";
 
@@ -372,12 +370,12 @@
 	}
 
 	echo "<td class='back'>&nbsp;&nbsp;<input type='Submit' value='".t("Etsi")."'></td></tr>\n\n";
-		
+
 	$kalalask = 1;
-	
+
 	while ($trow=mysql_fetch_array ($result)) {
 		echo "<tr class='aktiivi'>";
-		
+
 		for ($i=1; $i<mysql_num_fields($result)-1; $i++) {
 			if ($i == 1) {
 				if (trim($trow[1]) == '') $trow[1] = "".t("*tyhjä*")."";
@@ -390,8 +388,8 @@
 				echo "<td>$trow[$i]</td>";
 			}
 		}
-		
-		echo "<td class='back'>";		
+
+		echo "<td class='back'>";
 
 		if ($trow["puhelin"] != "" and $kukarow["puhno"] != "" and isset($VOIPURL)) {
 			$d = ereg_replace("[^0-9]", "", $trow["puhelin"]);  // dest
@@ -399,9 +397,9 @@
 			echo "<a href='$PHP_SELF?asos=$asos&asryhma=$asryhma&astila=$astila&aspiiri=$aspiiri&konserni=$konserni&ojarj=$ulisa&o=$o&d=$d&voipcall=call'>Soita $o -&gt; $d</a>";
 		}
 
-		echo "</td>";		
+		echo "</td>";
 		echo "</tr>\n\n";
-		
+
 		$kalalask++;
 	}
 	echo "</table>";
@@ -411,7 +409,7 @@
 	$asosresult = t_avainsana("ASIAKASTILA");
 
 	echo t("Vaihda asiakkaiden tila").": <select name='astila_vaihto'>";
-	
+
 	while ($asosrow = mysql_fetch_array($asosresult)) {
 		$sel2 = '';
 		if ($astila == $asosrow["selite"]) {
@@ -419,7 +417,7 @@
 		}
 		echo "<option value='$asosrow[selite]' $sel2>$asosrow[selite] - $asosrow[selitetark]</option>";
 	}
-	
+
 	echo "</select></td></tr>\n\n";
 
 	echo "<input type=\"submit\" name=\"oper\" value=\"".t("Vaihda listan kaikkien asiakkaiden tila")."\">";
--- crm/asiakasmemo.php
+++ crm/asiakasmemo.php
@@ -72,7 +72,7 @@
 			$meili .= t("Viesti").":\n".str_replace("\r\n","\n", $row["kentta01"])."\n\n";
 			$meili .= "-----------------------\n\n";
 
-			$tulos = mail($email, "Asiakasmemo $yhtiorow[nimi]", $meili,"From: ".mb_encode_mimeheader($kukarow["nimi"])." <$kukarow[eposti]>\nReply-To: ".mb_encode_mimeheader($kukarow["nimi"])." <".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
+			$tulos = mail($email, mb_encode_mimeheader(t("Asiakasmemo")." $yhtiorow[nimi]", "ISO-8859-1", "Q"), $meili,"From: ".mb_encode_mimeheader($kukarow["nimi"], "ISO-8859-1", "Q")." <$kukarow[eposti]>\nReply-To: ".mb_encode_mimeheader($kukarow["nimi"], "ISO-8859-1", "Q")." <".$row["eposti"].">\n", "-f $yhtiorow[postittaja_email]");
 
 			if ($row["tyyppi"] == "Lead") {
 				$eviesti = "$kukarow[nimi] lähetti leadin osoitteeseen: $email";

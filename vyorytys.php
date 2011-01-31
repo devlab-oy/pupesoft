c255146b19bb35652c843bb94b94bd238c7a7954
jarmotorvinen <jamppa@devlab.fi>
2011-01-31 14:42:07 +0000

    Vyörytys jossa voidaan valita tilivälit joilta vyöryttää arvot tositteelle

--- vyorytys.php
+++ vyorytys.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 
 	require("inc/parametrit.inc");
 
@@ -32,7 +32,7 @@
 
 	echo "<td><select name='tkausi' onchange='submit();'>";
 
-	while ($vrow=mysql_fetch_assoc($vresult)) {
+	while ($vrow = mysql_fetch_assoc($vresult)) {
 		$sel="";
 
 		if (!isset($savrow)) {
@@ -67,10 +67,12 @@
 	}
 
 
-	echo "<tr><th>".t("Tili")."</th><td width='200' valign='top'>".livesearch_kentta("tosite", "TILIHAKU", "tilino", 170, $tilino, "EISUBMIT")." $tilinimi</td></tr>";
+	echo "<tr><th>".t("Tilin alku")."</th><td width='200' valign='top'>".livesearch_kentta("tosite", "TILIHAKU", "tilinalku", 170, $tilinalku, "EISUBMIT")." $tilinimi</td></tr>";
+	echo "<tr><th>".t("Tilin loppu")."</th><td width='200' valign='top'>".livesearch_kentta("tosite", "TILIHAKU", "tilinloppu", 170, $tilinloppu, "EISUBMIT")." $tilinimi</td></tr>";
+
 	echo "<tr><th>".t("Tarkenne")."</th><td>";
 
-	$monivalintalaatikot = array("KUSTP", "KOHDE", "PROJEKTI");
+	$monivalintalaatikot = array("KUSTP");
 	$noautosubmit = TRUE;
 
 	require ("tilauskasittely/monivalintalaatikot.inc");
@@ -90,8 +92,6 @@
 
 		if (mysql_num_rows($vresult) == 1) {
 			$tilikausirow = mysql_fetch_assoc($vresult);
-
-			echo "<font class='head'>".t("Tilin %s saldo tilikaudelta", "", $tilino)." ".tv1dateconv($tilikausirow["tilikausi_alku"])." - ".tv1dateconv($tilikausirow["tilikausi_loppu"])."</font><hr>";
 		}
 		else {
 			echo "<font class='error'>".t("Tuntematon tilikausi")."</font>";
@@ -100,6 +100,8 @@
 
 		$lisa1 = "";
 		$where = "";
+		$kustp = "";
+		$groupby = "";
 
 		if ($alvk != "") {
 			$alvy = (int) substr($alvk, 0, 4);
@@ -110,42 +112,110 @@
 
 			$lisa1 = " sum(if(tiliointi.tapvm >= '$alvy-$alvm-01' and tiliointi.tapvm <= '$alvy-$alvm-$alvd', tiliointi.summa, 0)) Saldo";
 			$where = " and tiliointi.tapvm <= '$alvy-$alvm-$alvd'";
+
+			$tilikausirow["tilikausi_alku"] =  $alvy. "-". sprintf("%02s",$alvm) . "-01";
+			$tilikausirow["tilikausi_loppu"] =  $alvy. "-". sprintf("%02s",$alvm) . "-" . $alvd;
 		}
 		else {
 			$lisa1 = " sum(tiliointi.summa) Saldo";
 		}
 
-		$query = "	SELECT $lisa1, sum(tiliointi.summa) Kumulatiivinen
-					FROM tiliointi
-					WHERE tiliointi.yhtio  = '$kukarow[yhtio]'
-					and tiliointi.korjattu = ''
-					and tiliointi.tapvm   >= '$tilikausirow[tilikausi_alku]'
-					and tiliointi.tapvm   <= '$tilikausirow[tilikausi_loppu]'
-					and tiliointi.tilino   = '$tilino'
-					$where
-					$lisa";
-		$result = mysql_query($query) or pupe_error($query);
-		$trow = mysql_fetch_assoc($result);
-		
-		echo "<table>";
-		echo "<tr><th>".t("Tili").":</th><td>$tilino</td></tr>";
-		echo "<tr><th>".t("Saldo").":</th><td>$trow[Saldo]</td></tr>";
-		echo "<tr><th>".t("Kumulatiivinen saldo").":</th><td>$trow[Kumulatiivinen]</td></tr>";
-		echo "</table>";
-		
-		echo "<form name='tosite' action='tosite.php' method='post' autocomplete='off'>\n";
-		echo "<input type='hidden' name='tee' value='I'>\n";
-						
-		echo "<input type='hidden' name='maara' value='5'>\n";
-		echo "<input type='hidden' name='tpp' value='".date("d")."'>\n";
-		echo "<input type='hidden' name='tpk' value='".date("m")."'>\n";
-		echo "<input type='hidden' name='tpv' value='".date("Y")."'>\n";
-		echo "<input type='hidden' name='summa' value='".($trow["Kumulatiivinen"])."'>\n";
-		echo "<input type='hidden' name='itili[1]' value='$tilino'>\n";
-		echo "<input type='hidden' name='isumma[1]' value='".($trow["Kumulatiivinen"]*-1)."'>\n";
-					
-		echo "<br><input type='submit' value='".t("Tee tosite")."'></form><br><br>";
-		
+		echo "<font class='head'>".t("Kumulatiivinen saldo")." ".tv1dateconv($tilikausirow["tilikausi_alku"])." - ".tv1dateconv($tilikausirow["tilikausi_loppu"])."</font><hr>";
+
+		if ($tilinalku == '' and $tilinloppu == '') {
+			echo "<font class='error'>".t("Pitää syöttää ainakin yksi tili")."</font>";
+		}
+		else {
+
+			if (count($mul_kustp) > 0 and $mul_kustp[0] !='') {
+				$where .= " and tiliointi.kustp in (";
+				foreach ($mul_kustp as $kustp) {
+					$where .= "'$kustp',";
+				}
+				$where = substr($where, 0, -1); // vika pilkku pois
+				$where .= ")";
+				$groupby = ", kustp";
+			}
+
+			if (trim($tilinloppu) == '') {
+				$tilinloppu = $tilinalku;
+			}
+
+			if (trim($tilinalku) == '') {
+				$tilinalku = $tilinloppu;
+			}
+
+			if (trim($tilinalku) > trim($tilinloppu)) {
+				$swap = $tilinalku;
+				$tilinalku = $tilinloppu;
+				$tilinloppu = $swap;
+			}
+
+			$query = "	SELECT tiliointi.tilino, group_concat(distinct tiliointi.kustp) kustp, $lisa1, sum(tiliointi.summa) tilisaldo
+						FROM tiliointi
+						WHERE tiliointi.yhtio  = '$kukarow[yhtio]'
+						AND tiliointi.korjattu = ''
+						AND tiliointi.tapvm   >= '$tilikausirow[tilikausi_alku]'
+						AND tiliointi.tapvm   <= '$tilikausirow[tilikausi_loppu]'
+						AND tiliointi.tilino between '$tilinalku' AND '$tilinloppu'
+						$where
+						GROUP BY tilino $groupby";
+			$result = mysql_query($query) or pupe_error($query);
+			$laskuri = mysql_num_rows($result)+2;
+
+			echo "<form name='tosite' action='tosite.php' method='post' autocomplete='off'>\n";
+			echo "<input type='hidden' name='tee' value='I'>\n";
+			echo "<input type='hidden' name='maara' value='$laskuri'>\n";
+
+			$i=1;
+
+			echo "<table>";
+			echo "<tr>";
+			echo "<th>".t("Tili")."</th>";
+			echo "<th>".t("Saldo")."</th>";
+			if ($kustp != '') {
+				echo "<th>".t("Kustannuspaikka")."</th>";
+			}
+			echo "</tr>";
+
+			while ($trow = mysql_fetch_array($result)) {
+
+				echo "<tr>";
+				echo "<td>$trow[tilino]</td>";
+				echo "<td>$trow[Saldo]</td>";
+
+				if ($kustp != '') {
+
+					$query2 = "SELECT nimi, koodi FROM kustannuspaikka WHERE kustannuspaikka.yhtio = '$kukarow[yhtio]' AND kustannuspaikka.tunnus = '$trow[kustp]'";
+					$result2 = mysql_query($query2) or pupe_error($query2);
+					$tarkenne = mysql_fetch_assoc($result2);
+
+					if ($tarkenne[nimi] == '') {
+						$tarkenne[nimi] = t("Ei kustannuspaikkaa");
+					}
+
+					echo "<td>$tarkenne[koodi] $tarkenne[nimi]</td>";
+				}
+
+				echo "</tr>";
+
+				$kumulus = $kumulus + $trow["Saldo"];
+
+				echo "<input type='hidden' name='tpp' value='".date("d")."'>\n";
+				echo "<input type='hidden' name='tpk' value='".date("m")."'>\n";
+				echo "<input type='hidden' name='tpv' value='".date("Y")."'>\n";
+
+				echo "<input type='hidden' name='itili[$i]' value='$trow[tilino]'>\n";
+				echo "<input type='hidden' name='isumma[$i]' value='".($trow["tilisaldo"]*-1)."'>\n";
+				echo "<input type='hidden' name='ikustp[$i]' value='$trow[kustp]'>\n";
+				echo "<input type='hidden' name='iselite[$i]' value='".t("Vyörytys")."'>\n";
+				$i++;
+			}
+
+			echo "<input type='hidden' name='summa' value='$kumulus'>\n";
+			echo "</table>";
+			echo "<br><input type='submit' value='".t("Tee tosite")."'></form><br><br>";
+		}
 	}
 
 	require ("inc/footer.inc");

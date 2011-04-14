9f6df81ab68e1439b7a14cb488916436220c2c12
jonikanerva <joni@devlab.fi>
2011-04-14 06:27:31 +0000

    Raportti päivitetty 2000 luvulle.

--- kopioiasiakasalennus.php
+++ kopioiasiakasalennus.php
@@ -1,127 +1,225 @@
 <?php
-	require "inc/parametrit.inc";
-	echo "<font class='head'>".t("Kopioi asiakkaan alennuksia").":</font><hr>";
-	
+
+	require ("inc/parametrit.inc");
+
+	echo "<font class='head'>".t("Kopioi asiakkaan alennuksia")."</font><hr>";
+
 	if ($tee == "write") {
-	
-		if ($uusiytunnus != '' and $uusiytunnus != $vanhaytunnus) {
+
+		$uusiytunnus = mysql_real_escape_string(trim($uusiytunnus));
+		$uusitunnus = mysql_real_escape_string(trim($uusitunnus));
+
+		if ($uusiytunnus != '' and $uusitunnus != '') {
+			echo "<font class='error'>".t("Valitse joko ytunnus tai asiakastunnus")."!</font><br><br>";
+			$tee = 'edit';
+		}
+		else {
+
 			$query = "	SELECT tunnus
 						FROM asiakas
-						WHERE ytunnus='$uusiytunnus' and yhtio='$kukarow[yhtio]'";
-			$result = mysql_query($query) or pupe_error($query);
-			
-			if (mysql_num_rows($result) > 0) {		
-				$query = "	SELECT ytunnus, ryhma, alennus
-							FROM asiakasalennus
-							WHERE ytunnus='$vanhaytunnus' and yhtio='$kukarow[yhtio]'";
-				$result = mysql_query($query) or pupe_error($query);
-								
+						WHERE yhtio = '$kukarow[yhtio]'
+						AND ((ytunnus = '$uusiytunnus' AND ytunnus != '') OR (tunnus = '$uusitunnus'))";
+			$result = mysql_query($query) or die ("Kysely ei onnistu $query");
+
+			if (mysql_num_rows($result) > 0) {
+		        $query = "	(SELECT DISTINCT if(asiakasalennus.asiakas = 0, '', asiakasalennus.asiakas) asiakas, asiakasalennus.ytunnus, asiakasalennus.tuoteno, asiakasalennus.ryhma, asiakasalennus.alennus
+							FROM asiakas
+							JOIN asiakasalennus USE INDEX (yhtio_asiakas_ryhma) ON (asiakasalennus.yhtio = asiakasalennus.yhtio
+								AND asiakasalennus.asiakas = asiakas.tunnus
+								and asiakasalennus.asiakas != '')
+							WHERE asiakas.yhtio = '$kukarow[yhtio]'
+							AND asiakas.tunnus = '$tunnus'
+							AND asiakas.laji != 'P')
+
+							UNION
+
+							(SELECT DISTINCT if(asiakasalennus.asiakas = 0, '', asiakasalennus.asiakas) asiakas, asiakasalennus.ytunnus, asiakasalennus.tuoteno, asiakasalennus.ryhma, asiakasalennus.alennus
+							FROM asiakas
+							JOIN asiakasalennus USE INDEX (yhtio_ytunnus_ryhma) ON (asiakasalennus.yhtio = asiakasalennus.yhtio
+								AND asiakasalennus.ytunnus = asiakas.ytunnus
+								AND asiakasalennus.ytunnus != '')
+							WHERE asiakas.yhtio = '$kukarow[yhtio]'
+							AND asiakas.tunnus = '$tunnus'
+							AND asiakas.laji != 'P')
+
+							ORDER BY 2";
+				$result = mysql_query($query) or die ("Kysely ei onnistu $query");
+
 				while ($trow = mysql_fetch_array($result)) {
-						$query = "	INSERT INTO asiakasalennus
-									SET 
-									ytunnus		= '$uusiytunnus',
-									yhtio		= '$kukarow[yhtio]',
-									ryhma		= '$trow[ryhma]',
-									alennus 	= '$trow[alennus]',
-									muutospvm	= now()";
-						$insresult = mysql_query($query) or pupe_error($query);	
+					$query = "	INSERT INTO asiakasalennus SET
+								yhtio		= '$kukarow[yhtio]',
+								ytunnus		= '$uusiytunnus',
+								asiakas		= '$uusitunnus',
+								ryhma		= '$trow[ryhma]',
+								tuoteno		= '$trow[tuoteno]',
+								alennus 	= '$trow[alennus]',
+								laatija		= '$kukarow[kuka]',
+								luontiaika	= now()";
+					$insresult = mysql_query($query) or pupe_error($query);
 				}
-				echo "".t("Asiakasalennukset kopioitu asiakkaalta")." $vanhaytunnus --> $uusiytunnus<br><br>";
+
+				echo "<font class='message'>".t("Asiakasalennukset kopioitu").".</font><br><br>";
 				$tee = "";
 			}
 			else {
-				echo "".t("Syöttämälläsi ytunnuksella ei löytynyt yhtään asiakasta!")."!<br>";
+				echo "<font class='error'>".t("Asiakasta ei löydy")."!</font><br><br>";
+				$tee = 'edit';
 			}
-		}
-		else {
-			echo "".t("Et syöttänyt mitään/tai syötit surkean ytunnuksen!")."<br>";
 		}
 	}
-	
+
 	if ($tee == "edit") {
-		
-		// Kokeillaan geneeristä
-		$query = "	SELECT ytunnus, ryhma, alennus
-					FROM asiakasalennus
-					WHERE ytunnus='$ytunnus' and yhtio='$kukarow[yhtio]'";
-		$result = mysql_query($query) or die ("Kysely ei onnistu $query");
-		
+
+        $query = "	(SELECT DISTINCT if(asiakasalennus.asiakas = 0, '', asiakasalennus.asiakas) asiakas, asiakasalennus.ytunnus, asiakasalennus.tuoteno, asiakasalennus.ryhma, asiakasalennus.alennus
+					FROM asiakas
+					JOIN asiakasalennus USE INDEX (yhtio_asiakas_ryhma) ON (asiakasalennus.yhtio = asiakasalennus.yhtio
+						AND asiakasalennus.asiakas = asiakas.tunnus
+						and asiakasalennus.asiakas != '')
+					WHERE asiakas.yhtio = '$kukarow[yhtio]'
+					AND asiakas.tunnus = '$tunnus'
+					AND asiakas.laji != 'P')
+
+					UNION
+
+					(SELECT DISTINCT if(asiakasalennus.asiakas = 0, '', asiakasalennus.asiakas) asiakas, asiakasalennus.ytunnus, asiakasalennus.tuoteno, asiakasalennus.ryhma, asiakasalennus.alennus
+					FROM asiakas
+					JOIN asiakasalennus USE INDEX (yhtio_ytunnus_ryhma) ON (asiakasalennus.yhtio = asiakasalennus.yhtio
+						AND asiakasalennus.ytunnus = asiakas.ytunnus
+						AND asiakasalennus.ytunnus != '')
+					WHERE asiakas.yhtio = '$kukarow[yhtio]'
+					AND asiakas.tunnus = '$tunnus'
+					AND asiakas.laji != 'P')
+
+					ORDER BY 1, 2, 3, 4";
+		$result = mysql_query($query) or pupe_error("Kysely ei onnistu $query");
+
 		if (mysql_num_rows($result) > 0) {
-			echo "<form action = '$PHP_SELF' method = 'post'>";
-			echo "<input type = 'hidden' name = 'tee' value ='write'>";
-			echo "<input type = 'hidden' name = 'vanhaytunnus' value ='$ytunnus'>";			
+
+			echo "<form action='$PHP_SELF' method='post'>";
+			echo "<input type='hidden' name='tee' value='write'>";
+			echo "<input type='hidden' name='tunnus' value='$tunnus'>";
+
 			echo "<table>";
-			
-			echo "<tr><th>".t("Syötä ytunnus").":</th><td colspan='2'><input type = 'text' size='15' name = 'uusiytunnus'></td></tr>";
-			echo "<tr><td colspan='3' class='back'><br></td></tr>";
-			echo "<tr><th>".t("Ytunnus")."</th><th>".t("Alennusryhma")."</th><th>".t("Alennusprosentti")."</th></tr>";
-			
-			while ($trow = mysql_fetch_array($result)) {
-				echo "<tr><td>$trow[ytunnus]</td><td>$trow[ryhma]</td><td>$trow[alennus]</td></tr>";		
-			}
-			
+			echo "<tr>";
+			echo "<th>".t("Syötä ytunnus")."</th>";
+			echo "<td><input type = 'text' size='15' name = 'uusiytunnus'></td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<th>".t("tai")." ".t("Syötä Asiakastunnus")."</th>";
+			echo "<td><input type = 'text' size='15' name = 'uusitunnus'></td>";
+			echo "</tr>";
 			echo "</table><br>";
-			
-			echo "<input type = 'submit' value = '".t("Kopioi")."'>";
+
+			echo "<table>";
+			echo "<tr>";
+			echo "<th>".t("Asiakas")."</th>";
+			echo "<th>".t("Ytunnus")."</th>";
+			echo "<th>".t("Alennusryhma")."</th>";
+			echo "<th>".t("Tuotenumero")."</th>";
+			echo "<th>".t("Alennusprosentti")."</th>";
+			echo "</tr>";
+
+			while ($row = mysql_fetch_array($result)) {
+				echo "<tr class='aktiivi'>";
+				echo "<td>$row[asiakas]</td>";
+				echo "<td>$row[ytunnus]</td>";
+				echo "<td>$row[ryhma]</td>";
+				echo "<td>$row[tuoteno]</td>";
+				echo "<td>$row[alennus]</td>";
+				echo "</tr>";
+			}
+
+			echo "</table><br>";
+
+			echo "<input type='submit' value='".t("Kopioi")."'>";
 			echo "</form>";
-		
+
 		}
 		else {
 			echo "<br><br>".t("Tällä asiakaalla ei ole yhtään asiakasalennusta")."!<br><br>";
 			$tee = '';
 		}
 	}
-	
-	if($tee == ''){
-		
-		$kentat = 'tunnus, nimi, nimitark, postitp, ytunnus, ovttunnus';
-		$jarjestys = 'selaus, nimi';
-		
-		$array = explode(",", $kentat);
-        $count = count($array);
-		
-        for ($i=0; $i<=$count; $i++) {
-			if (strlen($haku[$i]) > 0) {
-					$lisa .= " and " . $array[$i] . " like '%" . $haku[$i] . "%'";
-					$ulisa .= "&haku[" . $i . "]=" . $haku[$i]; 
-			}
-        }
-        if (strlen($ojarj) > 0) {  
-			$jarjestys = $ojarj;
-        }       
-                
-        $query = "SELECT $kentat FROM asiakas WHERE yhtio = '$kukarow[yhtio]' $lisa ";
-        $query .= "$ryhma ORDER BY $jarjestys LIMIT 100";
 
-		$result = mysql_query ($query)
-				or die ("Kysely ei onnistu $query");
+	if ($tee == '') {
 
-		echo "	<table><tr>
-				<form action = '$PHP_SELF' method = 'post'>";
-	
-		for ($i = 1; $i < mysql_num_fields($result); $i++) {
-			echo "<th valign='top' align='left'><a href = '$PHP_SELF?ojarj=".$array[$i].$ulisa ."'> 
-					" . t(mysql_field_name($result,$i)) . "</a>";
-			echo "<br><input type='text' name = 'haku[" . $i . "]' value = '$haku[$i]'>";
-			echo "</th>";
-		}
-		echo "<td valign='bottom' class='back'><input type='Submit' value = '".t("Etsi")."'></td></form></tr>";
-	
-		while ($trow=mysql_fetch_array($result)) {
-			echo "<tr>";
-			for ($i=1; $i<mysql_num_fields($result); $i++) {
-				if ($i == 1) {
-					echo "<td><a href='$PHP_SELF?ytunnus=$trow[ytunnus]&tee=edit'>$trow[$i]</a></td>";
-				}
-				else {
-					echo "<td>$trow[$i]</td>";
-				}
-			}
+		enable_ajax();
+	    pupe_DataTables('asiakaslista', 7, 7);
+
+        $query = "	(SELECT DISTINCT
+					asiakas.tunnus,
+					concat(asiakas.nimi, '<br>', asiakas.toim_nimi) nimi,
+					concat(asiakas.nimitark, '<br>', asiakas.toim_nimitark) nimitark,
+					concat(asiakas.postitp, '<br>', asiakas.toim_postitp) postitp,
+					concat(asiakas.ytunnus) ytunnus,
+					concat(asiakas.ovttunnus, '<br>', asiakas.toim_ovttunnus) ovttunnus,
+					asiakas.asiakasnro
+					FROM asiakas
+					JOIN asiakasalennus USE INDEX (yhtio_asiakas_ryhma) ON (asiakasalennus.yhtio = asiakasalennus.yhtio
+						AND asiakasalennus.asiakas = asiakas.tunnus
+						AND asiakasalennus.asiakas != '')
+					WHERE asiakas.yhtio = '$kukarow[yhtio]'
+					AND asiakas.laji != 'P')
+
+					UNION
+
+					(SELECT DISTINCT
+					asiakas.tunnus,
+					concat(asiakas.nimi, '<br>', asiakas.toim_nimi) nimi,
+					concat(asiakas.nimitark, '<br>', asiakas.toim_nimitark) nimitark,
+					concat(asiakas.postitp, '<br>', asiakas.toim_postitp) postitp,
+					concat(asiakas.ytunnus) ytunnus,
+					concat(asiakas.ovttunnus, '<br>', asiakas.toim_ovttunnus) ovttunnus,
+					asiakas.asiakasnro
+					FROM asiakas
+					JOIN asiakasalennus USE INDEX (yhtio_ytunnus_ryhma) ON (asiakasalennus.yhtio = asiakasalennus.yhtio
+						AND asiakasalennus.ytunnus = asiakas.ytunnus
+						AND asiakasalennus.ytunnus != '')
+					WHERE asiakas.yhtio = '$kukarow[yhtio]'
+					AND asiakas.laji != 'P')
+
+					ORDER BY 2";
+		$result = mysql_query ($query) or pupe_error("Kysely ei onnistu $query");
+
+		echo "<table class='display' id='asiakaslista'>";
+		echo "<thead>";
+		echo "<tr>";
+		echo "<th>".t("Nimi")."</th>";
+		echo "<th>".t("Nimitark")."</th>";
+		echo "<th>".t("Postitp")."</th>";
+		echo "<th>".t("Ytunnus")."</th>";
+		echo "<th>".t("Ovttunnus")."</th>";
+		echo "<th>".t("Asiakasnro")."</th>";
+		echo "<th>".t("Asiakastunnus")."</th>";
+		echo "</tr>";
+		echo "<tr>";
+		echo "<td><input type='text' name='search_nimi'></td>";
+		echo "<td><input type='text' name='search_nimitark'></td>";
+		echo "<td><input type='text' name='search_postitp'></td>";
+		echo "<td><input type='text' name='search_ytunnus'></td>";
+		echo "<td><input type='text' name='search_ovttunnus'></td>";
+		echo "<td><input type='text' name='search_asiakasnro'></td>";
+		echo "<td><input type='text' name='search_tunnus'></td>";
+		echo "</tr>";
+		echo "</thead>";
+		echo "<tbody>";
+
+		while ($row = mysql_fetch_array($result)) {
+			echo "<tr class='aktiivi'>";
+			echo "<td><a href='$PHP_SELF?tunnus=$row[tunnus]&tee=edit'>$row[nimi]</a></td>";
+			echo "<td>$row[nimitark]</td>";
+			echo "<td>$row[postitp]</td>";
+			echo "<td>$row[ytunnus]</td>";
+			echo "<td>$row[ovttunnus]</td>";
+			echo "<td>$row[asiakasnro]</td>";
+			echo "<td>$row[tunnus]</td>";
 			echo "</tr>";
 		}
+
+		echo "</tbody>";
 		echo "</table>";
 	}
-	
-	require "inc/footer.inc";
+
+	require ("inc/footer.inc");
 
 ?>

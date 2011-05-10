75f32c6245e20da4d6b15691fa429c6579e2a507
juppe <juppe@devlab.fi>
2011-05-10 10:49:54 +0000

    Korjattu pari bugia ja lisätty sellanen juttu, että jos rivejä on yli 1000, niin ei näytetä matriisia ruudulla vaan luodaan vain exceli.

--- budjetinyllapito_tat.php
+++ budjetinyllapito_tat.php
@@ -24,6 +24,8 @@
 		if (!isset($liitostunnukset)) $liitostunnukset = '';
 		else $liitostunnukset = urldecode($liitostunnukset);
 
+		$maxrivimaara = 1000;
+
 		if (isset($vaihdaasiakas)) {
 			$ytunnus 		 = "";
 			$asiakasid 		 = 0;
@@ -67,6 +69,104 @@
 					${$a} = $b;
 				}
 			}
+		}
+
+		if (isset($_FILES['userfile']) and is_uploaded_file($_FILES['userfile']['tmp_name']) === TRUE) {
+
+			$path_parts = pathinfo($_FILES['userfile']['name']);
+			$ext = strtoupper($path_parts['extension']);
+
+			if ($ext != "XLS") {
+				die ("<font class='error'><br>".t("Ainoastaan .xls tiedostot sallittuja")."!</font>");
+			}
+
+			if ($_FILES['userfile']['size'] == 0) {
+				die ("<font class='error'><br>".t("Tiedosto on tyhj‰")."!</font>");
+			}
+
+			require_once ('excel_reader/reader.php');
+
+			// ExcelFile
+			$data = new Spreadsheet_Excel_Reader();
+
+			// Set output Encoding.
+			$data->setOutputEncoding('CP1251');
+			$data->setRowColOffset(0);
+			$data->read($_FILES['userfile']['tmp_name']);
+
+			echo "<font class='message'>".t("Tarkastetaan l‰hetetty tiedosto")."...<br></font>";
+
+			$headers	 		= array();
+			$budj_taulunrivit 	= array();
+			$liitostunnukset 	= "";
+
+			for ($excej = 0; $excej < $data->sheets[0]['numCols']; $excej++) {
+				$headers[] = trim($data->sheets[0]['cells'][0][$excej]);
+			}
+
+			for ($excej = (count($headers)-1); $excej > 0 ; $excej--) {
+				if ($headers[$excej] != "") {
+					break;
+				}
+				else {
+					unset($headers[$excej]);
+				}
+			}
+
+			// Huomaa n‰m‰ jos muutat excel-failin sarakkeita!!!!
+			if ($toim == "TUOTE") {
+				$lukualku = 2;
+			}
+			elseif ($toim == "TOIMITTAJA") {
+				$lukualku = 3;
+			}
+			elseif ($toim == "ASIAKAS") {
+				$lukualku = 4;
+			}
+
+			if ($headers[$lukualku] == "Tuoteryhm‰") {
+				$lukualku++;
+				$tuoteryhmittain = "on";
+			}
+
+			$insert_rivimaara = 0;
+
+			for ($excei = 1; $excei < $data->sheets[0]['numRows']; $excei++) {
+
+				$liitun = $data->sheets[0]['cells'][$excei][0];
+
+				$liitostunnukset .= "'$liitun',";
+
+				if ($tuoteryhmittain != "") {
+					$try = $data->sheets[0]['cells'][$excei][$lukualku-1];
+				}
+
+				for ($excej = $lukualku; $excej < count($headers); $excej++) {
+					$kasiind = str_replace("-", "", $headers[$excej]);
+
+					if ($tuoteryhmittain != "") {
+						$budj_taulunrivit[$liitun][$kasiind][$try] = trim($data->sheets[0]['cells'][$excei][$excej]);
+					}
+					else {
+						$budj_taulunrivit[$liitun][$kasiind][] = trim($data->sheets[0]['cells'][$excei][$excej]);
+					}
+
+					$insert_rivimaara++;
+				}
+			}
+
+			if ($insert_rivimaara >= $maxrivimaara) {
+				// Vied‰‰n suoraan kantaan
+				$luvut = $budj_taulunrivit;
+				$submit_button = "OK";
+
+				echo "<font class='error'>".t("HUOM: Maksimirivim‰‰r‰ ylittyi, rivej‰ ei n‰ytet‰ ruudulla. Rivit tallennetaan suoraan tietokantaan")."!<br><br></font>";
+			}
+			else {
+				echo "<font class='error'>".t("HUOM: Excel-tiedoston luvut eiv‰t viel‰ tallennettu tietokantaan")."!<br>".t("Klikkaa")." '",t("N‰yt‰/Tallenna"),"' ".t("tallentaaksesi luvut")."!<br></font>".t("Tiedosto ok")."!<br><br></font>";
+			}
+
+			$liitostunnukset = substr($liitostunnukset, 0, -1);
 		}
 
 		if (isset($luvut) and count($luvut) > 0 and $submit_button != '') {
@@ -208,91 +308,6 @@
 			}
 		}
 
-		if (isset($_FILES['userfile']) and is_uploaded_file($_FILES['userfile']['tmp_name']) === TRUE) {
-
-			$path_parts = pathinfo($_FILES['userfile']['name']);
-			$ext = strtoupper($path_parts['extension']);
-
-			if ($ext != "XLS") {
-				die ("<font class='error'><br>".t("Ainoastaan .xls tiedostot sallittuja")."!</font>");
-			}
-
-			if ($_FILES['userfile']['size'] == 0) {
-				die ("<font class='error'><br>".t("Tiedosto on tyhj‰")."!</font>");
-			}
-
-			require_once ('excel_reader/reader.php');
-
-			// ExcelFile
-			$data = new Spreadsheet_Excel_Reader();
-
-			// Set output Encoding.
-			$data->setOutputEncoding('CP1251');
-			$data->setRowColOffset(0);
-			$data->read($_FILES['userfile']['tmp_name']);
-
-			echo "<font class='message'>".t("Tarkastetaan l‰hetetty tiedosto")."...<br></font>";
-
-			$headers	 		= array();
-			$budj_taulunrivit 	= array();
-			$liitostunnukset 	= "";
-
-			for ($excej = 0; $excej < $data->sheets[0]['numCols']; $excej++) {
-				$headers[] = trim($data->sheets[0]['cells'][0][$excej]);
-			}
-
-			for ($excej = (count($headers)-1); $excej > 0 ; $excej--) {
-				if ($headers[$excej] != "") {
-					break;
-				}
-				else {
-					unset($headers[$excej]);
-				}
-			}
-
-			// Huomaa n‰m‰ jos muutat excel-failin sarakkeita!!!!
-			if ($toim == "TUOTE") {
-				$lukualku = 2;
-			}
-			elseif ($toim == "TOIMITTAJA") {
-				$lukualku = 3;
-			}
-			elseif ($toim == "ASIAKAS") {
-				$lukualku = 4;
-			}
-
-			if ($headers[$lukualku] == "Tuoteryhm‰") {
-				$lukualku++;
-				$tuoteryhmittain = "on";
-			}
-
-			for ($excei = 1; $excei < $data->sheets[0]['numRows']; $excei++) {
-
-				$liitun = $data->sheets[0]['cells'][$excei][0];
-
-				$liitostunnukset .= "'$liitun',";
-
-				if ($tuoteryhmittain != "") {
-					$try = $data->sheets[0]['cells'][$excei][$lukualku-1];
-				}
-
-				for ($excej = $lukualku; $excej < count($headers); $excej++) {
-					$kasiind = str_replace("-", "", $headers[$excej]);
-
-					if ($tuoteryhmittain != "") {
-						$budj_taulunrivit[$liitun][$kasiind][$try] = trim($data->sheets[0]['cells'][$excei][$excej]);
-					}
-					else {
-						$budj_taulunrivit[$liitun][$kasiind][] = trim($data->sheets[0]['cells'][$excei][$excej]);
-					}
-				}
-			}
-
-			echo "<font class='error'>".t("HUOM: Excel-tiedoston luvut eiv‰t viel‰ tallennettu tietokantaan")."!<br>".t("Klikkaa")." '",t("N‰yt‰/Tallenna"),"' ".t("tallentaaksesi luvut")."!<br></font>".t("Tiedosto ok")."!<br><br></font>";
-
-			$liitostunnukset = substr($liitostunnukset, 0, -1);
-		}
-
 		if ($asiakasid > 0 or $toimittajaid > 0 or $liitostunnukset != "") {
 			if ($toim == "TOIMITTAJA") {
 				echo "<form method='post'>
@@ -337,7 +352,7 @@
 				echo "<tr><th>",t("Valitse tuote"),"</th>";
 				echo "<td><input type='text' name='tuoteno' value='$tuoteno' /></td></tr>";
 
-				echo "<tr><th>".t("tai rajaa asiakaskategorialla")."</th><td>";
+				echo "<tr><th>".t("tai rajaa tuotekategorialla")."</th><td>";
 
 				$monivalintalaatikot = array('DYNAAMINEN_TUOTE', 'OSASTO', 'TRY');
 				$monivalintalaatikot_normaali = array();
@@ -441,7 +456,7 @@
 			$lisa .= " and asiakas.tunnus = $asiakasid ";
 		}
 
-		if ($submit_button != "" and ($tuoteno > 0 or $asiakasid > 0 or $toimittajaid > 0 or $lisa != "" or $lisa_parametri != "" or $lisa_dynaaminen != "" or $liitostunnukset != "") and is_array($tilikaudetrow)) {
+		if ($submit_button != "" and ($tuoteno != "" or $asiakasid > 0 or $toimittajaid > 0 or $lisa != "" or $lisa_parametri != "" or $lisa_dynaaminen != "" or $liitostunnukset != "") and is_array($tilikaudetrow)) {
 
 			if (!@include('Spreadsheet/Excel/Writer.php')) {
 				echo "<font class='error'>",t("VIRHE: Pupe-asennuksesi ei tue Excel-kirjoitusta."),"</font><br>";
@@ -505,6 +520,10 @@
 				$lisa_dynaaminen = "";
 			}
 
+			if ($toim == "TUOTE" and $tuoteno != "") {
+				$lisa .= " and tuote.tuoteno='$tuoteno' ";
+			}
+
 			if ($toim == "TUOTE") {
 				$query = "	SELECT DISTINCT tuote.tuoteno, tuote.nimitys
 							FROM tuote
@@ -515,7 +534,7 @@
 							$lisa";
 			}
 			elseif ($toim == "TOIMITTAJA") {
-				$query = "	SELECT toimi.tunnus toimittajan_tunnus, toimi.ytunnus, toimi.ytunnus toimittajanro, toimi.nimi, toimi.nimitark
+				$query = "	SELECT DISTINCT toimi.tunnus toimittajan_tunnus, toimi.ytunnus, toimi.ytunnus toimittajanro, toimi.nimi, toimi.nimitark
 							FROM toimi
 							$lisa_parametri
 							$lisa_dynaaminen
@@ -523,7 +542,7 @@
 							$lisa";
 			}
 			elseif ($toim == "ASIAKAS") {
-				$query = "	SELECT asiakas.tunnus asiakkaan_tunnus, asiakas.ytunnus, asiakas.asiakasnro, asiakas.nimi, asiakas.nimitark,
+				$query = "	SELECT DISTINCT asiakas.tunnus asiakkaan_tunnus, asiakas.ytunnus, asiakas.asiakasnro, asiakas.nimi, asiakas.nimitark,
 							IF(STRCMP(TRIM(CONCAT(asiakas.toim_nimi, ' ', asiakas.toim_nimitark)), TRIM(CONCAT(asiakas.nimi, ' ', asiakas.nimitark))) != 0, asiakas.toim_nimi, '') toim_nimi,
 							IF(STRCMP(TRIM(CONCAT(asiakas.toim_nimi, ' ', asiakas.toim_nimitark)), TRIM(CONCAT(asiakas.nimi, ' ', asiakas.nimitark))) != 0, asiakas.toim_nimitark, '') toim_nimitark
 							FROM asiakas
@@ -536,20 +555,34 @@
 			$result = mysql_query($query) or pupe_error($query);
 
 			echo "<br />";
-			echo "<table>";
+
+			if ($tuoteryhmittain != "") {
+				// Haetaan tuoteryhm‰t
+				$res = t_avainsana("TRY");
+				$rivimaara = mysql_num_rows($res)*mysql_num_rows($result);
+			}
+			else {
+				$rivimaara = mysql_num_rows($result);
+			}
+
+			if ($rivimaara >= $maxrivimaara) {
+				echo "<br><font class='error'>".t("HUOM: Maksimirivim‰‰r‰ ylittyi, rivej‰ ei n‰ytet‰ ruudulla. Tallenna Excel-tiedosto")."!</font><br><br>";
+			}
+
+			if ($rivimaara < $maxrivimaara) echo "<table>";
 
 			if ($toim == "TUOTE") {
-				echo "<tr><th>",t("Tuote"),"</th>";
+				if ($rivimaara < $maxrivimaara) echo "<tr><th>",t("Tuote"),"</th>";
 			}
 			elseif ($toim == "TOIMITTAJA") {
-				echo "<tr><th>",t("Toimittaja"),"</th>";
+				if ($rivimaara < $maxrivimaara) echo "<tr><th>",t("Toimittaja"),"</th>";
 			}
 			elseif ($toim == "ASIAKAS") {
-				echo "<tr><th>",t("Asiakas"),"</th>";
+				if ($rivimaara < $maxrivimaara) echo "<tr><th>",t("Asiakas"),"</th>";
 			}
 
 			if ($tuoteryhmittain != "") {
-				echo "<th>",t("Tuoteryhm‰"),"</th>";
+				if ($rivimaara < $maxrivimaara) echo "<th>",t("Tuoteryhm‰"),"</th>";
 
 				$worksheet->write($excelrivi, $excelsarake, t("Tuoteryhm‰"), $format_bold);
 				$excelsarake++;
@@ -577,19 +610,19 @@
 
 				$raja = $vuosi."-".$kk;
 
-			 	echo "<th>$raja</th>";
+			 	if ($rivimaara < $maxrivimaara) echo "<th>$raja</th>";
 
 				$worksheet->write($excelrivi, $excelsarake, $raja, $format_bold);
 				$excelsarake++;
 			}
 
-			echo "</tr>";
+			if ($rivimaara < $maxrivimaara) echo "</tr>";
 
 			$excelrivi++;
 			$xx = 0;
 
 			function piirra_budj_rivi ($row, $tryrow = "") {
-				global $kukarow, $toim, $worksheet, $excelrivi, $budj_taulu, $rajataulu, $budj_taulunrivit, $xx, $budj_sarak, $sarakkeet;
+				global $kukarow, $toim, $worksheet, $excelrivi, $budj_taulu, $rajataulu, $budj_taulunrivit, $xx, $budj_sarak, $sarakkeet, $rivimaara, $maxrivimaara;
 
 				$excelsarake = 0;
 
@@ -609,16 +642,16 @@
 				if ($toim == "TUOTE") {
 					$worksheet->writeString($excelrivi, $excelsarake, $row['nimitys']);
 					$excelsarake++;
-					echo "<tr><td>$row[tuoteno] $row[nimitys]</td>";
+					if ($rivimaara < $maxrivimaara) echo "<tr><td>$row[tuoteno] $row[nimitys]</td>";
 				}
 				elseif ($toim == "ASIAKAS" or $toim == "TOIMITTAJA") {
 					$worksheet->writeString($excelrivi, $excelsarake, $row['nimi'].' '.$row['nimitark']);
 					$excelsarake++;
-					echo "<tr><td>$row[ytunnus] $row[asiakasnro]<br>$row[nimi] $row[nimitark]<br>$row[toim_nimi] $row[toim_nimitark]</td>";
+					if ($rivimaara < $maxrivimaara) echo "<tr><td>$row[ytunnus] $row[asiakasnro]<br>$row[nimi] $row[nimitark]<br>$row[toim_nimi] $row[toim_nimitark]</td>";
 				}
 
 				if (is_array($tryrow)) {
-					echo "<td>$tryrow[selite] $tryrow[selitetark]</td>";
+					if ($rivimaara < $maxrivimaara) echo "<td>$tryrow[selite] $tryrow[selitetark]</td>";
 
 					$worksheet->write($excelrivi, $excelsarake, $tryrow["selite"]);
 					$excelsarake++;
@@ -655,33 +688,28 @@
 						}
 					}
 
-					echo "<td>";
+					if ($rivimaara < $maxrivimaara) echo "<td>";
 
 					if (is_array($tryrow)) {
-						echo "<input type='text' name = 'luvut[{$row[$budj_sarak]}][{$ik}][{$tryrow["selite"]}]' value='{$nro}' size='8'></td>";
+						if ($rivimaara < $maxrivimaara) echo "<input type='text' name = 'luvut[{$row[$budj_sarak]}][{$ik}][{$tryrow["selite"]}]' value='{$nro}' size='8'></td>";
 					}
 					else {
-						echo "<input type='text' name = 'luvut[{$row[$budj_sarak]}][{$ik}][]' value='{$nro}' size='8'>";
+						if ($rivimaara < $maxrivimaara) echo "<input type='text' name = 'luvut[{$row[$budj_sarak]}][{$ik}][]' value='{$nro}' size='8'>";
 					}
 
-					echo "</td>";
+					if ($rivimaara < $maxrivimaara) echo "</td>";
 
 					$worksheet->write($excelrivi, $excelsarake, $nro);
 					$excelsarake++;
 				}
 
-				echo "</tr>";
+				if ($rivimaara < $maxrivimaara) echo "</tr>";
 
 				$xx++;
 				$excelrivi++;
 			}
 
-
 			if ($tuoteryhmittain != "") {
-				// Haetaan tuoteryhm‰t
-				$res = t_avainsana("TRY");
-
-
 				while ($tryrow = mysql_fetch_assoc($res)) {
 					while ($row = mysql_fetch_assoc($result)) {
 						piirra_budj_rivi($row, $tryrow);
@@ -698,7 +726,9 @@
 
 			$workbook->close();
 
-			echo "</table></form><br />";
+			if ($rivimaara < $maxrivimaara) echo "</table>";
+
+			echo "</form><br />";
 
 			echo "<form method='post'>";
 			echo "<input type='hidden' name='toim' value='$toim'>";

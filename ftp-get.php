503ced7b2fd810bf90fb4afb17a19ff22f934d29
jarmotorvinen <jamppa@devlab.fi>
2011-06-10 12:01:46 +0000

    FTP-GET muutos: - salasanat.php pitää muuttaa kaikille jotka käyttävät ftp-get.php:tä kaikki $ftpget_ - muuttujat pitää muuttaa seuraavanlaisiksi $ftpget_host = 'ftp.itella.net' MUOTOON $ftpget_host["itella"] = 'ftp.itella.net';

--- ftp-get.php
+++ ftp-get.php
@@ -13,8 +13,18 @@
 	mt_srand((float) $sec + ((float) $usec * 100000));
 	$tmpfile = "/tmp/ftpget-tmp-".md5(uniqid(mt_rand(), true)).".txt";
 
-	if (!isset($ftpget_email)) $ftpget_email = "devlab@devlab.fi"; 			# kenelle meilataan jos on ongelma
-	if (!isset($ftpget_emailfrom)) $ftpget_emailfrom = "devlab@devlab.fi"; 	# mill‰ osoitteella meili l‰hetet‰‰n
+	if (!isset($ftpget_email)) $ftpget_email = "development@devlab.fi"; 			# kenelle meilataan jos on ongelma
+	if (!isset($ftpget_emailfrom)) $ftpget_emailfrom = "development@devlab.fi"; 	# mill‰ osoitteella meili l‰hetet‰‰n
+	 
+	// ja operaattori komentorivilt‰
+	$operaattori = $argv[1]; 
+	// itella, servinet, yms
+	// pit‰‰ olla m‰‰ritettyn‰ salasanat.inc:iss‰ tai sitten t‰m‰ menee puihin.
+	
+	if ($operaattori == "") {
+		mail($ftpget_email,  mb_encode_mimeheader("VIRHE: FTP-get!", "ISO-8859-1", "Q"), "Tilausten sis‰‰nluvussa ongelma, ei operaattoria valittuna. Tutki asia!", "From: ".mb_encode_mimeheader("Pupesoft", "ISO-8859-1", "Q")." <$ftpget_emailfrom>\n", "-f $ftpget_emailfrom");
+		die;
+	} 
 
 	# jos lukkofaili lˆytyy, mutta se on yli 15 minsaa vanha niin dellatan se
 	if (@fopen($tmpfile, "r") !== FALSE) {
@@ -30,36 +40,36 @@
 
 	// tarvitaan $ftpget_host $ftpget_user $ftpget_pass $ftpget_path
 	// palautetaan $syy
-	if (@fopen($tmpfile, "r") === FALSE and $ftpget_host != '' and $ftpget_user != '' and $ftpget_pass != '' and $ftpget_path != '' and $ftpget_dest != '') {
+	if (@fopen($tmpfile, "r") === FALSE and $ftpget_host[$operaattori] != '' and $ftpget_user[$operaattori] != '' and $ftpget_pass[$operaattori] != '' and $ftpget_path[$operaattori] != '' and $ftpget_dest[$operaattori] != '') {
 
 		if ($filehandle = fopen($tmpfile, "w")) {
 			fwrite($filehandle, "All variables are set!\n");
 		}
 
 		//l‰hetet‰‰n tiedosto
-		$conn_id = ftp_connect($ftpget_host);
+		$conn_id = ftp_connect($ftpget_host[$operaattori]);
 
 		// jos connectio ok, kokeillaan loginata
 		if ($conn_id) {
-			fwrite($filehandle, "FTP-connect was successful, trying to login with $ftpget_user...\n");
-			$login_result = ftp_login($conn_id, $ftpget_user, $ftpget_pass);
+			fwrite($filehandle, "FTP-connect was successful, trying to login with $ftpget_user[$operaattori]...\n");
+			$login_result = ftp_login($conn_id, $ftpget_user[$operaattori], $ftpget_pass[$operaattori]);
 		}
 
 		// jos viimeinen merkki pathiss‰ ei ole kauttaviiva lis‰t‰‰n kauttaviiva...
-		if (substr($ftpget_path, -1) != "/") {
-			$ftpget_path .= "/";
+		if (substr($ftpget_path[$operaattori], -1) != "/") {
+			$ftpget_path[$operaattori] .= "/";
 		}
 
 		if ($login_result) {
-			fwrite($filehandle, "Login was successful! Trying to change directory to $ftpget_path...\n");
-			$changedir = ftp_chdir($conn_id, $ftpget_path);
+			fwrite($filehandle, "Login was successful! Trying to change directory to $ftpget_path[$operaattori]...\n");
+			$changedir = ftp_chdir($conn_id, $ftpget_path[$operaattori]);
 		}
 
 		// haetaan filet active modella
 		if ($changedir) {
 
 			if ($debug != '') {
-				fwrite($filehandle, "Successfully changed working directory to $ftpget_path!\n");
+				fwrite($filehandle, "Successfully changed working directory to $ftpget_path[$operaattori]!\n");
 				fwrite($filehandle, "Changing to passive mode.\n");
 			}
 
@@ -79,11 +89,11 @@
 
 					fwrite($filehandle, "File $file\n");
 
-					if (substr($ftpget_dest, -1) != "/") {
-						$ftpget_dest .= "/";
+					if (substr($ftpget_dest[$operaattori], -1) != "/") {
+						$ftpget_dest[$operaattori] .= "/";
 					}
 
-					$fileget = ftp_get($conn_id, $ftpget_dest.$file, $file, FTP_ASCII);
+					$fileget = ftp_get($conn_id, $ftpget_dest[$operaattori].$file, $file, FTP_ASCII);
 
 					if ($fileget) {
 						fwrite($filehandle, "File $file was successfully downloaded!\n");
@@ -99,6 +109,9 @@
 						fwrite($filehandle, "Failed to download file $file!\n");
 					}
 				}
+			}
+			else {
+				fwrite($filehandle, "We didnt have anyfiles to handle...\n");
 			}
 
 		}
@@ -129,16 +142,16 @@
 			// ncftpput:in exit valuet
 			switch ($palautus) {
 				case  1:
-					$syy = "Could not connect to remote host. ($ftpget_host)";
+					$syy = "Could not connect to remote host. ($ftpget_host[$operaattori])";
 					break;
 				case  2:
-					$syy = "Could not login to remote host ($conn_id, $ftpget_user, $ftpget_pass)";
+					$syy = "Could not login to remote host ($conn_id, $ftpget_user[$operaattori], $ftpget_pass[$operaattori])";
 					break;
 				case  3:
-					$syy = "Changedir failed ($conn_id, $ftpget_path, ".realpath($ftpget_path).")";
+					$syy = "Changedir failed ($conn_id, $ftpget_path[$operaattori], ".realpath($ftpget_path[$operaattori]).")";
 					break;
 				case  4:
-					$syy = "Getting files failed ($conn_id, $ftpget_path)";
+					$syy = "Getting files failed ($conn_id, $ftpget_path[$operaattori])";
 					break;
 				default:
 					$syy = t("Tuntematon errorkoodi")." ($palautus)!!";
@@ -148,5 +161,9 @@
 		}
 
 		unlink($tmpfile);
+	}
+	else {
+		mail($ftpget_email,  mb_encode_mimeheader("VIRHE: FTP-get!", "ISO-8859-1", "Q"), "Tilausten sis‰‰nluvussa saattaa olla ongelma. Jokin tarvittavista tiedoista on v‰‰rin (operaattori: $operaattori)", "From: ".mb_encode_mimeheader("Pupesoft", "ISO-8859-1", "Q")." <$ftpget_emailfrom>\n", "-f $ftpget_emailfrom");
+		
 	}
 ?>

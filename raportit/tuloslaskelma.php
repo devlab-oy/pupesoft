59d48a99f4b7b5d0957eb24d63c31e55c81bf3d9
Sami Luukkonen <sami@devlab.fi>
2011-09-12 14:00:22 +0300

    Tiliöintikohtainen asiakas-/toimittajaliitos tuki

--- raportit/tuloslaskelma.php
+++ raportit/tuloslaskelma.php
@@ -601,16 +601,16 @@
 				$laskujoini = " JOIN lasku ON tiliointi.yhtio = lasku.yhtio and tiliointi.ltunnus = lasku.tunnus ";
 
 				if ($konsernirajaus == "AT") {
-					$konsernijoini  = "	LEFT JOIN asiakas ka ON lasku.yhtio = ka.yhtio and lasku.liitostunnus = ka.tunnus and ka.konserniyhtio != ''
-										LEFT JOIN toimi kt ON lasku.yhtio = kt.yhtio and lasku.liitostunnus = kt.tunnus and kt.konserniyhtio != '' ";
+					$konsernijoini  = "	LEFT JOIN asiakas ka ON lasku.yhtio = ka.yhtio and ((lasku.liitostunnus = ka.tunnus) or (tiliointi.liitos='asiakas' and ka.tunnus = tiliointi.liitostunnus)) and ka.konserniyhtio != ''
+										LEFT JOIN toimi kt ON lasku.yhtio = kt.yhtio and ((lasku.liitostunnus = kt.tunnus) or (tiliointi.liitos='toimi' and kt.tunnus = tiliointi.liitostunnus)) and kt.konserniyhtio != '' ";
 					$konsernilisa = " and (ka.tunnus is not null or kt.tunnus is not null) ";
 				}
 				elseif ($konsernirajaus == "T") {
-					$konsernijoini = "  LEFT JOIN toimi kt ON lasku.yhtio = kt.yhtio and lasku.liitostunnus = kt.tunnus and kt.konserniyhtio != '' ";
+					$konsernijoini = "  LEFT JOIN toimi kt ON lasku.yhtio = kt.yhtio and ((lasku.liitostunnus = kt.tunnus) or (tiliointi.liitos='toimi' and kt.tunnus = tiliointi.liitostunnus)) and kt.konserniyhtio != '' ";
 					$konsernilisa = " and kt.tunnus is not null ";
 				}
 				elseif ($konsernirajaus == "A") {
-					$konsernijoini = "  LEFT JOIN asiakas ka ON lasku.yhtio = ka.yhtio and lasku.liitostunnus = ka.tunnus and ka.konserniyhtio != '' ";
+					$konsernijoini = "  LEFT JOIN asiakas ka ON lasku.yhtio = ka.yhtio and ((lasku.liitostunnus = ka.tunnus) or (tiliointi.liitos='asiakas' and ka.tunnus = tiliointi.liitostunnus)) and ka.konserniyhtio != '' ";
 					$konsernilisa = " and ka.tunnus is not null ";
 				}
 			}

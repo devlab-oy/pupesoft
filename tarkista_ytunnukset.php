33eeb37179c0ed1de35d396ac79ace9c1df4da5d
Joni Kanerva <joni@devlab.fi>
2011-09-22 14:41:50 +0300

    Koodia vähän siistitty.

--- tarkista_ytunnukset.php
+++ tarkista_ytunnukset.php
@@ -1,17 +1,17 @@
 <?php
-	require "inc/parametrit.inc";
 
-	js_popup();	
+	require ("inc/parametrit.inc");
+
+	js_popup();
 	enable_ajax();
-	
-		
+
 	echo "<font class='head'>".t("Tarkista asiakkaiden ja toimittajien ytunnukset")."</font><hr><br>";
 
-	//	Mikäli asiakkaalla on jokin y-tunnustarkistus voimassa, tarkistetaan, että asiakkaiden ytunnukset ovat sitä mitä pitäisi..	
-	if($yhtiorow["ytunnus_tarkistukset"] != "E") {
+	//	Mikäli asiakkaalla on jokin y-tunnustarkistus voimassa, tarkistetaan, että asiakkaiden ytunnukset ovat sitä mitä pitäisi..
+	if ($yhtiorow["ytunnus_tarkistukset"] != "E") {
 
 		$rajaus = "";
-		
+
 		$asiakasQuery = "	SELECT 'asiakas' taulu, ytunnus, nimi, tunnus
 							FROM asiakas
 							WHERE yhtio = '{$kukarow["yhtio"]}' and laji = ''";
@@ -19,8 +19,8 @@
 		$toimittajaQuery = "	SELECT 'toimi' taulu, ytunnus, nimi, tunnus
 								FROM toimi
 								WHERE yhtio = '{$kukarow["yhtio"]}'";
-							
-		if($yhtiorow["ytunnus_tarkistukset"] == "") {
+
+		if ($yhtiorow["ytunnus_tarkistukset"] == "") {
 			$query = "	(
 							$asiakasQuery
 						)
@@ -30,17 +30,19 @@
 						)
 						ORDER BY taulu";
 		}
-		elseif($yhtiorow["ytunnus_tarkistukset"] == "T") {
+		elseif ($yhtiorow["ytunnus_tarkistukset"] == "T") {
 			$query = $toimittajaQuery;
 		}
-		elseif($yhtiorow["ytunnus_tarkistukset"] == "A") {
+		elseif ($yhtiorow["ytunnus_tarkistukset"] == "A") {
 			$query = $asiakasQuery;
 		}
-		
+
 		echo "<font class='info'>".t("Tarkistetaan yritysasiakkaiden ja toimittajien ytunnukset yhtiön parametrien mukaisesti.")."</font><br><br>";
-		
+
 		$result = pupe_query($query) or pupe_error($query);
-		if(mysql_num_rows($result)>0) {
+
+		if (mysql_num_rows($result) > 0) {
+
 			echo "<table>
 					<tr>
 						<th>".t("Rekisteri")."</th>
@@ -48,12 +50,17 @@
 						<th>".t("Nimi")."</th>
 						<td class='back'></td>
 					</tr>";
-			while($row = mysql_fetch_assoc($result)) {
+
+			while ($row = mysql_fetch_assoc($result)) {
+
 				$onHetu = tarkistahetu($row["ytunnus"]);
 				$onYtunnus = tarkistaytunnus($row["ytunnus"]);
-				if( $row["taulu"] == "asiakas" and $onYtunnus===false or ($row["taulu"] == "toimi" and $onYtunnus===false and $onHetu === false) ) {
+
+				if ($row["taulu"] == "asiakas" and $onYtunnus === FALSE or ($row["taulu"] == "toimi" and $onYtunnus === FALSE and $onHetu === FALSE)) {
+
 					$muokkaanappi = "";
-					if($row["taulu"] == "asiakas") {
+
+					if ($row["taulu"] == "asiakas") {
 						$muokkaanappi = "
 							<form action='$palvelin2/yllapito.php' method='post'>
 								<input type='hidden' name='toim'		value='asiakas!!!TUKKUMYYNTI!!!true'>
@@ -63,7 +70,7 @@
 								<input type='submit' value='".t("Muokkaa asiakasta")."'>
 							</form>";
 					}
-					elseif($row["taulu"] == "toimi") {
+					elseif ($row["taulu"] == "toimi") {
 						$muokkaanappi = "
 							<form action='$palvelin2/yllapito.php' method='post'>
 								<input type='hidden' name='toim'		value='toimi'>
@@ -72,7 +79,6 @@
 								<input type='hidden' name='lopetus'		value='$PHP_SELF?'>
 								<input type='submit' value='".t("Muokkaa toimittajaa")."'>
 							</form>";
-						
 					}
 					echo "	<tr>
 								<td>{$row["taulu"]}</td>
@@ -82,16 +88,15 @@
 							</tr>";
 				}
 			}
-			
+
 			echo "</table>";
 		}
 		else {
 			echo "<font class='message'>".t("Tarkastettavia kohteita ei ole.")."</font>";
 		}
-		
 	}
 	else {
 		echo "<font class='message'>".t("Yrityksellä ei ole y-tunnusten tarkistus käytössä.")."</font>";
 	}
-	require "inc/footer.inc";
-?>
+
+	require("inc/footer.inc");

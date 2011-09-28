d4e8f5ddd4bdc519fde7cd8b7087fa8320bc10ab
Johan Tötterman <juppe@devlab.fi>
2011-09-28 09:33:32 +0300

    Limit 100 pois (jäi testauksesta)

--- tyomaarays/tyojono.php
+++ tyomaarays/tyojono.php
@@ -183,8 +183,7 @@
 				GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
 				$omattyot
 				$lisa2
-				ORDER BY prioriteetti, luontiaika ASC
-				LIMIT 100";
+				ORDER BY prioriteetti, luontiaika ASC";
 	$vresult = pupe_query($query);
 
 	$tyomaarays_tunti_yhteensa = array();

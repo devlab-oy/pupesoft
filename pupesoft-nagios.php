b8596d2b7996ddf6756627c9351cf3a186a2b544
Johan Tötterman <juppe@devlab.fi>
2011-11-11 12:47:09 +0200

    Muutama tsekki lisää!

--- pupesoft-nagios.php
+++ pupesoft-nagios.php
@@ -28,15 +28,15 @@ if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1' or $_SERVER['REMOTE_ADDR'] !== '::1'
 
 		// Nostetaan virhe jos tilanne on huolestuttava
 		if ($used_percentage >= 60 and $used_percentage < 80) {
-			echo "WARNING - Highest usage of available connections: {$used_percentage}% ({$max_used_connections}/{$max_connections})\n$STATE_WARNING";
+			echo "WARNING - Highest usage of available connections: {$used_percentage}% ({$max_used_connections}/{$max_connections})##$STATE_WARNING";
 			exit;
 		}
 		elseif ($used_percentage > 80) {
-			echo "CRITICAL - Highest usage of available connections: {$used_percentage}% ({$max_used_connections}/{$max_connections})\n$STATE_CRITICAL";
+			echo "CRITICAL - Highest usage of available connections: {$used_percentage}% ({$max_used_connections}/{$max_connections})##$STATE_CRITICAL";
 			exit;
 		}
 		else {
-			echo "OK - Highest usage of available connections: {$used_percentage}% ({$max_used_connections}/{$max_connections})\n$STATE_OK";
+			echo "OK - Highest usage of available connections: {$used_percentage}% ({$max_used_connections}/{$max_connections})##$STATE_OK";
 			exit;
 		}
 	}
@@ -44,16 +44,16 @@ if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1' or $_SERVER['REMOTE_ADDR'] !== '::1'
 	if ($_GET["tee"] == "MASTER_TO_SLAVE_CONNECTION") {
 
 		if (isset($slavedb[1]) and $slavedb[1] != "" and isset($slaveuser[1]) and $slaveuser[1] != "" and isset($slavepass[1]) and $slavepass[1] != "") {
-			$link = mysql_connect($slavedb[1], $slaveuser[1], $slavepass[1]) or die ("CRITICAL - Master cannot connect to slave\n$STATE_CRITICAL");
-			mysql_select_db($dbkanta) or die ("CRITICAL - Pupesoft-database not found on slave\n$STATE_CRITICAL");
+			$link = mysql_connect($slavedb[1], $slaveuser[1], $slavepass[1]) or die ("CRITICAL - Master cannot connect to slave##$STATE_CRITICAL");
+			mysql_select_db($dbkanta) or die ("CRITICAL - Pupesoft-database not found on slave##$STATE_CRITICAL");
 
 			if (is_resource($link)) {
-				echo "OK - Master connection to slave OK\n$STATE_OK";
+				echo "OK - Master connection to slave OK##$STATE_OK";
 				exit;
 			}
 		}
 		else {
-			echo "CRITICAL - Slave username/password/database not set\n$STATE_CRITICAL";
+			echo "CRITICAL - Slave username/password/database not set##$STATE_CRITICAL";
 			exit;
 		}
 	}
@@ -64,23 +64,23 @@ if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1' or $_SERVER['REMOTE_ADDR'] !== '::1'
 		$row = mysql_fetch_assoc($res);
 
 		if ($row["Slave_IO_Running"] != "Yes") {
-			echo "CRITICAL - Slave IO Not running\n$STATE_CRITICAL";
+			echo "CRITICAL - Slave IO Not running##$STATE_CRITICAL";
 			exit;
 		}
 		elseif ($row["Slave_SQL_Running"] != "Yes") {
-			echo "CRITICAL - Slave SQL Not running\n$STATE_CRITICAL";
+			echo "CRITICAL - Slave SQL Not running##$STATE_CRITICAL";
 			exit;
 		}
 		elseif ($row["Seconds_Behind_Master"] == "NULL" or $row["Seconds_Behind_Master"] > 600) {
-			echo "CRITICAL - Slave 10 minutes behind master\n$STATE_CRITICAL";
+			echo "CRITICAL - Slave 10 minutes behind master##$STATE_CRITICAL";
 			exit;
 		}
 		elseif ($row["Seconds_Behind_Master"] == "NULL" or $row["Seconds_Behind_Master"] > 300) {
-			echo "WARNING - Slave 5 minutes behind master\n$STATE_WARNING";
+			echo "WARNING - Slave 5 minutes behind master##$STATE_WARNING";
 			exit;
 		}
 		else {
-			echo "OK - Slave OK\n$STATE_OK";
+			echo "OK - Slave OK##$STATE_OK";
 			exit;
 		}
 	}

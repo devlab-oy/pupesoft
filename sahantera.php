e9fec50dff745cab3f2be2c0490b7de0d6a034c4
Joni Kanerva <joni@devlab.fi>
2011-11-17 17:24:02 +0200

    Koodilogiikka laitettu kuntoon.

--- sahantera.php
+++ sahantera.php
@@ -1,7 +1,8 @@
 <?php
+
 	require("inc/parametrit.inc");
 
-	echo "<font class='head'>".t("Sahanterä tulostusohjelma")."</font><hr>";
+	echo "<font class='head'>".t("Etiketin tulostus")."</font><hr>";
 
 	// Vakio lomake
 	$formi  = 'formi';
@@ -12,7 +13,6 @@
 	echo "<input type='hidden' name='toim' value='$toim'>";
 
 	echo "<table>";
-	echo "<tr><th colspan='3'><center>".t("Tulostetaan sahanterätarrat tuotenumeron mukaan")."</center></th><tr>";
 	echo "<tr>";
 	echo "<th>".t("Tuotenumero")."</th>";
 	echo "<th>".t("KPL")."</th>";
@@ -24,8 +24,8 @@
 	echo "<td><select name='kirjoitin'>";
 	echo "<option value=''>".t("Ei kirjoitinta")."</option>";
 
-	$query = "	SELECT * 
-				FROM kirjoittimet 
+	$query = "	SELECT *
+				FROM kirjoittimet
 				WHERE yhtio = '$kukarow[yhtio]'
 				and komento != 'email'
 				order by kirjoitin";
@@ -38,83 +38,92 @@
 	}
 
 	echo "</select></td>";
-	
+
 	echo "<td class='back'><input name='submit' type='submit' value='".t("Tulosta")."'></td>";
 	echo "</tr>";
 	echo "</table>";
 	echo "</form>";
+	echo "<br>";
 
-	if ($tee == "hae" and $submit) {
-		$query = "	SELECT * 
-					FROM tuote 
-					WHERE yhtio = '{$kukarow["yhtio"]}' 
+	// Virhetarkastukset
+	if ($tee == "hae") {
+
+		$query = "	SELECT tunnus
+					FROM tuote
+					WHERE yhtio = '{$kukarow["yhtio"]}'
 					and tuoteno = '{$tuoteno}'";
 		$result = pupe_query($query);
-		
+
 		if (mysql_num_rows($result) == 1) {
 			$trow = mysql_fetch_assoc($result);
-			$tee = "kirjoita";
+			$tee = "jatka";
 		}
 		else {
-			echo "<p class='error'>".t("Virhe: Ei löytynyt tuotetta tuoterekisteristä")."</p>";
-			die();
-		}
-		
-
-		if ($tee == "kirjoita" and $submit) {
-	
-			$filenimi = "/tmp/sahantera_tulostus.txt";
-			$hammastus = t_tuotteen_avainsanat($trow,'HAMMASTUS');
-			
-			$fh = fopen($filenimi, "w+");
-			$out = chr(10).chr(10).chr(10).chr(10); // 5 riviltä aloitetaan tulostus
-			$out .= sprintf ('%6s', ' '); // 4 merkkiä alusta niin saadaan oikeaan kohtaan aloittamaan tulostus.
-			$out .= sprintf ('%-9s', $trow["try"]);
-			$out .= sprintf ('%1s', ' ');
-			$out .= sprintf ('%-24s', round($trow["tuotekorkeus"],0)." x ".round($trow["tuoteleveys"],0)." x ".round($trow["tuotesyvyys"],2)); // pituus, leveys, paksuus = korkeus, leveys, syvyys
-			$out .= sprintf ('%2s', ' ');
-			$out .= sprintf ('%-8s', $hammastus); // hammastus, avainsanoista. 2 vaihtoehtoa: (3/4) tuumakoko, (16) mm-koko, hampaita tuumalle
-			$out .= chr(10).chr(10);
-			$out .= chr(13);
-			$out .= sprintf ('%16s', ' '); // 16 merkkiä alusta niin saadaan "nimitys / tuoteno" oikeaan paikkaan.
-			$out .= sprintf ('%-24s', $trow["tuoteno"]);
-			$out .= chr(10).chr(13);
-			$out .= chr(12);
-
-			if (!fwrite($fh, $out)) {
-				echo "<p class='error'>".t("Virhe: Tiedoston kirjoittaminen ei onnistunut")."</p>";
-				die();
-			}
-			else {
-				fclose($fh);
-				$tee = "tulosta";
-			}
-
+			echo "<font class='error'>".t("Virhe: Tuotetta ei löydy")."</font><br>";
+			$tee = "";
 		}
 
-		if ($tee == "tulosta" and $tulostakappale > 0 and $kirjoitin != "") {
-			$query = "	SELECT komento, kirjoitin
-						FROM kirjoittimet
-						WHERE yhtio = '{$kukarow["yhtio"]}'
-						AND tunnus = '{$kirjoitin}'";
-			$result = pupe_query($query);			
-			$krivi = mysql_fetch_assoc($result);
-			$komento = $krivi["komento"];
-			$tulostinjono = $krivi["kirjoitin"];
-			
-			for ($i=0; $i < $tulostakappale; $i++) { 
-				// tulostuskomento sitten tulostimelle
-				$line = exec("$komento -P $tulostinjono $filenimi");
-			}
-			
-			echo "<p class='ok'>".t("Tulostettiin tuotteelle")." {$trow["tuoteno"]} ".t("tarroja")." {$tulostakappale} ".t("kpl")."</p>";
-			
-		}
-		else {
-			// virheilmoitin:
-			echo "<p class='error'>".t("Virhe: Et valinnut tulostinta tai kpl määrä on tyhjä tai 0")."</p>";
+		$tulostakappale = (int) $tulostakappale;
 
+		if ($tulostakappale <= 0) {
+			echo "<font class='error'>".t("Virhe: Syötä kappalemäärä")."</font><br>";
+			$tee = "";
+		}
+
+		if ($kirjoitin == "") {
+			echo "<font class='error'>".t("Virhe: Valitse kirjoitin")."</font><br>";
+			$tee = "";
 		}
 	}
+
+	if ($tee == "jatka") {
+
+		$filenimi = "/tmp/sahantera_tulostus.txt";
+		$hammastus = t_tuotteen_avainsanat($trow, 'HAMMASTUS');
+
+		$out = chr(10).chr(10).chr(10).chr(10); 	// 5 riviltä aloitetaan tulostus
+		$out .= sprintf ('%6s', ' ');				// 4 merkkiä alusta niin saadaan oikeaan kohtaan aloittamaan tulostus.
+		$out .= sprintf ('%-9s', $trow["try"]);
+		$out .= sprintf ('%1s', ' ');
+		$out .= sprintf ('%-24s', round($trow["tuotekorkeus"],0)." x ".round($trow["tuoteleveys"],0)." x ".round($trow["tuotesyvyys"],2)); // pituus, leveys, paksuus = korkeus, leveys, syvyys
+		$out .= sprintf ('%2s', ' ');
+		$out .= sprintf ('%-8s', $hammastus);		// hammastus, avainsanoista. 2 vaihtoehtoa: (3/4) tuumakoko, (16) mm-koko, hampaita tuumalle
+		$out .= chr(10).chr(10);
+		$out .= chr(13);
+		$out .= sprintf ('%16s', ' ');				// 16 merkkiä alusta niin saadaan "nimitys / tuoteno" oikeaan paikkaan.
+		$out .= sprintf ('%-24s', $trow["tuoteno"]);
+		$out .= chr(10).chr(13);
+		$out .= chr(12);
+
+		$boob = file_put_contents($filenimi, $out);
+
+		if ($boob === FALSE) {
+			echo "<font class='error'>".t("Virhe: Tiedoston kirjoittaminen ei onnistunut")."</font><br>";
+			$tee = "";
+		}
+		else {
+			$tee = "tulosta";
+		}
+	}
+
+	if ($tee == "tulosta") {
+
+		$query = "	SELECT komento, kirjoitin
+					FROM kirjoittimet
+					WHERE yhtio = '{$kukarow["yhtio"]}'
+					AND tunnus = '{$kirjoitin}'";
+		$result = pupe_query($query);
+		$krivi = mysql_fetch_assoc($result);
+
+		for ($i = 0; $i < $tulostakappale; $i++) {
+			// Tulostetaan kirjoittimelle
+			$line = exec("{$krivi["komento"]} $filenimi");
+		}
+
+		echo t("Tulostetaan")." {$tulostakappale} ".t("tarraa")."...<br>";
+
+		// dellataan tmp file
+		unlink($filenimi);
+	}
+
 	require("inc/footer.inc");
-?>

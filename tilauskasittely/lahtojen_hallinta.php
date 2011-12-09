ec8fafdfcd9acdc919f4eed2e0c3e71291c8ea41
Sami Luukkonen <sami@devlab.fi>
2011-12-09 13:50:54 +0200

    Parempi tekstin vertailu.

--- tilauskasittely/lahtojen_hallinta.php
+++ tilauskasittely/lahtojen_hallinta.php
@@ -326,15 +326,10 @@
 
 					// tekstin vertailu
 					function compareName(a, b) {
-						if (b.id.toLowerCase() > a.id.toLowerCase()) {
-							return 1;
-						}
-						else if (b.id.toLowerCase() < a.id.toLowerCase()) {
-							return -1;
-						}
-						else {
-							return 0;
-						}
+						a = a.id.toString(), b = b.id.toString();
+						for (var i = 0, n = Math.max(a.length, b.length); i < n && a.charAt(i) === b.charAt(i); ++i);
+						if (i === n) return 0;
+						return a.charAt(i) > b.charAt(i) ? -1 : 1;
 					}
 
 					function compareDate(a, b) {

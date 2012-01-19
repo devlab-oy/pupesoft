48a0053b6e99c341a61b9c0b1a57f8c036187a69
Sami Luukkonen <sami@devlab.fi>
2012-01-19 13:31:47 +0200

    oletus_varasto mukaan ja käytetään uutta keräyserän funktiota.

--- optiscan.php
+++ optiscan.php
@@ -213,16 +213,17 @@
 		}
 		else {
 
-			$query = "	SELECT keraysvyohyke
+			$query = "	SELECT keraysvyohyke, oletus_varasto
 						FROM kuka
 						WHERE yhtio = '{$kukarow['yhtio']}'
 						AND kuka = '{$kukarow['kuka']}'
 						AND extranet = ''
-						AND keraysvyohyke != ''";
+						AND keraysvyohyke != ''
+						AND oletus_varasto != ''";
 			$result = mysql_query($query) or die("1, Tietokantayhteydessä virhe keräysvyöhykettä haettaessa\r\n\r\n");
 			$row = mysql_fetch_assoc($result);
 
-			$erat = tee_keraysera($row['keraysvyohyke']);
+			$erat = tee_keraysera2($row['keraysvyohyke'], $row['oletus_varasto']);
 
 			if (count($erat['tilaukset']) != 0) {
 				$otunnukset = implode(",", $erat['tilaukset']);

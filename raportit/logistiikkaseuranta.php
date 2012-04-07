8fba310fb1b22b75cc6bee84a620e81f626002f4
Johan Tötterman <juppe@devlab.fi>
2012-04-07 13:53:01 +0300

    Kehitys kehittyy!

--- raportit/logistiikkaseuranta.php
+++ raportit/logistiikkaseuranta.php
@@ -4,14 +4,13 @@
 
 	echo "<font class='head'>".t("Logistiikkaseuranta")."</font><hr>";
 
-
-	$query = "	SELECT tunnus, nimi, toimitustapa, tila, alatila, tilaustyyppi, toimitustavan_lahto
+	$query = "	SELECT tunnus, nimi, toimitustapa, tila, alatila, tilaustyyppi, toimitustavan_lahto, varasto
 				FROM lasku
 				WHERE yhtio = '{$kukarow['yhtio']}'
-				AND tila in ('N','L')
-				#and lasku.tunnus='6704434'
-				and luontiaika >= '2012-04-05 00:00:00'
-				and luontiaika <= '2012-04-05 23:59:59'
+				AND tila 	= 'L'
+				AND alatila in ('A','B','C','D','X')
+				and luontiaika >= '2012-04-01 00:00:00'
+				and luontiaika <= '2012-04-03 23:59:59'
 				ORDER BY tunnus";
 	$lasku_res = pupe_query($query);
 
@@ -60,14 +59,13 @@
 		$query = "	SELECT tilausrivi.*, keraysvyohyke.nimitys kervyohyke
 					FROM tilausrivi
 					JOIN tuote ON (tilausrivi.yhtio = tuote.yhtio and tilausrivi.tuoteno = tuote.tuoteno and tuote.ei_saldoa ='')
-					LEFT JOIN tilausrivin_lisatiedot ON (tilausrivi.yhtio = tilausrivin_lisatiedot.yhtio and tilausrivi.tunnus = tilausrivin_lisatiedot.tilausrivitunnus)
+					JOIN tilausrivin_lisatiedot ON (tilausrivi.yhtio = tilausrivin_lisatiedot.yhtio and tilausrivi.tunnus = tilausrivin_lisatiedot.tilausrivitunnus and tilausrivin_lisatiedot.ohita_kerays = '')
 					LEFT JOIN varaston_hyllypaikat ON (varaston_hyllypaikat.yhtio = tilausrivi.yhtio AND varaston_hyllypaikat.hyllyalue = tilausrivi.hyllyalue AND varaston_hyllypaikat.hyllynro = tilausrivi.hyllynro AND varaston_hyllypaikat.hyllyvali = tilausrivi.hyllyvali AND varaston_hyllypaikat.hyllytaso = tilausrivi.hyllytaso)
 					LEFT JOIN keraysvyohyke ON (varaston_hyllypaikat.yhtio = keraysvyohyke.yhtio AND varaston_hyllypaikat.keraysvyohyke = keraysvyohyke.tunnus)
 					WHERE tilausrivi.yhtio = '{$kukarow['yhtio']}'
 					and tilausrivi.otunnus = '{$laskurow['tunnus']}'
 					and (tilausrivi.varattu+tilausrivi.kpl) > 0
-					and (tilausrivin_lisatiedot.ohita_kerays is null or tilausrivin_lisatiedot.ohita_kerays = '')
-					ORDER BY tilausrivi.tuoteno";
+					ORDER BY tilausrivi.tunnus";
 		$tilausrivi_res = pupe_query($query);
 
 		$rivi .= "<tr>";
@@ -91,8 +89,10 @@
 		$rivi .= "<th style='font-size:10px; padding:1px; margin:0px;'>Kererä kerätty</th>";
 		$rivi .= "</tr>";
 
+		$ohita_kerays = array();
+
 		while ($tilausrivirow = mysql_fetch_assoc($tilausrivi_res)) {
-		
+
 			// Kerayserä/erät
 			$query = "	SELECT group_concat(luontiaika) luontiaika,
 						group_concat(nro) nro,
@@ -108,13 +108,41 @@
 
 			$pakkaus_kirjain = ($kerayserarow["pakkausnro"] > 0) ? chr(64+$kerayserarow["pakkausnro"]) : "";
 
+			// Tsekataan vielä ohita_kerays jutut!
+			if ($tilausrivirow['perheid'] > 0 and $tilausrivirow['tunnus'] != $tilausrivirow['perheid']) {
+
+				// haetaan isä
+				$query_isa = " SELECT tuoteno
+							   FROM tilausrivi
+							   WHERE yhtio = '{$kukarow['yhtio']}'
+							   AND tunnus  = '{$tilausrivirow['perheid']}'
+							   AND perheid = '{$tilausrivirow['perheid']}'";
+				$isa_chk_res = pupe_query($query_isa);
+				$isa_chk_row = mysql_fetch_assoc($isa_chk_res);
+
+				$query_ohita = " SELECT ohita_kerays
+								 FROM tuoteperhe
+								 WHERE yhtio 	= '{$kukarow['yhtio']}'
+								 AND tyyppi 	= 'P'
+								 AND isatuoteno = '{$isa_chk_row['tuoteno']}'
+								 AND tuoteno 	= '{$tilausrivirow['tuoteno']}'";
+				$ohita_chk_res = pupe_query($query_ohita);
+				$ohita_chk_row = mysql_fetch_assoc($ohita_chk_res);
+
+				if ($ohita_chk_row['ohita_kerays'] != '') {
+					$ohita_kerays[$tilausrivirow['tunnus']] = TRUE;
+				}
+			}
+
+			$kereraekotus = (isset($ohita_kerays[$tilausrivirow['tunnus']])) ? "KERÄYS OHITETEAAN" : $kerayserarow["nro"];
+
 			$rivi .= "<tr>";
 			$rivi .= "<td><a target='Tuotekysely' href='{$palvelin2}tuote.php?tee=Z&tuoteno=".urlencode($tilausrivirow["tuoteno"])."'>$tilausrivirow[tuoteno]</a></td>";
 			$rivi .= "<td>".substr($tilausrivirow["nimitys"],0,20)."</td>";
 			$rivi .= "<td align='right'>".(float) ($tilausrivirow["tilkpl"])."</td>";
 			$rivi .= "<td align='right'>".(float) ($tilausrivirow["varattu"]+$tilausrivirow["kpl"])."</td>";
 			$rivi .= "<td>$tilausrivirow[var]</td>";
-			$rivi .= "<td>$kerayserarow[nro]</td>";
+			$rivi .= "<td>$kereraekotus</td>";
 			$rivi .= "<td>$pakkaus_kirjain</td>";
 			$rivi .= "<td>$tilausrivirow[kervyohyke]</td>";
 			$rivi .= "<td>".tv1dateconv($tilausrivirow["laadittu"], "PITKA", "LYHYT")."</td>";
@@ -124,8 +152,8 @@
 			$rivi .= "<td align='right'>$kerayserarow[kpl]</td>";
 			$rivi .= "<td align='right'>$kerayserarow[kpl_keratty]</td>";
 			$rivi .= "</tr>";
-			
-			if ($tilausrivirow["kpl"] != 0 and $tilausrivirow["keratty"] == "") {
+
+			if ($tilausrivirow["kpl"] != 0 and $tilausrivirow["keratty"] == "" and !isset($ohita_kerays[$tilausrivirow['tunnus']])) {
 				$naytarivi = TRUE;
 			}
 		}
@@ -133,7 +161,6 @@
 		$rivi .= "</table>";
 		$rivi .= "</td>";
 		$rivi .= "</tr>";
-
 
 		// Rahtikirjan  tiedot kannasta
 		$rivi .= "<tr>";
@@ -164,7 +191,7 @@
 			$rivi .= "</tr>";
 		}
 
-	
+		/*
 		// Rahtikirjan  tiedot, nii ku ne pitäis olla
 		$rivi .= "<tr>";
 		$rivi .= "<th style='font-size:10px; padding:1px; margin:0px;'>Rahtikirja</th>";
@@ -177,6 +204,7 @@
 		$query = "	SELECT
 					kerayserat.nro,
 					IFNULL(pakkaus.pakkaus, 'MUU KOLLI') pakkaus,
+					IFNULL(pakkaus.pakkauskuvaus, 'MUU KOLLI') pakkauskuvaus,
 					IFNULL(pakkaus.oma_paino, 0) oma_paino,
 					IF(pakkaus.puukotuskerroin is not null and pakkaus.puukotuskerroin > 0, pakkaus.puukotuskerroin, 1) puukotuskerroin,
 					SUM(tuote.tuotemassa * kerayserat.kpl_keratty) tuotemassa,
@@ -206,10 +234,12 @@
 			$rivi .= "</tr>";
 		}
 
+		*/
+
 		$rivi .= "</table>";
 		$rivi .= "</td>";
 		$rivi .= "</tr>";
-		
+
 		// Välirivi
 		$rivi .= "<tr>";
 		$rivi .= "<td class='back' colspan='4' style='height:5px;'></td>";

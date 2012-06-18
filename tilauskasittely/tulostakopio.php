70e22504d3dc4a01d1a4c737311c08dc9063c284
Johan Tötterman <juppe@devlab.fi>
2012-06-18 14:37:27 +0300

    Poistettu if (function_exists("tulosta_lahete")) { if fit ja fiilistelty hieman kommentteja.

--- tilauskasittely/tulostakopio.php
+++ tilauskasittely/tulostakopio.php
@@ -1585,7 +1585,7 @@
 				$laskurow = mysql_fetch_assoc($result);
 
 				//haetaan asiakkaan tiedot
-				$query = "  SELECT lahetetyyppi, luokka, puhelin, if (asiakasnro!='', asiakasnro, ytunnus) asiakasnro
+				$query = "  SELECT luokka, puhelin, if (asiakasnro!='', asiakasnro, ytunnus) asiakasnro
 							FROM asiakas
 							WHERE tunnus = '$laskurow[liitostunnus]'
 							and yhtio = '$kukarow[yhtio]'";
@@ -1837,7 +1837,7 @@
 				$tilausnumeroita = $otunnus;
 
 				//haetaan asiakkaan tiedot
-				$query = "  SELECT lahetetyyppi, luokka, puhelin, if (asiakasnro!='', asiakasnro, ytunnus) asiakasnro
+				$query = "  SELECT luokka, puhelin, if (asiakasnro!='', asiakasnro, ytunnus) asiakasnro
 							FROM asiakas
 							WHERE tunnus='$laskurow[liitostunnus]' and yhtio='$kukarow[yhtio]'";
 				$result = pupe_query($query);

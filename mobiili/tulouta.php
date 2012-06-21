1054bc6966821f496b5861073b8d7aaca9ee0198
Sami Luukkonen <sami@devlab.fi>
2012-06-21 14:01:42 +0300

    Nappeja.

--- mobiili/tulouta.php
+++ mobiili/tulouta.php
@@ -3,6 +3,29 @@
 if (@include_once("../inc/parametrit.inc"));
 elseif (@include_once("inc/parametrit.inc"));
 
+if (isset($submit) and trim($submit) == 'cancel') {
+	require('paavalikko.php');
+	exit;
+}
+
+$error = array(
+	'tulotyyppi' => '',
+);
+
+if (isset($tulotyyppi) and trim($tulotyyppi) != '') {
+
+	if ($tulotyyppi == 'suuntalava') {
+		require("alusta.php");
+		exit;
+	}
+}
+
+if (isset($submit) and trim($submit) == 'submit') {
+	if ($tulotyyppi == '') $error['tulotyyppi'] = "<font class='error'>".t("Valitse tulotyyppi!")."</font>";
+}
+
+
+
 echo "
 <html>
 	<head>
@@ -13,15 +36,15 @@ echo "
 		A				{color: #c0c0c0; text-decoration:none;}
 		A:hover			{color: #ff0000; text-decoration:none;}
 		IMG				{padding:10pt;}
-		BODY		{background:#fff;}
+		BODY			{background:#fff;}
 		FONT.info		{font-size:8pt;  font-family:Lucida,Verdana,Helvetica,Arial; color: #c0c0c0;}
 		FONT.head		{font-size:15pt; font-family:Lucida,Verdana,Helvetica,Arial; color: #666699; font-weight:bold; letter-spacing: .05em;}
 		FONT.menu		{font-size:10pt; font-family:Lucida,Verdana,Helvetica,Arial; color: #666;}
-		FONT.error		{font-size:9pt;  font-family:Lucida,Verdana,Helvetica,Arial; color: #ff6666;}
+		FONT.error		{font-size:8pt;  font-family:Lucida,Verdana,Helvetica,Arial; color: #ff6666;}
 		TD				{padding:3pt; width:50%; height: 100%; -moz-border-radius: 5pt; -webkit-border-radius: 5pt; background: #eee}
-		TD.head, TD.menu {text-align: center;}
-		TABLE.tulouta	{padding:7pt; border-width: 1px 1px 1px 1px; /* top right bottom left */ border-style: solid; border-color: #a0a0a0; vertical-align: top; background: #eee; -moz-border-radius: 10pt; -webkit-border-radius: 10pt;}
-		TABLE.inner		{width: 100%; background: #eee}
+		TD.head, TD.menu, TD.error {text-align: center;}
+		TABLE.tulouta, TABLE.inner	{padding:7pt; border-width: 1px 1px 1px 1px; /* top right bottom left */ border-style: solid; border-color: #a0a0a0; vertical-align: top; background: #eee; -moz-border-radius: 10pt; -webkit-border-radius: 10pt;}
+		TABLE.inner		{width: 100%;}
 		INPUT, BUTTON	{font-size:10pt; width:100%}
 		SELECT			{width:100%}
 	-->
@@ -32,38 +55,41 @@ echo "
 	</head>
 
 	<body>
-
-		<table class='tulouta' border='0'>
-			<tr>
-				<td class='head'><font class='head'>",t("Tulouta", $browkieli),"</font><br><br>
-
-					<table class='inner'>
-						<tr>
-							<td class='menu'>
-								<font class='menu'>",t("Tulotyyppi", $browkieli),"</font>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<select size='4'>
-									<option>ASN / Suuntalava</option>
-								</selecT>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<button value=''>Suuntalavat</button>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<button value=''>OK</button>
-								<button value=''>Kesk</button>
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-		</table>
+		<form method='post' action=''>
+			<table class='tulouta' border='0'>
+				<tr>
+					<td class='head'><font class='head'>",t("Tulouta", $browkieli),"</font><br /><br />
+						<table class='inner'>
+							<tr>
+								<td class='menu'>
+									<font class='menu'>",t("Tulotyyppi", $browkieli),"</font>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<select name='tulotyyppi' size='4'>
+										<option value='suuntalava'>ASN / Suuntalava</option>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<button value=''>Suuntalavat</button>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<button name='submit' value='submit' onclick='submit();'>OK</button>
+									<button name='submit' value='cancel' onclick='submit();'>Kesk</button>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td class='error'>{$error['tulotyyppi']}</td>
+				</tr>
+			</table>
+		</form>
 	</body>
 </html>";

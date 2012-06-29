a89c19a813da9cf0791929157f538a7c90ab91ec
Sami Luukkonen <sami@devlab.fi>
2012-06-29 13:28:30 +0300

    Virhetarkistukset.

--- mobiili/muokkaa_suuntalavan_rivia.php
+++ mobiili/muokkaa_suuntalavan_rivia.php
@@ -12,6 +12,10 @@ $alusta_tunnus = (int) $alusta_tunnus;
 $liitostunnus = (int) $liitostunnus;
 $selected_row = (int) $selected_row;
 
+$error = array(
+	'rivi' => ''
+);
+
 if (isset($submit) and trim($submit) != '') {
 
 	$data = array(
@@ -29,6 +33,19 @@ if (isset($submit) and trim($submit) != '') {
 
 $res = suuntalavan_tuotteet(array($alusta_tunnus), $liitostunnus, "", "", "", $selected_row);
 $row = mysql_fetch_assoc($res);
+
+if (isset($submit) and trim($submit) == 'submit') {
+
+	if (!isset($maara)) {
+		$error['rivi'] = t("Syˆt‰ m‰‰r‰", $browkieli).'!';
+	}
+	elseif (!is_numeric($maara)) {
+		$error['rivi'] = t("M‰‰r‰ pit‰‰ olla numero", $browkieli).'!';
+	}
+	elseif ($maara < 1 or $maara >= $row['varattu']) {
+		$error['rivi'] = t("Sallitut m‰‰r‰t ovat", $browkieli).' 1 - '.($row['varattu'] - 1);
+	}
+}
 
 echo "
 	<style type='text/css'>
@@ -63,7 +80,7 @@ echo "
 					</tr>
 					<tr>
 						<td nowrap>
-							<button name='submit' value='submit' onclick='submit();'>",t("Valitse", $browkieli),"</button>
+							<button name='submit' value='submit' onclick='submit();'>",t("Pois suuntalavalta", $browkieli),"</button>
 						</td>
 						<td nowrap>
 							<button name='submit' value='cancel' onclick='submit();'>",t("Takaisin", $browkieli),"</button>
@@ -72,7 +89,9 @@ echo "
 							&nbsp;
 						</td>
 					</tr>
+					<tr><td>&nbsp;</td></tr>
 				</table>
+				<span class='error'>{$error['rivi']}</span>
 				<input type='hidden' name='alusta_tunnus' value='{$alusta_tunnus}' />
 				<input type='hidden' name='liitostunnus' value='{$liitostunnus}' />
 				<input type='hidden' name='selected_row' value='{$selected_row}' />

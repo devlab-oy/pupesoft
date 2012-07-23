654063a87974c16ed4d855a086c1ae0bfa6656b3
Antti Jäppinen <antti@devlab.fi>
2012-07-23 15:35:07 +0300

    Uusi keräyspaikka toimintaa
    Käyttöliittymän muutos ja logiikan korjailuja.

--- mobiili/uusi_kerayspaikka.php
+++ mobiili/uusi_kerayspaikka.php
@@ -1,4 +1,5 @@
 <?php
+echo "<meta name='viewport' content='width=device-width,height=device-height, user-scalable=no'/>";
 
 $_GET['ohje'] = 'off';
 $_GET["no_css"] = 'yes';
@@ -35,7 +36,7 @@ if (isset($submit) and trim($submit) != '') {
 $res = suuntalavan_tuotteet(array($alusta_tunnus), $liitostunnus, "", "", "", $selected_row);
 $row = mysql_fetch_assoc($res);
 
-$oletuspaikka_chk = "checked";
+$oletuspaikka_chk = "";
 
 $onko_suoratoimitus_res = onko_suoratoimitus($selected_row);
 
@@ -45,28 +46,62 @@ if ($row_suoratoimitus = mysql_fetch_assoc($onko_suoratoimitus_res)) {
 
 if (isset($submit) and trim($submit) != '' and $submit == 'submit') {
 
+	# Tarkista että hyllypaikka on olemmassa
 	$kaikki_ok = tarkista_varaston_hyllypaikka($hyllyalue, $hyllynro, $hyllyvali, $hyllytaso);
-	var_dump($kaikki_ok);
+	$kaikki_ok = true;
+
 	if ($hyllyalue == '' and $hyllynro == '' and $hyllyvali == '' and $hyllytaso == '') {
 		$error['kerayspaikka'] = t("Hyllypaikka ei saa olla tyhjä", $browkieli).'.';
 	}
 	elseif ($kaikki_ok) {
 
-		$oletus = $oletuspaikka_chk != '' ? 'X' : '';
+		$oletus = $oletuspaikka != '' ? 'X' : '';
 
-		#lisaa_tuotepaikka($row['tuoteno'], $row['hyllyalue'], $row['hyllynro'], $row['hyllyvali'], $row['hyllytaso'], 'Saapumisessa', $oletus);
-		#lisaa_tuotepaikka($row['tuoteno'], $hyllyalue, $hyllynro, $hyllyvali, $hyllytaso, 'Saapumisessa', $oletus);
+		$hylly = array(
+				"hyllyalue" => $hyllyalue,
+				"hyllynro" 	=> $hyllynro,
+				"hyllyvali" => $hyllyvali,
+				"hyllytaso" => $hyllytaso
+			);
 
-		$hylly = implode(",", array($hyllyalue, $hyllynro, $hyllyvali, $hyllytaso));
+		# Tarkistetaan onko syötetty hyllypaikka jo tälle tuotteelle
+		$tuotteen_oma_hyllypaikka = "	SELECT * FROM tuotepaikat
+										WHERE tuoteno='$row[tuoteno]'
+										AND hyllyalue='$hyllyalue'
+										AND hyllynro='$hyllynro'
+										AND hyllyvali='$hyllyvali'
+										AND hyllytaso='$hyllytaso'";
+		$oma_paikka = mysql_query($tuotteen_oma_hyllypaikka);
 
-		echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=vahvista_kerayspaikka.php?{$url}&hylly=$hylly'>";
+		# Jos syötettyä paikkaa ei ole tämän tuotteen, lisätään uusi tuotepaikka
+		if (mysql_num_rows($oma_paikka) == 0) {
+			#lisaa_tuotepaikka($row['tuoteno'], $row['hyllyalue'], $row['hyllynro'], $row['hyllyvali'], $row['hyllytaso'], 'Saapumisessa', $oletus);
+			echo "lisaa_tuotepaikka!<br>";
+			lisaa_tuotepaikka($row['tuoteno'], $hyllyalue, $hyllynro, $hyllyvali, $hyllytaso, 'Saapumisessa', $oletus);
+		}
+
+		# Päivitetään oletuspaikat jos tehdään tästä oletuspaikka
+		if($oletus != '') {
+			# Asetetaan oletuspaikka uusiksi
+			echo "Oletuspaikka uusiksi!";
+			$paivitetty_paikka = paivita_oletuspaikka($row['tuoteno'], $hylly);
+			echo "Paivitetty paikka: ".$paivitetty_paikka;
+		}
+
+		# Asetetaan tuotepaikka tilausriville
+		paivita_tilausrivin_hylly($selected_row, $hylly);
+		echo "päivitetään tilausrivin hyllypaikka";
+
+		# Palataan edelliselle sivulle
+		echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=vahvista_kerayspaikka.php?{$url}'>";
 		exit;
 	}
 	else {
-		$error['kerayspaikka'] = t("Varaston keräyspaikkaa ei ole perustettu", $browkieli).'.';
+		$error['kerayspaikka'] = t("Varaston tuotepaikkaa ei ole perustettu", $browkieli).'.';
 	}
 }
 
+include("kasipaate.css");
 echo "
 	<style type='text/css'>
 	<!--
@@ -77,7 +112,7 @@ echo "
 
 	<table border='0'>
 		<tr>
-			<td><h1>",t("Uusi keräyspaikka", $browkieli),"</h1>
+			<td><h1>",t("UUSI KERÄYSPAIKKA", $browkieli),"</h1>
 				<form name='uusipaikkaformi' method='post' action=''>
 				<table>
 					<tr>
@@ -94,15 +129,24 @@ echo "
 					</tr>
 					<tr>
 						<td>",t("Alue", $browkieli),"</td>
-						<td>",t("Nro", $browkieli),"</td>
-						<td>",t("Väli", $browkieli),"</td>
-						<td>",t("Taso", $browkieli),"</td>
+						<td><input type='text' name='hyllyalue' value='' /></td>
 					</tr>
 					<tr>
-						<td><input type='text' name='hyllyalue' value='' /></td>
+						<td>",t("Nro", $browkieli),"</td>
 						<td><input type='text' name='hyllynro' value='' /></td>
+					</tr>
+						<td>",t("Väli", $browkieli),"</td>
 						<td><input type='text' name='hyllyvali' value='' /></td>
+					<tr>
+					</tr>
+					<tr>
+						<td>",t("Taso", $browkieli),"</td>
 						<td><input type='text' name='hyllytaso' value='' /></td>
+					</tr>
+					</table>
+					<table>
+					<tr>
+						<td colspan='2'>",t("Tee tästä oletuspaikka", $browkieli)," <input type='checkbox' name='oletuspaikka' checked /></td>
 					</tr>
 					<tr>
 						<td nowrap>
@@ -111,7 +155,6 @@ echo "
 						<td nowrap>
 							<button name='submit' value='cancel' onclick='submit();'>",t("Takaisin", $browkieli),"</button>
 						</td>
-						<td colspan='2'>",t("Tee tästä oletuspaikka", $browkieli)," <input type='checkbox' name='oletuspaikka' checked='{$oletuspaikka_chk}' /></td>
 					</tr>
 					<tr><td>&nbsp;</td></tr>
 				</table>
@@ -123,6 +166,5 @@ echo "
 			</td>
 		</tr>
 	</table>";
-var_dump($_REQUEST);
 
-require('inc/footer.inc');
+#require('inc/footer.inc');

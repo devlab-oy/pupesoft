df80651ed92110ae5633748f91d1f4d4b2894cd8
Johan Tötterman <juppe@devlab.fi>
2012-08-07 11:20:24 +0300

    Käytetään alkuperäisen tilauksen luontiaikaa tilauspäivämääränä.

--- tilauskasittely/verkkolasku.php
+++ tilauskasittely/verkkolasku.php
@@ -2135,6 +2135,7 @@
 										lasku.vienti_kurssi,
 										lasku.viesti laskuviesti,
 										lasku.asiakkaan_tilausnumero,
+										lasku.luontiaika tilauspaiva,
 										if (tuote.tuotetyyppi = 'K','2 Työt','1 Muut') tuotetyyppi,
 										if (tilausrivi.var2 = 'EIOST', 'EIOST', '') var2,
 										if (tuote.myyntihinta_maara = 0, 1, tuote.myyntihinta_maara) myyntihinta_maara,
@@ -2165,7 +2166,7 @@
 										and (tilausrivi.perheid = 0 or tilausrivi.perheid=tilausrivi.tunnus or tilausrivin_lisatiedot.ei_nayteta !='E' or tilausrivin_lisatiedot.ei_nayteta is null)
 										and tilausrivi.kpl != 0
 										and tilausrivi.otunnus in ($tunnukset)
-										GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
+										GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
 										ORDER BY tilausrivi.otunnus, if(tilausrivi.tuoteno in ('$yhtiorow[kuljetusvakuutus_tuotenumero]','$yhtiorow[laskutuslisa_tuotenumero]'), 2, 1), $pjat_sortlisa sorttauskentta $order_sorttaus, tilausrivi.tunnus";
 							$tilres = pupe_query($query);
 

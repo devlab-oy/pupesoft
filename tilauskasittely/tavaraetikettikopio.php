43242710c649e32421cb1d1003ee37a3252b218a
Sami Luukkonen <sami@devlab.fi>
2012-08-07 11:35:18 +0300

    LEFT JOIN kuka.

--- tilauskasittely/tavaraetikettikopio.php
+++ tilauskasittely/tavaraetikettikopio.php
@@ -79,12 +79,12 @@
 					lasku.ytunnus, lasku.tunnus AS otunnus,
 					TRIM(CONCAT(lasku.nimi, ' ', lasku.nimitark)) AS nimi,
 					suuntalavat_saapuminen.saapuminen, lasku.laskunro,
-					lasku.luontiaika, kuka.nimi AS laatija
+					lasku.luontiaika, IF(kuka.nimi != '', kuka.nimi, lasku.laatija) AS laatija
 					FROM suuntalavat
 					JOIN suuntalavat_saapuminen ON (suuntalavat_saapuminen.yhtio = suuntalavat.yhtio AND suuntalavat_saapuminen.suuntalava = suuntalavat.tunnus)
 					JOIN lasku ON (lasku.yhtio = suuntalavat_saapuminen.yhtio AND lasku.tunnus = suuntalavat_saapuminen.saapuminen AND lasku.tila = 'K')
 					JOIN toimi ON (toimi.yhtio = lasku.yhtio AND toimi.tunnus = lasku.liitostunnus)
-					JOIN kuka ON (kuka.yhtio = lasku.yhtio AND kuka.kuka = lasku.laatija)
+					LEFT JOIN kuka ON (kuka.yhtio = lasku.yhtio AND kuka.kuka = lasku.laatija)
 					WHERE suuntalavat.yhtio = '{$kukarow['yhtio']}'
 					AND suuntalavat.tila = 'P'
 					AND suuntalavat.sscc = '{$sscc}'
@@ -111,7 +111,7 @@
 				echo "<td>{$suuntalava_row['toimittajanro']}</td>";
 				echo "<td>{$suuntalava_row['ytunnus']}</td>";
 				echo "<td>{$suuntalava_row['nimi']}</td>";
-				echo "<td>{$suuntalava_row['laskunro']} ($suuntalava_row[tunnus])</td>";
+				echo "<td>{$suuntalava_row['laskunro']}</td>";
 				echo "<td>",tv1dateconv($suuntalava_row['luontiaika']),"</td>";
 				echo "<td>{$suuntalava_row['laatija']}</td>";
 

f8a4e50090ced8f9e3384ce2a97d438903bb52f9
Antti Jäppinen <antti@devlab.fi>
2012-08-16 16:31:16 +0300

    Uusi keräyspaikka rakennemuutos

--- mobiili/uusi_kerayspaikka.php
+++ mobiili/uusi_kerayspaikka.php
@@ -12,31 +12,22 @@ $alusta_tunnus = (int) $alusta_tunnus;
 $liitostunnus = (int) $liitostunnus;
 $tilausrivi = (int) $tilausrivi;
 
-$error = array(
-	'kerayspaikka' => ''
+$data = array(
+	'alusta_tunnus' => $alusta_tunnus,
+	'liitostunnus' => $liitostunnus,
+	'tilausrivi' => $tilausrivi
 );
+$url = http_build_query($data);
 
-if (isset($submit) and trim($submit) != '') {
+# Virheet
+$errors = array();
 
-	$data = array(
-		'alusta_tunnus' => $alusta_tunnus,
-		'liitostunnus' => $liitostunnus,
-		'tilausrivi' => $tilausrivi
-	);
-
-	$url = http_build_query($data);
-
-	if ($submit == 'cancel') {
-		echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=vahvista_kerayspaikka.php?{$url}'>";
-		exit;
-	}
-}
-
-# Jos suuntalava tiedetään
+# Suuntalavan kanssa
 if (!empty($alusta_tunnus)) {
 	$res = suuntalavan_tuotteet(array($alusta_tunnus), $liitostunnus, "", "", "", $tilausrivi);
 	$row = mysql_fetch_assoc($res);
 }
+# Ilman suuntalavaa
 else {
 	$query = "	SELECT
 				tilausrivi.*,
@@ -48,6 +39,69 @@ else {
 	$row = mysql_fetch_assoc(pupe_query($query));
 }
 
+if (isset($submit) and trim($submit) != '') {
+
+	switch ($submit) {
+		case 'cancel':
+			#echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=hyllytys.php?{$url}'>";
+			echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=vahvista_kerayspaikka.php?{$url}'>";
+			exit;
+			break;
+
+		case 'submit':
+
+			# Ei saa olla tyhjiä kenttiä
+			if ($hyllyalue == '' or $hyllynro == '' or $hyllyvali == '' or $hyllytaso == '') {
+				$errors[] = t("Hyllypaikka ei saa olla tyhjä", $browkieli).'.';
+			}
+			# Tarkistetaan että tuotepaikka on olemassa
+			if (!tarkista_varaston_hyllypaikka($hyllyalue, $hyllynro, $hyllyvali, $hyllytaso)) {
+				$errors[] = t("Varaston tuotepaikkaa ei ole perustettu", $browkieli).'.';
+			}
+			if (count($errors) == 0) {
+				$oletus = $oletuspaikka != '' ? 'X' : '';
+
+				$hylly = array(
+						"hyllyalue" => $hyllyalue,
+						"hyllynro" 	=> $hyllynro,
+						"hyllyvali" => $hyllyvali,
+						"hyllytaso" => $hyllytaso
+					);
+
+				# Tarkistetaan onko syötetty hyllypaikka jo tälle tuotteelle
+				$tuotteen_oma_hyllypaikka = "	SELECT * FROM tuotepaikat
+												WHERE tuoteno='$row[tuoteno]'
+												AND hyllyalue='$hyllyalue'
+												AND hyllynro='$hyllynro'
+												AND hyllyvali='$hyllyvali'
+												AND hyllytaso='$hyllytaso'";
+				$oma_paikka = mysql_query($tuotteen_oma_hyllypaikka);
+
+				# Jos syötettyä paikkaa ei ole tämän tuotteen, lisätään uusi tuotepaikka
+				if (mysql_num_rows($oma_paikka) == 0) {
+					#lisaa_tuotepaikka($row['tuoteno'], $row['hyllyalue'], $row['hyllynro'], $row['hyllyvali'], $row['hyllytaso'], 'Saapumisessa', $oletus);
+					lisaa_tuotepaikka($row['tuoteno'], $hyllyalue, $hyllynro, $hyllyvali, $hyllytaso, 'Saapumisessa', $oletus);
+				}
+
+				# Päivitetään oletuspaikat jos tehdään tästä oletuspaikka
+				if($oletus != '') {
+					# Asetetaan oletuspaikka uusiksi
+					$paivitetty_paikka = paivita_oletuspaikka($row['tuoteno'], $hylly);
+				}
+
+				# Asetetaan tuotepaikka tilausriville
+				paivita_tilausrivin_hylly($tilausrivi, $hylly);
+
+				# Palataan edelliselle sivulle
+					echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=vahvista_kerayspaikka.php?{$url}'>";
+				exit;
+			}
+
+			break;
+	}
+
+}
+
 $oletuspaikka_chk = "checked";
 
 $onko_suoratoimitus_res = onko_suoratoimitus($tilausrivi);
@@ -56,62 +110,21 @@ if ($row_suoratoimitus = mysql_fetch_assoc($onko_suoratoimitus_res)) {
 	if ($row_suoratoimitus["suoraan_laskutukseen"] == "") $oletuspaikka_chk = '';
 }
 
-if (isset($submit) and trim($submit) != '' and $submit == 'submit') {
-
-	# Ei saa olla tyhjiä kenttiä
-	if ($hyllyalue == '' or $hyllynro == '' or $hyllyvali == '' or $hyllytaso == '') {
-		$error['kerayspaikka'] = t("Hyllypaikka ei saa olla tyhjä", $browkieli).'.';
-	}
-	elseif ($hylly_ok = tarkista_varaston_hyllypaikka($hyllyalue, $hyllynro, $hyllyvali, $hyllytaso)) {
-		$oletus = $oletuspaikka != '' ? 'X' : '';
-
-		$hylly = array(
-				"hyllyalue" => $hyllyalue,
-				"hyllynro" 	=> $hyllynro,
-				"hyllyvali" => $hyllyvali,
-				"hyllytaso" => $hyllytaso
-			);
-
-		# Tarkistetaan onko syötetty hyllypaikka jo tälle tuotteelle
-		$tuotteen_oma_hyllypaikka = "	SELECT * FROM tuotepaikat
-										WHERE tuoteno='$row[tuoteno]'
-										AND hyllyalue='$hyllyalue'
-										AND hyllynro='$hyllynro'
-										AND hyllyvali='$hyllyvali'
-										AND hyllytaso='$hyllytaso'";
-		$oma_paikka = mysql_query($tuotteen_oma_hyllypaikka);
-
-		# Jos syötettyä paikkaa ei ole tämän tuotteen, lisätään uusi tuotepaikka
-		if (mysql_num_rows($oma_paikka) == 0) {
-			#lisaa_tuotepaikka($row['tuoteno'], $row['hyllyalue'], $row['hyllynro'], $row['hyllyvali'], $row['hyllytaso'], 'Saapumisessa', $oletus);
-			lisaa_tuotepaikka($row['tuoteno'], $hyllyalue, $hyllynro, $hyllyvali, $hyllytaso, 'Saapumisessa', $oletus);
-		}
-
-		# Päivitetään oletuspaikat jos tehdään tästä oletuspaikka
-		if($oletus != '') {
-			# Asetetaan oletuspaikka uusiksi
-			$paivitetty_paikka = paivita_oletuspaikka($row['tuoteno'], $hylly);
-		}
-
-		# Asetetaan tuotepaikka tilausriville
-		paivita_tilausrivin_hylly($tilausrivi, $hylly);
-
-		# Palataan edelliselle sivulle
-		echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=vahvista_kerayspaikka.php?{$url}'>";
-		exit;
-	}
-	else {
-		$error['kerayspaikka'] = t("Varaston tuotepaikkaa ei ole perustettu", $browkieli).'.';
-	}
-}
+####
 
 include("kasipaate.css");
 
 echo "<div class='header'><h1>",t("UUSI KERÄYSPAIKKA", $browkieli),"</h1></div>";
-echo "<span class='error'>{$error['kerayspaikka']}</span>";
 
+# Virheet
+if (isset($errors)) {
+	echo "<span class='error'>";
+	foreach($errors as $virhe) {
+		echo "{$virhe}<br>";
+	}
+	echo "</span>";
+}
 echo "<div class='main'>
-
 <form name='uusipaikkaformi' method='post' action=''>
 	<table>
 		<tr>

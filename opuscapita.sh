2df017bdd0d638c9e3b64c46a4e58f546c1ca64d
Joni Kanerva <joni@devlab.fi>
2012-08-20 16:12:43 +0300

    Params toistepäin

--- opuscapita.sh
+++ opuscapita.sh
@@ -16,14 +16,14 @@ if [[ ! -f /tmp/opuscapita.lock ]]; then
   send_local_dir_ok="/home/opuscapita/maksuaineisto/ok/"
 
   # Loopataan läpi kaikki filet remote hakemistosta ja haetaan ne local dirikkaan
-  for file in `ssh ${get_hostname} "find ${get_remote_dir} -type f -maxdepth 1"`
+  for file in `ssh ${get_hostname} "find ${get_remote_dir} -maxdepth 1 -type f"`
   do
     # Kopioidaan tiedosto lokaalidirikkaan, jos siirto onnistuu poistetaan remote file
     scp ${get_hostname}:${file} ${get_local_dir} > /dev/null && ssh ${get_hostname} "rm -f ${file}" > /dev/null
   done
 
   # Loopataan läpi kaikki filet local hakemistosta ja lähetetään ne remote hakemistoon
-  for file in `find ${send_local_dir} -type f -maxdepth 1`
+  for file in `find ${send_local_dir} -maxdepth 1 -type f`
   do
     # Kopioidaan tiedosto remote dirikkaan, jos siirto onnistuu siirretään local file ok-hakemistoon
     scp ${file} ${send_hostname}:${send_remote_dir} > /dev/null && mv ${file} ${send_local_dir_ok}

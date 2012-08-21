8786171922099b0975dcf867b125f87b503a55fa
Sami Luukkonen <sami@devlab.fi>
2012-08-21 16:45:44 +0300

    Osoitelappukorjaus.

--- rahtikirja-tulostus.php
+++ rahtikirja-tulostus.php
@@ -717,17 +717,15 @@
 						$viestirar = pupe_query($query);
 						$viestirarrow = mysql_fetch_assoc($viestirar);
 
-						for ($s=1; $s <= $kollityht; $s++) {
-							if (($toitarow["tulostustapa"] == "L" or $toitarow["tulostustapa"] == "K") and $toitarow["toim_nimi"] != '') {
-								$tiedot = "toimitusta";
-							}
+						if (($toitarow["tulostustapa"] == "L" or $toitarow["tulostustapa"] == "K") and $toitarow["toim_nimi"] != '') {
+							$tiedot = "toimitusta";
+						}
 
-							if ($toitarow['osoitelappu'] == 'intrade') {
-								require('tilauskasittely/osoitelappu_intrade_pdf.inc');
-							}
-							else {
-								require ("tilauskasittely/osoitelappu_pdf.inc");
-							}
+						if ($toitarow['osoitelappu'] == 'intrade') {
+							require('tilauskasittely/osoitelappu_intrade_pdf.inc');
+						}
+						else {
+							require ("tilauskasittely/osoitelappu_pdf.inc");
 						}
 					}
 				}
@@ -1079,5 +1077,3 @@
 
 		require("inc/footer.inc");
 	}
-
-?>

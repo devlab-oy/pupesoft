9933ebb6ae47f8f3d696fb9f4f73666637aad60d
Antti Jäppinen <antti@devlab.fi>
2012-08-29 13:43:07 +0300

    Varmistuskysymys uuden saapumisen luontiin
    Ostotilausten tuloutus ilman saapumista varmistaa käyttäjältä uuden saapumisen luonnin.

--- mobiili/tuotteella_useita_tilauksia.php
+++ mobiili/tuotteella_useita_tilauksia.php
@@ -11,26 +11,6 @@ elseif (@include_once("inc/parametrit.inc"));
 
 if(!isset($errors)) $errors = array();
 
-# Submit
-if (isset($submit)) {
-	switch($submit) {
-		case 'ok':
-			$url_array['ostotilaus'] = $ostotilaus;
-			$url_array['tilausrivi'] = $tilausrivi;
-			$url_array['saapuminen'] = $saapuminen;
-
-			echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=hyllytys.php?".http_build_query($url_array)."'>"; exit();
-
-			break;
-		case 'cancel':
-			echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=ostotilaus.php?ostotilaus=$ostotilaus'>"; exit();
-		   	break;
-		default:
-			echo "Virhe";
-			break;
-	}
-}
-
 # Joku parametri tarvii olla setattu.
 if ($ostotilaus != '' or $tuotenumero != '' or $viivakoodi != '') {
 
@@ -95,35 +75,59 @@ $result = pupe_query($query);
 $tilausten_lukumaara = mysql_num_rows($result);
 $tilaukset = mysql_fetch_assoc($result);
 
+# Submit
+if (isset($submit)) {
+	switch($submit) {
+		case 'ok':
+
+			if(empty($tilausrivi)) {
+				$errors[] = "Valitse rivi";
+				break;
+			}
+			# Jos kuka.kesken ei ole saapuminen, tehd‰‰n uusi saapuminen haettujen ostotilausten
+			# mukaan oikealle toimittajalle
+			if ($keskeneraiset['kesken'] == 0) {
+
+				# Haetaan toimittajan tiedot
+				$toimittaja_query = "SELECT * FROM toimi WHERE tunnus='{$tilaukset['liitostunnus']}'";
+				$toimittaja = mysql_fetch_assoc(pupe_query($toimittaja_query));
+
+				# Kaivetaan sopivaa saapumista
+				# kyseisen laatijan saapumiset, joissa ei-siirtovalmiita suuntalavoja
+				// $saapuminen_query = "	SELECT * FROM lasku
+				// 						WHERE tila='K'
+				// 						AND liitostunnus='{$tilaukset['liitostunnus']}'
+				// 						AND laatija='{$kukarow['kuka']}'
+				// 						ORDER BY tunnus DESC
+				// 						LIMIT 1";
+
+				$saapuminen = uusi_saapuminen($toimittaja);
+
+				# Asetetaan uusi saapuminen k‰ytt‰j‰lle kesken olevaksi
+				$update_kuka = "UPDATE kuka SET kesken={$saapuminen} WHERE yhtio='{$kukarow['yhtio']}' AND kuka='{$kukarow['kuka']}'";
+				$updated = pupe_query($update_kuka);
+			}
+
+			$url_array['ostotilaus'] = $ostotilaus;
+			$url_array['tilausrivi'] = $tilausrivi;
+			$url_array['saapuminen'] = $saapuminen;
+
+			echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=hyllytys.php?".http_build_query($url_array)."'>"; exit();
+
+			break;
+		case 'cancel':
+			echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=ostotilaus.php?ostotilaus=$ostotilaus'>"; exit();
+		   	break;
+		default:
+			echo "Virhe";
+			break;
+	}
+}
+
 # Ei osumia, palataan ostotilaus sivulle
 if ($tilausten_lukumaara == 0) {
 	echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=ostotilaus.php?virhe'>";
 	exit();
-}
-
-# Jos kuka.kesken ei ole saapuminen, tehd‰‰n uusi saapuminen haettujen ostotilausten
-# mukaan oikealle toimittajalle
-if ($keskeneraiset['kesken'] == 0) {
-
-	# Haetaan toimittajan tiedot
-	$toimittaja_query = "SELECT * FROM toimi WHERE tunnus='{$tilaukset['liitostunnus']}'";
-	$toimittaja = mysql_fetch_assoc(pupe_query($toimittaja_query));
-
-	# Kaivetaan sopivaa saapumista
-	# kyseisen laatijan saapumiset, joissa ei-siirtovalmiita suuntalavoja
-	// $saapuminen_query = "	SELECT * FROM lasku
-	// 						WHERE tila='K'
-	// 						AND liitostunnus='{$tilaukset['liitostunnus']}'
-	// 						AND laatija='{$kukarow['kuka']}'
-	// 						ORDER BY tunnus DESC
-	// 						LIMIT 1";
-
-	# Tehd‰‰n uusi saapuminen
-	$saapuminen = uusi_saapuminen($toimittaja);
-
-	# Asetetaan uusi saapuminen k‰ytt‰j‰lle kesken olevaksi
-	$update_kuka = "UPDATE kuka SET kesken={$saapuminen} WHERE yhtio='{$kukarow['yhtio']}' AND kuka='{$kukarow['kuka']}'";
-	$updated = pupe_query($update_kuka);
 }
 
 # Jos vain yksi osuma, menn‰‰n suoraan hyllytykseen
@@ -177,11 +181,9 @@ while($row = mysql_fetch_assoc($result)) {
 
 	$url_array['ostotilaus'] = $row['ostotilaus'];
 	$url_array['tilausrivi'] = $row['tunnus'];
-	$url_array['saapuminen'] = $saapuminen;
 
 	echo "<input type='hidden' name='ostotilaus' value={$row['ostotilaus']}>";
-	echo "<input type='hidden' name='saapuminen' value={$saapuminen}>";
-
+	echo "<input type='hidden' name='saapuminen' id='saapuminen' value={$saapuminen}>";
 	echo "<tr>";
 }
 
@@ -189,18 +191,30 @@ echo "</table></div>";
 echo "Rivej‰: ".mysql_num_rows($result);
 
 echo "<div class='controls'>
-
-<button name='submit' id='submit' value='ok' onclick='submit();'>",t("OK", $browkieli),"</button>
+<button type='submit' name='submit' value='ok' onsubmit='false' onclick='return tarkista_saapuminen();'>",t("OK", $browkieli),"</button>
 <button class='right' name='submit' id='takaisin' value='cancel' onclick='submit();'>",t("Takaisin", $browkieli),"</button>
 </form>
 </div>";
 
 echo "<div class='error'>";
-	foreach($errors as $virhe => $selite) {
-		echo strtoupper($virhe).": ".$selite;
+	foreach($errors as $virhe) {
+		echo $virhe;
 	}
 echo "</div>";
 
-## DEBUG ##
-#echo "<pre>$query</pre>";
-#var_dump($_POST);
+echo "<script type='text/javascript'>
+	function tarkista_saapuminen() {
+		var saapuminen = document.getElementById('saapuminen').value;
+		if (saapuminen == '') {
+			result = confirm('Huom, Keskener‰ist‰ saapumista ei lˆydy. Tuloutusta varten luodaan uusi saapuminen.');
+			if(result == true) {
+				console.log('tehd‰‰n uusi saapuminen');
+				return true;
+			}
+			else {
+				window.location = 'ostotilaus.php';
+				return false;
+			}
+		}
+	}
+</script>";

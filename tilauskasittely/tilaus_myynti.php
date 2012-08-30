d96088620d7e621d6a0da4ae07130d44cb3397a6
Tony Selkamo <tony@devlab.fi>
2012-08-30 14:03:31 +0300

    Tilauksen mitätöintiin tuki valmistusriveille joilla on var P. Jatkossa tyhjennetään P ja mitätöideen ne tyyppi D:ksi, jotta ei jää varaamaan myytävissä määrää.

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -758,6 +758,12 @@ if ($tee == 'POISTA' and $muokkauslukko == "" and $kukarow["mitatoi_tilauksia"] 
 		}
 	}
 
+	// valmistusriveille var tyhjäksi, että osataan mitätöidä ne seuraavassa updatessa
+	if ($toim == 'VALMISTAVARASTOON' OR $toim == 'VALMISTAASIAKKAALLE') {
+		$query = "UPDATE tilausrivi SET var='' where yhtio='$kukarow[yhtio]' and otunnus='$kukarow[kesken]' and var='P'";
+		$result = pupe_query($query);
+	}
+
 	// poistetaan tilausrivit, mutta jätetään PUUTE rivit analyysejä varten...
 	$query = "UPDATE tilausrivi SET tyyppi='D' where yhtio='$kukarow[yhtio]' and otunnus='$kukarow[kesken]' and var<>'P'";
 	$result = pupe_query($query);

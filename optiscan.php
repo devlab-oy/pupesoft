95167e6c23ede59b330dd0f7bf5cb0997aa3a735
Sami Luukkonen <sami@devlab.fi>
2012-10-15 14:25:04 +0300

    Varastotunniste muutos kardexiin ja optiscaniin.

--- optiscan.php
+++ optiscan.php
@@ -275,7 +275,9 @@
 				$pakkauskirjain = strtoupper(chr((64+$rivi_row['pakkausnro'])));
 				$tuotteen_nimitys = str_replace(array("'", ","), "", $rivi_row['nimitys']);
 
-				$hyllypaikka = $rivi_row['hyllyalue'];
+				$hyllyalue_value = $yhtiorow['varastontunniste'] != "" ? substr($rivi_row["hyllyalue"], $yhtiorow['varastontunniste']) : $rivi_row["hyllyalue"];
+
+				$hyllypaikka = $hyllyalue_value;
 				$hyllypaikka = trim($rivi_row['hyllynro'])  != '' ? $hyllypaikka." ".$rivi_row['hyllynro']  : $hyllypaikka;
 				$hyllypaikka = trim($rivi_row['hyllyvali']) != '' ? $hyllypaikka." ".$rivi_row['hyllyvali'] : $hyllypaikka;
 				$hyllypaikka = trim($rivi_row['hyllytaso']) != '' ? $hyllypaikka." ".$rivi_row['hyllytaso'] : $hyllypaikka;

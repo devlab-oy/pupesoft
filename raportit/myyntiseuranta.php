cb7ede4b27a210a50f6f17a532e27ca0871f87ef
Joonas Harjukallio <joonas@devlab.fi>
2012-10-18 13:35:30 +0300

    Myynninseurannan raporttiin lisätty tuki liitetiedostojen näyttämiselle.

--- raportit/myyntiseuranta.php
+++ raportit/myyntiseuranta.php
@@ -260,6 +260,7 @@
 			if ($ytunnus_mistatiedot != '')	$ytun_mistatiedot_sel	= "SELECTED";
 			if ($naytamaksupvm != '')		$naytamaksupvmchk 		= "CHECKED";
 			if ($asiakaskaynnit != '')		$asiakaskaynnitchk 		= "CHECKED";
+			if ($liitetiedostot != '')	$liitetiedostotchk		= "CHECKED";
 			if ($ytun_laajattied != '')		$ytun_laajattiedchk		= "CHECKED";
 
 			echo "<table>
@@ -515,8 +516,14 @@
 			echo "<tr>
 			<th>",t("Näytä asiakaskäynnit"),"</th>";
 			echo "<td colspan='3'><input type='checkbox' name='asiakaskaynnit' {$asiakaskaynnitchk}></td>
-			<td class='back'>".t("Toimii vain jos listaat asiakkaittain")."</td>
+			<td class='back'>".t("(Toimii vain jos listaat asiakkaittain)")."</td>
 			</tr>";
+
+			echo "<tr>";
+			echo "<th>".t('Näytä tilauksen liitetiedostot')."</th>";
+			echo "<td colspan='3'><input type='checkbox' name='liitetiedostot' {$liitetiedostotchk} /></td>";
+			echo "<td class='back'>".t('(Toimii vain jos listaat tilauksittain)')."</td>";
+			echo "</tr>";
 
 			echo "</table><br>";
 
@@ -1979,6 +1986,22 @@
 									}
 
 								}
+							}
+
+							if ($liitetiedostot != '') {
+									$liitetiedosto_query = "SELECT *
+															FROM liitetiedostot
+															WHERE yhtio = '{$kukarow['yhtio']}'
+															AND liitos = 'lasku'
+															AND liitostunnus = '{$row['tilausnumero']}'";
+									$liitetiedosto_result = pupe_query($liitetiedosto_query);
+
+									$liitetiedosto_indeksi = 1;
+									$row['liitetiedostot'] = '';
+									while($liitetiedosto_row = mysql_fetch_assoc($liitetiedosto_result)) {
+										$row['liitetiedostot'] .= "<a href='{$palvelin2}/view.php?id={$liitetiedosto_row['tunnus']}' >Liite {$liitetiedosto_indeksi}</a> ";
+										$liitetiedosto_indeksi++;
+									}
 							}
 
 							$rows[] = $row;

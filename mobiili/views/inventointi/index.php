b3b2f708f2595eda124221d138269a929dc55c1c
Antti Jäppinen <antti@devlab.fi>
2012-11-01 16:26:51 +0200

    Linkki bugfix

--- mobiili/views/inventointi/index.php
+++ mobiili/views/inventointi/index.php
@@ -4,7 +4,7 @@
 </div>
 
 <div class='main valikko'>
-	<p><a href='inventointi?tee=haku' class='button'><?php echo t("Vapaa inventointi") ?></a></p>
+	<p><a href='?tee=haku' class='button'><?php echo t("Vapaa inventointi") ?></a></p>
 	<p><a href='?tee=listat' class='button'><?php echo t("Keräyspaikat listalta") ?></a></p>
 	<p><a href='?tee=listat&reservipaikka=k' class='button'><?php echo t("Reservipaikat listalta") ?></a></p>
 </div>

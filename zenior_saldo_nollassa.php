8732e8a4aa7bbe284f76983f8f3205c19355f5ac
Johan Tötterman <juppe@devlab.fi>
2012-11-15 10:11:51 +0200

    Loppuviilaukset!

--- zenior_saldo_nollassa.php
+++ zenior_saldo_nollassa.php
@@ -2,11 +2,11 @@
 
 // Kutsutaanko CLI:stä
 if (php_sapi_name() != 'cli') {
-	die (t("Ajo ainoastaan cronista / komentoriviltä!"));
+	die ("Ajo ainoastaan cronista / komentoriviltä!");
 }
 
 if (!isset($argv[1])) {
-	die (t("Anna yhtio parametriksi!"));
+	die ("Anna yhtio parametriksi!");
 }
 
 $pupe_root_polku = dirname(__FILE__);
@@ -29,22 +29,18 @@ if ($yhtiorow["epakurantoinnin_myyntihintaleikkuri"] != 'Z') {
 	die(t("Tämä toiminto on käytettävissä vain, jos yhtiöparametri epakurantoinnin_myyntihintaleikkuri on 'Z'"));
 }
 
-// hae nollasaldoiset zeniorpartsit, tarvitaan tuoteno ja avainsanalle tallennettu alkuperäinen hinta
-
-$query  = "	SELECT
-					t.tuoteno,
-					a.selitetark        as orig_myyntihinta,
-                    MAX(t.myyntihinta)  as varahinta,
- 					SUM(p.saldo)        as saldosumma
-			FROM tuote                  t
-            JOIN tuotteen_avainsanat    a ON (t.yhtio = a.yhtio AND t.tuoteno = a.tuoteno)
-			JOIN tuotepaikat            p ON (a.yhtio = p.yhtio AND a.tuoteno = p.tuoteno)
-			WHERE
-                    t.yhtio = '{$kukarow["yhtio"]}'
-			  AND   a.laji  = 'zeniorparts'
+// hae nollasaldoiset epäkurantit, tarvitaan tuoteno ja avainsanalle tallennettu alkuperäinen hinta
+$query  = "	SELECT t.tuoteno,
+			a.selitetark        as orig_myyntihinta,
+			MAX(t.myyntihinta)  as varahinta,
+			SUM(p.saldo)        as saldosumma
+			FROM tuote t
+            JOIN tuotteen_avainsanat a ON (t.yhtio = a.yhtio AND a.kieli = '{$yhtiorow['kieli']}' AND t.tuoteno = a.tuoteno AND a.laji = 'zeniorparts')
+			JOIN tuotepaikat p ON (a.yhtio = p.yhtio AND a.tuoteno = p.tuoteno)
+			WHERE t.yhtio = '{$kukarow["yhtio"]}'
+			and t.epakurantti25pvm != '0000-00-00'
 			GROUP BY 1, 2
-			HAVING saldosumma = 0;";
-
+			HAVING saldosumma = 0";
 $result = pupe_query($query);
 
 while ($row = mysql_fetch_assoc($result)) {
@@ -54,37 +50,32 @@ while ($row = mysql_fetch_assoc($result)) {
     $selite = t("Epäkuranttimuutos") . ": ".t("Tuote")." {$row["tuoteno"]} ".t("päivitetään kurantiksi");
 
 	$t_query = "UPDATE tuote
-				SET
-					status            = 'T',
-					epakurantti25pvm  = '0000-00-00',
-					epakurantti50pvm  = '0000-00-00',
-					epakurantti75pvm  = '0000-00-00',
-					epakurantti100pvm = '0000-00-00',
-					myyntihinta       = {$hinta}
-				WHERE
-					yhtio = '{$kukarow["yhtio"]}'
-					AND tuoteno = '{$row["tuoteno"]}';";
+				SET status        = 'T',
+				epakurantti25pvm  = '0000-00-00',
+				epakurantti50pvm  = '0000-00-00',
+				epakurantti75pvm  = '0000-00-00',
+				epakurantti100pvm = '0000-00-00',
+				myyntihinta       = {$hinta}
+				WHERE yhtio = '{$kukarow["yhtio"]}'
+				AND tuoteno = '{$row["tuoteno"]}';";
 	$t_result = pupe_query($t_query);
 
 	$t_query = "DELETE FROM tuotteen_avainsanat
-				WHERE
-					yhtio = '{$kukarow["yhtio"]}'
-					AND laji = 'zeniorparts'
-					AND tuoteno = '{$row["tuoteno"]}';";
+				WHERE yhtio = '{$kukarow["yhtio"]}'
+				AND kieli 	= '{$yhtiorow["kieli"]}'
+				AND laji 	= 'zeniorparts'
+				AND tuoteno = '{$row["tuoteno"]}';";
 	$t_result = pupe_query($t_query);
 
-    $t_query = "INSERT INTO tapahtuma
-                SET
-                    yhtio   	= '{$kukarow["yhtio"]}',
-                    tuoteno 	= '{$row["tuoteno"]}',
-                    laji    	= 'Epäkurantti',
-                    kpl     	= '0',
-                    hinta   	= 0,
-                    kplhinta	= 0,
-                    selite  	= '$selite',
-                    laatija    	= '{$kukarow["kuka"]}',
-                    laadittu 	= now()";
-
+    $t_query = "INSERT INTO tapahtuma SET
+				yhtio    = '{$kukarow["yhtio"]}',
+				tuoteno  = '{$row["tuoteno"]}',
+				laji     = 'Epäkurantti',
+				kpl      = '0',
+				hinta    = 0,
+				kplhinta = 0,
+				selite   = '$selite',
+				laatija  = '{$kukarow["kuka"]}',
+				laadittu = now()";
     $t_result = pupe_query($t_query);
 }
-?>

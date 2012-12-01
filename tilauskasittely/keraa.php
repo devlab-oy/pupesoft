19ebf5a0c17d89de40b2a0f20f662ef6ec19d6ff
Johan Tötterman <juppe@devlab.fi>
2012-12-01 18:13:27 +0200

    FIX: vielä fiilataan

--- tilauskasittely/keraa.php
+++ tilauskasittely/keraa.php
@@ -1881,7 +1881,7 @@
 						}
 					}
 
-					if ($yhtiorow['kerayserat'] == 'K' and $toim == "" and $yhtiorow['karayksesta_rahtikirjasyottoon'] == 'Y') {
+					if ($yhtiorow['kerayserat'] == 'K' and $toim == "" and ($yhtiorow['karayksesta_rahtikirjasyottoon'] != 'Y' or $laskurow['nouto'] != '')) {
 						$query = "	UPDATE lasku
 									SET alatila = 'B'
 									WHERE yhtio = '{$kukarow['yhtio']}'

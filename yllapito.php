909876f702d23e638ec595f77e2cf9761c902e18
Johan Tötterman <juppe@devlab.fi>
2013-01-07 15:50:37 +0200

    HF: Korjattu ytunnus @-merkki tarkkahaku.

--- yllapito.php
+++ yllapito.php
@@ -928,7 +928,7 @@
 					$lisa .= " and {$array[$i]} = '{$haku[$i]}' ";
 				}
 			}
-			else if(trim($array[$i]) == 'ytunnus') {
+			elseif (trim($array[$i]) == 'ytunnus' and !$tarkkahaku) {
 				$lisa .= " and REPLACE({$array[$i]}, '-', '') = REPLACE('{$haku[$i]}', '-', '') ";
 			}
 			elseif ($from == "yllapito" and ($toim == 'rahtisopimukset' or $toim == 'asiakasalennus' or $toim == 'asiakashinta') and trim($array[$i]) == 'asiakas') {

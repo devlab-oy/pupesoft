21c113450566e9e9306d678e406c8900a4c0f0ac
Sami Luukkonen <sami@devlab.fi>
2013-01-17 18:09:08 +0200

    UIta

--- raportit/hinnoittelupoikkeamat.php
+++ raportit/hinnoittelupoikkeamat.php
@@ -4,4 +4,30 @@
 
 	echo "<font class='head'>",t("Hinnoittelupoikkeamat-raportti"),"</font><hr />";
 
+	if (!isset($app)) $app = "01";
+	if (!isset($akk)) $akk = date("m", mktime(0, 0, 0, date("m"), 0, date("Y")));
+	if (!isset($avv)) $avv = date("Y", mktime(0, 0, 0, date("m"), 0, date("Y")));
+
+	if (!isset($lpp)) $lpp = date("d", mktime(0, 0, 0, date("m"), 0, date("Y")));
+	if (!isset($lkk)) $lkk = date("m", mktime(0, 0, 0, date("m"), 0, date("Y")));
+	if (!isset($lvv)) $lvv = date("Y", mktime(0, 0, 0, date("m"), 0, date("Y")));
+
+	echo "<table>";
+
+	echo "<tr>";
+	echo "<th>",t("Alkup‰iv‰m‰‰r‰"),"</th>";
+	echo "<td><input type='text' name='app' value='{$app}' size='5' />";
+	echo "<input type='text' name='akk' value='{$akk}' size='5' />";
+	echo "<input type='text' name='avv' value='{$avv}' size='5' /></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<th>",t("Loppup‰iv‰m‰‰r‰"),"</th>";
+	echo "<td><input type='text' name='lpp' value='{$lpp}' size='5' />";
+	echo "<input type='text' name='lkk' value='{$lkk}' size='5' />";
+	echo "<input type='text' name='lvv' value='{$lvv}' size='5' /></td>";
+	echo "</tr>";
+
+	echo "</table>";
+
 	require("inc/footer.inc");

8d9cc4de9a46e08a391d205d87eed3bf4d4c6391
Johan Tötterman <juppe@devlab.fi>
2013-01-23 10:48:00 +0200

    Voidaan valita myös tilikausi suoraan päivämäärärajaukseksi.

--- raportit/matkallaolevat_laskuittain.php
+++ raportit/matkallaolevat_laskuittain.php
@@ -7,29 +7,88 @@
 
 	echo "<font class='head'>".t("Matkallaolevat laskuittain")."</font><hr>";
 
-	$llisa = "";
-	$alisa = "";
+	if (!isset($vv) or !isset($lvv)) {
+		$query = "	SELECT *
+					FROM tilikaudet
+					WHERE yhtio = '$kukarow[yhtio]'
+					and tilikausi_alku <= current_date
+					and tilikausi_loppu >= current_date";
+		$result = pupe_query($query);
+		$tilikausirow = mysql_fetch_assoc($result);
 
-	if ($vv != "") {
-		if (!checkdate($kk, $pp, $vv)) {
-			echo "<font class='error'>".t("Virheellinen p‰iv‰m‰‰r‰")."!</font><br><br>";
+		if (!isset($vv)) {
+			$vv = substr($tilikausirow['tilikausi_alku'], 0, 4);
+			$kk = substr($tilikausirow['tilikausi_alku'], 5, 2);
+			$pp = substr($tilikausirow['tilikausi_alku'], 8, 2);
 		}
-		else {
-	 		$alisa = " AND lasku.tapvm >= '$vv-$kk-$pp' ";
+
+		if (!isset($lvv)) {
+			$lvv = substr($tilikausirow['tilikausi_loppu'], 0, 4);
+			$lkk = substr($tilikausirow['tilikausi_loppu'], 5, 2);
+			$lpp = substr($tilikausirow['tilikausi_loppu'], 8, 2);
 		}
 	}
 
-	if ($lvv != "") {
-		if (!checkdate($lkk, $lpp, $lvv)) {
-			echo "<font class='error'>".t("Virheellinen p‰iv‰m‰‰r‰")."!</font><br><br>";
+	$llisa = "";
+	$alisa = "";
+
+	if (isset($tkausi) and $tkausi > 0) {
+		$query = "	SELECT *
+					FROM tilikaudet
+					WHERE yhtio = '$kukarow[yhtio]'
+					and tunnus  = '$tkausi'";
+		$vresult = pupe_query($query);
+		$tilikaudetrow = mysql_fetch_array($vresult);
+
+		$alisa = " AND lasku.tapvm >= '{$tilikaudetrow["tilikausi_alku"]}' ";
+		$llisa = " AND lasku.tapvm <= '{$tilikaudetrow["tilikausi_loppu"]}' ";
+	}
+	else {
+		if ($vv != "") {
+			if (!checkdate($kk, $pp, $vv)) {
+				echo "<font class='error'>".t("Virheellinen p‰iv‰m‰‰r‰")."!</font><br><br>";
+			}
+			else {
+		 		$alisa = " AND lasku.tapvm >= '$vv-$kk-$pp' ";
+			}
 		}
-		else {
-			$llisa = " AND lasku.tapvm < '$lvv-$lkk-$lpp' ";
+
+		if ($lvv != "") {
+			if (!checkdate($lkk, $lpp, $lvv)) {
+				echo "<font class='error'>".t("Virheellinen p‰iv‰m‰‰r‰")."!</font><br><br>";
+			}
+			else {
+				$llisa = " AND lasku.tapvm <= '$lvv-$lkk-$lpp' ";
+			}
 		}
 	}
 
 	echo "<form method='post'>";
 	echo "<table>";
+
+	$query = "	SELECT *
+				FROM tilikaudet
+				WHERE yhtio = '{$kukarow["yhtio"]}'
+				ORDER BY tilikausi_alku DESC";
+	$vresult = pupe_query($query);
+
+	echo "<tr>";
+	echo "<th>",t("Tilikausi"),"</th>";
+	echo "<td><select name='tkausi'>";
+	echo "<option value = ''>".t("Valitse")."</option>";
+
+	while ($vrow = mysql_fetch_assoc($vresult)) {
+		$sel = $tkausi == $vrow['tunnus'] ? ' selected' : '';
+		echo "<option value = '$vrow[tunnus]'$sel>".tv1dateconv($vrow["tilikausi_alku"])." - ".tv1dateconv($vrow["tilikausi_loppu"])."</option>";
+	}
+
+	echo "</select></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td colspan='2' class='back'> ".t("tai")." </td>";
+	echo "</tr>";
+
 	echo "<tr>";
 	echo "<th>".t("Syˆt‰ alkup‰iv‰")."</th>";
 	echo "<td><input type='text' name='pp' size='5' value='$pp'><input type='text' name='kk' size='5' value='$kk'><input type='text' name='vv' size='7' value='$vv'></td>";
@@ -97,6 +156,4 @@
 
 	}
 
-	require ("../inc/footer.inc");
-
-?>
+	require ("inc/footer.inc");

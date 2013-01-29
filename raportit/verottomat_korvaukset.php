438600553a6888b34b6c1924f1d6c785600537cc
Johan Tötterman <juppe@devlab.fi>
2013-01-29 10:51:58 +0200

    Eteenpäin

--- raportit/verottomat_korvaukset.php
+++ raportit/verottomat_korvaukset.php
@@ -37,24 +37,27 @@
 
 	if ($tee == "NAYTA") {
 
-		$query = "	SELECT if (kuka.nimi IS NULL, lasku.toim_ovttunnus, kuka.nimi) nimi,
+		$query = "	SELECT
+					toimi.tunnus,
+					toimi.ytunnus,
+					if(kuka.nimi IS NULL, concat('*POISTETTU* ', toimi.nimi), kuka.nimi) nimi,
 					tuote.kuvaus,
-					lasku.toim_ovttunnus,
-					lasku.ytunnus,
 					avg(tilausrivi.hinta) hinta,
 					sum(tilausrivi.kpl) kpl,
 					sum(tilausrivi.rivihinta) yhteensa
 					FROM lasku
+					JOIN toimi on (toimi.yhtio = lasku.yhtio and toimi.tunnus = lasku.liitostunnus)
+					LEFT JOIN kuka ON (kuka.yhtio = lasku.yhtio and kuka.kuka = toimi.nimi)
 					JOIN tilausrivi ON (tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus)
 					JOIN tuote ON (tuote.yhtio = lasku.yhtio and tuote.tuoteno = tilausrivi.tuoteno and tuote.tuotetyyppi IN ('A', 'B') and tuote.kuvaus in ('50', '56'))
-					LEFT JOIN kuka ON (kuka.yhtio = lasku.yhtio and kuka.kuka = lasku.toim_ovttunnus)
 					WHERE lasku.yhtio = '$kukarow[yhtio]'
-					AND tila = 'Y'
-					AND tilaustyyppi = 'M'
-					AND tapvm >= '$vv-01-01'
-					AND tapvm <= '$vv-12-31'
-					GROUP BY 1,2,3,4";
-		$result = mysql_query($query) or pupe_error($query);
+					AND lasku.tila = 'Y'
+					AND lasku.tilaustyyppi = 'M'
+					AND lasku.tapvm >= '$vv-01-01'
+					AND lasku.tapvm <= '$vv-12-31'
+					GROUP BY 1,2,3,4
+					ORDER BY nimi";
+		$result = pupe_query($query);
 
 		if (mysql_num_rows($result) > 0) {
 
@@ -68,13 +71,14 @@
 			echo "<th>".t("Yhteensä")."</th>";
 			echo "</tr>";
 
-			$ednimi 	= "";
-			$summat 	= array();
-			$kappaleet  = array();
-			$file 		= "";
-			$lask 		= 1;
+			$ednimi    = "";
+			$summat    = array();
+			$kappaleet = array();
+			$file 	   = "";
+			$lask 	   = 1;
+			$vspserie  = array();
 
-			while ($row = mysql_fetch_array($result)) {
+			while ($row = mysql_fetch_assoc($result)) {
 
 				if ($row["kuvaus"] == '50') {
 					$kuvaus = t("Päivärahat ja ateriakorvaukset");
@@ -85,6 +89,7 @@
 
 				if ($ednimi == "" or $ednimi != $row["nimi"]) {
 					$nimi = $row["nimi"];
+
 					if ($ednimi != "") {
 						echo "<tr><td class='back' colspan='5'></td></tr>";
 					}
@@ -94,31 +99,59 @@
 				}
 
 				echo "<tr class='aktiivi'>";
-				echo "<td>$nimi</td>";
+
+				if ($nimi != "") {
+					echo "<td>$nimi</td>";
+				}
+				else {
+					echo "<td class='back'></td>";
+				}
+
 				echo "<td>$row[kuvaus] $kuvaus</td>";
 				echo "<td align='right'>".number_format($row["kpl"], 0, ',', ' ')."</td>";
 				echo "<td align='right'>".number_format($row["hinta"], 2, ',', ' ')."</td>";
 				echo "<td align='right'>".number_format($row["yhteensa"], 2, ',', ' ')."</td>";
 				echo "</tr>";
 
+				if ($row['kuvaus'] == 50) {
+					$vspserie[$row["ytunnus"]]["paivarahat"] = $row["kpl"];
+				}
+
+				if ($row['kuvaus'] == 56) {
+					$vspserie[$row["ytunnus"]]["kilsat"] = $row["yhteensa"];
+				}
+
 				// erittely
 				$query = "	SELECT tilausrivi.tuoteno, tuote.nimitys, avg(tilausrivi.hinta) hinta, sum(tilausrivi.kpl) kpl, sum(tilausrivi.rivihinta) yhteensa
 							FROM lasku
 							JOIN tilausrivi ON (tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus)
-							JOIN tuote ON (tuote.yhtio = lasku.yhtio and tuote.tuoteno = tilausrivi.tuoteno and tuote.tuotetyyppi IN ('A', 'B') and tuote.kuvaus = '$row[kuvaus]')
+							JOIN tuote ON (tuote.yhtio = lasku.yhtio and tuote.tuoteno = tilausrivi.tuoteno and tuote.tuotetyyppi IN ('A','B') and tuote.kuvaus = '$row[kuvaus]')
 							LEFT JOIN kuka ON (kuka.yhtio = lasku.yhtio and kuka.kuka = lasku.toim_ovttunnus)
 							WHERE lasku.yhtio = '$kukarow[yhtio]'
-							AND tila = 'Y'
-							AND tilaustyyppi = 'M'
-							AND tapvm >= '$vv-01-01'
-							AND tapvm <= '$vv-12-31'
-							AND lasku.toim_ovttunnus = '$row[toim_ovttunnus]'
+							AND lasku.tila = 'Y'
+							AND lasku.tilaustyyppi = 'M'
+							AND lasku.tapvm >= '$vv-01-01'
+							AND lasku.tapvm <= '$vv-12-31'
+							AND lasku.liitostunnus = '$row[tunnus]'
 							GROUP BY tuote.tuoteno";
-				$eres = mysql_query($query) or pupe_error($query);
+				$eres = pupe_query($query);
 
-				while ($erow = mysql_fetch_array($eres)) {
+				while ($erow = mysql_fetch_assoc($eres)) {
+
+					if ($row['kuvaus'] == 56) {
+						$vspserie[$row["ytunnus"]]["kotimaanpaivat"] = 1;
+					}
+
+					if ($row['kuvaus'] == 56) {
+						$vspserie[$row["ytunnus"]]["kotimaanpuolipaivat"] = 1;
+					}
+
+					if ($row['kuvaus'] == 56) {
+						$vspserie[$row["ytunnus"]]["ulkomaanpaivat"] = 1;
+					}
+
 					echo "<tr class='aktiivi'>";
-					echo "<td class='spec'></td>";
+					echo "<td class='back'></td>";
 					echo "<td class='spec'><font class='info'>&raquo; $erow[tuoteno] - $erow[nimitys]</font></td>";
 					echo "<td class='spec' align='right'>".number_format($erow["kpl"], 0, ',', ' ')."</td>";
 					echo "<td class='spec' align='right'>".number_format($erow["hinta"], 2, ',', ' ')."</td>";
@@ -126,29 +159,10 @@
 					echo "</tr>";
 				}
 
-				$file .= "000:VSPSERIE";
-				$file .= "101:0";
-				$file .= "110:P";
-				$file .= "109:$vv";
-				$file .= "102:{$yhtiorow['ytunnus']}";
-				$file .= "111:{$row['ytunnus']}";
-				$file .= "114:0";
-				$file .= "115:0";
-				$file .= "150:0";
-				$file .= "151:0";
-				$file .= "152:0";
-				$file .= "153:0";
-				$file .= "154:0";
-				$file .= "155:404";
-				$file .= "156:18584";
-				$file .= "157:0";
-				$file .= "999:$lask";
-
 				$lask++;
 				$ednimi = $row["nimi"];
 				$summat[$row["kuvaus"]] += $row["yhteensa"];
 				$kappaleet[$row["kuvaus"]] += $row["kpl"];
-
 			}
 
 			echo "<tr><td class='back' colspan='5'></td></tr>";
@@ -166,6 +180,26 @@
 			echo "</tr>";
 
 			echo "</table>";
+
+
+			$file .= "000:VSPSERIE";
+			$file .= "101:0";
+			$file .= "110:P";
+			$file .= "109:$vv";
+			$file .= "102:{$yhtiorow['ytunnus']}";
+			$file .= "111:{$row['ytunnus']}";
+			$file .= "114:0";
+			$file .= "115:0";
+			$file .= "150:0";
+			$file .= "151:0";
+			$file .= "152:0";
+			$file .= "153:0";
+			$file .= "154:0";
+			$file .= "155:404";
+			$file .= "156:18584";
+			$file .= "157:0";
+			$file .= "999:$lask";
+
 
 			$filenimi = "VSPSERIE-$kukarow[yhtio]-".date("dmy-His").".txt";
 			file_put_contents("dataout/".$filenimi, $file);

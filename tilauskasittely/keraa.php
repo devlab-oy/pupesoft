a7f27ebafa87dda07b24216d237466a27dadc896
Joonas Harjukallio <joonas@devlab.fi>
2013-02-25 14:51:27 +0200

    Lasku otsikolle kerääjä.
    - Lasku otsikolle hyvak3 ja h3time merkataan kerääjän kuka ja keräys aika.
    - Kerääjän nimi ja numero haetaan laskuotsikon hyvak3 kentästä.

--- tilauskasittely/keraa.php
+++ tilauskasittely/keraa.php
@@ -2146,7 +2146,7 @@
 								((tilausrivi.laskutettu = '' AND tilausrivi.laskutettuaika 	= '0000-00-00') OR lasku.mapvm != '0000-00-00'))
 							JOIN kerayserat ON (kerayserat.yhtio = lasku.yhtio AND kerayserat.otunnus = lasku.tunnus AND kerayserat.tila = 'K' {$kerayserahaku})
 							JOIN asiakas ON (asiakas.yhtio = lasku.yhtio AND asiakas.tunnus = lasku.liitostunnus)
-							LEFT JOIN kuka ON (kuka.yhtio = kerayserat.yhtio AND kuka.kuka = kerayserat.laatija)
+							LEFT JOIN kuka ON (kuka.yhtio = lasku.yhtio AND kuka.kuka = lasku.hyvak3)
 							WHERE lasku.{$logistiikka_yhtiolisa}
 							AND lasku.tila = 'L'
 							AND lasku.alatila = 'A'

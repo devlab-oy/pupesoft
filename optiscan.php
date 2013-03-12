711ee8426e3e8d8d226043534aecb409fd4b5d7b
Johan Tötterman <juppe@devlab.fi>
2013-03-12 12:29:00 +0200

    Parami tyhjänä silloin kun sitä ei tarvita

--- optiscan.php
+++ optiscan.php
@@ -281,9 +281,7 @@
 				$pakkauskirjain = strtoupper(chr((64+$rivi_row['pakkausnro'])));
 				$tuotteen_nimitys = str_replace(array("'", ","), "", $rivi_row['nimitys']);
 
-				$hyllyalue_value = $yhtiorow['varastontunniste'] != "" ? substr("{$rivi_row["hyllyalue"]}", $yhtiorow['varastontunniste']) : $rivi_row["hyllyalue"];
-
-				$hyllypaikka = $hyllyalue_value;
+				$hyllypaikka = hyllyalue('', $rivi_row["hyllyalue"], 'riisuttu');
 				$hyllypaikka = trim($rivi_row['hyllynro'])  != '' ? $hyllypaikka." ".$rivi_row['hyllynro']  : $hyllypaikka;
 				$hyllypaikka = trim($rivi_row['hyllyvali']) != '' ? $hyllypaikka." ".$rivi_row['hyllyvali'] : $hyllypaikka;
 				$hyllypaikka = trim($rivi_row['hyllytaso']) != '' ? $hyllypaikka." ".$rivi_row['hyllytaso'] : $hyllypaikka;

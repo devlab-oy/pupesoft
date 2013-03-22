4406a5345bfbd5800ee90999d117569bd03ea3ab
Johan Tötterman <juppe@devlab.fi>
2013-03-22 15:23:32 +0200

    Snadisti siistitty!

--- odottaa_suoritusta.php
+++ odottaa_suoritusta.php
@@ -1,7 +1,6 @@
 <?php
 
-$debug = false;
-if (php_sapi_name() != 'cli' and !$debug) {
+if (php_sapi_name() != 'cli') {
 	die("Tätä scriptiä voi ajaa vain komentoriviltä!");
 }
 
@@ -15,20 +14,15 @@ require_once ('inc/functions.inc');
  *
  */
 
-if ((!isset($argv[1]) or $argv[1] == '') and !$debug) {
+if (!isset($argv[1]) or $argv[1] == '') {
 	echo "Anna yhtiö!!!\n";
 	die;
 }
 
-if(!$debug) {
 $yhtiorow = hae_yhtion_parametrit($argv[1]);
-}
-else {
-	$yhtiorow = hae_yhtion_parametrit('artr');
-}
-
 
 $tilaukset = hae_suoritusta_odottavat_tilaukset();
+
 kasittele_tilaukset($tilaukset);
 
 function hae_suoritusta_odottavat_tilaukset() {
@@ -37,46 +31,37 @@ function hae_suoritusta_odottavat_tilaukset() {
 	$query = "	SELECT *
 				FROM lasku
 				WHERE yhtio = '{$yhtiorow['yhtio']}'
-				AND tila = 'N'
+				AND tila 	= 'N'
 				AND alatila = 'G'
 				ORDER BY luontiaika ASC";
 	$result = pupe_query($query);
 
 	$tilaukset = array();
+
 	while ($tilaus = mysql_fetch_assoc($result)) {
 		$tilaukset[] = $tilaus;
 	}
 
 	return $tilaukset;
 }
+
 function kasittele_tilaukset($tilaukset) {
 	global $kukarow, $yhtiorow;
 
 	echo t("Otetaan") . ' ' . count($tilaukset) . ' ' . t("myyntitilausta käsittelyyn") . "\n";
-	
-	foreach ($tilaukset as $laskurow) {
-		// Parametrejä saatanat.php:lle
-		$sytunnus = $laskurow['ytunnus'];
-		$sliitostunnus = $laskurow['liitostunnus'];
-		$eiliittymaa = "ON";
-		$luottorajavirhe = "";
-		$jvvirhe = "";
-		$ylivito = 0;
-		$trattavirhe = "";
 
-		/*
-		 * echo "<th>".t("Mitkä Laskut Listataan").":</th>";
-		echo "<td><select name='laji'>
-				<option value='M'   $sel[M]>".t("myyntisaamiset")."</option>
-				<option value='MF'  $sel[MF]>".t("factoringmyyntisaamiset")."</option>
-				<option value='MK'  $sel[MK]>".t("konsernimyyntisaamiset")."</option>
-				<option value='MMK' $sel[MMK]>".t("myyntisaamiset + konsernimyyntisaamiset")."</option>
-				<option value='MA'  $sel[MA]>".t("myyntisaamiset + factoringmyyntisaamiset + konsernimyyntisaamiset")."</option>
-				</select></td>";
-		echo "</tr>";
-		 */
-		$laji = "MA";
-		$grouppaus = ($yhtiorow["myyntitilaus_saatavat"] == "Y") ? "ytunnus" : "";
+	foreach ($tilaukset as $laskurow) {
+
+		// Parametrejä saatanat.php:lle
+		$sytunnus 	 	 = $laskurow['ytunnus'];
+		$sliitostunnus	 = $laskurow['liitostunnus'];
+		$eiliittymaa 	 = "ON";
+		$luottorajavirhe = "";
+		$jvvirhe 		 = "";
+		$ylivito 		 = 0;
+		$trattavirhe 	 = "";
+		$laji 			 = "MA";
+		$grouppaus       = ($yhtiorow["myyntitilaus_saatavat"] == "Y") ? "ytunnus" : "";
 
 		$kukarow = hae_asiakas($laskurow['liitostunnus']);
 
@@ -86,8 +71,7 @@ function kasittele_tilaukset($tilaukset) {
 
 		ob_end_clean();
 
-		if(!empty($luottorajavirhe) or $ylivito > 0) {
-			//for debug
+		if (!empty($luottorajavirhe) or $ylivito > 0) {
 			echo t("Lasku") . ' ' . $laskurow['tunnus'] . ' ' . t("pysyy suoritusta odotus tilassa") . "\n";
 		}
 		else {
@@ -109,11 +93,12 @@ function aseta_tilaus_kesken_tilaan_ja_aseta_uusi_lahto($laskurow) {
 				AND alatila = 'G'";
 	pupe_query($query);
 
-	if(mysql_affected_rows() > 0) {
+	if (mysql_affected_rows() > 0) {
 		echo t("Lasku") . ' ' . $laskurow['tunnus'] . ' ' . t("asetettiin kesken tilaan") . "\n";
 
 		//tilaus-valmis.inc hoitaa meille järkevän lähdön kun tilauksen tila ja alatila on oikein
 		$kukarow['kesken'] = $laskurow['tunnus'];
+
 		require("tilauskasittely/tilaus-valmis.inc");
 	}
 }

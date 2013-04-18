a0382c81e4b89f2111eb423ee277a0009dea56e8
Joonas Harjukallio <joonas@devlab.fi>
2013-04-18 14:57:46 +0300

    Muutoksia.

--- tyomaarays/tyojono2.php
+++ tyomaarays/tyojono2.php
@@ -33,21 +33,49 @@ $request = array(
 	
 );
 
-hae_tyomaaraykset($request);
+$request['tyomaaraykset'] = hae_tyomaaraykset($request);
+
+$request['tyojonot'] = hae_tyojonot($request);
+
+$request['tyostatukset'] = hae_tyostatukset($request);
 
 echo_tyojono_table($request);
 
 function hae_tyomaaraykset($request) {
 	global $kukarow, $yhtiorow;
 
-	$query = "	SELECT *
+	$query = "	SELECT
+				lasku.tunnus as lasku_tunnus,
+				lasku.ytunnus as asiakas_ytunnus,
+				lasku.nimi as asiakas_nimi,
+				kohde.nimi as kohde_nimi,
+				lasku.toimaika,
+				a1.selite as tyojonokoodi,
+				a1.selitetark as tyojono,
+				a2.selitetark as tyostatus,
+				a2.selitetark_2 as tyostatusvari
 				FROM lasku
 				JOIN tyomaarays
-				ON ( tyomaarays.yhtio = lasku.tunnus
+				ON ( tyomaarays.yhtio = lasku.yhtio
 					AND tyomaarays.otunnus = lasku.tunnus )
 				JOIN laskun_lisatiedot
 				ON ( laskun_lisatiedot.yhtio = lasku.yhtio
 					AND laskun_lisatiedot.otunnus = lasku.tunnus )
+				JOIN tilausrivi
+				ON ( tilausrivi.yhtio = lasku.yhtio
+					AND tilausrivi.otunnus = lasku.tunnus )
+				JOIN tilausrivin_lisatiedot
+				ON ( tilausrivin_lisatiedot.yhtio = tilausrivi.yhtio
+					AND tilausrivin_lisatiedot.tilausrivitunnus = tilausrivi.tunnus )
+				JOIN laite
+				ON ( laite.yhtio = tilausrivin_lisatiedot.yhtio
+					AND laite.tunnus = tilausrivin_lisatiedot.asiakkaan_positio )
+				JOIN paikka
+				ON ( paikka.yhtio = laite.yhtio
+					AND paikka.tunnus = laite.paikka )
+				JOIN kohde
+				ON ( kohde.yhtio = paikka.yhtio
+					AND kohde.tunnus = paikka.kohde )
 				LEFT JOIN avainsana a1
 				ON ( a1.yhtio = tyomaarays.yhtio
 					AND a1.laji = 'TYOM_TYOJONO'
@@ -68,43 +96,9 @@ function hae_tyomaaraykset($request) {
 	return $tyomaaraykset;
 }
 
-function generoi_haku() {
-	$lisa = "";
-	
-	if ($myyntitilaus_haku != "") {
-		$lisa .= " and lasku.tunnus='$myyntitilaus_haku' ";
-	}
-
-	if ($viesti_haku != "") {
-		$lisa .= " and lasku.viesti like '%$viesti_haku%' ";
-	}
-
-	if ($asiakasnimi_haku != "") {
-		$lisa .= " and lasku.nimi like '%$asiakasnimi_haku%' ";
-	}
-
-	if ($asiakasnumero_haku != "") {
-		$lisa .= " and lasku.ytunnus like '$asiakasnumero_haku%' ";
-	}
-
-	if ($tyojono_haku != "") {
-		$lisa .= " and a1.selitetark like '$tyojono_haku%' ";
-	}
-
-	if ($tyostatus_haku != "") {
-		$lisa .= " and a2.selitetark like '$tyostatus_haku%' ";
-	}
-
-	if ($suorittaja_haku != "") {
-		$lisa2 .= " HAVING suorittajanimi like '%$suorittaja_haku%' or asekalsuorittajanimi like '%$suorittaja_haku%' ";
-	}
-
-	return $lisa;
-}
-
 function echo_tyojono_table($request = array()) {
 	global $kukarow, $yhtiorow;
-	
+
 	echo "<table class='display dataTable'>";
 	echo "<thead>";
 
@@ -115,7 +109,6 @@ function echo_tyojono_table($request = array()) {
 			<th>".t("Toimitetaan")."</th>
 			<th>".t("Työjono")."/<br>".t("Työstatus")."</th>
 			<th>".t("Muokkaa")."</th>
-			<th style='visibility:hidden; display:none;'></th>
 		</tr>";
 
 	echo "<tr>";
@@ -129,23 +122,14 @@ function echo_tyojono_table($request = array()) {
 	echo "<td>";
 
 	echo "<select class='tyojono_sort'>";
-	echo "<option value=''>".t('Ei rajausta')."</option>";
-	echo "<option value='EIJONOA'>".t("Ei jonossa")."</option>";
-
-	// Haetaan tyojono avainsanat
-	$tyojono_result = t_avainsana("TYOM_TYOJONO");
-	while ($tyojono_row = mysql_fetch_assoc($tyojono_result)) {
-		echo "<option value='$tyojono_row[selitetark]'>$tyojono_row[selitetark]</option>";
+	foreach ($request['tyojonot'] as $tyojono) {
+		echo "<option value='{$tyojono['value']}'>{$tyojono['text']}</option>";
 	}
-	echo "</select><br>";
+	echo "</select>";
+	echo "<br/>";
 	echo "<select class='tyostatus_sort'>";
-	echo "<option value=''>".t('Ei rajausta')."</option>";
-	echo "<option value='EISTATUSTA'>".t("Ei statusta")."</option>";
-
-	// Haetaan tyostatus avainsanat
-	$tyostatus_result = t_avainsana("TYOM_TYOSTATUS");
-	while ($tyostatus_row = mysql_fetch_assoc($tyostatus_result)) {
-		echo "<option value='$tyostatus_row[selitetark]'>$tyostatus_row[selitetark]</option>";
+	foreach ($request['tyostatukset'] as $tyostatus) {
+		echo "<option value='{$tyostatus['value']}'>{$tyostatus['text']}</option>";
 	}
 	echo "</select>";
 	echo "</td>";
@@ -153,9 +137,108 @@ function echo_tyojono_table($request = array()) {
 	echo "<td><input type='hidden' class='search_field' name='search_muokkaa_haku'></td>";
 	echo "<td style='visibility:hidden; display:none;'><input type='hidden' class='search_field' name='search_statusjono_haku'></td>";
 	echo "</tr>";
+
 	echo "</thead>";
 
 
+	echo "<tbody>";
+
+	foreach ($request['tyomaaraykset'] as $tyomaarays) {
+		echo "<tr>";
+
+		echo "<td>";
+		echo $tyomaarays['lasku_tunnus'];
+		echo "</td>";
+
+		echo "<td>";
+		echo $tyomaarays['asiakas_ytunnus'] . '<br/>' . $tyomaarays['asiakas_nimi'];
+		echo "</td>";
+
+		echo "<td>";
+		echo $tyomaarays['kohde_nimi'];
+		echo "</td>";
+
+		echo "<td>";
+		echo $tyomaarays['toimaika'];
+		echo "</td>";
+
+		if (!empty($tyomaarays["tyostatusvari"])) {
+			$td_taustavari = "style='background-color: {$tyomaarays['tyostatusvari']};'";
+		}
+		else {
+			$td_taustavari = "";
+		}
+		echo "<td {$td_taustavari}>";
+		
+		echo "<select class='tyojono_muutos'>";
+		foreach ($request['tyojonot'] as $tyojono) {
+			$sel = $tyomaarays['tyojono'] == $tyojono['selitetark'] ? ' SELECTED' : '';
+			echo "<option value='{$tyojono['selite']}' {$sel}>{$tyojono['selitetark']}</option>";
+		}
+		echo "</select>";
+		echo "<br/>";
+		echo "<select class='tyostatus_muutos'>";
+		foreach ($request['tyostatukset'] as $tyostatus) {
+			$sel = $tyomaarays['tyostatus'] == $tyostatus['selitetark'] ? ' SELECTED' : '';
+			echo "<option value='{$tyostatus['selite']}' {$sel}>{$tyostatus['selitetark']}</option>";
+		}
+		echo "</select>";
+
+		echo "</td>";
+
+		echo "<td>";
+		echo "Muokkaa";
+		echo "</td>";
+
+		echo "</tr>";
+	}
+
+	echo "</tbody>";
+	echo "</table>";
+}
+
+function hae_tyojonot($request = array()) {
+	global $kukarow, $yhtiorow;
+
+	$tyojono_result = t_avainsana("TYOM_TYOJONO");
+	$tyojonot = array();
+	$tyojonot[] = array(
+		'value'	 => '',
+		'text'	 => t('Ei rajausta'),
+	);
+	$tyojonot[] = array(
+		'value'	 => 'EIJONOA',
+		'text'	 => t('Ei jonossa'),
+	);
+	while ($tyojono_row = mysql_fetch_assoc($tyojono_result)) {
+		$tyojono_row['value'] = $tyojono_row['selitetark'];
+		$tyojono_row['text'] = $tyojono_row['selitetark'];
+		$tyojonot[] = $tyojono_row;
+	}
+
+	return $tyojonot;
+}
+
+function hae_tyostatukset($request = array()) {
+	global $kukarow, $yhtiorow;
+
+	$tyostatus_result = t_avainsana("TYOM_TYOSTATUS");
+	$tyostatukset = array();
+	$tyostatukset[] = array(
+		'value'	 => '',
+		'text'	 => t('Ei rajausta'),
+	);
+	$tyostatukset[] = array(
+		'value'	 => 'EISTATUSTA',
+		'text'	 => t('Ei statusta'),
+	);
+	while ($tyostatus_row = mysql_fetch_assoc($tyostatus_result)) {
+		$tyostatus_row['value'] = $tyostatus_row['selitetark'];
+		$tyostatus_row['text'] = $tyostatus_row['selitetark'];
+		$tyostatukset[] = $tyostatus_row;
+	}
+
+	return $tyostatukset;
 }
 require ("../inc/footer.inc");
 ?>

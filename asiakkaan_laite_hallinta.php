3f6aeb657d3393e3daa6ce5864024840859c9ecc
Joonas Harjukallio <joonas@devlab.fi>
2013-05-15 13:23:11 +0300

    debug

--- asiakkaan_laite_hallinta.php
+++ asiakkaan_laite_hallinta.php
@@ -507,7 +507,7 @@ function aja_ruby($filepath) {
 	$descriptorspec = array(
 		0	 => array("pipe", "r"), // stdin is a pipe that the child will read from
 		1	 => array("pipe", "w"), // stdout is a pipe that the child will write to
-		2	 => array("file", "/var/log/httpd/demo.devlab.fi_error_log", "a") // stderr is a file to write to
+		2	 => array("file", "/tmp/error.txt", "a") // stderr is a file to write to
 	);
 
 	$cwd = '/tmp';

8470f345db56001c5724f638a11de376a30a5345
Joonas Harjukallio <joonas@devlab.fi>
2013-05-15 15:09:50 +0300

    Temp lasku siivo fiksattu.

--- lasku_siivo.php
+++ lasku_siivo.php
@@ -1,20 +1,31 @@
 <?php
 
-require('../inc/parametrit.inc');
+require('inc/parametrit.inc');
 
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
 if (empty($tee)) {
 	echo "<form action='' method='POST'>";
 	echo "<input type='hidden' name='tee' value='poista' />";
+	echo "<input type='submit' value='Poista' />";
 	echo "</form>";
 }
 else {
 	echo "Poistettu";
-	$query = "	DELETE FROM lasku WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != '';
-			DELETE FROM tilausrivi WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != '';
-			DELETE FROM tyomaarays WHERE yhtio = '{$kukarow['yhtio']}' AND otunnus != '';
-			DELETE FROM laskun_lisatiedot WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != '';
-			DELETE FROM tilausrivin_lisatiedot WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != '';
-			UPDATE laite SET viimeinen_tapahtuma = NULL WHERE yhtio = '{$kukarow['yhtio']}';";
-	pupe_query($query);
+	$query = array(
+		"DELETE FROM lasku WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != ''",
+		"DELETE FROM tilausrivi WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != ''",
+		"DELETE FROM tyomaarays WHERE yhtio = '{$kukarow['yhtio']}' AND otunnus != ''",
+		"DELETE FROM laskun_lisatiedot WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != ''",
+		"DELETE FROM tilausrivin_lisatiedot WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != ''",
+		"UPDATE laite SET viimeinen_tapahtuma = NULL WHERE yhtio = '{$kukarow['yhtio']}'",
+	);
+		
+	foreach ($query as $q) {
+		pupe_query($q);
+	}
 }
+
+require('inc/footer.inc');
 ?>

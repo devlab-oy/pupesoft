9fb5fad504600527fe269ec9d57f640d16fc5435
Antti Jäppinen <antti@devlab.fi>
2013-05-17 15:37:26 +0300

    Myyntilaskujen sisäänluku

--- laskut_in.php
+++ laskut_in.php
@@ -6,6 +6,8 @@
 
 include "inc/parametrit.inc";
 
+$errors = array();
+
 // Formilta on saatu tiedosto
 if (isset($_FILES['userfile']['tmp_name']) and is_uploaded_file($_FILES['userfile']['tmp_name']) === TRUE) {
 
@@ -15,63 +17,90 @@ if (isset($_FILES['userfile']['tmp_name']) and is_uploaded_file($_FILES['userfil
 
 	// Tarkistetaan tiedostopääte
 	if ($ext != 'XLS') {
-		exit("<font class='error'><br>".t("Ainoastaan .xls tiedostot sallittuja")."!</font>");
+		$errors[] = "<font class='error'><br>".t("Ainoastaan .xls tiedostot sallittuja")."!</font>";
 	}
 
 	// Tarkistetaan että tiedosto ei ole tyhjä
 	if ($_FILES['userfile']['size'] == 0) {
-		exit("<font class='error'<br>" . t("Tiedosto on tyhjä") . "!</font>");
+		$errors[] = "<font class='error'<br>" . t("Tiedosto on tyhjä") . "!</font>";
 	}
 
-	// Luetaan excel tiedosto
-	$tiedosto = pupeFileReader($_FILES['userfile']['tmp_name'], 'xls');
+	if (empty($errors)) {
+		// Luetaan excel tiedosto
+		$tiedosto = pupeFileReader($_FILES['userfile']['tmp_name'], 'xls');
 
-	// Loopataan excelistä tunnukset läpi (maksamattomat laskut)
-	foreach ($tiedosto as $rivi) {
-		$maksamattomat_laskut[] = $rivi[0];
+		// Loopataan excelistä tunnukset läpi (maksamattomat laskut)
+		foreach ($tiedosto as $rivi) {
+			$maksamattomat_laskut[] = $rivi[0];
+		}
+
+		// Jos tunnus löytyy avoimista laskuista niin skipataan.
+		$query = "SELECT laskunro, tunnus
+					FROM lasku
+					WHERE yhtio='{$kukarow['yhtio']}'
+					AND mapvm = '0000-00-00 00:00'
+					AND tila = 'L'
+					AND alatila = 'X'";
+		$result = pupe_query($query);
+
+		echo "<font class='message'>";
+		echo t("Avoimia laskuja yhteensä") . ": " . mysql_num_rows($result) . "<br>";
+		echo "</font>";
+
+		/**
+		 * Toimii 'käänteisesti' ja päivittä ne lasku maksetuksi joita EI löydy
+		 * sisäänluettavasta tiedostosta.
+		 */
+
+		// Päivitetään ne laskut maksetuksi joita ei löydy maksamattomat_laskut-listasta
+		$query = "UPDATE lasku
+					SET mapvm = now()
+					WHERE yhtio='{$kukarow['yhtio']}'
+					AND mapvm = '0000-00-00 00:00'
+					AND tila = 'L'
+					AND alatila = 'X'
+					AND laskunro NOT IN (" . implode(', ', $maksamattomat_laskut) . ")";
+		#echo $query."<br>";
+		$result = pupe_query($query);
+
+		echo "<font class='message'>";
+		echo mysql_affected_rows() . " " . t("laskua päivitetty maksetuksi") . "<br>";
+		echo "</font>";
+
+		// Päivitetään kaikki kaikki laskut jotka löytyvät maksamattomat listasta.
+		// Tämä on vain varokeino jos joku lasku on merkattu maksetuksi.
+		$query = "UPDATE lasku
+					SET mapvm = '0000-00-00 00:00'
+					WHERE yhtio='{$kukarow['yhtio']}'
+					AND tila = 'L'
+					AND alatila = 'X'
+					AND mapvm != '0000-00-00 00:00'
+					AND laskunro IN (" . implode(', ', $maksamattomat_laskut) . ")";
+		#echo $query."<br>";
+		$result = pupe_query($query);
+
+		echo "<font class='message'>";
+		echo mysql_affected_rows() . " " . t("laskua korjattu maksamattomaksi") . "<br>";
+		echo "</font>";
 	}
-
-	// TODO: Haetaan kaikki avoimet laskut.
-	// Jos tunnus löytyy avoimista laskuista niin skipataan.
-	/**
-	 * Toimii 'käänteisesti' ja päivittä ne lasku maksetuksi joita EI löydy
-	 * sisäänluettavasta tiedostosta.
-	 */
-
-	// Päivitetään ne laskut maksetuksi joita ei löydy maksamattomat_laskut-listasta
-	$query = "UPDATE lasku
-				SET mapvm = now()
-				WHERE yhtio='{$kukarow['yhtio']}'
-				AND mapvm = '0000'
-				AND laskunro NOT IN (" . implode(', ', $maksamattomat_laskut) . ")";
-	echo "Päivitetään laskuja maksetuiksi<br>";
-	echo $query."<br>";
-	# $result = pupe_query();
-
-	// Päivitetään kaikki kaikki laskut jotka löytyvät maksamattomat listasta.
-	// Tämä on vain varokeino jos joku lasku on merkattu maksetuksi.
-	$query = "UPDATE lasku
-				SET mapvm = '0000'
-				WHERE yhtio='{$kukarow['yhtio']}'
-				AND mapvm != '0000'
-				AND laksunro IN (" . implode(', ', $maksamattomat_laskut) . ")";
-	echo "Päititetään laskuja maksamattomiksi<br>";
-	echo $query."<br>";
-	# $result = pupe_query();
 
 }
 // Form
-else {
+echo "<font class='head'>".t("Myyntireskontran sisäänluku")."</font><hr>";
 
-	echo "<font class='head'>".t("Myyntireskontran sisäänluku")."</font><hr>";
+echo "<form method='post' name='sendfile' enctype='multipart/form-data'>
+		<table>
+			<tr>
+				<th>". t("Valitse tiedosto") . ":</th>
+				<td><input name='userfile' type='file'></td>
+				<td class='back'><input type='submit' value='" . t("Lähetä") . "'></td>
+			<tr>
+		</table>
+	</form>";
 
-	echo "<form method='post' name='sendfile' enctype='multipart/form-data'>
-			<table>
-				<tr>
-					<th>". t("Valitse tiedosto") . ":</th>
-					<td><input name='userfile' type='file'></td>
-					<td class='back'><input type='submit' value='" . t("Lähetä") . "'></td>
-				<tr>
-			</table>
-		</form>";
+// Virheet
+if ($errors) {
+	foreach($errors as $error) {
+		echo $error;
+	}
 }

b3fe609d16c0e1418490ad925fd581f2e3a5eb03
Antti Jäppinen <antti@devlab.fi>
2013-05-17 12:05:40 +0300

    Autoloader

---
+++ models/tapahtumat.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Laskun tapahtumat
+ *
+ */
+class Tapahtumat {
+
+    /**
+     * Luo tapahtuman kun käyttäjä muokkaa työmääräystä
+     */
+    public static function lasku($tyomaarayksen_numero) {
+        global $kukarow;
+
+        // Virheentarkistus
+        $errors = array();
+        if ($kukarow['kuka'] == '') {
+            $errors[] = "Käyttäjää ei löydy";
+        }
+        if ($kukarow['yhtio'] == '') {
+            $errors[] = "Yhtiötä ei löydy";
+        }
+
+        // Lisätään tapahtuma
+        if (empty($errors)) {
+            $query = "INSERT INTO laskun_tapahtumat
+                    SET yhtio = '{$kukarow['yhtio']}',
+                    tyomaarays_numero = $tyomaarayksen_numero,
+                    muuttaja = '{$kukarow['kuka']}',
+                    muutettu = now()";
+
+            echo $query."\n";
+
+            #pupe_query($query);
+            return true;
+        }
+        else {
+            foreach($errors as $error) {
+                echo $error."<br>";
+            }
+            return false;
+        }
+    }
+}

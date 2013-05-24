ac5a72f59144aca68b667c4c8dcc7397f04ee385
Antti Jäppinen <antti@devlab.fi>
2013-05-24 15:22:55 +0300

    Tilausten haku
    Noudetaan 'pendign' tilassa olevat laskut.

--- rajapinnat/magento_tilaukset.php
+++ rajapinnat/magento_tilaukset.php
@@ -13,14 +13,14 @@ require ("{$pupe_root_polku}/rajapinnat/magento_client.php");
 require ("{$pupe_root_polku}/rajapinnat/edi.php");
 
 if (empty($magento_api_edi) or empty($magento_api_url) or empty($magento_api_usr) or empty($magento_api_pas)) {
-	exit;
+	exit("Parametrejä puuttuu\n");
 }
 
 // Magenton soap client
 $magento = new MagentoClient($magento_api_url, $magento_api_usr, $magento_api_pas);
 
 // Haetaan maksetut tilaukset magentosta
-$tilaukset = $magento->hae_tilaukset('paid');
+$tilaukset = $magento->hae_tilaukset('pending');
 
 // Tehdään EDI-tilaukset
 foreach($tilaukset as $tilaus) {

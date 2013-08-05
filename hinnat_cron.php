b9cabe3f91575f55e7df608e51ad9a89f48ae516
Joni Kanerva <joni@devlab.fi>
2013-08-05 16:51:06 +0300

    ini_set tai itket ja ini_set

--- hinnat_cron.php
+++ hinnat_cron.php
@@ -39,6 +39,7 @@
 	ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(__FILE__));
 	error_reporting(E_ALL);
 	ini_set("display_errors", 1);
+	ini_set("memory_limit", "2G");
 
 	// otetaan tietokanta connect ja funktiot
 	require("inc/connect.inc");

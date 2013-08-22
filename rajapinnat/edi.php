613a162333cde23aef9aa630a1fdf2d3693bd019
Joni Kanerva <joni@devlab.fi>
2013-08-22 18:05:37 +0300

    HF: transliteration activated!

--- rajapinnat/edi.php
+++ rajapinnat/edi.php
@@ -190,7 +190,7 @@ class Edi {
 	   	$edi_order .= "*ME\n";
 	   	$edi_order .= "*IE";
 
-	   	$edi_order = iconv("UTF-8", "ISO-8859-1", $edi_order);
+	   	$edi_order = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $edi_order);
 
 		$filename = $magento_api_ana_edi."/magento-order-{$order['increment_id']}-".date("Ymd")."-".md5(uniqid(rand(),true)).".txt";
 		file_put_contents($filename, $edi_order);

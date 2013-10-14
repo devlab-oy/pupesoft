07c4c5088d4bacdfb348d7693efcc1d8fa1b11fb
Sami Luukkonen <sami@devlab.fi>
2013-10-14 15:23:28 +0300

    Seurantakoodin päivitys cron

---
+++ seurantakoodi_ulkoisesta_jarjestelmasta.php
@@ -0,0 +1,120 @@
+<?php
+
+	// Kutsutaanko CLI:stä
+	if (php_sapi_name() != 'cli') {
+		die ("Tätä scriptiä voi ajaa vain komentoriviltä!\n");
+	}
+
+	date_default_timezone_set('Europe/Helsinki');
+
+	if (trim($argv[1]) == '') {
+		die ("Et antanut lähettävää yhtiötä!\n");
+	}
+
+	if (trim($argv[2]) == '') {
+		die ("Et antanut luettavien tiedostojen polkua!\n");
+	}
+
+	// lisätään includepathiin pupe-root
+	ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(__FILE__));
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+	ini_set("memory_limit", "2G");
+
+	// otetaan tietokanta connect ja funktiot
+	require("inc/connect.inc");
+	require("inc/functions.inc");
+
+	// Sallitaan vain yksi instanssi tästä skriptistä kerrallaan
+	pupesoft_flock();
+
+	$yhtio = mysql_escape_string(trim($argv[1]));
+	$yhtiorow = hae_yhtion_parametrit($yhtio);
+
+	// Haetaan kukarow
+	$query = "	SELECT *
+				FROM kuka
+				WHERE yhtio = '{$yhtio}'
+				AND kuka = 'admin'";
+	$kukares = pupe_query($query);
+
+	if (mysql_num_rows($kukares) != 1) {
+		exit("VIRHE: Admin käyttäjä ei löydy!\n");
+	}
+
+	$kukarow = mysql_fetch_assoc($kukares);
+
+	if ($handle = opendir($path)) {
+
+		while (false !== ($file = readdir($handle))) {
+
+			if ($file == '.' or $file == '..' or $file == '.DS_Store' or is_dir($path.$file)) continue;
+
+			$path_parts = pathinfo($file);
+			$ext = strtoupper($path_parts['extension']);
+
+			if ($ext == 'TC') {
+
+				$filehandle = fopen($path.$file, "r");
+
+				while ($tietue = fgets($filehandle)) {
+
+					// Tyhjät rivit skipataan
+					if (trim($tietue) == "") continue;
+
+					list($seurantakoodi, $posten_lahetenumero, $tilausnumero) = explode(';', $tietue);
+
+					$tilausnumero = (int) $tilausnumero;
+					$seurantakoodi = mysql_fetch_assoc($seurantakoodi);
+
+					if ($tilausnumero == 0 or trim($seurantakoodi) == '') continue;
+
+					$query = "	UPDATE rahtikirjat SET
+								rahtikirjanro = '{$seurantakoodi}'
+								WHERE yhtio = '{$kukarow['yhtio']}'
+								AND otsikkonro = '{$tilausnumero}'";
+					pupe_query($query);
+
+					// Katsotaan onko magento-API konffattu, eli verkkokauppa käytössä, silloin merkataan tilaus toimitetuksi Magentossa kun rahtikirja tulostetaan
+					if (isset($magento_api_url) and $magento_api_url != "" and $magento_api_usr != "" and  $magento_api_pas != "") {
+
+						$query = "	SELECT toimitustapa
+									FROM rahtikirjat
+									WHERE yhtio = '{$kukarow['yhtio']}'
+									AND otsikkonro = '{$tilausnumero}'";
+						$chk_res = pupe_query($query);
+
+						if (mysql_num_rows($chk_res) > 0) {
+
+							$chk_row = mysql_fetch_assoc($chk_res);
+
+							$query = "	SELECT *
+										FROM toimitustapa
+										WHERE yhtio = '{$kukarow['yhtio']}'
+										AND selite = '{$chk_row['toimitustapa']}'";
+							$toitares = pupe_query($query);
+							$toitarow = mysql_fetch_assoc($toitares);
+
+							$query = "	SELECT asiakkaan_tilausnumero
+										FROM lasku
+										WHERE yhtio = '{$kukarow['yhtio']}'
+										AND tunnus = '{$tilausnumero}'
+										AND laatija = 'Magento'
+										AND asiakkaan_tilausnumero != ''";
+							$mageres = pupe_query($query);
+
+							while ($magerow = mysql_fetch_assoc($mageres)) {
+								$magento_api_met = $toitarow['virallinen_selite'] != '' ? $toitarow['virallinen_selite'] : $toitarow['selite'];
+								$magento_api_rak = $seurantakoodi;
+								$magento_api_ord = $magerow["asiakkaan_tilausnumero"];
+
+								require("magento_toimita_tilaus.php");
+							}
+						}
+					}
+				}
+			}
+		}
+
+		closedir($handle);
+	}

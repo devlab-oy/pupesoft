4e4cb04063ad8bab63d5653a15b49a7d0bb1fb94
Johan Tötterman <juppe@devlab.fi>
2013-10-25 17:24:43 +0300

    Sorttaus korjattu!

--- hyvak.php
+++ hyvak.php
@@ -1503,7 +1503,7 @@
 		}
 
 		$sarakkeet_vis = 11;
-		
+
 		if ($liitetaanko_editilaus_laskulle_hakemisto != '') {
 			$sarakkeet_vis++;
 			$sarakkeet_base++;
@@ -1556,7 +1556,7 @@
 
 			 // Eli vain tasolla 1/2/3 ja ensimmäiselle hyväksyjälle.
 			if (($kukarow["taso"] == '2' or $kukarow["taso"] == '3') and $onko_eka_hyvaksyja) {
-				echo "<td valign='top'><a href='$PHP_SELF?tee=M&tunnus=$trow[tunnus]'>".tv1dateconv($trow["tapvm"])."</a></td>";
+				echo "<td valign='top'>".pupe_DataTablesEchoSort($trow['tapvm'])."<a href='$PHP_SELF?tee=M&tunnus=$trow[tunnus]'>".tv1dateconv($trow["tapvm"])."</a></td>";
 			}
 			else {
 				echo "<td valign='top'>".pupe_DataTablesEchoSort($trow['tapvm']).tv1dateconv($trow["tapvm"])."</td>";

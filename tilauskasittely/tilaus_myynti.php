5817f21eb1c70b51bb08f098ce6156dfd50aa2b3
Sami Luukkonen <sami@devlab.fi>
2013-11-04 15:49:33 +0200

    checked pois oletuksena

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -8298,7 +8298,7 @@ if ($tee == '') {
 					)
 				) {
 					echo "<br/><br />";
-					echo t("Tulosta lähete uudestaan")," <input type='checkbox' name='tulosta_lahete_uudestaan' value='tulostetaan' checked /> ";
+					echo t("Tulosta lähete uudestaan")," <input type='checkbox' name='tulosta_lahete_uudestaan' value='tulostetaan' /> ";
 
 					$query = "	SELECT *
 								FROM varastopaikat

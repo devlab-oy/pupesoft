35d1ef6e840c426993f3e70b9a139477000a0487
Joonas Harjukallio <joonas@devlab.fi>
2013-11-08 12:02:49 +0200

    Vahinkopöytäkirja import.
    * Lisätty CSVDumper.
    * CSVDumperiin lisätty tuki non-assoc csv-tiedostoille sekä monen tiedoston konvertointi tuki.
    * Vahinkopöytäkirjalle lisätty import tuki.

---
+++ konversio/VahinkopoytakirjaCSVDumper.php
@@ -0,0 +1,124 @@
+<?php
+
+require_once('CSVDumper.php');
+require_once('tilauskasittely/luo_myyntitilausotsikko.inc');
+
+class VahinkopoytakirjaCSVDumper extends CSVDumper {
+
+	protected $unique_values = array();
+
+	public function __construct(array &$kukarow) {
+		parent::__construct($kukarow, false);
+
+		$konversio_array = array(
+			'rekno'			 => 0, //tilivuosi: vvvv
+			'takuunumero'	 => 1, //nippunro: char(10)
+			'prioriteetti'	 => 2, //kiireellisyysluokka: char(1)
+			'valmis'		 => 3, //valmistumispvm (työ päättyi): char(12) vvvvkkpphhmi
+			'viite'			 => 5, //teleliikennealue (TLA): char(3)
+			'suorittaja'	 => 6, //työalue: char(10) selvityksen antaja/urakoitsija
+			'jalleenmyyja'	 => 7, //verkostoalue: char(6)
+			'poistui'		 => 8, //aloitettupvm: char(12) työ alkoi
+			'vpk_valmis'	 => 9, //valmistumispvm (tekn. valmis, työ päättyi): char(12) vvvvkkpphhmi
+			'komm1'			 => 10, //lisatieto: char(80) lisätietoja soneralla ja tapahtumapaikka
+		);
+		$required_fields = array(
+			'nippunro',
+		);
+		$mandatory_fields = array(
+			'lisakuluprosentti' => 7
+		);
+
+		$this->setFolder('/tmp/sp/');
+		$this->setValmisFolder('/tmp/sp/valmis/');
+		$this->setErrorFolder('/tmp/sp/error/');
+		$this->setSeparator(';');
+		$this->setKonversioArray($konversio_array);
+		$this->setRequiredFields($required_fields);
+		$this->addMandatoryFields($mandatory_fields);
+		$this->setTable('tyomaarays');
+	}
+
+	protected function konvertoi_rivit() {
+		$progressbar = new ProgressBar(t('Konvertoidaan rivit'));
+		$progressbar->initialize(count($this->rivit));
+
+		foreach ($this->rivit as $index => &$rivi) {
+			$rivi = $this->konvertoi_rivi($rivi);
+			$rivi = $this->lisaa_pakolliset_kentat($rivi);
+
+			//index + 2, koska eka rivi on header ja laskenta alkaa riviltä 0
+			$valid = $this->validoi_rivi($rivi, $index + 2);
+
+			if (!$valid) {
+				unset($this->rivit[$index]);
+			}
+
+			$progressbar->increase();
+		}
+	}
+
+	protected function konvertoi_rivi($rivi) {
+		$rivi_temp = array();
+
+		foreach ($this->konversio_array as $konvertoitu_header => $csv_header) {
+			if (array_key_exists($csv_header, $rivi)) {
+				if (in_array($konvertoitu_header, array('valmis', 'poistui', 'vpk_valmis'))) {
+					$year = substr($rivi[$csv_header], 0, 4);
+					$month = substr($rivi[$csv_header], 4, 2);
+					$day = substr($rivi[$csv_header], 6, 2);
+					$hour = substr($rivi[$csv_header], 8, 2);
+					$minutes = substr($rivi[$csv_header], 10, 2);
+
+					$rivi_temp[$konvertoitu_header] = "$year-$month-$day $hour:$minutes:00";
+				}
+				else {
+					$rivi_temp[$konvertoitu_header] = $rivi[$csv_header];
+				}
+			}
+		}
+
+		return $rivi_temp;
+	}
+
+	protected function validoi_rivi(&$rivi, $index) {
+		$valid = true;
+		foreach ($rivi as $key => $value) {
+			if (in_array($key, $this->required_fields) and $value == '') {
+				$this->errors[$index][] = t('Pakollinen kenttä')." <b>{$key}</b> ".t('puuttuu');
+				$valid = false;
+			}
+		}
+
+		return $valid;
+	}
+
+	protected function dump_data() {
+		$progress_bar = new ProgressBar(t('Ajetaan rivit tietokantaan'));
+		$progress_bar->initialize(count($this->rivit));
+		$this->kukarow['kesken'] = 0;
+		foreach ($this->rivit as $rivi) {
+			//@TODO työmääräykset liitetään toistaiseksi kaato-asiakkaalle. muuta tämä. aineistossa tulee vahingon aiheuttaja
+			$asiakas = hae_asiakas(100);
+			$lasku_tunnus = luo_myyntitilausotsikko('TYOMAARAYS', $asiakas['tunnus']);
+
+			$query = "	UPDATE {$this->table}\nSET ";
+			foreach ($rivi as $key => $value) {
+				if ($key == 'luontiaika') {
+					$query .= "{$key} = {$value},";
+				}
+				else {
+					$query .= "{$key} = '{$value}',";
+				}
+			}
+			$query = substr($query, 0, -1);
+			$query .= " WHERE otunnus = '{$lasku_tunnus}'";
+
+			pupe_query($query);
+
+			$this->kukarow['kesken'] = 0;
+			$progress_bar->increase();
+		}
+	}
+
+}
---
+++ konversio/konversio.php
@@ -0,0 +1,92 @@
+<?php
+
+ini_set("memory_limit", "5G");
+
+require("../inc/parametrit.inc");
+require_once('VahinkopoytakirjaCSVDumper.php');
+
+$request = array(
+	'action'			 => $action,
+	'konversio_tyyppi'	 => $konversio_tyyppi,
+	'kukarow'			 => $kukarow
+);
+
+$request['konversio_tyypit'] = array(
+	'vahinkopoytakirja' => t('Vahinkopoytakirja'),
+);
+
+if ($request['action'] == 'aja_konversio') {
+	echo_kayttoliittyma($request);
+	echo "<br/>";
+
+	switch ($request['konversio_tyyppi']) {
+		case 'vahinkopoytakirja':
+			$dumper = new VahinkopoytakirjaCSVDumper($kukarow);
+			break;
+
+		default:
+			die('Ei onnistu tämä');
+			break;
+	}
+
+	$dumper->aja();
+}
+else if ($request['action'] == 'poista_konversio_aineisto_kannasta') {
+	$query_array = array(
+		'DELETE FROM tyomaarays',
+		'DELETE FROM lasku',
+		'DELETE FROM laskun_lisatiedot',
+	);
+	foreach ($query_array as $query) {
+		pupe_query($query);
+	}
+
+	echo t('Poistettu');
+	echo "<br/>";
+
+	echo_kayttoliittyma($request);
+}
+else {
+	echo_kayttoliittyma($request);
+}
+
+require('inc/footer.inc');
+
+function echo_kayttoliittyma($request) {
+	global $kukarow, $yhtiorow;
+
+	echo "<form action='' method='POST'>";
+	echo "<input type='hidden' name='action' value='aja_konversio' />";
+	echo "<table>";
+
+	echo "<tr>";
+	echo "<th>".t('Tiedosto')."</th>";
+	echo "<td>";
+	echo "</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<th>".t('Konversio tyyppi')."</th>";
+	echo "<td>";
+	echo "<select name='konversio_tyyppi'>";
+	foreach ($request['konversio_tyypit'] as $konversio_tyyppi => $selitys) {
+		$sel = "";
+		if ($request['konversio_tyyppi'] == $konversio_tyyppi) {
+			$sel = "SELECTED";
+		}
+		echo "<option value='{$konversio_tyyppi}' {$sel}>{$selitys}</option>";
+	}
+	echo "</select>";
+	echo "</td>";
+	echo "</tr>";
+
+	echo "</table>";
+
+	echo "<input type='submit' value='".t('Lähetä')."' />";
+	echo "</form>";
+
+	echo "<form action='' method='POST'>";
+	echo "<input type='hidden' name='action' value='poista_konversio_aineisto_kannasta' />";
+	echo "<input type='submit' value='".t('Poista koko konversio aineisto')."' />";
+	echo "</form>";
+}

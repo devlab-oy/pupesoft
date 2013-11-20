5dbedc4079b19102e6c82583bded04691fcd960a
Hanna-Maria Hagberg <hanna@devlab.fi>
2013-11-20 16:24:14 +0200

    Tehdaspalautukset mukaan ryhma 5teen

--- raportit/alv_laskelma_viro.php
+++ raportit/alv_laskelma_viro.php
@@ -404,6 +404,10 @@
 
 		}
 		elseif ($tilirow['tilit100'] != '' or $tilirow['tilitMUU'] != '') {
+			
+			if ($ryhma != 5) {
+				$tehdaspalautukset_ei = "AND lasku.tilaustyyppi != '9'";
+			}
 
 			echo "<table>";
 			echo "<tr>";
@@ -423,7 +427,7 @@
 						sum(round(tiliointi.summa / if(lasku.vienti_kurssi = 0, 1, lasku.vienti_kurssi) * vero / 100, 2)) $kerroin verot_valuutassa,
 						count(*) kpl
 						FROM tiliointi
-						JOIN lasku ON (lasku.yhtio = tiliointi.yhtio AND lasku.tunnus = tiliointi.ltunnus AND lasku.tilaustyyppi != '9')
+						JOIN lasku ON (lasku.yhtio = tiliointi.yhtio AND lasku.tunnus = tiliointi.ltunnus $tehdaspalautukset_ei)
 						LEFT JOIN tili ON (tili.yhtio = tiliointi.yhtio AND tiliointi.tilino = tili.tilino)
 						WHERE tiliointi.yhtio = '$kukarow[yhtio]'
 						AND tiliointi.korjattu = ''

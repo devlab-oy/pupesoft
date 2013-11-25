21ba852a2110c7f68269ec4436e97a26321542fb
Joonas Harjukallio <joonas@devlab.fi>
2013-11-25 14:04:48 +0200

    Ei välitetä magenton lahjakortti-tuotteista.

--- rajapinnat/magento_client.php
+++ rajapinnat/magento_client.php
@@ -581,10 +581,10 @@ class MagentoClient {
 	 * @param array $kaikki_tuotteet Kaikki tuotteet, jotka pitää LÖYTYÄ Magentosta
 	 * @return   Poistettujen tuotteiden määrä
 	 */
-	public function poista_poistetut(array $kaikki_tuotteet) {
+	public function poista_poistetut(array $kaikki_tuotteet, $exclude_giftcards = false) {
 
 		$count = 0;
-		$skus = $this->getProductList(true);
+		$skus = $this->getProductList(true, $exclude_giftcards);
 
 		// Poistetaan tuottee jotka löytyvät arraysta $kaikki_tuotteet arraystä $skus
 		$poistettavat_tuotteet = array_diff($skus, $kaikki_tuotteet);
@@ -981,11 +981,19 @@ class MagentoClient {
 	 * @param boolean $only_skus 	Palauttaa vain tuotenumerot (true)
 	 * @return array
 	 */
-	private function getProductList($only_skus = false) {
+	private function getProductList($only_skus = false, $exclude_giftcards = false) {
 
 		try {
 			$result = $this->_proxy->call($this->_session, 'catalog_product.list');
 
+			if ($exclude_giftcards) {
+				foreach ($result as $index => $product) {
+					if ($product['type'] == 'giftcards') {
+						unset($result[$index]);
+					}
+				}
+			}
+			
 			if ($only_skus == true) {
 				$skus = array();
 

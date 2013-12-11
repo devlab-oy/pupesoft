5abbbc9df43ff2619915a47b2bae3167cfb6427f
Henri Hesse <hesse@devlab.fi>
2013-12-11 18:13:02 +0200

    Jälkitoimitusten automaattinen vapauttaminen keräykseen.   - tehty croni scripti, joka vapauttaa jt_heti rivejä keräykseen automaattisesti

---
+++ myyntires/jalkitoimitus_cron.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * HOW TO CLI:
+ * php myyntitilauksien_generointi.php yhtio to@example.com
+ */
+
+if (php_sapi_name() != 'cli') {
+    die();
+}
+    
+// otetaan includepath aina rootista
+$pupe_root_polku = dirname(dirname(__FILE__));
+ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . $pupe_root_polku . PATH_SEPARATOR . "/usr/share/pear");
+error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
+ini_set("display_errors", 0);
+
+// otetaan tietokantayhteys ja funkkarit
+require("inc/connect.inc");
+require("inc/functions.inc");
+
+$yhtio = $argv[1];
+
+$varastot = array_slice($argv, 2);
+
+if (empty($yhtio) or empty($varastot)) {
+    echo "\nUsage: php " . basename($argv[0]) . " yhtio varasto varasto\n\n";
+    die;
+}
+
+$yhtiorow = hae_yhtion_parametrit($yhtio);
+
+// Haetaan käyttäjän tiedot
+$query = "	SELECT *
+                FROM kuka
+                WHERE yhtio = '{$yhtio}'
+                AND kuka = 'admin'";
+$result = pupe_query($query);
+
+if (mysql_num_rows($result) == 0) {
+    die("User admin not found");
+}
+
+// Adminin oletus
+$kukarow = mysql_fetch_assoc($result);
+
+
+jt_toimita('', '', $varastot, array(), array(), 'tosi_automaaginen', '', '', '', '', '', 'j');

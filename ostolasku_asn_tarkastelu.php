49276351eba1c5a018627c69702905666874638d
Tony Selkamo <tony@devlab.fi>
2014-01-09 15:50:55 +0200

    Jos jostain syystä löytyy useampi lasku, otetaan tuorein

--- ostolasku_asn_tarkastelu.php
+++ ostolasku_asn_tarkastelu.php
@@ -1860,7 +1860,8 @@
 						WHERE yhtio = '{$kukarow['yhtio']}'
 						AND laskunro = '{$lasku}'
 						AND tila in ('H','Y','M','P','Q')
-						AND vienti in ('B', 'C', 'E', 'F', 'H', 'I')";
+						AND vienti in ('B', 'C', 'E', 'F', 'H', 'I')
+						ORDER BY tapvm desc";
 			$laskures = pupe_query($query);
 
 			if (mysql_num_rows($laskures) == 0) {
@@ -1869,7 +1870,8 @@
 							WHERE yhtio = '{$kukarow['yhtio']}'
 							AND comments = '{$lasku}'
 							AND tila in ('H','Y','M','P','Q')
-							AND vienti in ('B', 'C', 'E', 'F', 'H', 'I')";
+							AND vienti in ('B', 'C', 'E', 'F', 'H', 'I')
+							ORDER BY tapvm desc";
 				$laskures = pupe_query($query);
 			}
 

31dd7a546ef813afe65654e3d4be00c847cebd87
Joonas Harjukallio <joonas@devlab.fi>
2014-02-11 14:04:44 +0200

    Kalustoraportti refaktoroitu lisää

--- pdfs/ruby/kalustoraportti2.rb
+++ pdfs/ruby/kalustoraportti2.rb
@@ -8,83 +8,83 @@ require 'logger'
 require 'date'
 require 'base64'
 
-class KalustoraporttiPDF2
+class KalustoraporttiPDF
   @logo     = nil
   @customer = nil
   @company  = nil
 
   @margin = nil
-  @data   = nil
+  @spot   = nil
+
+  @pdf = nil
 
   def initialize
     @margin = 20
   end
 
-  def generate(_pdf)
+  def generate(_pdf, header_called)
     if _pdf.nil?
-      filepath = "/tmp/Kalustoraportti_#{@data['kohde_tunnus'].to_s}.pdf"
-      filename = "Kalustoraportti_#{@data['kohde_tunnus'].to_s}.pdf"
+      filepath = "/tmp/Kalustoraportti_#{@spot['kohde_tunnus'].to_s}.pdf"
+      filename = "Kalustoraportti_#{@spot['kohde_tunnus'].to_s}.pdf"
       Prawn::Document.generate(filepath,
                                { :page_size   => 'A4',
                                  :page_layout => :landscape,
-                                 :margin      => [@margin, @margin, @margin, @margin]
+                                 :margin      => [100, @margin, @margin, @margin]
                                }) do |pdf|
+        @pdf = pdf
+        @pdf.font 'Helvetica', :style => :normal, :size => 8
+        header
 
-        pdf.font 'Helvetica', :style => :normal, :size => 8
-        header pdf
-
-        info pdf
+        info
 
         filename
       end
     else
-      _pdf.font 'Helvetica', :style => :normal, :size => 8
-      header _pdf
+      @pdf = _pdf
+      @pdf.font 'Helvetica', :style => :normal, :size => 8
+      header unless header_called
+
+      @pdf.move_down 75
+      @pdf.font 'Helvetica', :size => 8
+
+      header_info
+
+      @pdf.move_down 20
+
+      spot_devices
 
       'Kalustoraportit.pdf'
     end
   end
 
-  def header(pdf)
-    pdf.repeat(:all, :dynamic => true) do
-      pdf.draw_text pdf.page_number, :at => [770, 520]
+  def header
+    @pdf.repeat(:all, :dynamic => true) do
+      @pdf.draw_text @pdf.page_number, :at => [770, 520]
 
-      logo pdf
+      logo
 
-      y_temp = pdf.y
-
-      company_info pdf
-
-      pdf.move_up y_temp - pdf.y
-
-      pdf.font 'Helvetica', :size => 8
-
-      header_info pdf
-
-      pdf.move_down 20
-
-      spot_devices pdf
+      company_info
     end
   end
 
-  def logo(pdf)
+  def logo
     filepath = '/tmp/logo.jpeg'
     File.open(filepath, 'a+') { |file|
       file.write Base64.decode64 @logo
     }
-    pdf.image filepath, :scale => 0.7
+    @pdf.image filepath, :scale => 0.7
   end
 
-  def company_info(pdf)
-    pdf.font 'Helvetica', :size => 10
-    pdf.text @company['nimi']
+  def company_info
+    @pdf.font 'Helvetica', :size => 10
+    @pdf.text @company['nimi']
 
-    pdf.move_down 25
-    pdf.text 'KALUSTORAPORTTI', :style => :bold
+    @pdf.move_down 25
+    @pdf.text 'KALUSTORAPORTTI', :style => :bold
   end
 
-  def header_info(pdf)
-    y_temp        = pdf.y
+  def header_info
+    y_temp        = @pdf.y
     customer_data = [
         {
             :header => 'Asiakas nro',
@@ -96,15 +96,15 @@ class KalustoraporttiPDF2
         },
         {
             :header => 'Kohde',
-            :value  => @customer['osoite'].empty? ? ' ' : @customer['osoite']
+            :value  => @spot['kohde_nimi'].empty? ? ' ' : @spot['kohde_nimi']
         },
     ]
 
-    pdf.indent(200) do
-      header_table(pdf, customer_data)
+    @pdf.indent(200) do
+      header_table(customer_data)
     end
 
-    pdf.move_up y_temp - pdf.y
+    @pdf.move_up y_temp - @pdf.y
 
     other_data = [
         {
@@ -117,60 +117,59 @@ class KalustoraporttiPDF2
         },
     ]
 
-    pdf.indent(400) do
-      header_table(pdf, other_data)
+    @pdf.indent(400) do
+      header_table(other_data)
     end
   end
 
-  def header_table(pdf, data)
+  def header_table(data)
     data.each do |value|
-      pdf.float do
-        pdf.text value[:header], :style => :bold
+      @pdf.float do
+        @pdf.text value[:header], :style => :bold
       end
-      pdf.indent(100) do
-        pdf.text value[:value], :style => :normal
+      @pdf.indent(100) do
+        @pdf.text value[:value], :style => :normal
       end
-      pdf.move_down 10
+      @pdf.move_down 10
     end
   end
 
-  def spot_devices(pdf)
-    row_headers pdf
+  def spot_devices
+    row_headers
 
-    pdf.move_down 10
+    @pdf.move_down 10
 
-    rows pdf
+    rows
   end
 
-  def row_headers(pdf)
-    horizontal_line pdf
+  def row_headers
+    horizontal_line
 
     table_cells = [
-        pdf.make_cell(:content => 'Järjestys nro'),
-        pdf.make_cell(:content => 'Laitteen sijainti'),
-        pdf.make_cell(:content => 'Nimike'), #muuttunut sijainti
-        pdf.make_cell(:content => 'Säiliön nro'),
-        pdf.make_cell(:content => 'Ponnep nro'),
-        pdf.make_cell(:content => 'Sammute'), #teholuokka
-        pdf.make_cell(:content => 'Palo-/teholuokka'),
-        pdf.make_cell(:content => 'Valm. vuosi'),
-        pdf.make_cell(:content => 'Tark. väli'), #ponnop nro
-        pdf.make_cell(:content => 'Viimeinen tark kkvv'),
-        pdf.make_cell(:content => 'huolto kkvv'),
-        pdf.make_cell(:content => 'painekoe kkvv'),
-        pdf.make_cell(:content => 'Poikk. raportti'),
+        @pdf.make_cell(:content => 'Järjestys nro'),
+        @pdf.make_cell(:content => 'Laitteen sijainti'),
+        @pdf.make_cell(:content => 'Nimike'), #muuttunut sijainti
+        @pdf.make_cell(:content => 'Säiliön nro'),
+        @pdf.make_cell(:content => 'Ponnep nro'),
+        @pdf.make_cell(:content => 'Sammute'), #teholuokka
+        @pdf.make_cell(:content => 'Palo-/teholuokka'),
+        @pdf.make_cell(:content => 'Valm. vuosi'),
+        @pdf.make_cell(:content => 'Tark. väli'), #ponnop nro
+        @pdf.make_cell(:content => 'Viimeinen tark kkvv'),
+        @pdf.make_cell(:content => 'huolto kkvv'),
+        @pdf.make_cell(:content => 'painekoe kkvv'),
+        @pdf.make_cell(:content => 'Poikk. raportti'),
     ]
 
-    #pdf.table wants 2 dimensional table
-    table pdf, [table_cells]
+    #@pdf.table wants 2 dimensional table
+    table [table_cells]
 
-    horizontal_line pdf
+    horizontal_line
   end
 
-  def rows(pdf)
-
+  def rows
     table_cells = []
-    @data['paikat'].each do |index, place|
+    @spot['paikat'].each do |index, place|
       place['laitteet'].each do |device|
 
         begin
@@ -191,59 +190,58 @@ class KalustoraporttiPDF2
         rescue
         end
 
-
         table_cells << [
-            pdf.make_cell(:content => device['oma_numero']),
-            pdf.make_cell(:content => device['sijainti']),
-            pdf.make_cell(:content => device['tuote_nimi']),
-            pdf.make_cell(:content => device['sarjanro']),
-            pdf.make_cell(:content => device['ponnop_nro']),
-            pdf.make_cell(:content => device['sammutin_tyyppi']),
-            pdf.make_cell(:content => device['palo_luokka']),
-            pdf.make_cell(:content => Date.parse(device['valm_pvm']).year.to_s),
-            pdf.make_cell(:content => device['huoltovali']),
-            pdf.make_cell(:content => tarkastus),
-            pdf.make_cell(:content => huolto),
-            pdf.make_cell(:content => koeponnistus),
-            pdf.make_cell(:content => device['poikkeus']),
+            device['oma_numero'],
+            device['sijainti'],
+            device['tuote_nimi'],
+            device['sarjanro'],
+            device['ponnop_nro'],
+            device['sammutin_tyyppi'],
+            device['palo_luokka'],
+            Date.parse(device['valm_pvm']).year.to_s,
+            device['huoltovali'],
+            tarkastus,
+            huolto,
+            koeponnistus,
+            device['poikkeus']
         ]
       end
     end
 
-    table pdf, table_cells
+    table table_cells
   end
 
-  def table(pdf, table_cells)
-    pdf.table(table_cells,
-              :column_widths => {
-                  0  => 45,
-                  1  => 175,
-                  2  => 70,
-                  3  => 50,
-                  4  => 50,
-                  5  => 70,
-                  6  => 90,
-                  7  => 30,
-                  8  => 40,
-                  9  => 50,
-                  10 => 40,
-                  11 => 50,
-                  12 => 40,
-              },
-              :cell_style    => {
-                  :borders => []
-              })
+  def table(table_cells)
+    @pdf.table(table_cells,
+               :column_widths => {
+                   0  => 45,
+                   1  => 175,
+                   2  => 70,
+                   3  => 50,
+                   4  => 50,
+                   5  => 70,
+                   6  => 90,
+                   7  => 30,
+                   8  => 40,
+                   9  => 50,
+                   10 => 40,
+                   11 => 50,
+                   12 => 40,
+               },
+               :cell_style    => {
+                   :borders => []
+               })
   end
 
-  def horizontal_line(pdf)
+  def horizontal_line
     #Line defines the drawing path. Stroke actually draws the line
-    lines_cross_y = pdf.cursor
-    pdf.line [pdf.bounds.left, lines_cross_y], [pdf.bounds.right, lines_cross_y]
-    pdf.stroke
+    lines_cross_y = @pdf.cursor
+    @pdf.line [@pdf.bounds.left, lines_cross_y], [@pdf.bounds.right, lines_cross_y]
+    @pdf.stroke
   end
 
-  def data=(data)
-    @data = data
+  def spot=(spot)
+    @spot = spot
   end
 
   def customer=(customer)
@@ -280,21 +278,24 @@ if !ARGV[0].empty?
 
   spots = SpotDAO.new(ARGV[0])
 
-  file   = ''
-  margin = 20
-  _pdf   = Prawn::Document.new(:page_size   => 'A4',
-                               :page_layout => :landscape,
-                               :margin      => margin
+  file          = ''
+  margin        = 20
+  _pdf          = Prawn::Document.new(:page_size   => 'A4',
+                                      :page_layout => :landscape,
+                                      :margin      => margin
   )
-  i      = 0
+  i             = 0
+  header_called = false
   spots.data['kohteet'].each do |index, spot|
-    pdf          = KalustoraporttiPDF2.new
+    pdf          = KalustoraporttiPDF.new
     pdf.customer = spots.data['asiakas']
     pdf.company  = spots.data['yhtio']
     pdf.logo     = spots.data['logo']
-    pdf.data     = spot
+    pdf.spot     = spot
 
-    file = pdf.generate _pdf
+    file = pdf.generate _pdf, header_called
+
+    header_called = true
 
     if i != spots.data['kohteet'].count - 1
       _pdf.start_new_page

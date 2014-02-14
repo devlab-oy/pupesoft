7fdbf1648ffbe3a4a05e747bbfc36b7ecca4dcc7
Joonas Harjukallio <joonas@devlab.fi>
2014-02-14 14:34:04 +0200

    * kohteen ensimmäiselle työmääräykselle käyntiveloitus

--- lasku_siivo.php
+++ lasku_siivo.php
@@ -19,35 +19,35 @@ else {
 		"DELETE FROM tyomaarays WHERE yhtio = '{$kukarow['yhtio']}' AND otunnus != ''",
 		"DELETE FROM laskun_lisatiedot WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != ''",
 		"DELETE FROM tilausrivin_lisatiedot WHERE yhtio = '{$kukarow['yhtio']}' AND tunnus != ''",
-		"	UPDATE huoltosyklit_laitteet
-			JOIN huoltosykli
-			ON ( huoltosykli.yhtio = huoltosyklit_laitteet.yhtio
-				AND huoltosykli.tunnus = huoltosyklit_laitteet.huoltosykli_tunnus )
-			JOIN tuote
-			ON ( tuote.yhtio = huoltosykli.yhtio
-				AND tuote.tuoteno = huoltosykli.toimenpide)
-			LEFT JOIN tuotteen_avainsanat AS tt
-			ON ( tt.yhtio = tuote.yhtio
-				AND tt.tuoteno = tuote.tuoteno)
-			SET huoltosyklit_laitteet.viimeinen_tapahtuma = NULL
-			WHERE huoltosyklit_laitteet.yhtio = '{$kukarow['yhtio']}'
-			AND tt.selite != 'koeponnistus'",
-		"	UPDATE huoltosyklit_laitteet
-			JOIN laite
-			ON ( laite.yhtio = huoltosyklit_laitteet.yhtio
-				AND laite.tunnus = huoltosyklit_laitteet.laite_tunnus )
-			JOIN huoltosykli
-			ON ( huoltosykli.yhtio = huoltosyklit_laitteet.yhtio
-				AND huoltosykli.tunnus = huoltosyklit_laitteet.huoltosykli_tunnus )
-			JOIN tuote
-			ON ( tuote.yhtio = huoltosykli.yhtio
-				AND tuote.tuoteno = huoltosykli.toimenpide)
-			LEFT JOIN tuotteen_avainsanat AS tt
-			ON ( tt.yhtio = tuote.yhtio
-				AND tt.tuoteno = tuote.tuoteno)
-			SET huoltosyklit_laitteet.viimeinen_tapahtuma = laite.valm_pvm
-			WHERE huoltosyklit_laitteet.yhtio = '{$kukarow['yhtio']}'
-			AND tt.selite = 'koeponnistus'",
+//		"	UPDATE huoltosyklit_laitteet
+//			JOIN huoltosykli
+//			ON ( huoltosykli.yhtio = huoltosyklit_laitteet.yhtio
+//				AND huoltosykli.tunnus = huoltosyklit_laitteet.huoltosykli_tunnus )
+//			JOIN tuote
+//			ON ( tuote.yhtio = huoltosykli.yhtio
+//				AND tuote.tuoteno = huoltosykli.toimenpide)
+//			LEFT JOIN tuotteen_avainsanat AS tt
+//			ON ( tt.yhtio = tuote.yhtio
+//				AND tt.tuoteno = tuote.tuoteno)
+//			SET huoltosyklit_laitteet.viimeinen_tapahtuma = NULL
+//			WHERE huoltosyklit_laitteet.yhtio = '{$kukarow['yhtio']}'
+//			AND tt.selite != 'koeponnistus'",
+//		"	UPDATE huoltosyklit_laitteet
+//			JOIN laite
+//			ON ( laite.yhtio = huoltosyklit_laitteet.yhtio
+//				AND laite.tunnus = huoltosyklit_laitteet.laite_tunnus )
+//			JOIN huoltosykli
+//			ON ( huoltosykli.yhtio = huoltosyklit_laitteet.yhtio
+//				AND huoltosykli.tunnus = huoltosyklit_laitteet.huoltosykli_tunnus )
+//			JOIN tuote
+//			ON ( tuote.yhtio = huoltosykli.yhtio
+//				AND tuote.tuoteno = huoltosykli.toimenpide)
+//			LEFT JOIN tuotteen_avainsanat AS tt
+//			ON ( tt.yhtio = tuote.yhtio
+//				AND tt.tuoteno = tuote.tuoteno)
+//			SET huoltosyklit_laitteet.viimeinen_tapahtuma = laite.valm_pvm
+//			WHERE huoltosyklit_laitteet.yhtio = '{$kukarow['yhtio']}'
+//			AND tt.selite = 'koeponnistus'",
 	);
 	//toka vika query: ei p‰ivitet‰ koeponnistus huoltosyklien viimeist‰ tapahtumap‰iv‰‰ nulliksi.
 	//vika query: p‰ivitet‰‰n laite.valm_pvm koeponnistuksen huoltosyklit_laitteet riville

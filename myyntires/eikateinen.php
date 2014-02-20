462d26b2be525e0eb3f5b1c4fdbfb23ae99986e0
Satu Aalto <satu@devlab.fi>
2014-02-20 15:03:52 +0200

    Kommenttirivi pois

--- myyntires/eikateinen.php
+++ myyntires/eikateinen.php
@@ -39,7 +39,6 @@ if ((int) $maksuehto != 0 and (int) $tunnus != 0) {
 	$tilikausi = tarkista_saako_laskua_muuttaa($tapahtumapaiva);
 	$tilikausi_lasku = tarkista_saako_laskua_muuttaa($laskurow['tapvm']);
 
-	#if (empty($tilikausi) and empty($tilikausi_lasku) and !$laskupvmerror and !$laskumaksettuerror) {
 	if (!$laskupvmerror and ((empty($tilikausi) and empty($tilikausi_lasku)) or ($toim == 'KATEINEN' and !$laskumaksettuerror))) {
 		$mehtorow 		= hae_maksuehto($maksuehto);
 		$konsrow  		= hae_asiakas($laskurow);

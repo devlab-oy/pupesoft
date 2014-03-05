b4dbdd364a877f7e67e5b6c4be1e3303e7d46f21
Oskari Paakkanen <oskari@devlab.fi>
2014-03-05 16:07:28 +0200

    välicommit

--- raportit/siirtokehotus.php
+++ raportit/siirtokehotus.php
@@ -1,45 +1,48 @@
 <?php
 
-	if (isset($_POST["tee"])) {
-		if($_POST["tee"] == 'lataa_tiedosto') $lataa_tiedosto=1;
-		if(isset($_POST["kaunisnimi"]) and $_POST["kaunisnimi"] != '') $_POST["kaunisnimi"] = str_replace("/","",$_POST["kaunisnimi"]);
-	}
+if (isset($_POST["tee"])) {
+	if($_POST["tee"] == 'lataa_tiedosto'){ $lataa_tiedosto=1; }
+	if(isset($_POST["kaunisnimi"]) and $_POST["kaunisnimi"] != ''){ $_POST["kaunisnimi"] = str_replace("/","",$_POST["kaunisnimi"]); }
+}
 
-	if (strpos($_SERVER['SCRIPT_NAME'], "siirtokehotus.php") !== FALSE) {
-		require ("../inc/parametrit.inc");
-	}
+if (strpos($_SERVER['SCRIPT_NAME'], "siirtokehotus.php") !== FALSE) {
+	require ("../inc/parametrit.inc");
+}
 
-	if (isset($tee) and $tee == "lataa_tiedosto") {
-		readfile("/tmp/".$tmpfilenimi);
-		exit;
-	}
+if (isset($tee) and $tee == "lataa_tiedosto") {
+	readfile("/tmp/".$tmpfilenimi);
+	exit;
+}
+
+echo "<font class='head'>" . t("Siirtokehotusraportti") . "</font><hr>";
+
+if (isset($tee) and $tee == "hae_raportti" and count($varasto) < 1) {
+	$tee = '';
+	$ei_varastoa = true;
+}
 
 if (isset($tee) and $tee == "hae_raportti") {
 
-$query = "SELECT t4.tuoteno AS tuoteno,
-			t2.tunnus AS varasto,
-			t4.halytysraja AS  haly,
-			t4.oletus AS oletus,
-			CONCAT(t4.hyllyalue, '-', t4.hyllynro, '-', t4.hyllyvali, '-', t4.hyllytaso ) AS tuotepaikka,
-			t4.hyllyalue AS alue,
-			t4.hyllynro AS nro,
-			t4.hyllyvali AS vali,
-			t4.hyllytaso AS taso
-			FROM keraysvyohyke AS t1
-			JOIN varastopaikat AS t2
-				ON ( t1.yhtio = t2.yhtio AND t1.varasto = t2.tunnus )
-			JOIN tuote AS t3
-				ON ( t2.yhtio = t3.yhtio AND t1.tunnus = t3.keraysvyohyke)
-			JOIN tuotepaikat AS t4
-				ON ( t3.yhtio = t4.yhtio AND t3.tuoteno = t4.tuoteno)
-			WHERE t1.yhtio = '{$kukarow['yhtio']}'
-			AND t2.tunnus IN (139,121)
-			AND t4.halytysraja > 0
-			AND t4.oletus = 'X'";
+		//$varastot = implode(",",$varasto);
 
-		$result = pupe_query($query);
 
-echo '<table>';
+		$query = "SELECT 	tuotepaikat.tuoteno AS tuoteno,
+							tuotepaikat.halytysraja AS haly,
+							tuotepaikat.oletus AS oletus,
+							CONCAT(tuotepaikat.hyllyalue, '-', tuotepaikat.hyllynro, '-', tuotepaikat.hyllyvali, '-', tuotepaikat.hyllytaso ) AS tuotepaikka,
+							tuotepaikat.hyllyalue AS alue,
+							tuotepaikat.hyllynro AS nro,
+							tuotepaikat.hyllyvali AS vali,
+							tuotepaikat.hyllytaso AS taso,
+							concat(rpad(upper(hyllyalue), 5, '0'),lpad(upper(hyllynro), 5, '0')) AS varastokoodi
+							FROM tuotepaikat
+							WHERE tuotepaikat.yhtio = '{$kukarow['yhtio']}'
+							AND tuotepaikat.halytysraja > 0
+							AND tuotepaikat.oletus = 'X'";
+
+			$result = pupe_query($query);
+
+		echo '<table>';
 
 		echo '<tr>';
 		echo '<th>';
@@ -61,6 +64,34 @@ echo '<table>';
 
 		while ($row = mysql_fetch_assoc($result)) {
 
+
+			if( isset($keraysvyohyke) and count($keraysvyohyke) > 0 ){
+				$keraysvyohykkeet = implode(",",$keraysvyohyke);
+				$kv_join = "JOIN keraysvyohyke ON (keraysvyohyke.yhtio = varastopaikat.yhtio AND keraysvyohyke.varasto = varastopaikat.tunnus)";
+				$kv_and = "AND keraysvyohyke.tunnus IN ({$keraysvyohykkeet})";
+			}
+			else{
+				$kv_join = "";
+				$kv_and = "";
+			}
+
+			$varasto_query = "	SELECT varastopaikat.tunnus AS vt, keraysvyohyke.tunnus AS kt
+								FROM varastopaikat
+								{$kv_join}
+								WHERE concat(rpad(upper(alkuhyllyalue), 5, '0'),lpad(upper(alkuhyllynro), 5, '0')) <= '{$row['varastokoodi']}'
+								AND concat(rpad(upper(loppuhyllyalue), 5, '0'),lpad(upper(loppuhyllynro), 5, '0')) >= '{$row['varastokoodi']}'
+								AND varastopaikat.yhtio = '{$kukarow['yhtio']}'
+								AND tyyppi != 'P'
+								{$kv_and}";
+
+			$varasto_result = pupe_query($varasto_query);
+
+			if( !in_array(mysql_result($varasto_result, 0), $varasto)  ){
+				continue;
+			}
+
+			$row['kv'] = mysql_result($varasto_result, 0, 'kt');
+
 			$saldo_info = saldo_myytavissa($row['tuoteno'], '', $row['varasto'], $kukarow['yhtio'], $row['alue'], $row['nro'], $row['vali'], $row['taso'] );
 			$row['hyllyssa'] = $saldo_info[1];
 
@@ -68,16 +99,15 @@ echo '<table>';
 				continue;
 			}
 
-
 			$query2 = "SELECT CONCAT(hyllyalue, '-', hyllynro, '-', hyllyvali, '-', hyllytaso ) AS tuotepaikka,
-						hyllyalue AS alue,
-						hyllynro AS nro,
-						hyllyvali AS vali,
-						hyllytaso AS taso
-						FROM tuotepaikat
-						WHERE tuoteno = '{$row['tuoteno']}'
-						AND yhtio = '{$kukarow['yhtio']}'
-						AND oletus != 'X'";
+							hyllyalue AS alue,
+							hyllynro AS nro,
+							hyllyvali AS vali,
+							hyllytaso AS taso
+							FROM tuotepaikat
+							WHERE tuoteno = '{$row['tuoteno']}'
+							AND yhtio = '{$kukarow['yhtio']}'
+							AND oletus != 'X'";
 
 			$varapaikka_echo = '';
 			$result2 = pupe_query($query2);
@@ -90,11 +120,11 @@ echo '<table>';
 				}
 
 				$varapaikka_echo .= '<tr>';
-				$varapaikka_echo .= '<td>';
+				$varapaikka_echo .= '<th>';
 				$varapaikka_echo .= 'Varapaikka';
-				$varapaikka_echo .= '</td>';
-				$varapaikka_echo .= '<td>';
-				$varapaikka_echo .= '';
+				$varapaikka_echo .= '</th>';
+				$varapaikka_echo .= '<td style="color:silver;">';
+				$varapaikka_echo .= $row['tuoteno'];
 				$varapaikka_echo .= '</td>';
 				$varapaikka_echo .= '<td>';
 				$varapaikka_echo .= $row2['tuotepaikka'];
@@ -109,25 +139,22 @@ echo '<table>';
 
 			}
 
-
 			if( $varapaikka_echo == '' ){
 				continue;
 			}
 
-
-
+			//tyhjä rivi ennen jokaista oletuspaikkaa
 			echo '<tr>';
-			echo '<td colspan="12" style="background:#cbd9e1">';
+			echo '<td colspan="12" style="background:#cbd9e1; padding:4px;">';
 			echo '</td>';
-			echo '<tr>';
+			echo '</tr>';
 
-
 			echo '<tr>';
-			echo '<td>';
+			echo '<th>';
 			echo 'Oletuspaikka';
-			echo '</td>';
+			echo '</th>';
 			echo '<td>';
-			echo $row['tuoteno'];
+			echo $row['tuoteno'] . "-" . $row['kv'];
 			echo '</td>';
 			echo '<td>';
 			echo $row['tuotepaikka'];
@@ -138,81 +165,61 @@ echo '<table>';
 			echo '<td>';
 			echo $row['haly'];
 			echo '</td>';
-			echo '<tr>';
-
+			echo '</tr>';
 			echo $varapaikka_echo;
 
 		}
 
-echo '</table>';
-
+		echo '</table>';
 }
-	else {
-		echo "<font class='head'>",t("Siirtokehotusraportti"),"</font><hr>";
+else {
 
-		if (!aja_kysely()) {
-			unset($_REQUEST);
-		}
-
-
-		echo "<form action='$PHP_SELF' method='post' autocomplete='off'>";
-		echo "<table>";
-
-
-
-		echo "<tr><th align='left'>",t("Varasto"),":</td>";
-		echo "<td>";
-
-		$query  = "	SELECT *
-					FROM varastopaikat
-					WHERE yhtio = '{$kukarow['yhtio']}' AND tyyppi != 'P'
-					ORDER BY tyyppi, nimitys";
-
-		$vares = pupe_query($query);
-
-		$varastot_array = explode(",", $krow["varasto"]);
-
-		while ($varow = mysql_fetch_assoc($vares)) {
-			$sel = $eri = '';
-			if (in_array($varow['tunnus'], $varastot_array)) $sel = 'CHECKED';
-			if ($varow["tyyppi"] == "E") $eri = "(E)";
-			echo "<input type='checkbox' name='varasto[]' value='{$varow['tunnus']}' {$sel}> {$varow['nimitys']} {$eri}<br>";
-		}
-
-
-
-
-		$query = "SELECT tunnus, nimitys FROM keraysvyohyke WHERE yhtio = '{$kukarow['yhtio']}' AND nimitys != ''";
-		$keraysvyohyke_result = pupe_query($query);
-
-		if (mysql_num_rows($keraysvyohyke_result) > 0) {
-			echo "<tr><th align='left'>",t("Keräysvyöhyke"),":</th><td>";
-			echo "<input type='hidden' name='keraysvyohyke[]' value='default' />";
-
-			while ($keraysvyohyke_row = mysql_fetch_assoc($keraysvyohyke_result)) {
-				$chk = strpos($krow['keraysvyohyke'], $keraysvyohyke_row['tunnus']) !== false ? ' checked' : '';
-				echo "<input type='checkbox' name='keraysvyohyke[]' value='{$keraysvyohyke_row['tunnus']}'{$chk} />&nbsp;{$keraysvyohyke_row['nimitys']}<br />";
-			}
-			echo "</td></tr>";
-		}
-
-
-
-		echo "</td></tr>";
-		echo "</table>";
-		echo "<input type='hidden' name='tee' value='hae_raportti'>";
-		echo "<br><input type='submit' name='hae_raportti' value='",t("Hae raportti"),"'></form>";
-
-		if (isset($tee) and $tee == "hae_raportti") {
-
-
-			echo '<br />';
-			echo '<hr />';
-			echo "raporttien haku";
-
-		}
-
-		if (strpos($_SERVER['SCRIPT_NAME'], "siirtokehotus.php") !== FALSE) {
-			require ("../inc/footer.inc");
-		}
+	if( $ei_varastoa === true ){
+		echo "<font class='error'>Vähintään yksi varasto on valittava</font>";
 	}
+
+	echo "<form action='$PHP_SELF' method='post' autocomplete='off'>";
+	echo "<table>";
+	echo "<tr><th align='left'>" . t("Varasto") . ": <br /><br /><span style='text-transform: none;'>Valitse vähintään<br />yksi varasto.</span></td>";
+	echo "<td>";
+
+	$query  = "	SELECT *
+				FROM varastopaikat
+				WHERE yhtio = '{$kukarow['yhtio']}' AND tyyppi != 'P'
+				ORDER BY tyyppi, nimitys";
+
+	$vares = pupe_query($query);
+
+	$varastot_array = explode(",", $krow["varasto"]);
+
+	while ($varow = mysql_fetch_assoc($vares)) {
+		$sel = $eri = '';
+		if (in_array($varow['tunnus'], $varastot_array)) $sel = 'CHECKED';
+		if ($varow["tyyppi"] == "E") $eri = "(E)";
+		echo "<input type='checkbox' name='varasto[]' value='{$varow['tunnus']}' {$sel}> {$varow['nimitys']} {$eri}<br>";
+	}
+
+	$query = "SELECT tunnus, nimitys FROM keraysvyohyke WHERE yhtio = '{$kukarow['yhtio']}' AND nimitys != ''";
+	$keraysvyohyke_result = pupe_query($query);
+
+	if (mysql_num_rows($keraysvyohyke_result) > 0) {
+		echo "<tr><th align='left'>",t("Keräysvyöhyke"),":</th><td>";
+		//echo "<input type='hidden' name='keraysvyohyke[]' value='default' />";
+
+		while ($keraysvyohyke_row = mysql_fetch_assoc($keraysvyohyke_result)) {
+			$chk = strpos($krow['keraysvyohyke'], $keraysvyohyke_row['tunnus']) !== false ? ' checked' : '';
+			echo "<input type='checkbox' name='keraysvyohyke[]' value='{$keraysvyohyke_row['tunnus']}'{$chk} />&nbsp;{$keraysvyohyke_row['nimitys']}<br />";
+		}
+		echo "</td></tr>";
+	}
+
+	echo "</td></tr>";
+	echo "</table>";
+	echo "<input type='hidden' name='tee' value='hae_raportti'>";
+	echo "<br><input type='submit' name='hae_raportti' value='",t("Hae raportti"),"'></form>";
+}
+
+if (strpos($_SERVER['SCRIPT_NAME'], "siirtokehotus.php") !== FALSE) {
+	require ("../inc/footer.inc");
+}
+

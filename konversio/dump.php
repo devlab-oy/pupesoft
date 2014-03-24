8aeaeb5391d262f48a85c9daabd19af30ad9f9d6
Joonas Harjukallio <joonas@devlab.fi>
2014-03-24 15:13:48 +0200

    * dump bugfix

--- konversio/dump.php
+++ konversio/dump.php
@@ -5,7 +5,9 @@ function dump_seed_data() {
 
 	echo "Ajetaan.....";
 	echo "<br/>";
-	$query = "DELETE FROM avainsana WHERE yhtio = '{$kukarow['yhtio']}' AND laji = 'TYOM_TYOSTATUS';
+	$query = "DELETE FROM avainsana WHERE yhtio = '{$kukarow['yhtio']}' AND laji IN ('TYOM_TYOSTATUS','LAITE_TILA');";
+	pupe_query($query);
+	$query = "
 INSERT INTO `avainsana` (`yhtio`, `perhe`, `kieli`, `laji`, `nakyvyys`, `selite`, `selitetark`, `selitetark_2`, `selitetark_3`, `selitetark_4`, `selitetark_5`, `jarjestys`, `laatija`, `luontiaika`, `muutospvm`, `muuttaja`)
 VALUES
 	('{$kukarow['yhtio']}', 7, 'fi', 'TYOM_TYOSTATUS', '', 'A', 'Aikataulutettu', '#00ff00', '', '', NULL, 0, 'import', NOW(), NOW(), 'import'),
@@ -13,8 +15,9 @@ VALUES
 	('{$kukarow['yhtio']}', 9, 'fi', 'TYOM_TYOSTATUS', '', 'T', 'Työlista tulostettu', '#000000', '', '', NULL, 0, 'import', NOW(), NOW(), 'import'),
 	('{$kukarow['yhtio']}', 10, 'fi', 'TYOM_TYOSTATUS', '', 'X', 'Tehty', '', '', '', NULL, 0, 'import', NOW(), NOW(), 'import'),
 	('{$kukarow['yhtio']}', 12, 'fi', 'TYOM_TYOSTATUS', '', 'V', 'Laite vaihdettu', '#FFFB00', '', '', NULL, 0, 'import', NOW(), NOW(), 'import'),
-	('{$kukarow['yhtio']}', 12, 'fi', 'TYOM_TYOSTATUS', '', 'K', 'Laite kadonnut', '#cd7f31', '', '', NULL, 0, 'import', NOW(), NOW(), 'import');
-DELETE FROM avainsana WHERE yhtio = '{$kukarow['yhtio']}' AND laji = 'LAITE_TILA';
+	('{$kukarow['yhtio']}', 12, 'fi', 'TYOM_TYOSTATUS', '', 'K', 'Laite kadonnut', '#cd7f31', '', '', NULL, 0, 'import', NOW(), NOW(), 'import');";
+	pupe_query($query);
+	$query = "
 INSERT INTO `avainsana` (`yhtio`, `perhe`, `kieli`, `laji`, `nakyvyys`, `selite`, `selitetark`, `selitetark_2`, `selitetark_3`, `selitetark_4`, `selitetark_5`, `jarjestys`, `laatija`, `luontiaika`, `muutospvm`, `muuttaja`)
 VALUES
 	('{$kukarow['yhtio']}', 382085, 'fi', 'LAITE_TILA', '', 'N', 'Normaali', '', '', '', NULL, 1, 'import', NOW(), NOW(), 'import'),
@@ -25,26 +28,31 @@ VALUES
 	pupe_query($query);
 
 	if (!isset($yhtiorow['laite_huolto'])) {
-		$query =	"ALTER TABLE yhtion_parametrit ADD COLUMN laite_huolto CHAR(1) NOT NULL DEFAULT '' AFTER vastaavat_tuotteet_esitysmuoto;
-					UPDATE yhtion_parametrit SET laite_huolto = 'X' WHERE yhtio = '{$kukarow['yhtio']}';";
+		$query =	"ALTER TABLE yhtion_parametrit ADD COLUMN laite_huolto CHAR(1) NOT NULL DEFAULT '' AFTER vastaavat_tuotteet_esitysmuoto;";
+		pupe_query($query);
+		$query =	"UPDATE yhtion_parametrit SET laite_huolto = 'X' WHERE yhtio = '{$kukarow['yhtio']}';";
 		pupe_query($query);
 	}
 
-	$query = "DELETE FROM avainsana where yhtio = '{$kukarow['yhtio']}' AND laji = 'OLOSUHDE';
+	$query = "DELETE FROM avainsana where yhtio = '{$kukarow['yhtio']}' AND laji = 'OLOSUHDE';";
+	pupe_query($query);
+	$query = "
 INSERT INTO `avainsana` (`yhtio`, `perhe`, `kieli`, `laji`, `nakyvyys`, `selite`, `selitetark`, `selitetark_2`, `selitetark_3`, `selitetark_4`, `selitetark_5`, `jarjestys`, `laatija`, `muuttaja`)
 VALUES
 	('{$kukarow['yhtio']}', 3, 'fi', 'OLOSUHDE', '', 'X', 'Ulkona / tärinässä', '', '', '', NULL, 0, 'import', 'import'),
 	('{$kukarow['yhtio']}', 4, 'fi', 'OLOSUHDE', '', 'A', 'Sisällä', '', '', '', NULL, 0, 'import', 'import');";
 	pupe_query($query);
 
-	$query = "DELETE FROM oikeu WHERE yhtio = '{$kukarow['yhtio']}' AND sovellus = 'Sammutinhuolto';
+	$query = "DELETE FROM oikeu WHERE yhtio = '{$kukarow['yhtio']}' AND sovellus = 'Sammutinhuolto';";
+	pupe_query($query);
+	$query = "
 INSERT INTO `oikeu` (`kuka`, `sovellus`, `nimi`, `alanimi`, `paivitys`, `lukittu`, `nimitys`, `jarjestys`, `jarjestys2`, `profiili`, `yhtio`, `hidden`, `laatija`, `luontiaika`, `muutospvm`, `muuttaja`)
 VALUES
 	('', 'Sammutinhuolto', 'tyomaarays/tyojono2.php', '', '', '', 'Työjono', 10, 0, '', '{$kukarow['yhtio']}', '', 'import', NOW(), NOW(), 'import'),
 	('', 'Sammutinhuolto', 'tyomaarays/tyojono2.php', 'TEHDYT_TYOT', '', '', 'Tehdyt työ', 20, 0, '', '{$kukarow['yhtio']}', '', 'import', NOW(), NOW(), 'import'),
 	('', 'Sammutinhuolto', 'tyomaarays/tulevat_tyot.php', '', '', '', 'Tulevat työt', 21, 0, '', '{$kukarow['yhtio']}', '', 'import', NOW(), NOW(), 'import'),
-	('', 'Sammutinhuolto', 'asiakkaan_laite_hallinta.php', '', '', '', 'Asiakkaan laitehallinta', 30, 0, '', '{$kukarow['yhtio']}', '', NOW(), NOW(), 'import'),
-	('', 'Sammutinhuolto', 'yllapito.php', 'asiakas!!!TURVANASI!!!true', '', '', 'Asiakasylläpito', 40, 0, '', '{$kukarow['yhtio']}', '', NOW(), NOW(), 'import'),
+	('', 'Sammutinhuolto', 'asiakkaan_laite_hallinta.php', '', '', '', 'Asiakkaan laitehallinta', 30, 0, '', '{$kukarow['yhtio']}', '', 'import',NOW(), NOW(), 'import'),
+	('', 'Sammutinhuolto', 'yllapito.php', 'asiakas!!!TURVANASI!!!true', '', '', 'Asiakasylläpito', 40, 0, '', '{$kukarow['yhtio']}', '', 'import',NOW(), NOW(), 'import'),
 	('', 'Sammutinhuolto', 'yllapito.php', 'kohde', '', '', 'Kohteet', 50, 0, '', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
 	('', 'Sammutinhuolto', 'yllapito.php', 'paikka', '', '', 'Paikat', 60, 0, '', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
 	('', 'Sammutinhuolto', 'yllapito.php', 'laite', '', '', 'Laitteet', 80, 0, '', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
@@ -58,8 +66,9 @@ VALUES
 	('', 'Sammutinhuolto', 'tilauskasittely/laitteen_vaihto.php', '', '', '', 'Laitteenvaihto', 400, 0, '', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
 	('', 'Sammutinhuolto', 'tyomaaraysten_generointi.php', '', '', '', 'Työmääräysten generointi', 410, 0, '', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
 	('', 'Sammutinhuolto', 'lasku_siivo.php', '', '', '', 'Työmääräysten poistaminen', 420, 0, '', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
-	('', 'Sammutinhuolto', 'yllapito.php', 'liitetiedostot', '', '', 'Liitetiedostot', 430, 0, '', '{$kukarow['yhtio']}', '', 'import', NOW(), NOW(), 'import');
-
+	('', 'Sammutinhuolto', 'yllapito.php', 'liitetiedostot', '', '', 'Liitetiedostot', 430, 0, '', '{$kukarow['yhtio']}', '', 'import', NOW(), NOW(), 'import');";
+	pupe_query($query);
+	$query = "
 INSERT INTO `oikeu` (`kuka`, `sovellus`, `nimi`, `alanimi`, `paivitys`, `lukittu`, `nimitys`, `jarjestys`, `jarjestys2`, `profiili`, `yhtio`, `hidden`, `laatija`, `luontiaika`, `muutospvm`, `muuttaja`)
 VALUES
 	('Sammutinhuolto', 'Sammutinhuolto', 'tyomaarays/tyojono2.php', '', '1', '', 'Työjono', 10, 0, 'Sammutinhuolto', '{$kukarow['yhtio']}', '', 'import', NOW(), NOW(), 'import'),
@@ -78,8 +87,7 @@ VALUES
 	('Sammutinhuolto', 'Sammutinhuolto', 'tilauskasittely/tilaus_myynti.php', 'TYOMAARAYS', '1', '', 'Työmääräys', 390, 0, 'Sammutinhuolto', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
 	('Sammutinhuolto', 'Sammutinhuolto', 'tilauskasittely/laitteen_vaihto.php', '', '1', '', 'Laitteenvaihto', 400, 0, 'Sammutinhuolto', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
 	('Sammutinhuolto', 'Sammutinhuolto', 'tyomaaraysten_generointi.php', '', '1', '', 'Työmääräysten generointi', 410, 0, 'Sammutinhuolto', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
-	('Sammutinhuolto', 'Sammutinhuolto', 'lasku_siivo.php', '', '1', '', 'Työmääräysten poistaminen', 420, 0, 'Sammutinhuolto', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import'),
-";
+	('Sammutinhuolto', 'Sammutinhuolto', 'lasku_siivo.php', '', '1', '', 'Työmääräysten poistaminen', 420, 0, 'Sammutinhuolto', '{$kukarow['yhtio']}', 'H', 'import', NOW(), NOW(), 'import');";
 	pupe_query($query);
 	echo "Ajettu";
 }

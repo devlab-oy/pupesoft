af88ba63ca3f7dc859324cb2011be768b7f3d3da
Oskari Paakkanen <oskari@devlab.fi>
2014-05-08 10:59:59 +0300

    echo

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -81,6 +81,9 @@ for ($alepostfix = 1; $alepostfix <= $yhtiorow['myynnin_alekentat']; $alepostfix
 	if (!isset(${'ale_array'.$alepostfix})) ${'ale_array'.$alepostfix} = "";
 }
 
+print_r($_POST);
+echo '<hr>';
+
 if (!isset($alv)) 					$alv = "";
 if (!isset($alv_array)) 			$alv_array = "";
 if (!isset($asiakasid)) 			$asiakasid = "";

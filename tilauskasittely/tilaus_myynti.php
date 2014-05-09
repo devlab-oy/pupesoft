5467f2b3a1748712796439d077bdbd28649f721b
Tony Selkamo <tony@devlab.fi>
2014-05-09 15:16:09 +0300

    Siirretään tarkistusta ja lisätään ehtoja
    Siirretään tarkistus alemmaksi, että ollaan saatu jo rivejä tilaukselle. Lisäksi jos varasto kuuluu tilauksen toimipaikkaan, niin ei tehdä herjaa mikäli

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -2777,52 +2777,6 @@ if ($tee == '') {
 		$tilausok++;
 	}
 
-	// Tsekataa onko tilausrivien varastojen toimipaikoilla lähdöt päällä, ja onko kyseisen lähdevaraston toimitustavalla lähtöjä
-	if ($yhtiorow['toimipaikkakasittely'] == 'L') {
-
-		$tilausrivien_varastot = tilausrivien_varastot($laskurow['otunnus']);
-
-		foreach ($tilausrivien_varastot as $tilausrivin_varasto) {
-			$varaston_toimipaikka = hae_varaston_toimipaikka($tilausrivin_varasto);
-			$varasto = hae_varasto($tilausrivin_varasto);
-
-			if (in_array($toim, array('RIVISYOTTO', 'PIKATILAUS')) and !empty($varaston_toimipaikka)) {
-				if ($varaston_toimipaikka['tunnus'] == 0) {
-					$kukarow_toimipaikka_temp = $kukarow['toimipaikka'];
-					$kukarow['toimipaikka'] = 0;
-				}
-
-				$toimipaikan_yhtiorow = hae_yhtion_parametrit($kukarow['yhtio'], $varaston_toimipaikka['tunnus']);
-				$kukarow['toimipaikka'] = (isset($kukarow_toimipaikka_temp) ? $kukarow_toimipaikka_temp : $kukarow['toimipaikka']);
-				$_toimipaikan_kerayserat_mittatiedot = ($toimipaikan_yhtiorow['kerayserat'] == 'K');
-
-				if ($_toimipaikan_kerayserat_mittatiedot) {
-
-					$toimitustavat = hae_kaikki_toimitustavat();
-					$toimitustapa = search_array_key_for_value_recursive($toimitustavat, 'selite', $laskurow['toimitustapa']);
-					$toimitustapa = $toimitustapa[0];
-
-					$query = "	SELECT *
-								FROM lahdot
-								WHERE yhtio = '{$kukarow['yhtio']}'
-								AND liitostunnus = {$toimitustapa['tunnus']}
-								AND varasto = {$laskurow['varasto']}
-								AND aktiivi = ''
-								AND pvm >= CURRENT_DATE
-								AND viimeinen_tilausaika > CURRENT_TIME";
-					$lahdot_result = pupe_query($query);
-
-					if (mysql_num_rows($lahdot_result) == 0) {
-						echo "<font class='error'>".t("VIRHE: Tilauksen toimitustavalla ei ole")." {$varasto['nimitys']} ".t("varastossa lähtöjä")."!</font><br><br>";
-						$tilausok++;
-					}
-				}
-			}
-
-			unset($kukarow_toimipaikka_temp);
-		}
-	}
-
 	if ($kukarow['extranet'] == '' and ($laskurow["liitostunnus"] != 0 or ($laskurow["liitostunnus"] == 0 and $kukarow["kesken"] > 0 and $toim != "PIKATILAUS"))) {
 
 		echo "	<script type='text/javascript'>
@@ -5456,6 +5410,60 @@ if ($tee == '') {
 			}
 
 			echo "</td></tr>";
+
+			// Tsekataa onko tilausrivien varastojen toimipaikoilla lähdöt päällä, ja onko kyseisen lähdevaraston toimitustavalla lähtöjä
+			if ($yhtiorow['toimipaikkakasittely'] == 'L') {
+
+				$tilausrivien_varastot = tilausrivien_varastot($laskurow['otunnus']);
+
+				foreach ($tilausrivien_varastot as $tilausrivin_varasto) {
+
+					$varaston_toimipaikka = hae_varaston_toimipaikka($tilausrivin_varasto);
+					$varasto = hae_varasto($tilausrivin_varasto);
+
+					if (in_array($toim, array('RIVISYOTTO', 'PIKATILAUS')) and !empty($varaston_toimipaikka)) {
+
+						if ($varaston_toimipaikka['tunnus'] == 0) {
+							$kukarow_toimipaikka_temp = $kukarow['toimipaikka'];
+							$kukarow['toimipaikka'] = 0;
+						}
+
+						$toimipaikan_yhtiorow = hae_yhtion_parametrit($kukarow['yhtio'], $varaston_toimipaikka['tunnus']);
+						$kukarow['toimipaikka'] = (isset($kukarow_toimipaikka_temp) ? $kukarow_toimipaikka_temp : $kukarow['toimipaikka']);
+						$_toimipaikan_kerayserat_mittatiedot = ($toimipaikan_yhtiorow['kerayserat'] == 'K');
+						$toimipaikka_ja_varasto_ei_sama = ($varaston_toimipaikka['tunnus'] != $laskurow['yhtio_toimipaikka']);
+						$tarvii_lahdon = ($laskurow['eilahetetta'] == '' and $laskurow['sisainen'] == '');
+
+						// jos varaston toimipaikka ei ole tilauksen toimipaikka, niin aina true.
+						$tarvii_lahdon = ($toimipaikka_ja_varasto_ei_sama ? TRUE : $tarvii_lahdon);
+
+						if ($_toimipaikan_kerayserat_mittatiedot and $tarvii_lahdon) {
+
+							$toimitustavat = hae_kaikki_toimitustavat();
+							$toimitustapa = search_array_key_for_value_recursive($toimitustavat, 'selite', $laskurow['toimitustapa']);
+							$toimitustapa = $toimitustapa[0];
+
+							$query = "	SELECT *
+										FROM lahdot
+										WHERE yhtio = '{$kukarow['yhtio']}'
+										AND liitostunnus = {$toimitustapa['tunnus']}
+										AND varasto = {$laskurow['varasto']}
+										AND aktiivi = ''
+										AND pvm >= CURRENT_DATE
+										AND viimeinen_tilausaika > CURRENT_TIME";
+							$lahdot_result = pupe_query($query);
+
+							if (mysql_num_rows($lahdot_result) == 0) {
+								echo "<font class='error'>".t("VIRHE: Tilauksen toimitustavalla ei ole")." {$varasto['nimitys']} ".t("varastossa lähtöjä")."!</font><br><br>";
+								$tilausok++;
+							}
+						}
+					}
+
+					unset($kukarow_toimipaikka_temp);
+				}
+			}
+
 
 			$tuotetyyppi				= "";
 			$positio_varattu			= "";

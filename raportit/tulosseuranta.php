e2257a5c791b3e345e2c75bba400c16113adbe35
Johan Tötterman <juppe@devlab.fi>
2014-05-14 10:30:00 +0300

    Kyselyjen sisennykset näteiks

--- raportit/tulosseuranta.php
+++ raportit/tulosseuranta.php
@@ -36,10 +36,10 @@
   else {
     // Haetaan ko. ajan tilikausi
     $query = "  SELECT tilikausi_alku
-          FROM tilikaudet
-          WHERE tilikaudet.yhtio = '{$kukarow['yhtio']}'
-          AND tilikausi_alku <= '$alku_pvm'
-          AND tilikausi_loppu >= '$loppu_pvm'";
+                FROM tilikaudet
+                WHERE tilikaudet.yhtio = '{$kukarow['yhtio']}'
+                AND tilikausi_alku <= '$alku_pvm'
+                AND tilikausi_loppu >= '$loppu_pvm'";
     $result = pupe_query($query);
 
     if (mysql_num_rows($result) != 1) {
@@ -107,74 +107,71 @@
     echo "<br><br>";
 
     // Katostaan halutaanko hakea myynti/varastonmuutos myynnin puolelta
-    $query = "  SELECT *
-          FROM taso
-          WHERE yhtio = '{$kukarow["yhtio"]}'
-          AND tyyppi = 'B'
-          AND summattava_taso LIKE '%nettokate%'";
-//          AND summattava_taso LIKE '%varastonmuutos%'";
+    $query = "SELECT *
+              FROM taso
+              WHERE yhtio = '{$kukarow["yhtio"]}'
+              AND tyyppi = 'B'
+              AND summattava_taso LIKE '%nettokate%'";
     $result = pupe_query($query);
 
     if (mysql_num_rows($result) != 0) {
 
       // Haetaan myynti/kate tilausriveiltä
-      $query = "SELECT tuote.yhtio, sum(if(tilausrivi.laskutettuaika >= '$alku_pvm' 
-      AND tilausrivi.laskutettuaika <= '$loppu_pvm', tilausrivi.rivihinta, 0)) myyntinyt, sum(if(tilausrivi.laskutettuaika >= '$alku_pvm' 
-      AND tilausrivi.laskutettuaika <= '$loppu_pvm', tilausrivi.kate - (tilausrivi.rivihinta * IFNULL(asiakas.kuluprosentti, 0)/100) - (tilausrivi.rivihinta * IFNULL(toimitustapa.kuluprosentti, 0)/100) - (tilausrivi.rivihinta * IFNULL(tuote.kuluprosentti, 0)/100) - (tilausrivi.rivihinta * IFNULL(yhtio.kuluprosentti, 0)/100), 0)) nettokatenyt
-      FROM lasku use index (yhtio_tila_tapvm)
-      JOIN yhtio ON (yhtio.yhtio = lasku.yhtio)
-      JOIN tilausrivi use index (uusiotunnus_index) ON (tilausrivi.yhtio=lasku.yhtio 
-      AND tilausrivi.uusiotunnus=lasku.tunnus 
-      AND tilausrivi.tyyppi='L')
-      JOIN tuote use index (tuoteno_index) ON (tuote.yhtio=tilausrivi.yhtio 
-      and tuote.tuoteno=tilausrivi.tuoteno)
-      JOIN asiakas use index (PRIMARY) ON (asiakas.yhtio = lasku.yhtio 
-      AND asiakas.tunnus = lasku.liitostunnus  
-      AND asiakas.myynninseuranta = '' )
-      LEFT JOIN toimitustapa ON (lasku.yhtio=toimitustapa.yhtio 
-      and lasku.toimitustapa=toimitustapa.selite)
-      WHERE lasku.yhtio = '{$kukarow["yhtio"]}'
-      AND lasku.tila = 'U'
-      AND lasku.alatila = 'X'
-      AND lasku.tapvm >= '$alku_pvm'
-      AND lasku.tapvm <= '$loppu_pvm'
-      AND tuote.myynninseuranta = ''  
-      AND tilausrivi.tuoteno != 'MAKSUERÄ'
-      $lisa_haku_kustp";
-      
+      $query = "SELECT tuote.yhtio, sum(if(tilausrivi.laskutettuaika >= '$alku_pvm'
+                AND tilausrivi.laskutettuaika <= '$loppu_pvm', tilausrivi.rivihinta, 0)) myyntinyt, sum(if(tilausrivi.laskutettuaika >= '$alku_pvm'
+                AND tilausrivi.laskutettuaika <= '$loppu_pvm', tilausrivi.kate - (tilausrivi.rivihinta * IFNULL(asiakas.kuluprosentti, 0)/100) - (tilausrivi.rivihinta * IFNULL(toimitustapa.kuluprosentti, 0)/100) - (tilausrivi.rivihinta * IFNULL(tuote.kuluprosentti, 0)/100) - (tilausrivi.rivihinta * IFNULL(yhtio.kuluprosentti, 0)/100), 0)) nettokatenyt
+                FROM lasku use index (yhtio_tila_tapvm)
+                JOIN yhtio ON (yhtio.yhtio = lasku.yhtio)
+                JOIN tilausrivi use index (uusiotunnus_index) ON (tilausrivi.yhtio=lasku.yhtio
+                AND tilausrivi.uusiotunnus=lasku.tunnus
+                AND tilausrivi.tyyppi='L')
+                JOIN tuote use index (tuoteno_index) ON (tuote.yhtio=tilausrivi.yhtio
+                and tuote.tuoteno=tilausrivi.tuoteno)
+                JOIN asiakas use index (PRIMARY) ON (asiakas.yhtio = lasku.yhtio
+                AND asiakas.tunnus = lasku.liitostunnus
+                AND asiakas.myynninseuranta = '' )
+                LEFT JOIN toimitustapa ON (lasku.yhtio=toimitustapa.yhtio
+                and lasku.toimitustapa=toimitustapa.selite)
+                WHERE lasku.yhtio = '{$kukarow["yhtio"]}'
+                AND lasku.tila = 'U'
+                AND lasku.alatila = 'X'
+                AND lasku.tapvm >= '$alku_pvm'
+                AND lasku.tapvm <= '$loppu_pvm'
+                AND tuote.myynninseuranta = ''
+                AND tilausrivi.tuoteno != 'MAKSUERÄ'
+                $lisa_haku_kustp";
+
       $result = pupe_query($query);
       $row = mysql_fetch_assoc($result);
 
       $tulosseuranta["myynti"]["summa"] = $row["myyntinyt"];
       $tulosseuranta["myynti"]["nimi"]  = "Myynti";
-//      $tulosseuranta["varastonmuutos"]["summa"] = ($row["summa"] - $row["kate"]) * -1;
-//      $tulosseuranta["varastonmuutos"]["nimi"]  = "Varastonmuutos";
       $tulosseuranta["nettokate"]["summa"] = $row["nettokatenyt"];
       $tulosseuranta["nettokate"]["nimi"]  = "Nettokate";
     }
 
     // Haetaan kaikki tulosseurannan tasot sekä katsotaan löytyykö niille kirjauksia
-    $query = "  SELECT taso.taso,
-          taso.nimi,
-          taso.summattava_taso,
-          taso.kerroin,
-          taso.jakaja,
-          taso.kumulatiivinen,
-          round(ifnull(sum(tiliointi.summa) * -1, taso.oletusarvo)) summa,
-          group_concat(distinct concat('\'', tili.tilino, '\'')) tilit
-          FROM taso
-          LEFT JOIN tili ON (tili.yhtio = taso.yhtio
-            AND tili.tulosseuranta_taso = taso.taso)
-          LEFT JOIN tiliointi ON (tiliointi.yhtio = tili.yhtio
-            AND tiliointi.tilino = tili.tilino
-            AND tiliointi.tapvm >= '$alku_pvm'
-            AND tiliointi.tapvm <= '$loppu_pvm'
-            AND tiliointi.korjattu = ''
-            $lisa)
-          WHERE taso.yhtio = '{$kukarow['yhtio']}'
-          AND taso.tyyppi = 'B'
-          GROUP BY taso.taso, taso.nimi, taso.summattava_taso, taso.kerroin, taso.jakaja, taso.kumulatiivinen
-          ORDER BY taso.taso+1";
+    $query = "SELECT taso.taso,
+              taso.nimi,
+              taso.summattava_taso,
+              taso.kerroin,
+              taso.jakaja,
+              taso.kumulatiivinen,
+              round(ifnull(sum(tiliointi.summa) * -1, taso.oletusarvo)) summa,
+              group_concat(distinct concat('\'', tili.tilino, '\'')) tilit
+              FROM taso
+              LEFT JOIN tili ON (tili.yhtio = taso.yhtio
+                AND tili.tulosseuranta_taso = taso.taso)
+              LEFT JOIN tiliointi ON (tiliointi.yhtio = tili.yhtio
+                AND tiliointi.tilino = tili.tilino
+                AND tiliointi.tapvm >= '$alku_pvm'
+                AND tiliointi.tapvm <= '$loppu_pvm'
+                AND tiliointi.korjattu = ''
+                $lisa)
+              WHERE taso.yhtio = '{$kukarow['yhtio']}'
+              AND taso.tyyppi = 'B'
+              GROUP BY taso.taso, taso.nimi, taso.summattava_taso, taso.kerroin, taso.jakaja, taso.kumulatiivinen
+              ORDER BY taso.taso+1";
     $result = pupe_query($query);
 
     while ($row = mysql_fetch_assoc($result)) {
@@ -182,13 +179,13 @@
       // Jos taso tulee olla kumulatiivinen, haetaan tason kumulatiivinen summa kirjanpidosta
       if ($row["kumulatiivinen"] == "X" and $row["tilit"] != "") {
         $query = "  SELECT round(ifnull(sum(tiliointi.summa) * -1, 0)) summa
-              FROM tiliointi
-              WHERE tiliointi.yhtio = '{$kukarow["yhtio"]}'
-              AND tiliointi.tilino in ({$row["tilit"]})
-              AND tiliointi.tapvm >= '$tilikausi_alku'
-              AND tiliointi.tapvm < '$alku_pvm'
-              AND tiliointi.korjattu = ''
-              $lisa";
+                    FROM tiliointi
+                    WHERE tiliointi.yhtio = '{$kukarow["yhtio"]}'
+                    AND tiliointi.tilino in ({$row["tilit"]})
+                    AND tiliointi.tapvm >= '$tilikausi_alku'
+                    AND tiliointi.tapvm < '$alku_pvm'
+                    AND tiliointi.korjattu = ''
+                    $lisa";
         $kumulatiivinen_result = pupe_query($query);
         $kumulatiivinen_row = mysql_fetch_assoc($kumulatiivinen_result);
         $row["summa"] += $kumulatiivinen_row["summa"];

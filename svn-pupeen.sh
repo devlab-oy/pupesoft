7ca34d5fb0e54b72e8207a0cecb29e1df8c975ab
Joni Kanerva <joni@devlab.fi>
2014-06-09 18:00:14 +0300

    yksinkertaistettu narupalvelua

--- svn-pupeen.sh
+++ svn-pupeen.sh
@@ -1,52 +1,85 @@
 #!/bin/bash
 
-hosti=`hostname`
-underline=`tput -Txterm-color smul`
-nounderline=`tput -Txterm-color rmul`
-green=`tput -Txterm-color setaf 2`
-red=`tput -Txterm-color setaf 1`
-white=`tput -Txterm-color setaf 7`
-normal=`tput -Txterm-color sgr0`
+hosti=$(hostname)
+underline=$(tput -Txterm-color smul)
+nounderline=$(tput -Txterm-color rmul)
+green=$(tput -Txterm-color setaf 2)
+red=$(tput -Txterm-color setaf 1)
+white=$(tput -Txterm-color setaf 7)
+normal=$(tput -Txterm-color sgr0)
 
 echo
 echo "${green}${underline}Tervetuloa ${hosti} Pupesoft-narupalveluun!${nounderline}${normal}"
 echo
 
-if [ "`whoami`" = "root" ]; then
+if [[ "$(whoami)" = "root" ]]; then
   echo "${red}Ei ole suositeltavaa, että ajat tämän root -käyttäjällä!${normal}"
+  echo
+  exit
+fi
+
+pupedir=$(dirname ${0})
+salasanat=${pupedir}/inc/salasanat.php
+
+if [[ ! -f ${salasanat} ]]; then
+  echo "${red}Salasanat.php ei löytynyt!${normal}"
+  echo
+  exit
+fi
+
+function parse_salasanat {
+  grep "^[ \t]*${1}[ \t]*=" ${salasanat} \
+  | sed 's/^.*['\''"]\([^'\''"]*\)['\''"];/\1/' \
+  | tail -1
+}
+
+dbhost=$(parse_salasanat '$dbhost')
+dbuser=$(parse_salasanat '$dbuser')
+dbpass=$(parse_salasanat '$dbpass')
+dbname=$(parse_salasanat '$dbkanta')
+
+# Katsotaan jos meillä on annettu poikkeava portti hostnamessa
+host_array=(${dbhost//:/ })
+
+if [ -n "${host_array[1]}" ]; then
+  dbport=${host_array[1]}
+else
+  dbport="3306"
+fi
+
+# Testataan tietokantayhteys
+mysql_komento="mysql --host=${dbhost} --user=${dbuser} --password=${dbpass} --database=${dbname} --port=${dbport} --verbose"
+${mysql_komento} --execute="use ${dbname}" &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+  echo "${red}Tietokantayhteys ei onnistu! Tarkista salasanat.php!${normal}"
   echo
   exit
 fi
 
 echo "Haetaan tietokantamuutokset.."
 
-pupedir=`dirname $0`
-
 # Katsotaan, onko parami syötetty
-if [ ! -z $1 ]; then
-  JATKETAAN=$1
+if [ ! -z ${1} ]; then
+  jatketaan=${1}
 fi
 
 # Tutkitaan tietokantarakenne...
-dumppi=`php ${pupedir}/dumppaa_mysqlkuvaus.php komentorivilta`
+mysqlkuvaus_file="/tmp/_mysqlkuvaus.tmp"
 
-if [ -z "$dumppi" ]; then
+php ${pupedir}/dumppaa_mysqlkuvaus.php 1> ${mysqlkuvaus_file} 2> /dev/null
+
+if [[ ! -s ${mysqlkuvaus_file} ]]; then
   echo "${green}Tietokanta ajantasalla!${normal}"
-  echo
-  rm -f /tmp/_mysqlkuvays.sql
 else
-  echo -e $dumppi > /tmp/_mysqlkuvaus.tmp
+  echo
+  echo "${green}Tarvittavat muutokset: ${normal}"
 
-  while read line
-  do
-    if [ -n "$line" ]; then
-      echo $line
-    fi
-  done < "/tmp/_mysqlkuvaus.tmp"
+  cat ${mysqlkuvaus_file}
+  echo
 
-  if [[ ! -z "${JATKETAAN}" && ("${JATKETAAN}" = "auto" || "${JATKETAAN}" = "autopupe") ]]; then
-
-    if [[ "${JATKETAAN}" = "autopupe" ]]; then
+  if [[ ! -z "${jatketaan}" && ("${jatketaan}" = "auto" || "${jatketaan}" = "autopupe") ]]; then
+    if [[ "${jatketaan}" = "autopupe" ]]; then
       jatketaanko="e"
     else
       jatketaanko="k"
@@ -58,60 +91,61 @@ else
   fi
 
   if [ "$jatketaanko" = "k" ]; then
-    while read line
-    do
-      if [ -n "$line" ]; then
-        eval $line
-      fi
-    done < "/tmp/_mysqlkuvaus.tmp"
+    ${mysql_komento} < ${mysqlkuvaus_file} 2> /dev/null
 
-    echo -n "${green}Tietokantamuutokset tehty!${normal}"
+    if [[ $? -eq 0 ]]; then
+      echo "${green}Tietokantamuutokset tehty!${normal}"
+    else
+      echo "${red}Tietokantamuutoksien ajo epäonnistui!${normal}"
+    fi
   else
-    echo -n "${red}Tietokantamuutoksia ei tehty!${normal}"
+    echo "${red}Tietokantamuutoksia ei tehty!${normal}"
   fi
-
-  rm -f /tmp/_mysqlkuvays.sql
-  rm -f /tmp/_mysqlkuvays.tmp
 fi
 
-if [[ ! -z "${JATKETAAN}" && ("${JATKETAAN}" = "auto" || "${JATKETAAN}" = "autopupe") ]]; then
+rm -f ${mysqlkuvaus_file}
+
+if [[ ! -z "${jatketaan}" && ("${jatketaan}" = "auto" || "${jatketaan}" = "autopupe") ]]; then
   jatketaanko="k"
   echo
 else
-  echo
   echo
   echo -n "${white}Päivitetäänkö Pupesoft (k/e)? ${normal}"
   read jatketaanko
 fi
 
 if [ "$jatketaanko" = "k" ]; then
-  cd $pupedir
-  git fetch origin        # paivitetaan lokaali repo remoten tasolle
-  git checkout .          # revertataan kaikki local muutokset
+  cd $pupedir &&
+  git fetch origin &&     # paivitetaan lokaali repo remoten tasolle
+  git checkout . &&       # revertataan kaikki local muutokset
 
-  BRANCHFILE="/home/devlab/pupe_branch"
+  branchfile="/home/devlab/pupe_branch"
 
   # Onko spessubranchi käytössä?
-  if [[ -f "$BRANCHFILE" && -r "$BRANCHFILE" ]]; then
-    pupebranch=$(cat $BRANCHFILE | tr -d '\n')
+  if [[ -f "$branchfile" && -r "$branchfile" ]]; then
+    pupebranch=$(cat $branchfile | tr -d '\n')
 
-    git checkout $pupebranch
-    git pull origin $pupebranch
+    git checkout $pupebranch &&
+    git pull origin $pupebranch &&
   else
-    git checkout master     # varmistetaan, etta on master branchi kaytossa
-    git pull origin master  # paivitetaan master branchi
+    git checkout master &&     # varmistetaan, etta on master branchi kaytossa
+    git pull origin master &&  # paivitetaan master branchi
   fi
 
   git remote prune origin # poistetaan ylimääriset branchit
 
-  NRFILE="/home/devlab/newrelic_trigger"
-
-  # Onko triggeri käytössä?
-  if [[ -x "$NRFILE" ]]; then
-    eval $NRFILE
+  if [[ $? -eq 0 ]]; then
+    echo "${green}Pupesoft päivitetty!${normal}"
+  else
+    echo "${red}Pupesoft päivitys epäonnistui!${normal}"
   fi
 
-  echo "${green}Pupesoft päivitetty!${normal}"
+  nrfile="/home/devlab/newrelic_trigger"
+
+  # Onko triggeri käytössä?
+  if [[ -x "$nrfile" ]]; then
+    eval $nrfile
+  fi
 else
   echo "${red}Pupesoftia ei päivitetty!${normal}"
 fi

16eba0e27a4ff8a715eff497c1808c32a5ba5aed
tiere <tommi@devlab.fi>
2014-06-30 16:21:47 +0300

    Echot takaisin

--- pankkiyhteys.php
+++ pankkiyhteys.php
@@ -1,40 +1,45 @@
 <?php
 
 require("inc/parametrit.inc");
-?>
-  <font class='head'><?= t("SEPA-pankkiyhteys") ?></font>
-  <hr>
-<?php
+
+echo "<font class='head'>" . t('SEPA-pankkiyhteys') . "</font><hr>";
 
 $tee = $_REQUEST["tee"];
 
+echo "Post:<br>";
+echo var_dump($_POST);
+echo "<br><br>Request<br>";
+echo var_dump($_REQUEST);
+echo "<br><br>Files<br>";
+echo var_dump($_FILES["certificate"]);
+
 if (isset($tee) and $tee == "lataa_sertifikaatti") {
-  ?>
+  echo "
   <form action='pankkiyhteys.php' method='post' enctype='multipart/form-data'>
-    <label for='private_key'><?= t("Yksityinen avain") ?></label>
-    <input type='file' name='private_key' id='private_key'>
+    <label for='private_key'>" . t('Yksityinen avain') . " </label>
+    <input type = 'file' name = 'private_key' id = 'private_key'>
     <br>
-    <input type='submit' name='submit' value='Submit'>
-  </form>
-<?php
+    <label for='certificate'>" . t('Sertifikaatti') . "</label>
+    <input type='file' name='certificate' id='certificate'/>
+    <input type='submit' name='submit' value='" . t('Lähetä') . "'>
+  </form>";
 }
 else {
-  ?>
+  echo "
   <form method='post' action='pankkiyhteys.php'>
     <table>
       <tbody>
-      <tr>
-        <td>Mita haluat tehdä?</td>
-        <td>
-          <select name='tee'>
-            <option value='lataa_sertifikaatti'><?= t('Lataa sertifikaatti') ?></option>
-          </select>
-        </td>
-      </tr>
+        <tr>
+          <td>Mita haluat tehdä?</td>
+          <td>
+            <select name='tee'>
+              <option value='lataa_sertifikaatti'>" . t('Lataa sertifikaatti') . "</option>
+            </select>
+          </td>
+        </tr>
       </tbody>
     </table>
 
-    <input type='submit' value='<?= t("OK") ?>'>
-  </form>
-<?php
+    <input type='submit' value='" . t('OK') . "'>
+  </form>";
 }

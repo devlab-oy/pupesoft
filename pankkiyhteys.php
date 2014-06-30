894c684a0869b633a93ddc1d3073dca31db96aab
tiere <tommi@devlab.fi>
2014-06-30 15:05:43 +0300

    Echot pois

--- pankkiyhteys.php
+++ pankkiyhteys.php
@@ -1,30 +1,40 @@
 <?php
 
 require("inc/parametrit.inc");
-
-echo "<font class='head'>" . t("SEPA-pankkiyhteys") . "</font><hr>";
+?>
+  <font class='head'><?= t("SEPA-pankkiyhteys") ?></font>
+  <hr>
+<?php
 
 $tee = $_REQUEST["tee"];
 
 if (isset($tee) and $tee == "lataa_sertifikaatti") {
-  echo "Sertifikaatin lataaminen";
+  ?>
+  <form action='pankkiyhteys.php' method='post' enctype='multipart/form-data'>
+    <label for='private_key'><?= t("Yksityinen avain") ?></label>
+    <input type='file' name='private_key' id='private_key'>
+    <br>
+    <input type='submit' name='submit' value='Submit'>
+  </form>
+<?php
 }
 else {
-  echo "
+  ?>
   <form method='post' action='pankkiyhteys.php'>
     <table>
       <tbody>
-        <tr>
-          <td>Mita haluat tehdä?</td>
-          <td>
-            <select name='tee'>
-              <option value='lataa_sertifikaatti'>" . t('Lataa sertifikaatti') . "</option>
-            </select>
-          </td>
-        </tr>
+      <tr>
+        <td>Mita haluat tehdä?</td>
+        <td>
+          <select name='tee'>
+            <option value='lataa_sertifikaatti'><?= t('Lataa sertifikaatti') ?></option>
+          </select>
+        </td>
+      </tr>
       </tbody>
     </table>
 
-    <input type='submit' value='" . t("OK") . "'>
-  </form>";
+    <input type='submit' value='<?= t("OK") ?>'>
+  </form>
+<?php
 }

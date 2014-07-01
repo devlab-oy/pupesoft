1cac6df191734e1be901c6756f05ee7109f6380b
tiere <tommi@devlab.fi>
2014-07-01 09:26:07 +0300

    Echot omille riveilleen, jotta lisäyksiä on myöhemmin helpompi tehdä

--- pankkiyhteys.php
+++ pankkiyhteys.php
@@ -2,7 +2,8 @@
 
 require("inc/parametrit.inc");
 
-echo "<font class='head'>" . t('SEPA-pankkiyhteys') . "</font><hr>";
+echo "<font class='head'>" . t('SEPA-pankkiyhteys') . "</font>";
+echo "<hr>";
 
 $tee = $_REQUEST["tee"];
 
@@ -14,32 +15,29 @@ echo "<br><br>Files<br>";
 echo var_dump($_FILES["certificate"]);
 
 if (isset($tee) and $tee == "lataa_sertifikaatti") {
-  echo "
-  <form action='pankkiyhteys.php' method='post' enctype='multipart/form-data'>
-    <label for='private_key'>" . t('Yksityinen avain') . " </label>
-    <input type = 'file' name = 'private_key' id = 'private_key'>
-    <br>
-    <label for='certificate'>" . t('Sertifikaatti') . "</label>
-    <input type='file' name='certificate' id='certificate'/>
-    <input type='submit' name='submit' value='" . t('Lähetä') . "'>
-  </form>";
+  echo "<form action='pankkiyhteys.php' method='post' enctype='multipart/form-data'>";
+  echo "<label for='private_key'>" . t('Yksityinen avain') . "</label>";
+  echo "<input type='file' name='private_key' id='private_key'>";
+  echo "<br>";
+  echo "<label for='certificate'>" . t('Sertifikaatti') . "</label>";
+  echo "<input type='file' name='certificate' id='certificate'/>";
+  echo "<input type='submit' name='submit' value='" . t('Lähetä') . "'>";
+  echo "</form>";
 }
 else {
-  echo "
-  <form method='post' action='pankkiyhteys.php'>
-    <table>
-      <tbody>
-        <tr>
-          <td>Mita haluat tehdä?</td>
-          <td>
-            <select name='tee'>
-              <option value='lataa_sertifikaatti'>" . t('Lataa sertifikaatti') . "</option>
-            </select>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
-    <input type='submit' value='" . t('OK') . "'>
-  </form>";
+  echo "<form method='post' action='pankkiyhteys.php'>";
+  echo "<table>";
+  echo "<tbody>";
+  echo "<tr>";
+  echo "<td>Mita haluat tehdä?</td>";
+  echo "<td>";
+  echo "<select name='tee'>";
+  echo "<option value='lataa_sertifikaatti'>" . t('Lataa sertifikaatti') . "</option>";
+  echo "</select>";
+  echo "</td>";
+  echo "</tr>";
+  echo "</tbody>";
+  echo "</table>";
+  echo "<input type='submit' value='" . t('OK') . "'>";
+  echo "</form>";
 }

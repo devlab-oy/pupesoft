523444fe1a3bcc58c52fbb0a994c453d1fdac68d
tiere <tommi@devlab.fi>
2014-07-03 10:45:28 +0300

    Pakotetaan kayttamaan salattua https-yhteytta

--- pankkiyhteys.php
+++ pankkiyhteys.php
@@ -1,5 +1,13 @@
 <?php
 
+if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) {
+  $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+  header('Location: ' . $url);
+
+  exit;
+}
+
 require("inc/parametrit.inc");
 
 echo "<font class='head'>" . t('SEPA-pankkiyhteys') . "</font>";

e255cb99c8b7fc76befbbfe40548220d0119e2e1
Joonas Harjukallio <joonas@devlab.fi>
2014-07-03 14:08:18 +0300

    Tulevat työt format & bugfix

--- tyomaarays/tulevat_tyot.php
+++ tyomaarays/tulevat_tyot.php
@@ -2,11 +2,16 @@
 
 require_once('../inc/parametrit.inc');
 require_once("inc/laite_huolto_functions.inc");
+require_once('tilauskasittely/tarkastuspoytakirja_pdf.php');
+require_once('tilauskasittely/poikkeamaraportti_pdf.php');
+require_once('tilauskasittely/tyolista_pdf.php');
 
 if (isset($livesearch_tee) and $livesearch_tee == "ASIAKASHAKU") {
   livesearch_asiakashaku();
   exit;
 }
+
+enable_ajax();
 
 if (!isset($ala_tee)) {
   $ala_tee = '';
@@ -14,9 +19,27 @@ if (!isset($ala_tee)) {
 if (!isset($valittu_asiakas)) {
   $valittu_asiakas = '';
 }
+if (!isset($ppa)) {
+  $ppa = '01';
+}
+if (!isset($kka)) {
+  $kka = '01';
+}
+if (!isset($vva)) {
+  $vva = date('Y');
+}
+if (!isset($ppl)) {
+  $ppl = '01';
+}
+if (!isset($kkl)) {
+  $kkl = '01';
+}
+if (!isset($vvl)) {
+  $vvl = date('Y') + 1;
+}
 
 if (isset($tee) and $tee == 'lataa_tiedosto') {
-  $filepath = "/tmp/".$tmpfilenimi;
+  $filepath = "/tmp/" . $tmpfilenimi;
   if (file_exists($filepath)) {
     readfile($filepath);
     unlink($filepath);
@@ -29,11 +52,18 @@ if (isset($ajax_request)) {
   exit;
 }
 
-require_once('tilauskasittely/tarkastuspoytakirja_pdf.php');
-require_once('tilauskasittely/poikkeamaraportti_pdf.php');
-require_once('tilauskasittely/tyolista_pdf.php');
+$request = array(
+    'ala_tee'         => $ala_tee,
+    'valittu_asiakas' => $valittu_asiakas,
+    'ppa'             => $ppa,
+    'kka'             => $kka,
+    'vva'             => $vva,
+    'ppl'             => $ppl,
+    'kkl'             => $kkl,
+    'vvl'             => $vvl,
+);
 
-echo "<font class='head'>".t("Tulevat työt").":</font>";
+echo "<font class='head'>" . t("Tulevat työt") . ":</font>";
 echo "<hr/>";
 echo "<br/>";
 
@@ -43,70 +73,12 @@ $css = hae_tyojono2_css();
 echo $js;
 echo $css;
 
-if ($ala_tee != 'hae_tyomaaraykset') {
+echo_kust_tulevat_tyot_kayttoliittyma($request);
 
-  enable_ajax();
+echo "<br/>";
+echo "<br/>";
 
-  if (!isset($ppa)) {
-    $ppa = '01';
-  }
-  if (!isset($kka)) {
-    $kka = '01';
-  }
-  if (!isset($vva)) {
-    $vva = date('Y');
-  }
-  if (!isset($ppl)) {
-    $ppl = '01';
-  }
-  if (!isset($kkl)) {
-    $kkl = '01';
-  }
-  if (!isset($vvl)) {
-    $vvl = date('Y') + 1;
-  }
-
-  echo "<div>";
-
-  echo "<form name='tulevat_tyot' method = 'post'>";
-  echo "<input type='hidden' name='toim' value='{$toim}'>";
-  echo "<input type='hidden' name='ala_tee' value='hae_tyomaaraykset'>";
-
-  echo "<table>";
-
-  echo "<tr>";
-  echo "<th>".t("Asiakas")."</th>";
-  echo "<td colspan='3'>";
-
-  echo livesearch_kentta("asiakashinnasto_haku_form", "ASIAKASHAKU", "valittu_asiakas", 315, $valittu_asiakas, 'EISUBMIT', '', 'valittu_asiakas', 'ei_break_all');
-
-  echo "</td>";
-  echo "</tr>";
-
-  echo "<tr>";
-  echo "<th>".t("Alku pvm. Muodossa pp-kk-vvvv")."</th>";
-  echo "<td><input type='text' name='ppa' value='".$ppa."' size='3' /></td>";
-  echo "<td><input type='text' name='kka' value='".$kka."' size='3' /></td>";
-  echo "<td><input type='text' name='vva' value='".$vva."' size='5' /></td>";
-  echo "</tr>";
-
-  echo "<tr>";
-  echo "<th>".t("Loppu pvm. Muodossa pp-kk-vvvv")."</th>";
-  echo "<td><input type='text' name='ppl' value='".$ppl."' size='3' /></td>";
-  echo "<td><input type='text' name='kkl' value='".$kkl."' size='3' /></td>";
-  echo "<td><input type='text' name='vvl' value='".$vvl."' size='5' /></td>";
-  echo "</tr>";
-
-  echo "</table>";
-
-  echo "<br />";
-  echo "<input type='submit' value='".t("Hae")."'>";
-  echo "</form>";
-
-  echo "</div>";
-}
-else {
-
+if ($ala_tee == 'hae_tyomaaraykset') {
   pupe_query("TRUNCATE TABLE lasku_temp");
   pupe_query("TRUNCATE TABLE laskun_lisatiedot_temp");
   pupe_query("TRUNCATE TABLE tyomaarays_temp");
@@ -115,15 +87,15 @@ else {
 
   $temp = 1;
 
-  $start = date('Y-m-d', strtotime($vva.'-'.$kka.'-'.$ppa));
-  $end = date('Y-m-d', strtotime($vvl.'-'.$kkl.'-'.$ppl));
+  $start = date('Y-m-d', strtotime("{$vva}-{$kka}-{$ppa}"));
+  $end = date('Y-m-d', strtotime("{$vvl}-{$kkl}-{$ppl}"));
 
   $laitteiden_huoltosyklirivit = hae_laitteet_ja_niiden_huoltosyklit_ajalta($start, $end, $valittu_asiakas);
 
-  list($huollettavien_laitteiden_huoltosyklirivit, $laitteiden_huoltosyklirivit_joita_ei_huolleta) = paata_mitka_huollot_tehdaan($laitteiden_huoltosyklirivit);
-
-  foreach ($huollettavien_laitteiden_huoltosyklirivit as $huollettavan_laitteen_huoltosyklirivi) {
-    generoi_tyomaarays_temp($huollettavan_laitteen_huoltosyklirivi);
+  foreach ($laitteiden_huoltosyklirivit as $laitteen_tietyn_paivan_huollot) {
+    foreach ($laitteen_tietyn_paivan_huollot as $laitteen_huoltosykli) {
+      generoi_tyomaarays_temp($laitteen_huoltosykli);
+    }
   }
 
   $request['tyojonot'] = hae_tyojonot($request);
@@ -136,4 +108,5 @@ else {
   echo_tyomaaraykset_table($request);
   echo "</div>";
 }
+
 require ("inc/footer.inc");

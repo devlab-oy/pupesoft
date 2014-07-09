626a764a18427dc0a4c80cf0960f7edc649a2eac
Joonas Harjukallio <joonas@devlab.fi>
2014-07-09 12:48:49 +0300

    Työmääräysten generointi refaktor

--- tyomaaraysten_generointi.php
+++ tyomaaraysten_generointi.php
@@ -3,49 +3,37 @@
 ini_set("memory_limit", "5G");
 set_time_limit(0);
 
-$debug = true;
-if (php_sapi_name() != 'cli' and !$debug) {
-  die("Tätä scriptiä voi ajaa vain komentoriviltä!");
+if (php_sapi_name() == 'cli') {
+
+  $pupe_root_polku = dirname(__FILE__);
+  ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . $pupe_root_polku . PATH_SEPARATOR . "/usr/share/pear");
+  error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
+  ini_set("display_errors", 0);
+
+  require("inc/connect.inc");
+  require("inc/functions.inc");
+
+  if (trim(empty($argv[1]))) {
+    echo "Et antanut yhtiötä!\n";
+    exit;
+  }
+
+  $yhtio = pupesoft_cleanstring($argv[1]);
+
+  $kukarow = hae_kukarow('admin', $yhtio);
+}
+else {
+  require ("inc/parametrit.inc");
+
+  $yhtio = $kukarow['yhtio'];
 }
 
-require ("inc/parametrit.inc");
 require_once("tilauskasittely/luo_myyntitilausotsikko.inc");
 require_once("inc/laite_huolto_functions.inc");
 require_once("inc/ProgressBar.class.php");
 
-if (trim(empty($argv[1])) and !$debug) {
-  echo "Et antanut yhtiötä!\n";
-  exit;
-}
-else {
-  $yhtio = $kukarow['yhtio'];
-}
-
-if (!$debug) {
-  // Parametrit
-  $yhtio = pupesoft_cleanstring($argv[1]);
-}
-
 // Haetaan yhtiön tiedot
 $yhtiorow = hae_yhtion_parametrit($yhtio);
-
-// Haetaan käyttäjän tiedot
-$query = "  SELECT *
-      FROM kuka
-      WHERE yhtio = '$yhtio'
-      AND kuka = 'admin'";
-$result = pupe_query($query);
-
-if (mysql_num_rows($result) == 0) {
-  die("User admin not found");
-}
-
-// Adminin oletus, mutta kuka konversio
-$kukarow = mysql_fetch_assoc($result);
-
-$request = array(
-  'laitteiden_huoltosyklirivit' => $laitteiden_huoltosyklirivit,
-);
 
 $laitteiden_huoltosyklirivit = hae_laitteet_ja_niiden_huoltosyklit_joiden_huolto_lahestyy();
 

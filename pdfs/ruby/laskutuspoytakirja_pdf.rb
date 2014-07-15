47abb55b524f97e78ecb2f5987a1d4d25cd544e1
Joonas Harjukallio <joonas@devlab.fi>
2014-07-15 13:01:30 +0300

    Laskutuspöytäkirja refaktor

--- pdfs/ruby/laskutuspoytakirja_pdf.rb
+++ pdfs/ruby/laskutuspoytakirja_pdf.rb
@@ -9,10 +9,10 @@ require 'date'
 require 'base64'
 
 class LaskutuspoytakirjaPDF
-  @logo = nil
+  @logo   = nil
   @margin = nil
-  @data = nil
-  @pdf = nil
+  @data   = nil
+  @pdf    = nil
 
   def initialize
     @margin = 20
@@ -20,26 +20,33 @@ class LaskutuspoytakirjaPDF
 
   def generate _pdf
     init
+
     @pdf = _pdf
     @pdf.font 'Helvetica', :style => :bold, :size => 8
+
     header
+
     info
+
     rows
+
     filename = "Laskutuspoytakirja.pdf"
   end
 
   def init
-    @logo   = @data['logo']
+    @logo = @data['logo']
   end
 
   def header
     @pdf.repeat(:all, :dynamic => true) do
       @pdf.draw_text "#{@pdf.page_number}  (#{@pdf.page_count})", :at => [770, 540]
-      #@pdf.draw_text "Laskutuspöytäkirjan nro", :at => [680, 520]
-      #@pdf.draw_text "Faktureringsprotokoll nr", :at => [680, 510]
+
       logo
+
       @pdf.draw_text @data['yhtio']['nimi'], :at => [0, 465]
+
       @pdf.move_down 100
+
       @pdf.horizontal_rule
       @pdf.stroke
     end
@@ -47,114 +54,116 @@ class LaskutuspoytakirjaPDF
 
   def info
     @pdf.move_down 120
+
     @pdf.text "LASKUTUSPÖYTÄKIRJA / FAKTURERINGSPROTOKOLL"
-    #@pdf.text_box "Tarkastuspöytäkirjan nro", :at => [680, @pdf.cursor+5]
-    #@pdf.text_box "Faktureringsprotokoll nr", :at => [680, @pdf.cursor-5]
+
     @pdf.move_down 15
+
     @pdf.horizontal_rule
     @pdf.stroke
+
     @pdf.move_down 15
 
     @pdf.float do
-        @pdf.text "Asiakas/Kund"
+      @pdf.text "Asiakas/Kund"
     end
+
     @pdf.float do
       @pdf.indent(100) do
         @pdf.text @data['asiakas']['nimi']
       end
     end
+
     @pdf.float do
       @pdf.indent(250) do
         @pdf.text "Asiakasnro./Kund nr. #{@data['kohde']['tunnus']}"
       end
     end
+
     @pdf.move_down 20
+
     @pdf.horizontal_rule
     @pdf.stroke
+
     @pdf.move_down 15
+
     @pdf.float do
-       @pdf.text "Kohde/Plats"
+      @pdf.text "Kohde/Plats"
     end
+
     @pdf.float do
       @pdf.indent(100) do
         @pdf.text @data['kohde']['nimi']
       end
     end
 
-=begin
-    @pdf.move_up 6
-    @pdf.float do
-      @pdf.indent(350) do
-        @pdf.text "Kust.paikka/Merkki<br />Kostnadsställe/Märke", :inline_format => true
-      end
-    end
-    @pdf.float do
-      @pdf.indent(500) do
-        @pdf.text "Tilausnumero/<br />Order nr.", :inline_format => true
-      end
-    end
-    @pdf.move_down 6
-=end
-
     @pdf.move_down 20
+
     @pdf.horizontal_rule
     @pdf.stroke
   end
 
   def rows
     row_headers
+
     @pdf.move_down 15.76
 
-    @data['rivit'].each_with_index do |r,i|
+    @data['rivit'].each_with_index do |r, i|
 
-    bottom = @data['rivit'].size-1 == i ?  80 :  10
+      bottom = @data['rivit'].size-1 == i ? 80 : 10
 
-    if @pdf.cursor < bottom
-      @pdf.start_new_page
-      @pdf.move_down 100.28
-      row_headers
-      @pdf.move_down 15
-    end
+      if @pdf.cursor < bottom
+        @pdf.start_new_page
+        @pdf.move_down 100.28
+        row_headers
+        @pdf.move_down 15
+      end
 
-    row(r)
+      row(r)
 
-    if @data['rivit'].size-1 == i
-      footer
-    end
-
+      if @data['rivit'].size-1 == i
+        footer
+      end
     end
   end
 
   def row_headers
     @pdf.move_down 10
+
     @pdf.float do
-        @pdf.text "Tuote nro. / Produkt nr."
+      @pdf.text "Tuote nro. / Produkt nr."
     end
+
     @pdf.float do
       @pdf.indent(100) do
         @pdf.text "Nimike / Produkt"
       end
     end
+
     @pdf.float do
       @pdf.indent(350) do
         @pdf.text "Kpl. / St."
       end
     end
+
     @pdf.float do
       @pdf.indent(450) do
         @pdf.text "á-hinta / á-pris"
       end
     end
-     @pdf.float do
+
+    @pdf.float do
       @pdf.indent(550) do
         @pdf.text "Alennus % / Rabatt %"
       end
     end
-     @pdf.float do
+
+    @pdf.float do
       @pdf.indent(650) do
         @pdf.text "Yhteensä / Totalt"
       end
     end
+
     @pdf.move_down 15
     @pdf.horizontal_rule
     @pdf.stroke
@@ -163,33 +172,39 @@ class LaskutuspoytakirjaPDF
   def row(r)
     @pdf.font 'Helvetica', :style => :normal, :size => 8
     @pdf.float do
-         @pdf.text r['tuoteno']
+      @pdf.text r['tuoteno']
     end
+
     @pdf.float do
       @pdf.indent(100) do
-         @pdf.text r['nimitys']
+        @pdf.text r['nimitys']
       end
     end
+
     @pdf.float do
       @pdf.indent(350) do
-         @pdf.text r['kpl']
+        @pdf.text r['kpl']
       end
     end
+
     @pdf.float do
       @pdf.indent(450) do
-         @pdf.text r['hinta']
+        @pdf.text r['hinta']
       end
     end
+
     @pdf.float do
       @pdf.indent(550) do
-         @pdf.text r['ale1']
+        @pdf.text r['alennusprosentti']
       end
     end
+
     @pdf.float do
       @pdf.indent(650) do
-         @pdf.text r['total']
+        @pdf.text r['hinta_yhteensa']
       end
     end
+
     @pdf.move_down 15
   end
 
@@ -197,21 +212,15 @@ class LaskutuspoytakirjaPDF
     @pdf.line [@pdf.bounds.left, 60], [@pdf.bounds.right, 60]
 
     @pdf.text_box "Yht. alv / Totalt moms 0 %", :at => [500, 50], :width => 100, :align => :right
-    @pdf.text_box "alv / moms #{@data['alv']} %", :at => [500, 36], :width => 100, :align => :right
-    @pdf.text_box "Yhteensä alv / Totalt moms 23,00 %", :at => [500, 10], :width => 100, :align => :right
+    @pdf.text_box "alv / moms #{@data['alv_prosentti']} %", :at => [500, 36], :width => 100, :align => :right
+    @pdf.text_box "Yhteensä alv / Totalt moms", :at => [500, 10], :width => 100, :align => :right
 
     @pdf.font 'Helvetica', :style => :bold, :size => 8
-    @pdf.text_box @data['full_total'], :at => [500, 50], :width => 170, :align => :right
+    @pdf.text_box @data['kaikki_yhteensa'], :at => [500, 50], :width => 170, :align => :right
 
-    #alv = (@data['full_total'].to_f * (@data['alv'].to_f / 100)).round(2)
-    # aiemmissa ruby versioissa kuin 1.9.0 round() ei ota argumentteja
-    # joten tässä yleispätevä pyöristys
-    alv = @data['full_total'].to_f * (@data['alv'].to_f / 100)
-    alv = (alv * 100).round() / 100.0
+    @pdf.text_box @data['alv_maara_yhteensa'], :at => [500, 36], :width => 170, :align => :right
 
-    @pdf.text_box alv.to_s, :at => [500, 36], :width => 170, :align => :right
-
-    total_with_alv = (@data['full_total'].to_i + alv)
+    total_with_alv = (@data['kaikki_yhteensa'].to_f + @data['alv_maara_yhteensa'].to_f)
     @pdf.text_box total_with_alv.to_s, :at => [500, 10], :width => 170, :align => :right
 
     @pdf.line [@pdf.bounds.left, 20], [@pdf.bounds.right, 20]
@@ -237,17 +246,17 @@ if !ARGV[0].empty?
 
   @data = JSON.load(File.read(ARGV[0]))
 
-      file          = ''
-      margin        = 20
-      _pdf          = Prawn::Document.new(:page_size   => 'A4',
-                                          :page_layout => :landscape,
-                                          :margin      => margin
-      )
+  file   = ''
+  margin = 20
+  _pdf   = Prawn::Document.new(:page_size   => 'A4',
+                               :page_layout => :landscape,
+                               :margin      => margin
+  )
 
-    pdf      = LaskutuspoytakirjaPDF.new
-    pdf.data = @data
-    file = pdf.generate _pdf
-    _pdf.render_file "/tmp/#{file}"
-    puts file
+  pdf      = LaskutuspoytakirjaPDF.new
+  pdf.data = @data
+  file     = pdf.generate _pdf
+  _pdf.render_file "/tmp/#{file}"
+  puts file
 
 end
--- tilauskasittely/laskutuspoytakirja_pdf.php
+++ tilauskasittely/laskutuspoytakirja_pdf.php
@@ -14,103 +14,25 @@ else {
 
 function hae_laskutuspoytakirja($lasku_tunnukset) {
   if (!empty($lasku_tunnukset)) {
-    $tyomaaraykset = \PDF\Laskutuspoytakirja\pdf_hae_tyomaaraykset($lasku_tunnukset);
+    $tyomaarays = \PDF\Laskutuspoytakirja\pdf_hae_tyomaaraykset($lasku_tunnukset);
 
-    sorttaa($tyomaaraykset);
-
-    die();
-    foreach ($tyomaaraykset as $key1 => $value1) {
-
-      foreach ($value1['rivit'] as $key2 => $value2) {
-        $a1 = $value2;
-        $a2 = array(
-            "tuoteno" => "1",
-            "nimitys" => "x",
-            "kpl"     => 1,
-            "hinta"   => 1,
-            "ale1"    => 1
-        );
-        $uusi_rivi = array_intersect_key($a1, $a2);
-        $uudet_rivit[] = $uusi_rivi;
-      }
-
-      $groups = array();
-
-      $full_total = 0;
-
-      foreach ($uudet_rivit as $index => $uusi_rivi) {
-
-        $full_total += $uusi_rivi['hinta'];
-        $tuoteno = $uusi_rivi['tuoteno'];
-
-        if (!isset($groups[$tuoteno])) {
-          $count = 1;
-          $groups[$tuoteno] = 1;
-        }
-        else {
-          $count += 1;
-          unset($uudet_rivit[$index - 1]);
-        }
-
-        if (isset($uudet_rivit[$index])) {
-          $uudet_rivit[$index]['kpl'] = $count;
-          $uudet_rivit[$index]['hinta'] = number_format($uudet_rivit[$index]['hinta'], 2);
-          $total = $uudet_rivit[$index]['kpl'] * $uudet_rivit[$index]['hinta'];
-          $total = $total - ( ( $total / 100 ) * $uudet_rivit[$index]['ale1'] );
-          $uudet_rivit[$index]['total'] = number_format($total, 2);
-        }
-      }
-      $uudet_rivit = array_values($uudet_rivit);
-      usort($uudet_rivit, "\PDF\Laskutuspoytakirja\kpl_sort");
-      $tyomaaraykset[$key1]['full_total'] = number_format($full_total, 2);
-      $tyomaaraykset[$key1]['rivit'] = $uudet_rivit;
-      $filepath = kirjoita_json_tiedosto($tyomaaraykset[$key1], "laskutuspoytakirja_{$tyomaaraykset[$key1]['tunnus']}");
-      $pdf_tiedosto = aja_ruby($filepath, 'laskutuspoytakirja_pdf');
-    }
-  }
-  return $pdf_tiedosto;
-}
-
-function sorttaa($tyomaarays) {
-  global $kukarow, $yhtiorow;
-
-  $laskutuspoytakirja_rivit = array();
-  //SELECT DISTINCT tuoteno...
-  $unique_tuotenos = array_unique(array_column($tyomaarays['rivit'], 'tuoteno'));
-
-  foreach ($unique_tuotenos as $tuoteno) {
-    $rivit = search_array_key_for_value_recursive($tyomaarays['rivit'], 'tuoteno', $tuoteno);
-    
     $params = array(
         'direction' => 'y',
-        'key'       => 'hinta'
+        'key'       => 'hinta_yhteensa'
     );
-    $hinta = sum_array($rivit, $params);
+    $tyomaarays['kaikki_yhteensa'] = sum_array($tyomaarays['rivit'], $params);
+
     $params = array(
         'direction' => 'y',
-        'key'       => 'tilkpl'
+        'key'       => 'alv_maara_yhteensa'
     );
-    $kpl = sum_array($rivit, $params);
-    $laskutuspoytakirja_rivit[$tuoteno] = array(
-        'tuoteno' => $tuoteno,
-        'nimitys' => $rivit[0]['nimitys'],
-        'hinta' => $hinta,
-        'kpl' => $kpl,
-    );
-  }
-  
-  return $laskutuspoytakirja_rivit;
-}
+    $tyomaarays['alv_maara_yhteensa'] = (float) number_format(sum_array($tyomaarays['rivit'], $params), 2);
+    $tyomaarays['alv_prosentti'] = $tyomaarays['rivit'][0]['alv_prosentti'];
 
-function kpl_sort($a, $b) {
-  if ($a['kpl'] < $b['kpl']) {
-    return 1;
-  }
-  else if ($a['kpl'] > $b['kpl']) {
-    return -1;
-  }
-  else {
-    return 0;
+    $filepath = kirjoita_json_tiedosto($tyomaarays, "laskutuspoytakirja_{$tyomaarays['tunnus']}");
+    $pdf_tiedosto = aja_ruby($filepath, 'laskutuspoytakirja_pdf');
+
+    return $pdf_tiedosto;
   }
 }
 
@@ -126,6 +48,7 @@ function pdf_hae_tyomaaraykset($lasku_tunnukset) {
   }
 
   //queryyn joinataan tauluja kohteeseen saakka, koska tarkastusp?yt?kirjat halutaan tulostaa per kohde mutta ty?m??r?ykset on laite per ty?m??r?in
+  //Käyttöliittymästä tulee vain yhden kohteen lasku.tunnuksia
   $query = "SELECT lasku.*,
             kohde.tunnus as kohde_tunnus,
             laite.tunnus as laite_tunnus
@@ -148,165 +71,109 @@ function pdf_hae_tyomaaraykset($lasku_tunnukset) {
               AND kohde.tunnus = paikka.kohde )
             WHERE lasku.yhtio = '{$kukarow['yhtio']}'
             AND lasku.tunnus IN({$lasku_tunnukset})
-            GROUP BY lasku.tunnus";
+            LIMIT 1";
 
   $result = pupe_query($query);
 
-  $tyomaarays = false;
-  $rivit = array();
-  while ($tyomaarays_temp = mysql_fetch_assoc($result)) {
-    if (!$tyomaarays) {
-      $tyomaarays['kohde'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_kohde($tyomaarays_temp['laite_tunnus']);
-      $tyomaarays['asiakas'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_asiakas($tyomaarays_temp['liitostunnus']);
-      $tyomaarays['yhtio'] = $yhtiorow;
-      $tyomaarays['logo'] = base64_encode(hae_yhtion_lasku_logo());
-      $tyomaarays['tyomaarays'] = $tyomaarays_temp;
-    }
-
-    $tyomaaraysrivit_temp = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_rivit($tyomaarays_temp['tunnus']);
-    foreach ($tyomaaraysrivit_temp as $rivi_temp) {
-      $rivit[] = $rivi_temp;
-    }
-  }
-
-  $tyomaarays['rivit'] = $rivit;
+  $tyomaarays = mysql_fetch_assoc($result);
+  $tyomaarays['kohde'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_kohde($tyomaarays['kohde_tunnus']);
+  $tyomaarays['asiakas'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_asiakas($tyomaarays['liitostunnus']);
+  $tyomaarays['yhtio'] = $yhtiorow;
+  $tyomaarays['logo'] = base64_encode(hae_yhtion_lasku_logo());
+  $tyomaarays['rivit'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_rivit($lasku_tunnukset);
 
   return $tyomaarays;
-
-
-
-  $tyomaaraykset = array();
-  while ($tyomaarays = mysql_fetch_assoc($result)) {
-    //vaikka ty?m??r?yksen kaikki tiedot saisi yll? olevasta queryst?,
-    //haetaan ne silti erillisill? queryill?,
-    //koska ruby:lle p?ss?tt?v?st? arrayst? halutaan multidimensional array.
-    //T?m? sen takia ett? pdf koodiin ei tarvitsisi koskea.
-    if (isset($tyomaaraykset[$tyomaarays['kohde_tunnus']])) {
-      $tyomaaraysrivit_temp = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_rivit($tyomaarays['tunnus']);
-      foreach ($tyomaaraysrivit_temp as $rivi_temp) {
-        $tyomaaraykset[$tyomaarays['kohde_tunnus']]['rivit'][] = $rivi_temp;
-      }
-    }
-    else {
-      $tyomaarays['rivit'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_rivit($tyomaarays['tunnus']);
-      $tyomaarays['kohde'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_kohde($tyomaarays['laite_tunnus']);
-      $tyomaarays['asiakas'] = \PDF\Laskutuspoytakirja\hae_tyomaarayksen_asiakas($tyomaarays['liitostunnus']);
-      $tyomaarays['yhtio'] = $yhtiorow;
-      $tyomaarays['logo'] = base64_encode(hae_yhtion_lasku_logo());
-      $tyomaaraykset[$tyomaarays['kohde_tunnus']] = $tyomaarays;
-    }
-  }
-
-  return $tyomaaraykset;
 }
 
-function hae_tyomaarayksen_rivit($lasku_tunnus) {
+function hae_tyomaarayksen_rivit($lasku_tunnukset) {
   global $kukarow, $yhtiorow;
 
-  $query = "SELECT tilausrivi.*,
-            tilausrivin_lisatiedot.*,
-            tilausrivin_lisatiedot.asiakkaan_positio AS laite_tunnus,
-            toimenpiteen_tyyppi.selite AS toimenpiteen_tyyppi,
-            huoltosyklit_laitteet.huoltovali AS toimenpiteen_huoltovali,
-            poikkeamarivi.tunnus AS poikkeamarivi_tunnus
+  $alekentta = generoi_alekentta('M', 'tilausrivi', 'ei');
+  $query = "SELECT tilausrivi.tuoteno,
+            tilausrivi.nimitys,
+            tilausrivi.tilkpl,
+            tilausrivi.hinta,
+            tilausrivi.alv,
+            tilausrivi.hinta * tilausrivi.tilkpl AS alentamaton_rivihinta,
+            {$alekentta} * tilausrivi.hinta * tilausrivi.tilkpl AS rivihinta,
+            ({$alekentta} * tilausrivi.hinta * tilausrivi.tilkpl) * (tilausrivi.alv / 100) AS alv_maara
             FROM tilausrivi
-            JOIN tyomaarays
-            ON ( tyomaarays.yhtio = tilausrivi.yhtio
-              AND tyomaarays.otunnus = tilausrivi.otunnus
-              AND tyomaarays.tyostatus != 'K')
-            JOIN tilausrivin_lisatiedot
-            ON ( tilausrivin_lisatiedot.yhtio = tilausrivi.yhtio
-              AND tilausrivin_lisatiedot.tilausrivitunnus = tilausrivi.tunnus
-              AND tilausrivin_lisatiedot.asiakkaan_positio != 0 )
-            LEFT JOIN tilausrivi AS poikkeamarivi
-            ON ( poikkeamarivi.yhtio = tilausrivin_lisatiedot.yhtio
-              AND poikkeamarivi.tunnus = tilausrivin_lisatiedot.tilausrivilinkki )
-            LEFT JOIN tuotteen_avainsanat AS toimenpiteen_tyyppi
-            ON ( toimenpiteen_tyyppi.yhtio = tilausrivi.yhtio
-              AND toimenpiteen_tyyppi.tuoteno = tilausrivi.tuoteno
-              AND toimenpiteen_tyyppi.laji = 'tyomaarayksen_ryhmittely' )
-            JOIN laite
-            ON ( laite.yhtio = tilausrivin_lisatiedot.yhtio
-              AND laite.tunnus = tilausrivin_lisatiedot.asiakkaan_positio )
-            JOIN paikka
-            ON ( paikka.yhtio = laite.yhtio
-              AND paikka.tunnus = laite.paikka )
-            JOIN tuote AS laite_tuote
-            ON ( laite_tuote.yhtio = laite.yhtio
-              AND laite_tuote.tuoteno = laite.tuoteno )
-            JOIN tuotteen_avainsanat AS sammutin_koko
-            ON ( sammutin_koko.yhtio = laite_tuote.yhtio
-              AND sammutin_koko.tuoteno = laite_tuote.tuoteno
-              AND sammutin_koko.laji = 'sammutin_koko' )
-            JOIN tuotteen_avainsanat AS sammutin_tyyppi
-            ON ( sammutin_tyyppi.yhtio = laite_tuote.yhtio
-              AND sammutin_tyyppi.tuoteno = laite_tuote.tuoteno
-              AND sammutin_tyyppi.laji = 'sammutin_tyyppi' )
-            LEFT JOIN huoltosykli
-            ON ( huoltosykli.yhtio = tilausrivi.yhtio
-              AND huoltosykli.toimenpide = tilausrivi.tuoteno
-              AND huoltosykli.olosuhde = paikka.olosuhde
-              AND huoltosykli.koko = sammutin_koko.selite
-              AND huoltosykli.tyyppi = sammutin_tyyppi.selite )
-            LEFT JOIN huoltosyklit_laitteet
-            ON ( huoltosyklit_laitteet.yhtio = huoltosykli.yhtio
-              AND huoltosyklit_laitteet.huoltosykli_tunnus = huoltosykli.tunnus
-              AND huoltosyklit_laitteet.laite_tunnus = laite.tunnus )
             WHERE tilausrivi.yhtio = '{$kukarow['yhtio']}'
-            AND tilausrivi.otunnus = '{$lasku_tunnus}'
-            AND tilausrivi.var != 'P'";
+            AND tilausrivi.var != 'P'
+            AND tilausrivi.otunnus IN ({$lasku_tunnukset})";
 
   $result = pupe_query($query);
 
   $rivit = array();
   while ($rivi = mysql_fetch_assoc($result)) {
-
-    $rivi['laite'] = \PDF\Laskutuspoytakirja\hae_rivin_laite($rivi['asiakkaan_positio']);
-
-    if (search_array_key_for_value_recursive($rivi['sisaltyvat_tyot'], 'toimenpiteen_tyyppi', 'tarkastus')) {
-      $rivi['tarkastus'] = date('my', strtotime($rivi['toimitettuaika']));
-    }
-
-    if (search_array_key_for_value_recursive($rivi['sisaltyvat_tyot'], 'toimenpiteen_tyyppi', 'huolto')) {
-      $rivi['huolto'] = date('my', strtotime($rivi['toimitettuaika']));
-    }
-
     $rivit[] = $rivi;
   }
 
-  return $rivit;
-}
+  $unique_tuotenos = array_unique(array_column($rivit, 'tuoteno'));
 
-function hae_rivin_laite($laite_tunnus) {
-  global $kukarow, $yhtiorow;
+  $laskutuspoytakirja_rivit = array();
+  foreach ($unique_tuotenos as $tuoteno) {
+    $rivit_temp = search_array_key_for_value_recursive($rivit, 'tuoteno', $tuoteno);
 
-  $query = "SELECT laite.*,
-            concat_ws(' ', tuote.nimitys, tuote.tuoteno) as nimitys,
-            sammutin_koko.selite as sammutin_koko,
-            sammutin_tyyppi.selite as sammutin_tyyppi
-            FROM laite
-            JOIN tuote
-            ON ( tuote.yhtio = laite.yhtio
-              AND tuote.tuoteno = laite.tuoteno )
-            JOIN tuotteen_avainsanat as sammutin_koko
-            ON ( sammutin_koko.yhtio = tuote.yhtio
-              AND sammutin_koko.tuoteno = tuote.tuoteno
-              AND sammutin_koko.laji = 'sammutin_koko' )
-            JOIN tuotteen_avainsanat as sammutin_tyyppi
-            ON ( sammutin_tyyppi.yhtio = tuote.yhtio
-              AND sammutin_tyyppi.tuoteno = tuote.tuoteno
-              AND sammutin_tyyppi.laji = 'sammutin_tyyppi' )
-            WHERE laite.yhtio = '{$kukarow['yhtio']}'
-            AND laite.tunnus = '{$laite_tunnus}'";
-  $result = pupe_query($query);
+    $params = array(
+        'direction' => 'y',
+        'key'       => 'rivihinta'
+    );
+    $rivihinta_yhteensa = sum_array($rivit_temp, $params);
 
-  $laite = mysql_fetch_assoc($result);
+    $params = array(
+        'direction' => 'y',
+        'key'       => 'tilkpl'
+    );
+    $kpl_yhteensa = sum_array($rivit_temp, $params);
 
-  return $laite;
+    $params = array(
+        'direction' => 'y',
+        'key'       => 'alv_maara'
+    );
+    $alv_maara_yhteensa = sum_array($rivit_temp, $params);
+
+    $params = array(
+        'direction' => 'y',
+        'key'       => 'alentamaton_rivihinta'
+    );
+    $alentamaton_rivihinta = sum_array($rivit_temp, $params);
+
+    if ($rivihinta_yhteensa != 0 and $alentamaton_rivihinta != 0 and $rivihinta_yhteensa != $alentamaton_rivihinta) {
+      $alennusprosentti = number_format($rivihinta_yhteensa / $alentamaton_rivihinta, 2);
+    }
+    else {
+      $alennusprosentti = 0.00;
+    }
+
+    $laskutuspoytakirja_rivit[] = array(
+        'tuoteno'            => $tuoteno,
+        'nimitys'            => $rivit_temp[0]['nimitys'],
+        'hinta'              => $rivit_temp[0]['hinta'],
+        'hinta_yhteensa'     => $rivihinta_yhteensa,
+        'alv_maara_yhteensa' => $alv_maara_yhteensa,
+        'kpl'                => $kpl_yhteensa,
+        'alennusprosentti'   => $alennusprosentti,
+        'alv_prosentti'      => $rivit_temp[0]['alv'],
+    );
+  }
+
+  $kpl_sort = array();
+  foreach ($laskutuspoytakirja_rivit as $key => $row) {
+    $kpl_sort[$key] = $row['kpl'];
+  }
+
+  array_multisort($kpl_sort, SORT_DESC, $laskutuspoytakirja_rivit);
+
+  return $laskutuspoytakirja_rivit;
 }
 
 function hae_tyomaarayksen_asiakas($asiakas_tunnus) {
   global $kukarow, $yhtiorow;
+
+  if (empty($asiakas_tunnus)) {
+    return array();
+  }
 
   $query = "SELECT *
             FROM asiakas
@@ -317,19 +184,17 @@ function hae_tyomaarayksen_asiakas($asiakas_tunnus) {
   return mysql_fetch_assoc($result);
 }
 
-function hae_tyomaarayksen_kohde($laite_tunnus) {
+function hae_tyomaarayksen_kohde($kohde_tunnus) {
   global $kukarow, $yhtiorow;
+
+  if (empty($kohde_tunnus)) {
+    return array();
+  }
 
   $query = "SELECT kohde.*
             FROM kohde
-            JOIN paikka
-            ON ( paikka.yhtio = kohde.yhtio
-              AND paikka.kohde = kohde.tunnus )
-            JOIN laite
-            ON ( laite.yhtio = kohde.yhtio
-              AND laite.paikka = paikka.tunnus
-              AND laite.tunnus = '{$laite_tunnus}')
-            WHERE kohde.yhtio = '{$kukarow['yhtio']}'";
+            WHERE kohde.yhtio = '{$kukarow['yhtio']}'
+            AND kohde.tunnus = '{$kohde_tunnus}'";
   $result = pupe_query($query);
 
   return mysql_fetch_assoc($result);

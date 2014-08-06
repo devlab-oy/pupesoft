d718af5a917f8fa8458ae638044a67d741c1d11e
Sami Luukkonen <sami@devlab.fi>
2014-08-06 15:04:15 +0300

    Käytetään toimipaikkaa

--- infobar.php
+++ infobar.php
@@ -1,15 +1,21 @@
 <?php
 
-$query = "SELECT *
-          FROM lasku use index (tila_index)
-          WHERE yhtio = '{$kukarow['yhtio']}'
-          AND tila = 'N'
-          AND alatila = 'F'
-          AND myyja = '{$kukarow['kuka']}'";
-$res = pupe_query($query);
-$kpl = mysql_num_rows($res);
+if ($kukarow['toimipaikka'] > 0) {
 
-echo "<div style='text-align:right; vertical-align:top;'>";
-echo "<font class='message'>";
-echo t("EXT-tilauksia %d kpl", "", $kpl);
-echo "</font></div>";
+  $query = "SELECT *
+            FROM lasku use index (tila_index)
+            WHERE yhtio = '{$kukarow['yhtio']}'
+            AND tila = 'N'
+            AND alatila = 'F'
+            AND yhtio_toimipaikka = '{$kukarow['toimipaikka']}'";
+  $res = pupe_query($query);
+  $kpl = mysql_num_rows($res);
+
+  if ($kpl > 0) {
+
+    echo "<div style='text-align:right; vertical-align:top;'>";
+    echo "<font class='message'>";
+    echo t("EXT-tilauksia %d kpl", "", $kpl);
+    echo "</font></div>";
+  }
+}

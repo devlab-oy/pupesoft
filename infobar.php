d5287b66bad345daa2774022dc50111e239001f8
Sami Luukkonen <sami@devlab.fi>
2014-08-08 13:34:28 +0300

    Oh well, so sry

--- infobar.php
+++ infobar.php
@@ -1,32 +1,32 @@
 <?php
 
-$query = "SELECT *
+$info_query = "SELECT *
           FROM lasku use index (tila_index)
           WHERE yhtio = '{$kukarow['yhtio']}'
           AND tila = 'N'
           AND alatila = 'F'
           AND yhtio_toimipaikka = '{$kukarow['toimipaikka']}'";
-$res = pupe_query($query);
-$kpl = mysql_num_rows($res);
+$info_res = pupe_query($info_query);
+$info_kpl = mysql_num_rows($info_res);
 
-while ($row = mysql_fetch_assoc($res)) {
-  $query = "SELECT nimi, kuka
+while ($info_row = mysql_fetch_assoc($info_res)) {
+  $info_query = "SELECT nimi, kuka
             FROM kuka
             WHERE yhtio  = '{$kukarow['yhtio']}'
-            AND kesken   = '{$row['tunnus']}'
+            AND kesken   = '{$info_row['tunnus']}'
             AND kesken  != 0
             AND kuka    != '{$kukarow['kuka']}'";
-  $result = pupe_query($query);
+  $info_result = pupe_query($info_query);
 
-  if (mysql_num_rows($result) != 0) {
-    $kpl -= 1;
+  if (mysql_num_rows($info_result) != 0) {
+    $info_kpl -= 1;
   }
 }
 
-if ($kpl > 0) {
+if ($info_kpl > 0) {
 
   echo "<div style='text-align:right; vertical-align:top; margin-right: 10px;'>";
   echo "<font class='message ok'>";
-  echo t("EXT-tilauksia %d kpl", "", $kpl);
+  echo t("EXT-tilauksia %d kpl", "", $info_kpl);
   echo "</font></div>";
 }

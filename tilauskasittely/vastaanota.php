fcedf61b29be8db129df3b2fc66d3e303a27b743
Sami Luukkonen <sami@devlab.fi>
2014-09-01 12:22:56 +0300

    Palautettu continue ehto looppiin

--- tilauskasittely/vastaanota.php
+++ tilauskasittely/vastaanota.php
@@ -996,8 +996,6 @@ if (empty($id) and $echotaanko) {
 
   $group_per_lahto = $yhtiorow['siirtolistat_vastaanotetaan_per_lahto'];
 
-  $having_lisa = $yhtiorow['siirtolistan_tulostustapa'] == 'U' ? "HAVING lahdot.aktiivi = 'S'" : "";
-
   $query = "SELECT IF(siirtolistan_vastaanotto = 0, 'x', siirtolistan_vastaanotto) siirtolistan_vastaanotto,
             IF((siirtolistan_vastaanotto != 0 OR toimitustavan_lahto = 0 OR '{$group_per_lahto}' = ''), 'x', toimitustavan_lahto) lahto,
             IF(siirtolistan_vastaanotto = 0, lasku.clearing, 0) clearing,
@@ -1019,7 +1017,6 @@ if (empty($id) and $echotaanko) {
             and tilausrivi.keratty    != ''
             $varasto
             GROUP BY 1,2,3,4
-            {$having_lisa}
             ORDER BY siirtolistan_vastaanotto, lahto, clearing";
   $tilre = pupe_query($query);
 
@@ -1078,6 +1075,12 @@ if (empty($id) and $echotaanko) {
   $ed_lahto = $ed_vastaanottonro = null;
 
   while ($tilrow = mysql_fetch_assoc($tilre)) {
+
+     if ($yhtiorow['siirtolistan_tulostustapa'] == 'U' and !is_null($tilrow['lahdon_aktiivi']) and $tilrow['lahdon_aktiivi'] != 'S') {
+        //Jos siirtolistan_tulostustapa = Siirtolistat tulostetaan keräyserien kautta
+        //Ei näytetä siirtolistoja, joihin on liitetty aukioleva lähtö
+        continue;
+    }
 
     // etsitään sopivia tilauksia
     $query = "SELECT varasto,

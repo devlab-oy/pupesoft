442924497671c9f1e411d22e248dc9e8caf74718
tiere <tommi@devlab.fi>
2014-09-01 16:05:02 +0300

    Korjataan include ja tiedostojen haku query

--- tiedostokirjasto.php
+++ tiedostokirjasto.php
@@ -1,6 +1,6 @@
 <?php
 
-(@include "inc/parametrit.inc") || (@include "parametric.inc") || exit;
+(@include "inc/parametrit.inc") || (@include "parametrit.inc") || exit;
 
 echo "<font class='head'>" . t('Tiedostokirjasto') . "</font>";
 echo "<hr>";
@@ -98,10 +98,8 @@ function hae_tiedostot($toimittajan_tunnus, $tiedoston_tyyppi) {
               liitetiedostot.selite
             FROM liitetiedostot
             INNER JOIN tuote ON (tuote.tunnus = liitetiedostot.liitostunnus)
-            INNER JOIN tuotteen_toimittajat ON (tuote.tuoteno = tuotteen_toimittajat.tuoteno)
-            INNER JOIN toimi ON (toimi.tunnus = tuotteen_toimittajat.liitostunnus)
-            WHERE toimi.tunnus = '{$toimittajan_tunnus}'
-            AND liitetiedostot.kayttotarkoitus = 'ohjekirja'
+            INNER JOIN toimi ON (toimi.tunnus = '{$toimittajan_tunnus}')
+            WHERE liitetiedostot.kayttotarkoitus = '{$tiedoston_tyyppi}'
             AND liitetiedostot.yhtio = '{$kukarow['yhtio']}'
             ORDER BY liitetiedostot.selite";
   $result = pupe_query($query);

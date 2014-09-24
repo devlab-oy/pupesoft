da758e4ec5290dc3e36dae956086ebf80d1a9b42
Johan Tötterman <juppe@devlab.fi>
2014-09-24 15:51:39 +0300

    Sisennys korjattu!

--- tuotepaallikkomuistutus.php
+++ tuotepaallikkomuistutus.php
@@ -1,129 +1,129 @@
 <?php
 
-	//* Tämä skripti käyttää slave-tietokantapalvelinta *//
-	$useslave = 1;
+  //* Tämä skripti käyttää slave-tietokantapalvelinta *//
+  $useslave = 1;
 
-	// Kutsutaanko CLI:stä
-	if (php_sapi_name() != 'cli') {
-		die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
-	}
+  // Kutsutaanko CLI:stä
+  if (php_sapi_name() != 'cli') {
+    die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
+  }
 
-	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
 
-	require ("inc/connect.inc");
+  require ("inc/connect.inc");
   require ("inc/functions.inc");
 
   // Logitetaan ajo
   cron_log();
 
-	if ($argv[1] == '') {
-		echo "Yhtiötä ei ole annettu, ei voida toimia\n";
-		die;
-	}
-	else {
-		$kukarow["yhtio"] = $argv[1];
-		$yhtiorow = hae_yhtion_parametrit($kukarow['yhtio']);
-	}
+  if ($argv[1] == '') {
+    echo "Yhtiötä ei ole annettu, ei voida toimia\n";
+    die;
+  }
+  else {
+    $kukarow["yhtio"] = $argv[1];
+    $yhtiorow = hae_yhtion_parametrit($kukarow['yhtio']);
+  }
 
-	$haku = date("d.m");
+  $haku = date("d.m");
 
-	// Ajopäivämääräluokat
-	$arvot = array(	1 => array('pvm1'=>'01.01','pvm2'=>''     ,'pvm3'=>''     ,'pvm4'=>''),
-					2 => array('pvm1'=>'01.01','pvm2'=>'01.07','pvm3'=>''     ,'pvm4'=>''),
-					3 => array('pvm1'=>'01.01','pvm2'=>'01.05','pvm3'=>'01.09','pvm4'=>''),
-					4 => array('pvm1'=>'01.01','pvm2'=>'01.04','pvm3'=>'01.07','pvm4'=>'01.10'));
-	$syklit = "";
+  // Ajopäivämääräluokat
+  $arvot = array(1 => array('pvm1'=>'01.01', 'pvm2'=>'',      'pvm3'=>'',      'pvm4'=>''),
+                 2 => array('pvm1'=>'01.01', 'pvm2'=>'01.07', 'pvm3'=>'',      'pvm4'=>''),
+                 3 => array('pvm1'=>'01.01', 'pvm2'=>'01.05', 'pvm3'=>'01.09', 'pvm4'=>''),
+                 4 => array('pvm1'=>'01.01', 'pvm2'=>'01.04', 'pvm3'=>'01.07', 'pvm4'=>'01.10'));
+  $syklit = "";
 
-	foreach ($arvot as $nro => $arr) {
-		foreach ($arr as $pvm_tag => $paivamaara) {
-			if ($paivamaara == $haku) {
-				$syklit .= $nro.",";
-			}
-		}
-	}
+  foreach ($arvot as $nro => $arr) {
+    foreach ($arr as $pvm_tag => $paivamaara) {
+      if ($paivamaara == $haku) {
+        $syklit .= $nro.",";
+      }
+    }
+  }
 
-	$syklit = substr($syklit, 0, -1);
+  $syklit = substr($syklit, 0, -1);
 
-	if (trim($syklit) != "") {
+  if (trim($syklit) != "") {
 
-		// 1. nämä toimittajat ovat toimittaneet meille tavaraa ja jotka ovat AKTIIVISIA, ei poistettuja tai poistuvia
-		$sql = "	SELECT tuotteen_toimittajat.liitostunnus, toimi.hintojenpaivityssykli, toimi.nimi
-					FROM tuotteen_toimittajat
-					JOIN toimi ON (toimi.yhtio = tuotteen_toimittajat.yhtio
-						AND toimi.tunnus = tuotteen_toimittajat.liitostunnus
-						AND toimi.tyyppi not in ('P', 'PP')
-						AND toimi.hintojenpaivityssykli in ($syklit))
-					WHERE tuotteen_toimittajat.yhtio = '$kukarow[yhtio]'
-					GROUP BY tuotteen_toimittajat.liitostunnus";
-		$result = pupe_query($sql);
+    // 1. nämä toimittajat ovat toimittaneet meille tavaraa ja jotka ovat AKTIIVISIA, ei poistettuja tai poistuvia
+    $sql = "  SELECT tuotteen_toimittajat.liitostunnus, toimi.hintojenpaivityssykli, toimi.nimi
+              FROM tuotteen_toimittajat
+              JOIN toimi ON (toimi.yhtio = tuotteen_toimittajat.yhtio
+                AND toimi.tunnus = tuotteen_toimittajat.liitostunnus
+                AND toimi.tyyppi not in ('P', 'PP')
+                AND toimi.hintojenpaivityssykli in ($syklit))
+              WHERE tuotteen_toimittajat.yhtio = '$kukarow[yhtio]'
+              GROUP BY tuotteen_toimittajat.liitostunnus";
+    $result = pupe_query($sql);
 
-		$laheta_meilit = array();
+    $laheta_meilit = array();
 
-		if (mysql_num_rows($result) > 0) {
+    if (mysql_num_rows($result) > 0) {
 
-			while ($ttrow = mysql_fetch_assoc($result)) {
+      while ($ttrow = mysql_fetch_assoc($result)) {
 
-				// 2. tämä hakee toimittajan tuotteet, jotka ovat aktiivisia tuotteita
-				$tuotteet="	SELECT group_concat(distinct tuote.tunnus) lista_tunnuksista
-							FROM tuotteen_toimittajat
-							JOIN tuote ON (tuote.yhtio = tuotteen_toimittajat.yhtio
-									AND tuote.tuoteno = tuotteen_toimittajat.tuoteno
-									AND tuote.status not in ('P','X'))
-							WHERE tuotteen_toimittajat.yhtio = '$kukarow[yhtio]'
-							AND tuotteen_toimittajat.liitostunnus = '$ttrow[liitostunnus]'";
-				$result2 = pupe_query($tuotteet);
-				$tuoterow = mysql_fetch_assoc($result2);
+        // 2. tämä hakee toimittajan tuotteet, jotka ovat aktiivisia tuotteita
+        $tuotteet=" SELECT group_concat(distinct tuote.tunnus) lista_tunnuksista
+                    FROM tuotteen_toimittajat
+                    JOIN tuote ON (tuote.yhtio = tuotteen_toimittajat.yhtio
+                        AND tuote.tuoteno = tuotteen_toimittajat.tuoteno
+                        AND tuote.status not in ('P','X'))
+                    WHERE tuotteen_toimittajat.yhtio = '$kukarow[yhtio]'
+                    AND tuotteen_toimittajat.liitostunnus = '$ttrow[liitostunnus]'";
+        $result2 = pupe_query($tuotteet);
+        $tuoterow = mysql_fetch_assoc($result2);
 
-				// 3. haetaan kyseisien tuotteiden tuotepäällikkönro
-				$tuotesql="	SELECT DISTINCT tuotepaallikko
-							FROM tuote
-							WHERE yhtio = '$kukarow[yhtio]'
-							AND tunnus in ($tuoterow[lista_tunnuksista])
-							AND tuotepaallikko != 0";
-				$result3 = pupe_query($tuotesql);
+        // 3. haetaan kyseisien tuotteiden tuotepäällikkönro
+        $tuotesql=" SELECT DISTINCT tuotepaallikko
+                    FROM tuote
+                    WHERE yhtio = '$kukarow[yhtio]'
+                    AND tunnus in ($tuoterow[lista_tunnuksista])
+                    AND tuotepaallikko != 0";
+        $result3 = pupe_query($tuotesql);
 
-				while ($henkilot = mysql_fetch_assoc($result3)) {
-					$laheta_meilit[$henkilot["tuotepaallikko"]][$ttrow["liitostunnus"]] = $ttrow["nimi"];
-				}
+        while ($henkilot = mysql_fetch_assoc($result3)) {
+          $laheta_meilit[$henkilot["tuotepaallikko"]][$ttrow["liitostunnus"]] = $ttrow["nimi"];
+        }
 
-			}
+      }
 
-			if (count($laheta_meilit) > 0) {
+      if (count($laheta_meilit) > 0) {
 
-				foreach ($laheta_meilit as $key => $values) {
+        foreach ($laheta_meilit as $key => $values) {
 
-					$lista = "";
-					foreach ($values as $firma => $value) {
-						$lista .= "\n- $value";
-					}
+          $lista = "";
+          foreach ($values as $firma => $value) {
+            $lista .= "\n- $value";
+          }
 
-					// 4. Haetaan ostajan ja tuotepyällikön sähköpostiosoitteet esille.
-					$postisql = "	SELECT nimi, eposti
-									FROM kuka
-									WHERE yhtio = '$kukarow[yhtio]'
-									AND myyja = '$key'";
-					$resuposti = pupe_query($postisql);
+          // 4. Haetaan ostajan ja tuotepyällikön sähköpostiosoitteet esille.
+          $postisql = " SELECT nimi, eposti
+                        FROM kuka
+                        WHERE yhtio = '$kukarow[yhtio]'
+                        AND myyja = '$key'";
+          $resuposti = pupe_query($postisql);
 
-					while ($posti = mysql_fetch_assoc($resuposti)) {
+          while ($posti = mysql_fetch_assoc($resuposti)) {
 
-						$meili = t("Toimittajien hinnantarkistuspyyntö")."\n";
-						$meili .= "\nTervehdys {$posti["nimi"]} \n\n";
-						$meili .= t("Pyyntö").":\n".str_replace("\r\n","\n","Tarkista seuraavilta toimittajilta hinnat Pupesoftiin\n");
-						$meili .= $lista;
+            $meili = t("Toimittajien hinnantarkistuspyyntö")."\n";
+            $meili .= "\nTervehdys {$posti["nimi"]} \n\n";
+            $meili .= t("Pyyntö").":\n".str_replace("\r\n","\n","Tarkista seuraavilta toimittajilta hinnat Pupesoftiin\n");
+            $meili .= $lista;
 
-						if ($posti['eposti'] == "") {
-							$email_osoite = $yhtiorow['alert_email'];
-						}
-						else {
-							$email_osoite = $posti['eposti'];
-						}
+            if ($posti['eposti'] == "") {
+              $email_osoite = $yhtiorow['alert_email'];
+            }
+            else {
+              $email_osoite = $posti['eposti'];
+            }
 
-						$tulos = mail($email_osoite, mb_encode_mimeheader(t("Hinnan tarkistuspyyntö")." $yhtiorow[nimi]", "ISO-8859-1", "Q"), $meili, "From: ".mb_encode_mimeheader($yhtiorow["postittaja_email"], "ISO-8859-1", "Q")." <$yhtiorow[postittaja_email]>\n", "-f $yhtiorow[postittaja_email]");
-					}
-				}
-			}
-		}
-	}
+            $tulos = mail($email_osoite, mb_encode_mimeheader(t("Hinnan tarkistuspyyntö")." $yhtiorow[nimi]", "ISO-8859-1", "Q"), $meili, "From: ".mb_encode_mimeheader($yhtiorow["postittaja_email"], "ISO-8859-1", "Q")." <$yhtiorow[postittaja_email]>\n", "-f $yhtiorow[postittaja_email]");
+          }
+        }
+      }
+    }
+  }
 
 ?>

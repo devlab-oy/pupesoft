b4d76f5c4f21bbe5dff68ca32f4e42db4e8ab4c1
Hanna-Maria Hagberg <hanna@devlab.fi>
2014-09-25 15:39:07 +0300

    Ei nollata lasten määriä

--- tilauskasittely/keraa.php
+++ tilauskasittely/keraa.php
@@ -1247,7 +1247,7 @@ if ($tee == 'P') {
               $keraysera_update_res = pupe_query($query_upd);
             }
 
-            if (trim($maara[$apui]) != '') {
+            if (trim($maara[$apui]) != '' and $maara[$apui] != 0) {
               // haetaan lapset joilla on ohita_kerays täpätty ja tehdään poikkeama myös niille
               $query_lapset = "SELECT tilausrivi.tunnus, tilausrivi.varattu
                                FROM tilausrivi

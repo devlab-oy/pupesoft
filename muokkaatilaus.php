7eafb2d6b7b982aff147fe11df479aba86332682
Hanna-Maria Hagberg <hanna@devlab.fi>
2014-10-08 12:51:51 +0300

    Näytetään toimitusosoitteen postitoimpaikka

--- muokkaatilaus.php
+++ muokkaatilaus.php
@@ -847,7 +847,7 @@ else {
   }
 
   if ($asiakastiedot == "toimitus") {
-    $asiakasstring = " concat_ws('<br>', lasku.ytunnus, lasku.nimi, lasku.nimitark, if(lasku.nimi != lasku.toim_nimi, concat_ws(' ', lasku.toim_nimi, lasku.toim_nimitark, if(lasku.postitp != lasku.toim_postitp, lasku.toim_postitp, NULL)), NULL))";
+    $asiakasstring = " concat_ws('<br>', lasku.ytunnus, lasku.nimi, lasku.nimitark, if(lasku.nimi != lasku.toim_nimi, concat_ws(' ', lasku.toim_nimi, lasku.toim_nimitark, lasku.toim_postitp), NULL))";
     $assel1 = "";
     $assel2 = "CHECKED";
   }

e55d478406171dc7b4e944280a6ec237af7a2355
Oskari Paakkanen <oskari@devlab.fi>
2014-10-20 14:50:51 +0300

    sarjanumero-optio

--- mobiili/kuittaa.php
+++ mobiili/kuittaa.php
@@ -9,6 +9,8 @@ $valinta = "Etsi";
 if (@include_once "../inc/parametrit.inc");
 elseif (@include_once "inc/parametrit.inc");
 
+require 'generoi_edifact.inc';
+
 if (!isset($errors)) $errors = array();
 
 // Jos haulla ei löytyny mitään, ollaan palattu tälle sivulle virheparametrilla.
@@ -19,21 +21,28 @@ if (isset($virhe)) {
 if (isset($submit)) {
   switch ($submit) {
   case 'rahtikirjanumero':
-    if (empty($rahtikirjanumero)) {
-      $errors[] = t("Syötä rahtikirjanumero");
+  case 'sarjanumero':
+
+    if (empty($rahtikirjanumero) and empty($sarjanumero)) {
+      $errors[] = t("Syötä rahtikirjanumero tai sarjanumero");
       break;
     }
+    elseif (empty($rahtikirjanumero)) {
+      $parametrit = kuittaus_parametrit($sarjanumero, 'S');
+    }
+    else {
+      $parametrit = kuittaus_parametrit($rahtikirjanumero, 'R');
+    }
 
-    require 'generoi_edifact.inc';
-    $parametrit = kuittaus_parametrit($rahtikirjanumero);
-
-    if ($parametrit) {
+    if (is_string($parametrit)) {
+      $errors[] = t("Rahtikirja: {$parametrit} on jo kuitattu.");
+    }
+    elseif ($parametrit) {
       $sanoma = laadi_edifact_sanoma($parametrit);
     }
     else{
       $errors[] = t("Rahtikirjaa ei löytynyt!");
     }
-
     if ($sanoma) {
       $lahetys = 'X';
       if (laheta_sanoma($sanoma)) {
@@ -47,6 +56,7 @@ if (isset($submit)) {
       $errors[] = t("Ei sanomaa");
     }
     break;
+
   case 'takaisin':
     echo "<META HTTP-EQUIV='Refresh'CONTENT='0;URL=index.php'>"; exit();
     break;
@@ -71,22 +81,34 @@ echo "</div>";
 if ($lahetys == 'OK') {
   echo "<div style='text-align:center;'>";
   echo "Sanoma lähetetty!";
+
+echo '<hr>', $sanoma, '<hr>';
+
   echo "</div>";
 }
 
 echo "
 <form method='post' action=''>
   <div style='text-align:center;padding:10px;'>
-    <label for='rahtikirjanumero'>", t("Rahtikirjanumero"), "</label><br>
+    <label for='rahtikirjanumero'>", t("Syötä rahtikirjanumero"), "</label><br>
     <input type='text' id='rahtikirjanumero' name='rahtikirjanumero' style='margin:10px;' />
     <br>
     <button name='submit' value='rahtikirjanumero' onclick='submit();' class='button'>", t("OK"), "</button>
   </div>
 </form>
 
+<form method='post' action=''>
+  <div style='text-align:center;padding:10px;'>
+    <label for='sarjanumero'>", t("Tai lue jokin rahdin sarjanumero"), "</label><br>
+    <input type='text' id='sarjanumero' name='sarjanumero' style='margin:10px;' />
+    <br>
+    <button name='submit' value='sarjanumero' onclick='submit();' class='button'>", t("OK"), "</button>
+  </div>
+</form>
+
 <script type='text/javascript'>
   $(document).ready(function() {
-    $('#rahtikirjanumero').focus();
+    $('#sarjanumero').focus();
   });
 </script>";
 

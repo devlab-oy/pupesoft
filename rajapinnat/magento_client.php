cf32d4a9d1f694a30506f417883990cd665ffa90
Johan Tötterman <juppe@devlab.fi>
2014-10-24 14:07:30 +0300

    Kyselyjen sisennykset kuntoon.

--- rajapinnat/magento_client.php
+++ rajapinnat/magento_client.php
@@ -1384,9 +1384,9 @@ class MagentoClient {
                 liitetiedostot.filetype,
                 liitetiedostot.filename
                 FROM liitetiedostot
-                WHERE liitetiedostot.yhtio = '{$kukarow['yhtio']}'
-                AND liitetiedostot.liitostunnus = '{$tunnus}'
-                AND liitetiedostot.liitos = 'tuote'
+                WHERE liitetiedostot.yhtio         = '{$kukarow['yhtio']}'
+                AND liitetiedostot.liitostunnus    = '{$tunnus}'
+                AND liitetiedostot.liitos          = 'tuote'
                 AND liitetiedostot.kayttotarkoitus = 'TK'
                 ORDER BY liitetiedostot.jarjestys DESC,
                 liitetiedostot.tunnus DESC";
@@ -1610,7 +1610,7 @@ class MagentoClient {
                 tuotteen_avainsanat
                 WHERE yhtio = '{$kukarow['yhtio']}'
                 AND tuoteno = '{$tuotenumero}'
-                AND laji IN ('nimitys','kuvaus')";
+                AND laji    IN ('nimitys','kuvaus')";
       $result = pupe_query($query);
 
       while ($avainsana = mysql_fetch_assoc($result)) {

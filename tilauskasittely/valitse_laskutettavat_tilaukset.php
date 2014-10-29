4feabd5081f5d1979002b7f3931ddd27686fc2b4
Johan Tötterman <juppe@devlab.fi>
2014-10-29 15:31:59 +0200

    Verollisuudet oikein.

--- tilauskasittely/valitse_laskutettavat_tilaukset.php
+++ tilauskasittely/valitse_laskutettavat_tilaukset.php
@@ -79,14 +79,10 @@ if ($tee == 'TOIMITA' and isset($maksutapa) and $maksutapa == 'seka') {
   }
 
   $query_rivi = "SELECT lasku.valkoodi, lasku.maksuehto, lasku.hinta,
-                 sum(round(tilausrivi.hinta / if('$yhtiorow[alv_kasittely]' = '' 
-                   and tilausrivi.alv = 0, (1+tilausrivi.alv/100), 1) 
-                   * (tilausrivi.varattu+tilausrivi.kpl) 
-                   * {$query_ale_lisa},$yhtiorow[hintapyoristys])) verollinen,
-                 sum(round(tilausrivi.hinta * if('$yhtiorow[alv_kasittely]' != '' 
-                   and tilausrivi.alv > 0 and tilausrivi.alv < 500, (1+tilausrivi.alv/100), 1) 
-                   * (tilausrivi.varattu+tilausrivi.kpl) 
-                   * {$query_ale_lisa},$yhtiorow[hintapyoristys])) veroton
+                 sum(round(tilausrivi.hinta * if('$yhtiorow[alv_kasittely]' != ''
+                   and tilausrivi.alv > 0 and tilausrivi.alv < 500, (1+tilausrivi.alv/100), 1)
+                   * (tilausrivi.varattu+tilausrivi.kpl)
+                   * {$query_ale_lisa},$yhtiorow[hintapyoristys])) verollinen
                  FROM tilausrivi
                  JOIN lasku ON (lasku.yhtio=tilausrivi.yhtio AND lasku.tunnus=tilausrivi.otunnus)
                  WHERE var            != 'J'
@@ -102,13 +98,8 @@ if ($tee == 'TOIMITA' and isset($maksutapa) and $maksutapa == 'seka') {
             and yhtio='$kukarow[yhtio]'";
   $asres = pupe_query($query);
   $asrow = mysql_fetch_array($asres);
-  
-  if ($yhtiorow[alv_kasittely] == '') {
-    $summa = $laskurow["verollinen"];
-  }
-  else {
-    $summa = $laskurow["veroton"];
-  }
+
+  $summa = $laskurow["verollinen"];
 
   //Käsin syötetty summa johon lasku pyöristetään
   if ($laskurow["hinta"] <> 0 and abs($laskurow["hinta"]-$summa) <= 0.5 and abs($summa) >= 0.5) {

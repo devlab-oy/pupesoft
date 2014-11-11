92b7e1dcd2909b86078264a23777aa6ff776ba39
Oskari Paakkanen <oskari@devlab.fi>
2014-11-11 15:32:58 +0200

    uudet filet

---
+++ edifact_haku_cron.php
@@ -0,0 +1,77 @@
+<?php
+
+// Kutsutaanko CLI:stä
+if (php_sapi_name() != 'cli') {
+  die ("Tätä scriptiä voi ajaa vain komentoriviltä!");
+}
+
+require "inc/connect.inc";
+require "inc/functions.inc";
+require "inc/edifact_functions.inc";
+
+$host = $ftp_info['host'];
+$user = $ftp_info['user'];
+$pass = $ftp_info['pass'];
+
+$yhteys = ftp_connect($host);
+
+$login = ftp_login($yhteys, $user, $pass);
+
+if ((!$yhteys) || (!$login)) {
+  echo t("Ftp-yhteyden muodostus epaonnistui! Tarkista salasanat."); die;
+}
+else {
+  echo t("Ftp-yhteys muodostettu.")."<br/>";
+}
+
+ftp_chdir($yhteys, 'out-prod');
+
+ftp_pasv($yhteys, true);
+
+$files = ftp_nlist($yhteys, ".");
+
+$bookkaukset = array();
+$rahtikirjat array();
+$iftstat array();
+
+foreach ($files as $file) {
+
+  if (substr($file, -3) == 'IFF') {
+    $bookkaukset[] = $file;
+  }
+
+  if (substr($file, -3) == 'DAD') {
+    $rahtikirjat[] = $file;
+  }
+
+  if (substr($file, -3) == 'IFS') {
+    $iftstat[] = $file;
+  }
+
+}
+
+foreach ($bookkaukset as $bookkaus) {
+  $temp_file = tempnam("/tmp", "IFF-");
+  ftp_get($yhteys, $temp_file, $bookkaus, FTP_ASCII);
+  $edi_data = file_get_contents($temp_file);
+  kasittele_bookkaussanoma($edi_data);
+  unlink($temp_file);
+}
+
+foreach ($rahtikirjat as $rahtikirja) {
+  $temp_file = tempnam("/tmp", "DAD-");
+  ftp_get($yhteys, $temp_file, $rahtikirja, FTP_ASCII);
+  $edi_data = file_get_contents($temp_file);
+  kasittele_rahtikirjasanoma($edi_data);
+  unlink($temp_file);
+}
+
+foreach ($iftstat as $iftsta) {
+  $temp_file = tempnam("/tmp", "IFT-");
+  ftp_get($yhteys, $temp_file, $iftsta, FTP_ASCII);
+  $edi_data = file_get_contents($temp_file);
+  kasittele_iftsta($edi_data);
+  unlink($temp_file);
+}
+
+ftp_close($yhteys);

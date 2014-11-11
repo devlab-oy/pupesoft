39b8f993245c972041192d7980d77f1ed7f9948f
Johan Tötterman <juppe@devlab.fi>
2014-11-11 17:19:23 +0200

    Etenee

--- pupesoft_changelog.php
+++ pupesoft_changelog.php
@@ -1,61 +1,122 @@
 <?php
 
-
 require "inc/parametrit.inc";
 
-echo "<font class='head'>".t("Pupesoft uudet ominaisuudet")."</font><hr><br>";
+echo "<font class='head'>".t("Uudet ominaisuudet")."</font><hr><br>";
 
-exec("git log --merges 08ed62251d182f06d01a027d900c9c311405ef91^..ef9413eb9aa5b4f6dd53984e3b32095a05e006d5 |grep \"pull request\"", $pulkkarit);
-
-foreach ($pulkkarit as $pulkkari) {
-  preg_match("/pull request #([0-9]*) from/", $pulkkari, $pulkkarinro);
-  
-  echo "$pulkkarinro[1]: $pulkkari: <a target='pulkkari' href='https://github.com/devlab-oy/pupesoft/pull/$pulkkarinro[1]'>$pulkkarinro[1]</a><br>";
-}
-
-exit;
-
-
-
-// Haetaan pulkkareita
-function get_pulkkarit ($page=1, $branch='master') {
-	$ch  = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/devlab-oy/pupesoft/pulls?state=closed&sort=updated&direction=desc&base=$branch&page=$page");
+// Haetaan pulkkareita githubista
+function github_api ($url) {
+  $ch  = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HEADER, FALSE);
   curl_setopt($ch, CURLOPT_USERAGENT, "Pupesoft");
-	$pulkkarit = curl_exec($ch);
-  $pulkkarit = json_decode($pulkkarit);
 
-	return($pulkkarit);
+  $pulkkarit = curl_exec($ch);
+
+  $code = (int) curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+  if ($code != 200) {
+    return FALSE;
+  }
+  else {
+    $pulkkarit = json_decode($pulkkarit);
+	  return($pulkkarit);
+  }
 }
 
-// Nykyinen branchi
-$branch = exec("git rev-parse --abbrev-ref HEAD");
+$haetaanpulkkarit = TRUE;
 
-// Mikä on viimeisin merge joka meillä lokaalisti on
-$date = exec("git log -n 1 --merges --date=iso --pretty='%cd'");
-$updatedtime = strtotime($date);
+// Haetaan muuttuneet/uudet pulkkarit kantaan
+$query  = "SELECT max(updated) updated
+           FROM git_pulkkarit";
+$prres = pupe_query($query);
+$prrow = mysql_fetch_assoc($prres);
 
-$page = 1;
+if (!empty($prrow['updated'])) {
+  $updatedtime = strtotime($prrow['updated']);
 
-while ($pulkkarit = get_pulkkarit($page, $branch)) {
-  $page++;
+  if ($updatedtime > strtotime("6 hour ago")) {
+    $haetaanpulkkarit = FALSE;
+  }
+}
+else {
+  // Ekalla ajolla haetaan vaikka parin kuukauden takaa
+  $updatedtime = strtotime("2 month ago");
+}
 
-  foreach ($pulkkarit as $pulkkari) {
-    $merged = $pulkkari->merged_at;
+if ($haetaanpulkkarit) {
 
-    if ($merged != null) {
-      $mergedtime = strtotime($merged);
+  $page = 1;
 
-      if ($mergedtime > $updatedtime) {
-        $title   = utf8_decode($pulkkari->title);
-        $body    = utf8_decode($pulkkari->body);
-        $kuka    = utf8_decode($pulkkari->user->login);
+  while ($pulkkarit = github_api("https://api.github.com/repos/devlab-oy/pupesoft/pulls?state=closed&sort=updated&direction=desc&page=$page")) {
+    $page++;
 
-        echo "$title, $kuka, $merged<br>";
+    if ($pulkkarit === FALSE) break;
+
+    foreach ($pulkkarit as $pulkkari) {
+      $updated = $pulkkari->updated_at;
+
+      if ($updated != NULL) {
+        if (strtotime($updated) > $updatedtime) {
+          $number  = $pulkkari->number;
+          $merged  = $pulkkari->merged_at;
+          $title   = utf8_decode($pulkkari->title);
+
+          $newfeature = (substr(trim($title), 0, 1) == "*") ? 1 : 0;
+
+          $pulkkari_ser = mysql_real_escape_string(serialize($pulkkari));
+
+          // Haetaan muuttuneet failit
+          $filet = github_api("https://api.github.com/repos/devlab-oy/pupesoft/pulls/$number/files");
+
+          $filetarr = array();
+
+          foreach ($filet as $file) {
+            $filename = $file->filename;
+
+            //Tsekataan kannasta mikä softa
+            if (substr($filename, -4) == ".php"
+              and strpos($filename, "tulostakopio.php") === FALSE
+              and strpos($filename, "yllapito.php") === FALSE
+            ) {
+              $query  = "SELECT DISTINCT sovellus, nimi, nimitys
+                         FROM oikeu
+                         WHERE yhtio   = '{$kukarow['yhtio']}'
+                         and kuka      = ''
+                         and sovellus != ''
+                         and nimi like '%$filename'
+                         ORDER BY sovellus, nimi";
+              $menures = pupe_query($query);
+
+              while ($menurow = mysql_fetch_assoc($menures)) {
+                $filetarr[] = array($menurow["sovellus"], $menurow["nimi"], $menurow["nimitys"]);
+              }
+            }
+          }
+
+          $filet_ser = mysql_real_escape_string(serialize($filetarr));
+
+          $query  = "INSERT INTO git_pulkkarit
+                     SET id       = $number,
+                     updated      = '$updated',
+                     merged       = '$merged',
+                     feature      = $newfeature,
+                     pull_request = '$pulkkari_ser',
+                     files        = '$filet_ser'
+                     ON DUPLICATE KEY UPDATE
+                     updated      = '$updated',
+                     merged       = '$merged',
+                     feature      = $newfeature,
+                     pull_request = '$pulkkari_ser',
+                     files        = '$filet_ser'";
+          pupe_query($query);
+        }
+        else {
+          break 2;
+        }
       }
       else {
         break 2;
@@ -63,3 +124,127 @@ while ($pulkkarit = get_pulkkarit($page, $branch)) {
     }
   }
 }
+
+// Haetaan kymmenen uiusnta narustavetoa kannasta
+$query  = "SELECT *
+           FROM git_paivitykset
+           ORDER BY id DESC
+           LIMIT 10";
+$vetores = pupe_query($query);
+
+if (mysql_num_rows($vetores)) {
+
+  $vedot = array();
+
+  while ($vetorow = mysql_fetch_assoc($vetores)) {
+    $vedot[] = $vetorow;
+  }
+
+  // Saadaan mukaan myös tulevat ominaisuudet
+  array_unshift($vedot, "HEAD");
+
+  echo "<table style='width: 90%;'>";
+
+  foreach ($vedot as $i => $veto) {
+
+    $taveto_hash = $veto["hash"];
+
+    if (isset($vedot[$i+1])) {
+      $edveto_hash = $vedot[$i+1]["hash"];
+    }
+    else {
+      continue;
+    }
+
+    if ($veto == "HEAD") {
+      $query  = "SELECT group_concat(id) idt
+                 FROM git_pulkkarit
+                 WHERE merged > '{$vedot[$i+1]["date"]}'
+                 ORDER BY feature DESC, id";
+      $pulres = pupe_query($query);
+      $pulrow = mysql_fetch_assoc($pulres);
+
+      if ($pulrow['idt'] == "") {
+        continue;
+      }
+
+      echo "<tr><th colspan='3'>".t("Tulossa olevat ominaisuudet").":</th></tr>";
+      $pull_ids = $pulrow['idt'];
+    }
+    else {
+      echo "<tr><th colspan='3'>Pupesoft ".t("päivitys").": $veto[date]</th></tr>";
+
+      $pulkkarit = array();
+      exec("git log --merges $edveto_hash..$taveto_hash |grep \"pull request\"", $pulkkarit);
+
+      $pull_ids = array();
+
+      foreach ($pulkkarit as $pulkkari) {
+        preg_match("/pull request #([0-9]*) from/", $pulkkari, $pulkkarinro);
+
+        $pull_ids[] = $pulkkarinro[1];
+      }
+
+      $pull_ids = implode(",", $pull_ids);
+    }
+
+    if ($pull_ids != "") {
+
+      $query  = "SELECT *
+                 FROM git_pulkkarit
+                 WHERE id in ($pull_ids)
+                 ORDER BY feature DESC, id";
+      $pulres = pupe_query($query);
+
+      while ($pulrow = mysql_fetch_assoc($pulres)) {
+
+        $pulkkaridata = unserialize($pulrow["pull_request"]);
+
+        $title = utf8_decode($pulkkaridata->title);
+        $body  = utf8_decode($pulkkaridata->body);
+
+        echo "<tr>";
+
+        if ($pulrow['feature'] == 1) {
+          $title     = ltrim($title, " *");
+          $class     = "spec";
+          $fclass    = "message";
+          $titlelisa = t("Uusi ominaisuus");
+        }
+        else {
+          $class     = "";
+          $fclass    = "";
+          $titlelisa = t("Pienkehitys");
+        }
+
+        echo "<td class='$class'><font class='message'>$titlelisa</font>: <font class='$fclass'>$title</font></td>";
+        echo "<td class='$class'><a target='pulkkari' href='https://github.com/devlab-oy/pupesoft/pull/$pulrow[id]'>$pulrow[id]</a></td>";
+        echo "</tr>";
+        echo "<tr><td colspan='3'><pre style='white-space: pre-wrap;'>$body</pre>";
+
+        $files = unserialize($pulrow['files']);
+
+        if (count($files)) {
+          echo "<br>Päivitetyt ohjelmat:<br><table>";
+
+          foreach ($files as $file) {
+            list($sovellus, $filenimi, $nimitys) = $file;
+            echo "<tr><td>$sovellus</td><td>$nimitys</td></tr>";
+          }
+
+          echo "</table>";
+        }
+
+        echo "</td></tr>";
+        echo "<tr><td class='back'><br></td></tr>";
+
+      }
+
+      echo "<tr><td class='back'><br></td></tr>";
+    }
+  }
+
+  echo "</table>";
+}
+
+require("inc/footer.inc");

cd8e10bad2000add45af4e0757bced903aeead5d
Oskari Paakkanen <oskari@devlab.fi>
2014-11-14 10:32:12 +0200

    fix-test

--- sarjanumero/kontitus.php
+++ sarjanumero/kontitus.php
@@ -257,7 +257,8 @@ if (isset($submit)) {
                 ON trlt.yhtio = tilausrivi.yhtio
                 AND trlt.tilausrivitunnus = tilausrivi.tunnus
               WHERE laskun_lisatiedot.yhtio = '{$kukarow['yhtio']}'
-              AND laskun_lisatiedot.konttiviite = '{$konttiviite}'";
+              AND laskun_lisatiedot.konttiviite = '{$konttiviite}'
+              ORDER BY trlt.konttinumero DESC";
     $result = pupe_query($query);
     $konttiinfo = mysql_fetch_assoc($result);
     $konttiinfo = $konttiinfo['konttinumero'];

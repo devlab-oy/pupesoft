9bf83b28000a3e957f16c74b360a9b581a804a87
Oskari Paakkanen <oskari@devlab.fi>
2014-11-21 15:10:26 +0200

    näytetään nappi jos halutaan

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -682,7 +682,18 @@ if ((int) $kukarow["kesken"] > 0) {
   $toimtapa_kv = t_tunnus_avainsanat($laskurow['toimitustapa'], "selite", "TOIMTAPAKV");
 }
 
-if (($toim == "TARJOUS" or $toim == "EXTTARJOUS") or (isset($laskurow["tilaustyyppi"]) and $laskurow["tilaustyyppi"] == "T") or $toim == "PROJEKTI") {
+if (($toim == 'RIVISYOTTO' or $toim == 'PIKATILAUS') and $yhtiorow['naytetaan_tilausvahvistusnappi'] != '') {
+  $naytetaan_tilausvahvistusnappi = true;
+}
+else {
+  $naytetaan_tilausvahvistusnappi = false;
+}
+
+if (($naytetaan_tilausvahvistusnappi or
+     $toim == "TARJOUS" or $toim == "EXTTARJOUS") or
+     (isset($laskurow["tilaustyyppi"]) and $laskurow["tilaustyyppi"] == "T") or
+     $toim == "PROJEKTI") {
+
   // ekotetaan javascriptiä jotta saadaan pdf:ät uuteen ikkunaan
   js_openFormInNewWindow();
 }
@@ -8685,6 +8696,10 @@ if ($tee == '') {
           if ($toim == 'TARJOUS' and $yhtiorow['salli_jyvitys_tarjouksella'] != '') {
             $sallijyvitys = TRUE;
           }
+
+          if ($naytetaan_tilausvahvistusnappi) {
+            $sallijyvitys = TRUE;
+          }
         }
 
         //annetaan mahdollisuus antaa loppusumma joka jyvitetään riveille arvoosuuden mukaan
@@ -8696,7 +8711,7 @@ if ($tee == '') {
             $jyvsumma = '0.00';
           }
 
-          if ($toim == "TARJOUS" or $toim == "EXTTARJOUS" or $laskurow["tilaustyyppi"] == "T" or $toim == "PROJEKTI") {
+          if ($naytetaan_tilausvahvistusnappi or $toim == "TARJOUS" or $toim == "EXTTARJOUS" or $laskurow["tilaustyyppi"] == "T" or $toim == "PROJEKTI") {
             echo "  <th colspan='2' nowrap>".t("Näytä").":</th>
                 <td colspan='2' nowrap>
                 <form action='tulostakopio.php' method='post' name='tulostaform_tmyynti' id='tulostaform_tmyynti' class='multisubmit'>
@@ -8774,7 +8789,7 @@ if ($tee == '') {
             $koko = '7';
           }
 
-          if ($toim != "PROJEKTI") {
+          if ($toim != "PROJEKTI" and !$naytetaan_tilausvahvistusnappi) {
             if ($toim == 'TARJOUS' and !empty($yhtiorow['salli_jyvitys_tarjouksella'])) {
               echo "  <th colspan='5'>".t("Pyöristä katetta").":</th>
                   <td class='spec'>

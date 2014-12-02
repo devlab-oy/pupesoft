3ae84c355512f916e4c509f28d4013c194b4110a
Hanna Hagberg <hanna@devlab.fi>
2014-12-02 16:32:37 +0200

    Korjaus oikeaan paikkaan.

--- synkronoi_mysqlaliakset.php
+++ synkronoi_mysqlaliakset.php
@@ -61,8 +61,7 @@ if ($tee == "TEE" or strpos($_SERVER['SCRIPT_NAME'], "iltasiivo.php") !== FALSE)
 
     if (mysql_num_rows($sanakirjaresult) > 0) {
       $sanakirjaquery  = "UPDATE avainsana SET
-                          selitetark       = '$selitetark',
-                          nakyvyys         = '$nakyvyys'
+                          selitetark       = '$selitetark'
                           WHERE yhtio      = '$kukarow[yhtio]'
                           AND laji         = 'MYSQLALIAS'
                           AND selite       = '$selite'

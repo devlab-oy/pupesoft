540345b5926eecdbc57feb57199710be4a461059
Johan Tötterman <juppe@devlab.fi>
2014-12-05 11:30:46 +0200

    Nappien taustoja voi säätää!

--- csstest.php
+++ csstest.php
@@ -45,7 +45,13 @@ $css_kuvaus = array(
   ,  'TR_COLOR'               =>  'Taulukon solun teksti'
   ,  'TUMMA_BACKGROUND'       =>  'Tumman elementin tausta'
   ,  'TUMMA_COLOR'            =>  'Tumman elementin teksti'
-  ,  'ASIAKASFAKTA_COLOR'     =>  'Asiakasfakta otsikolla ja myyntitilauksella'
+  ,  'ASIAKASFAKTA_COLOR'     =>  'Asiakasfakta otsikolla ja myyntitilauksella'    
+  ,  'SUBMITBUTTON_BACKGROUND'          =>  'Normaalin Submit-nappulan tausta'
+  ,  'SUBMITBUTTON_LISAA_BACKGROUND'    =>  'Lisää Submit-nappulan tausta'
+  ,  'SUBMITBUTTON_HAE_BACKGROUND'      =>  'Hae Submit-nappulan tausta'
+  ,  'SUBMITBUTTON_TALLENNA_BACKGROUND' =>  'Tallenna Submit-nappulan tausta'  
+  ,  'SUBMITBUTTON_POISTA_BACKGROUND'   =>  'Poista Submit-nappulan tausta'   
+ 
 );
 
 function getCSSDescription($css_name) {
@@ -84,7 +90,15 @@ Tässä näkee miten formit käyttäytyy:<hr>
 <input type='radio'   name='2'>
 <input type='radio'   name='2'>
 <input type='submit' value='Normaali submit-nappula'>
-</form> pitäisi pysyä nipussa ilman suurempia aukkoja ja rivinvaihtoja.
+ pitäisi pysyä nipussa ilman suurempia aukkoja ja rivinvaihtoja.
+
+<br><br>
+
+<input type='submit' class='lisaa_btn' value='Lisää submit-nappula'>
+<input type='submit' class='hae_btn' value='Hae submit-nappula'>
+<input type='submit' class='tallenna_btn' value='Tallenna submit-nappula'>
+<input type='submit' class='poista_btn' value='Poista submit-nappula'>
+</form>
 
 <br>
 <br>

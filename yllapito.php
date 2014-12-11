489bfcbad1e226da059288560579f780b66b7071
Joonas Harjukallio <joonas@devlab.fi>
2014-12-11 11:10:02 +0200

    Toiminnallisuutta funktioitu

--- yllapito.php
+++ yllapito.php
@@ -2265,18 +2265,7 @@ if ($tunnus > 0 or $uusi != 0 or $errori != '') {
     echo "<th>".t('K‰yt‰ kohteen suosituinta p‰iv‰‰')."</th>";
     echo "<td><button id='suosituimmat_paivat' type='button'>".t('K‰yt‰')."</button></td>";
     echo "</tr>";
-    $query = "SELECT selite,
-              selitetark
-              FROM tuotteen_avainsanat
-              WHERE yhtio = '{$kukarow['yhtio']}'
-              AND laji = 'tyomaarayksen_ryhmittely'
-              GROUP BY 1,2
-              ORDER BY selitetark DESC";
-    $toimenpide_result = pupe_query($query);
-    $toimenpide_tyypit = array();
-    while ($toimenpide_tyyppi = mysql_fetch_assoc($toimenpide_result)) {
-      $toimenpide_tyypit[] = $toimenpide_tyyppi['selite'];
-    }
+    $toimenpide_tyypit = hae_toimenpide_tyypit();
 
     $request = array(
       'tuoteno'      => $t[1],

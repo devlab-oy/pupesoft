dcf35ffd47542c95b969e9407ac98c55a95b16cb
tiere <tommi@devlab.fi>
2015-01-02 16:31:22 +0200

    Tilinhallinta pupenextiin
    * Poistetaan tilinhallintaan liittyvä logiikka, koska siirretty pupenextiin
    * Poistetaan tasohallintaan liittyvä logiikka, koska siirretty pupenextiin
    * Lisätään tili ja taso ohjelmiin, joita ei voi käyttää vanhasta pupesta

--- yllapito.php
+++ yllapito.php
@@ -21,6 +21,8 @@ if (strpos($_SERVER['SCRIPT_NAME'], "yllapito.php")  !== FALSE) {
 
 $psx_ohjelmat = array(
   "valuu" => "{$palvelin2}pupenext/currencies",
+  "tili" => "{$palvelin2}pupenext/accounts",
+  "taso" => "{$palvelin2}pupenext/sum_levels"
 );
 
 if (array_key_exists($toim, $psx_ohjelmat)) {
@@ -2223,8 +2225,6 @@ if ($tunnus > 0 or $uusi != 0 or $errori != '') {
     $toim == "keraysvyohyke" or
     $toim == "avainsana" or
     $toim == "pakkaus" or
-    $toim == "tili" or
-    $toim == "taso" or
     $toim == "asiakasalennus" or
     $toim == "asiakashinta" or
     $toim == "perusalennus" or

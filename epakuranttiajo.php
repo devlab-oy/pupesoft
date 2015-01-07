f8d9afddf06934bd47997a7359262b64f8b398f2
Tony Selkamo <tony@devlab.fi>
2015-01-07 15:38:16 +0200

    Ei huomioida konversioissa muodostuneita tapahtumia

--- epakuranttiajo.php
+++ epakuranttiajo.php
@@ -216,12 +216,14 @@ if ($php_cli or (isset($ajo_tee) and ($ajo_tee == "NAYTA" or $ajo_tee == "NAYTAP
       }
 
       // Haetaan tuotteen viimeisin laskutus (ei huomioida hyvityksiä)
+      // Ei myöskään huomioida fuusioissa muodostuneita tapahtumia (rivitunnus 0 tai -1)
       $query  = "SELECT laadittu
                  FROM tapahtuma
                  WHERE yhtio = '$kukarow[yhtio]'
                  AND laji    in ('laskutus', 'kulutus')
                  AND tuoteno = '$epakurantti_row[tuoteno]'
                  AND kpl     < 0
+                 AND rivitunnus not in ('0', '-1')
                  ORDER BY laadittu DESC
                  LIMIT 1;";
       $tapres = pupe_query($query);

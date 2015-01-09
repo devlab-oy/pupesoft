3ba5ac4258477dc0b5e0f122dae895b250292e2d
Oskari Paakkanen <oskari@devlab.fi>
2015-01-09 10:59:28 +0200

    parannus

--- saapuva_rahti.php
+++ saapuva_rahti.php
@@ -14,13 +14,9 @@ else {
 
 $query = "SELECT
           trlt.rahtikirja_id AS rahtikirja,
-          group_concat(DISTINCT trlt.asiakkaan_tilausnumero SEPARATOR '<br>') AS tilaukset,
-          group_concat(DISTINCT trlt.asiakkaan_rivinumero) AS rivit,
-          count(tr.tunnus) AS rullia,
-          sum(ss.massa) AS paino,
-          lasku.luontiaika,
-          tr.toimitettuaika,
-          group_concat(DISTINCT ss.varasto) AS varastot,
+          concat(trlt.asiakkaan_tilausnumero, ':', trlt.asiakkaan_rivinumero) AS tilaus,
+          count(tr.tunnus) AS rullia, sum(ss.massa) AS paino,
+          lasku.luontiaika, tr.toimitettuaika,
           sum(if(ss.varasto IS NULL, 0, 1)) AS varastossa,
           sum(if(ss.lisatieto = 'Hyl‰tty', 1, 0)) AS hylatyt,
           sum(if(ss.lisatieto = 'Hyl‰t‰‰n', 1, 0)) AS hylattavat,
@@ -29,17 +25,17 @@ $query = "SELECT
           trlt.kuljetuksen_rekno
           FROM lasku
           JOIN tilausrivi AS tr
-            ON tr.yhtio = lasku.yhtio
-            AND tr.otunnus = lasku.tunnus
+          ON tr.yhtio = lasku.yhtio
+          AND tr.otunnus = lasku.tunnus
           JOIN tilausrivin_lisatiedot AS trlt
-            ON trlt.yhtio = lasku.yhtio
-            AND trlt.tilausrivitunnus = tr.tunnus
+          ON trlt.yhtio = lasku.yhtio
+          AND trlt.tilausrivitunnus = tr.tunnus
           JOIN sarjanumeroseuranta AS ss
-            ON ss.yhtio = lasku.yhtio
-            AND ss.ostorivitunnus = tr.tunnus
+          ON ss.yhtio = lasku.yhtio
+          AND ss.ostorivitunnus = tr.tunnus
           WHERE lasku.yhtio = '{$kukarow['yhtio']}'
           {$lisa}
-          GROUP BY lasku.tunnus
+          GROUP BY rahtikirja, tilaus
           ORDER BY trlt.rahtikirja_id";
 $result = pupe_query($query);
 
@@ -50,11 +46,19 @@ if (mysql_num_rows($result) == 0) {
 }
 else {
 
+  $rahdit = array();
+
+  while ($rahti = mysql_fetch_assoc($result)) {
+    $rahdit[] = $rahti;
+    $rahtikirjat[] = $rahti['rahtikirja'];
+  }
+
+  $apulaskuri = array_count_values($rahtikirjat);
+
   echo "<table>";
   echo "<tr>";
   echo "<th>".t("Rahtikirja#")."</th>";
-  echo "<th>".t("Tilausnumerot")."</th>";
-  echo "<th>".t("Tilausrivit")."</th>";
+  echo "<th>".t("Tilausnumero:rivi")."</th>";
   echo "<th>".t("Rullien m‰‰r‰")."</th>";
   echo "<th>".t("Rahdin paino")."</th>";
   echo "<th>".t("Rekisterinumero")."</th>";
@@ -65,7 +69,9 @@ else {
   echo "<th class='back'></th>";
   echo "</tr>";
 
-  while ($rahti = mysql_fetch_assoc($result)) {
+  $esitetyt_rahtikirjat = array();
+
+  foreach ($rahdit as $rahtikirja => $rahti) {
 
     echo "<tr>";
 
@@ -77,16 +83,18 @@ else {
     $liite_result = pupe_query($liite_query);
     $liite = mysql_result($liite_result, 0);
 
-    echo "<td valign='top' align='center'>";
-    echo "<a href='view.php?id={$liite}' target='_blank'>" . $rahti['rahtikirja'] . "</a>";
-    echo "</td>";
+    if (!in_array($rahti['rahtikirja'], $esitetyt_rahtikirjat)) {
+
+      $tilauksia_rahdissa = $apulaskuri[$rahti['rahtikirja']];
+
+      echo "<td valign='top' align='center' rowspan='{$tilauksia_rahdissa}'>";
+      echo "<a href='view.php?id={$liite}' target='_blank'>" . $rahti['rahtikirja'] . "</a>";
+      echo "</td>";
+      $esitetyt_rahtikirjat[] = $rahti['rahtikirja'];
+    }
 
     echo "<td valign='top' align='center'>";
-    echo $rahti['tilaukset'];
-    echo "</td>";
-
-    echo "<td valign='top' align='center'>";
-    echo $rahti['rivit'];
+    echo $rahti['tilaus'];
     echo "</td>";
 
     echo "<td valign='top' align='center'>";

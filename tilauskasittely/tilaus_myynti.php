f141df2beeec865100a1a35236c3eaab87e60fcb
Sami Luukkonen <sami@devlab.fi>
2015-02-24 14:28:19 +0200

    Hipsut hipsut...

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -9754,7 +9754,7 @@ if ($tee == '') {
                     AND kerayslista != 0
                     AND kerayslista = '{$laskurow['kerayslista']}'
                     AND tila = 'C'
-                    AND alatila = ''C";
+                    AND alatila = 'C'";
           $takaisin_keraa_res = pupe_query($query);
           $takaisin_keraa_row = mysql_fetch_assoc($takaisin_keraa_res);
 

98226330cfe417b01ba9bab5ea66b0b102b1b9a9
Oskari Paakkanen <oskari@devlab.fi>
2015-03-09 08:53:32 +0200

    toimitusnäkymä

---
+++ tullivarastointi_toimitus.php
@@ -0,0 +1,108 @@
+<?php
+require "inc/parametrit.inc";
+
+$errors = array();
+
+if (!isset($task)) {
+  $otsikko = t("Perustetut tulonumerot");
+  $view = "perus";
+}
+
+if (isset($task) and $task == 'etsi') {
+
+  $query = "SELECT tilausrivi.*,
+            CONCAT(SUBSTRING(tilausrivi.hyllyalue, 3, 4), tilausrivi.hyllynro) AS varastopaikka,
+            lasku.asiakkaan_tilausnumero AS saapumiskoodi
+            FROM tilausrivi
+            JOIN lasku
+             ON lasku.yhtio = tilausrivi.yhtio
+             AND lasku.tunnus = tilausrivi.otunnus
+            JOIN tilausrivin_lisatiedot
+             ON tilausrivin_lisatiedot.yhtio = tilausrivi.yhtio
+             AND tilausrivin_lisatiedot.tilausrivitunnus = tilausrivi.tunnus
+            WHERE tilausrivi.yhtio = '{$kukarow['yhtio']}'
+            AND lasku.viesti = 'tullivarasto'
+            AND tilausrivi.otunnus != 0";
+
+  $lisa = " AND lasku.asiakkaan_tilausnumero = '{$hakusana}'";
+  $result = pupe_query($query.$lisa);
+
+  if (mysql_num_rows($result) == 0) {
+    $lisa = " AND tilausrivi.nimitys LIKE '%{$hakusana}%'";
+    $result = pupe_query($query.$lisa);
+  }
+  elseif (mysql_num_rows($result) == 0) {
+    $lisa = " AND tilausrivin_lisatiedot.kontin_mrn = '{$hakusana}'";
+    $result = pupe_query($query.$lisa);
+  }
+  elseif (mysql_num_rows($result) == 0) {
+    $errors[] = t("Ei osumia");
+  }
+
+  if (count($errors) < 1) {
+
+   $rivit = array();
+    while ($rivi = mysql_fetch_assoc($result)) {
+      $rivit[] = $rivi;
+    }
+  }
+  $view = 'perus';
+}
+
+
+echo "<font class='head'>{$otsikko}</font><hr><br>";
+
+if ($view == 'perus') {
+
+  echo "<form method='post'>";
+  echo "<input type='hidden' name='task' value='etsi' />";
+  echo "<table><tr><th>Etsi tuotteita</th><td><input type='text' name='hakusana'></td><td class='back'><input type='submit' class='hae_btn' value='Etsi'></td></tr></table>";
+  echo "</form><br>";
+
+  if (isset($rivit) and count($rivit) > 0) {
+
+    echo "<table>";
+
+    echo "
+    <tr>
+      <th>" . t("Saapumiskoodi") ."</th>
+      <th>" . t("Nimitys") ."</th>
+      <th>" . t("Kpl") ."</th>
+      <th>" . t("Varastopaikka") ."</th>
+    </tr>";
+
+    foreach ($rivit as $rivi) {
+
+      echo "
+      <tr>
+        <td>" . $rivi['saapumiskoodi'] ."</td>
+        <td>" . $rivi['nimitys'] ."</td>
+        <td>" . $rivi['tilkpl'] ."</td>
+        <td>" . $rivi['varastopaikka'] ."</td>
+      </tr>";
+
+    }
+  echo "</table>";
+  }
+
+  if (count($errors) > 0) {
+    echo "<div class='error' style='text-align:center'>";
+    foreach ($errors as $error) {
+      echo $error."<br>";
+    }
+    echo "</div>";
+  }
+
+}
+
+
+
+
+
+
+
+require "inc/footer.inc";
+
+
+
+

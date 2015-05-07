4af241eb57ccfdaf635703fe5a053cfc480399f7
Oskari Paakkanen <oskari@devlab.fi>
2015-05-07 13:15:04 +0300

    testi

--- tilauskasittely/edifact_haku.php
+++ tilauskasittely/edifact_haku.php
@@ -3,44 +3,91 @@
 
 require "../inc/parametrit.inc";
 require "../inc/edifact_functions.inc";
-/*
 
 
-$query = "SELECT tunnus, asiakkaan_tilausnumero, asiakkaan_rivinumero FROM tilausrivin_lisatiedot";
-$result = pupe_query($query);
+if ($task == 'matkakoodipaivitys') {
 
-while ($rivi = mysql_fetch_assoc($result)) {
+  $query = "SELECT tunnus, asiakkaan_tilausnumero, asiakkaan_rivinumero FROM tilausrivin_lisatiedot WHERE matkakoodi = ''";
+  $result = pupe_query($query);
 
-  $numero_rivi = $rivi['asiakkaan_tilausnumero'] . ':' . $rivi['asiakkaan_rivinumero'];
+  while ($rivi = mysql_fetch_assoc($result)) {
 
-  $query = "SELECT data FROM liitetiedostot WHERE selite = '{$numero_rivi}'";
-  $res = pupe_query($query);
+    $numero_rivi = $rivi['asiakkaan_tilausnumero'] . ':' . $rivi['asiakkaan_rivinumero'];
 
-  $data = mysql_result($res, 0);
+    $query = "SELECT data FROM liitetiedostot WHERE selite = '{$numero_rivi}' AND kayttotarkoitus = 'bookkaussanoma'";
+    $res = pupe_query($query);
 
-  $datarivit = explode("'", $data);
+    $data = mysql_result($res, 0);
+    $datarivit = explode("'", $data);
 
-  foreach ($datarivit as $datarivi) {
+    foreach ($datarivit as $datarivi) {
 
-    if (substr($datarivi, 0, 8) == 'BGM+335+') {
-      $osat = explode("+", $datarivi);
-      $matkakoodi = $osat[2];
+      if (substr($datarivi, 0, 8) == 'BGM+335+') {
+        $osat = explode("+", $datarivi);
+        $matkakoodi = $osat[2];
+        break;
+      }
+    }
+
+    if (!empty($matkakoodi)) {
+      $update = "UPDATE tilausrivin_lisatiedot SET matkakoodi = '{$matkakoodi}' WHERE tunnus = '{$rivi['tunnus']}'";
+      pupe_query($update);
+      echo $matkakoodi . 'päivitetty riville ' . $rivi['tunnus'] . '<hr>';
     }
   }
-
-  if (!empty($matkakoodi)) {
-    $update = "UPDATE tilausrivin_lisatiedot SET matkakoodi = '{$matkakoodi}' WHERE tunnus = '{$rivi['tunnus']}'";
-    pupe_query($update);
-    echo $matkakoodi . 'päivitetty riville ' . $rivi['tunnus'] . '<hr>';
-  }
-
 }
 
-*/
+
+if ($task == 'tullinimikepalautus') {
+
+  $query = "SELECT
+            rahtikirja_id,
+            asiakkaan_tilausnumero,
+            asiakkaan_rivinumero,
+            tilausrivin_lisatiedot.tilausrivitunnus AS otr,
+            ss.myyntirivitunnus AS mtr
+            FROM tilausrivin_lisatiedot
+            LEFT JOIN sarjanumeroseuranta AS ss
+              ON ss.ostorivitunnus = tilausrivin_lisatiedot.tilausrivitunnus
+            WHERE tullinimike = 0
+            AND rahtikirja_id != ''
+            LIMIT 500";
+  $result = pupe_query($query);
+
+  while ($rivi = mysql_fetch_assoc($result)) {
+
+    unset($tullinimike);
+
+    $query = "SELECT data FROM liitetiedostot WHERE kayttotarkoitus = 'rahtikirjasanoma' AND selite = '{$rivi['rahtikirja_id']}'";
+    $res = pupe_query($query);
+    $data = mysql_result($res, 0);
+    $datarivit = explode("'", $data);
 
 
+    foreach ($datarivit as $datarivi) {
 
+      if (substr($datarivi, 0, 5) == "PIA+1") {
+        $osat = explode("+", $datarivi);
+        $tullinimike_info = $osat[2];
+        $tullinimike_info_osat = explode(":", $tullinimike_info);
+        $tullinimike = $tullinimike_info_osat[0];
+      }
 
+      if ($datarivi == "RFF+CU:" . $rivi['asiakkaan_tilausnumero'] . ":" . $rivi['asiakkaan_rivinumero']) {
+
+        if (isset($tullinimike)) {
+          $update = "UPDATE tilausrivin_lisatiedot SET tullinimike = '{$tullinimike}' WHERE tilausrivitunnus IN ({$rivi['otr']},{$rivi['mtr']})";
+          pupe_query($update);
+          echo $update;
+          echo '<br>';
+          break;
+        }
+      }
+    }
+  }
+}
+
+/*
 
 if ($task == 'input') {
 
@@ -58,7 +105,7 @@ if ($task == 'input') {
 
 }
 
-/*
+
 
 if ($task == 'nollaa') {
 
@@ -99,6 +146,15 @@ if ($task == 'nollaa') {
     <input type='submit' value='".t("Nollaa tilanne")."'>
   </form>
 
+  <form action='' method='post'>
+    <input type='hidden' name='task' value='tullinimikepalautus' />
+    <input type='submit' value='".t("tullinimikkeet")."'>
+  </form>
+
+  <form action='' method='post'>
+    <input type='hidden' name='task' value='matkakoodipaivitys' />
+    <input type='submit' value='".t("matkakoodit")."'>
+  </form>
 
   ";
 

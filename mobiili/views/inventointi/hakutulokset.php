762967374bb919bce57b13a18c32e2573cf54bcc
Sami Luukkonen <sami@devlab.fi>
2015-05-13 15:15:21 +0300

    if fix

--- mobiili/views/inventointi/hakutulokset.php
+++ mobiili/views/inventointi/hakutulokset.php
@@ -11,7 +11,7 @@
     </tr>
 
     <?php foreach($tuotteet as $tuote) { ?>
-      <?php if ($tuote['inventointilista'] != 0 and $tuote['inventointilista_aika'] !== null) { ?>
+      <?php if ($tuote['inventointilista'] !== null) { ?>
         <tr>
           <td><?php echo $tuote['tuoteno'] ?></td>
           <td><?php echo $tuote['tuotepaikka'] ?></td>
@@ -26,7 +26,7 @@
         <tr>
           <td><a href='inventointi.php?<?php echo $url ?>'><?php echo $tuote['tuoteno'] ?></a></td>
           <td><?php echo $tuote['tuotepaikka'] ?></td>
-          <td><?php if($tuote['inventointilista'] != 0 and $tuote['inventointilista_aika'] !== null) echo "(listalla {$tuote['inventointilista']})" ?></td>
+          <td><?php if($tuote['inventointilista'] !== null) echo "(listalla {$tuote['inventointilista']})" ?></td>
         </tr>
       <?php } ?>
     <?php } ?>

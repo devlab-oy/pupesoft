72ab0d2594d15382e34284674feb13b3b2575791
Tony Selkamo <tony@devlab.fi>
2015-05-25 16:11:00 +0300

    Pientä fiksiä

--- inventoi_korjaus_tiliointi_kirjanpitoon.php
+++ inventoi_korjaus_tiliointi_kirjanpitoon.php
@@ -8,149 +8,152 @@ ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(__FILE__)
 // otetaan tietokanta connect ja funktiot
 require "inc/connect.inc";
 require "inc/functions.inc";
-
-$kukarow['yhtio'] = 'visio';
   
-$query = "SELECT * from yhtio
-              WHERE yhtio = '$kukarow[yhtio]'";
+$query = "SELECT * FROM yhtio";
 $row = pupe_query($query);
 
-$yhtiorow = mysql_fetch_assoc($row);
+echo "\nSuoritetaan tiliˆintien korjaus inventointeihin!\n";
 
-        ///* Luetaan tapahtuma *///
-$query = "SELECT tapahtuma.*, lasku.tunnus as laskutunnus from tapahtuma 
-              left join lasku on lasku.yhtio = tapahtuma.yhtio 
-                and tila       = 'X'
-                and alatila    = 'I'
-                and viite      = tapahtuma.tunnus
-              where tapahtuma.yhtio       =  '$kukarow[yhtio]'
-                and tapahtuma.laji      = 'Inventointi'
-                and tapahtuma.laadittu  > '2015-05-22'
-                and lasku.tunnus is null";
-            $tresult = pupe_query($query);
+while ($yhtio = mysql_fetch_assoc($row)) {
+  
+  $yhtiorow = hae_yhtion_parametrit($yhtio['yhtio']);
+  
+  ///* Luetaan tapahtuma *///
+  $query = "  SELECT tapahtuma.*, lasku.tunnus AS laskutunnus 
+              FROM tapahtuma 
+              LEFT JOIN lasku ON lasku.yhtio = tapahtuma.yhtio 
+              AND tila       = 'X'
+              AND alatila    = 'I'
+              AND viite      = tapahtuma.tunnus
+              WHERE tapahtuma.yhtio       =  '$yhtiorow[yhtio]'
+              AND tapahtuma.laji      = 'Inventointi'
+              AND tapahtuma.laadittu  > '2015-05-21 00:00:00'
+              AND lasku.tunnus is NULL";
+  $tresult = pupe_query($query);
 
-            while ($tapahtumarow = mysql_fetch_assoc($tresult)) {
+  while ($tapahtumarow = mysql_fetch_assoc($tresult)) {
 
+    $tapahtumaid = $tapahtumarow["tunnus"];
+    $selite = $tapahtumarow["selite"];
+    $tiliointisumma = round($tapahtumarow["kpl"] * $tapahtumarow["hinta"],2);
+    $tapvm = $tapahtumarow["laadittu"];
+    $hyllyalue = $tapahtumarow["hyllyalue"];
+    $hyllynro = $tapahtumarow["hyllynro"];
+    $tuoteno = $tapahtumarow['tuoteno'];
 
-              $tapahtumaid = $tapahtumarow["tunnus"];
-              $selite = $tapahtumarow["selite"];
-              $tiliointisumma = round($tapahtumarow["kpl"] * $tapahtumarow["hinta"],2);
-              $tapvm = $tapahtumarow["laadittu"];
-  echo "tapahtuma: $tapahtumaid, $tapahtumarow[tuoteno], $tapahtumarow[laadittu], $tiliointisumma, $yhtiorow[varastonmuutos], $yhtiorow[varastonmuutos_inventointi]  \n";
+    echo "\nYhtio: {$yhtiorow['yhtio']},tapahtuma: $tapahtumaid, $tapahtumarow[tuoteno], $tapahtumarow[laadittu], $tiliointisumma, $yhtiorow[varastonmuutos], $yhtiorow[varastonmuutos_inventointi]  \n";
 
-              // P‰iv‰m‰‰r‰ll‰ inventoitaessa laitetaan t‰m‰p‰iv‰m‰‰r‰,
+    // P‰iv‰m‰‰r‰ll‰ inventoitaessa laitetaan t‰m‰p‰iv‰m‰‰r‰,
 
-              $query = "INSERT into lasku set
-                        yhtio      = '$kukarow[yhtio]',
-                        tapvm      = '{$lasku_tapvm}',
-                        tila       = 'X',
-                        alatila    = 'I',
-                        laatija    = '$tapahtumarow[laatija]',
-                        viite      = '$tapahtumaid',
-                        luontiaika = '$tapahtumarow[laadittu]'";
-              $result = pupe_query($query);
-              $laskuid = mysql_insert_id($GLOBALS["masterlink"]);
+    $query = "INSERT INTO lasku SET
+              yhtio      = '$yhtiorow[yhtio]',
+              tapvm      = '{$tapvm}',
+              tila       = 'X',
+              alatila    = 'I',
+              laatija    = '$tapahtumarow[laatija]',
+              viite      = '$tapahtumaid',
+              luontiaika = '$tapahtumarow[laadittu]'";
+    $result = pupe_query($query);
+    $laskuid = mysql_insert_id($GLOBALS["masterlink"]);
 
-              // Seuraako myyntitiliˆinti tuotteen tyyppi‰ ja onko kyseess‰ raaka-aine?
-              $raaka_aine_tiliointi = $yhtiorow["raaka_aine_tiliointi"];
-              $raaka_ainetililta = ($raaka_aine_tiliointi == "Y" and $row["tuotetyyppi"] == "R");
+    // Seuraako myyntitiliˆinti tuotteen tyyppi‰ ja onko kyseess‰ raaka-aine?
+    $raaka_aine_tiliointi = $yhtiorow["raaka_aine_tiliointi"];
+    $raaka_ainetililta = ($raaka_aine_tiliointi == "Y" and $row["tuotetyyppi"] == "R");
 
-              // M‰‰ritet‰‰n varastonmuutostili
-              if ($raaka_ainetililta) {
-                $varastonmuutos_tili = $yhtiorow["raaka_ainevarastonmuutos"];
-              }
-              elseif ($yhtiorow["varastonmuutos_inventointi"] != "") {
-                  $varastonmuutos_tili = $yhtiorow["varastonmuutos_inventointi"];
-              }
-              else {
-                $varastonmuutos_tili = $yhtiorow["varastonmuutos"];
-              }
+    // M‰‰ritet‰‰n varastonmuutostili
+    if ($raaka_ainetililta) {
+      $varastonmuutos_tili = $yhtiorow["raaka_ainevarastonmuutos"];
+    }
+    elseif ($yhtiorow["varastonmuutos_inventointi"] != "") {
+        $varastonmuutos_tili = $yhtiorow["varastonmuutos_inventointi"];
+    }
+    else {
+      $varastonmuutos_tili = $yhtiorow["varastonmuutos"];
+    }
 
-              // M‰‰ritet‰‰n varastotili
-              if ($raaka_ainetililta) {
-                $varastotili = $yhtiorow["raaka_ainevarasto"];
-              }
-              else {
-                $varastotili = $yhtiorow["varasto"];
-              }
+    // M‰‰ritet‰‰n varastotili
+    if ($raaka_ainetililta) {
+      $varastotili = $yhtiorow["raaka_ainevarasto"];
+    }
+    else {
+      $varastotili = $yhtiorow["varasto"];
+    }
 
-              if ($yhtiorow["tarkenteiden_prioriteetti"] == "T") {
+    $query = "  SELECT * FROM tuote WHERE yhtio = '$yhtiorow[yhtio]' AND tuoteno = '$tuoteno'";
+    $tuoteres = pupe_query($query);
+    $tuote_row = mysql_fetch_assoc($tuoteres);
 
-                $query = "SELECT toimipaikka
-                          FROM varastopaikat
-                          WHERE yhtio = '$kukarow[yhtio]'
-                          AND concat(rpad(upper(alkuhyllyalue),  5, '0'),lpad(upper(alkuhyllynro),  5, '0')) <= concat(rpad(upper('$hyllyalue'), 5, '0'),lpad(upper('$hyllynro'), 5, '0'))
-                          AND concat(rpad(upper(loppuhyllyalue), 5, '0'),lpad(upper(loppuhyllynro), 5, '0')) >= concat(rpad(upper('$hyllyalue'), 5, '0'),lpad(upper('$hyllynro'), 5, '0'))";
-                $varcheckres = pupe_query($query);
-                $varcheckrow = mysql_fetch_assoc($varcheckres);
+    if ($yhtiorow["tarkenteiden_prioriteetti"] == "T") {
 
-                unset($toimipaikkarow);
+      $query = "SELECT toimipaikka
+                FROM varastopaikat
+                WHERE yhtio = '$yhtiorow[yhtio]'
+                AND concat(rpad(upper(alkuhyllyalue),  5, '0'),lpad(upper(alkuhyllynro),  5, '0')) <= concat(rpad(upper('$hyllyalue'), 5, '0'),lpad(upper('$hyllynro'), 5, '0'))
+                AND concat(rpad(upper(loppuhyllyalue), 5, '0'),lpad(upper(loppuhyllynro), 5, '0')) >= concat(rpad(upper('$hyllyalue'), 5, '0'),lpad(upper('$hyllynro'), 5, '0'))";
+      $varcheckres = pupe_query($query);
+      $varcheckrow = mysql_fetch_assoc($varcheckres);
 
-                if ($varcheckrow['toimipaikka'] > 0) {
-                  $query = "SELECT kustp, kohde, projekti
-                            FROM yhtion_toimipaikat
-                            WHERE yhtio = '{$kukarow['yhtio']}'
-                            AND tunnus  = {$varcheckrow['toimipaikka']}";
-                  $toimipaikkares = pupe_query($query);
-                  $toimipaikkarow = mysql_fetch_assoc($toimipaikkares);
-                }
+      unset($toimipaikkarow);
 
-                // Otetaan ensisijaisesti kustannuspaikka toimipaikan takaa
-                $kustp_ins     = (isset($toimipaikkarow) and $toimipaikkarow["kustp"] > 0) ? $toimipaikkarow["kustp"] : $tuote_row["kustp"];
-                $kohde_ins     = (isset($toimipaikkarow) and $toimipaikkarow["kohde"] > 0) ? $toimipaikkarow["kohde"] : $tuote_row["kohde"];
-                $projekti_ins  = (isset($toimipaikkarow) and $toimipaikkarow["projekti"] > 0) ? $toimipaikkarow["projekti"] : $tuote_row["projekti"];
-              }
-              else {
-                // Otetaan ensisijaisesti kustannuspaikka tuotteen takaa
-                $kustp_ins     = $tuote_row["kustp"];
-                $kohde_ins     = $tuote_row["kohde"];
-                $projekti_ins   = $tuote_row["projekti"];
-              }
+      if ($varcheckrow['toimipaikka'] > 0) {
+        $query = "SELECT kustp, kohde, projekti
+                  FROM yhtion_toimipaikat
+                  WHERE yhtio = '{$yhtiorow['yhtio']}'
+                  AND tunnus  = {$varcheckrow['toimipaikka']}";
+        $toimipaikkares = pupe_query($query);
+        $toimipaikkarow = mysql_fetch_assoc($toimipaikkares);
+      }
 
-              // Kokeillaan varastonmuutos tilin oletuskustannuspaikalle
-              list($kustp_ins, $kohde_ins, $projekti_ins) = kustannuspaikka_kohde_projekti($varastonmuutos_tili, $kustp_ins, $kohde_ins, $projekti_ins);
+      // Otetaan ensisijaisesti kustannuspaikka toimipaikan takaa
+      $kustp_ins     = (isset($toimipaikkarow) and $toimipaikkarow["kustp"] > 0) ? $toimipaikkarow["kustp"] : $tuote_row["kustp"];
+      $kohde_ins     = (isset($toimipaikkarow) and $toimipaikkarow["kohde"] > 0) ? $toimipaikkarow["kohde"] : $tuote_row["kohde"];
+      $projekti_ins  = (isset($toimipaikkarow) and $toimipaikkarow["projekti"] > 0) ? $toimipaikkarow["projekti"] : $tuote_row["projekti"];
+    }
+    else {
+      // Otetaan ensisijaisesti kustannuspaikka tuotteen takaa
+      $kustp_ins     = $tuote_row["kustp"];
+      $kohde_ins     = $tuote_row["kohde"];
+      $projekti_ins   = $tuote_row["projekti"];
+    }
 
-              // Toissijaisesti kokeillaan viel‰ varasto-tilin oletuskustannuspaikkaa
-              list($kustp_ins, $kohde_ins, $projekti_ins) = kustannuspaikka_kohde_projekti($yhtiorow["varasto"], $kustp_ins, $kohde_ins, $projekti_ins);
+    // Kokeillaan varastonmuutos tilin oletuskustannuspaikalle
+    list($kustp_ins, $kohde_ins, $projekti_ins) = kustannuspaikka_kohde_projekti($varastonmuutos_tili, $kustp_ins, $kohde_ins, $projekti_ins);
 
-              //$tiliointisumma = round($summa, 2);
+    // Toissijaisesti kokeillaan viel‰ varasto-tilin oletuskustannuspaikkaa
+    list($kustp_ins, $kohde_ins, $projekti_ins) = kustannuspaikka_kohde_projekti($yhtiorow["varasto"], $kustp_ins, $kohde_ins, $projekti_ins);
 
-              $tapvm = $tapahtumarow['laadittu'];  // date('Y-m-d');
+    //$tiliointisumma = round($summa, 2);
 
-              if ($paivamaaran_kasisyotto == "JOO" and (!empty($inventointipvm_pp) and !empty($inventointipvm_kk) and !empty($inventointipvm_vv))) {
-                $tapvm = "$inventointipvm_vv-$inventointipvm_kk-$inventointipvm_pp";
-              }
+    $query = "INSERT INTO tiliointi SET
+              yhtio    = '$yhtiorow[yhtio]',
+              ltunnus  = '$laskuid',
+              tilino   = '{$varastotili}',
+              kustp    = '{$kustp_ins}',
+              kohde    = '{$kohde_ins}',
+              projekti = '{$projekti_ins}',
+              tapvm    = '{$tapvm}',
+              summa    = $tiliointisumma,
+              vero     = 0,
+              lukko    = '',
+              selite   = 'Inventointi: ".t("Tuotteen")." {$tapahtumarow["tuoteno"]} $selite',
+              laatija  = '$tapahtumarow[laatija]',
+              laadittu = '$tapahtumarow[laadittu]'";
+    $result = pupe_query($query);
 
-              $query = "INSERT into tiliointi set
-                        yhtio    = '$kukarow[yhtio]',
-                        ltunnus  = '$laskuid',
-                        tilino   = '{$varastotili}',
-                        kustp    = '{$kustp_ins}',
-                        kohde    = '{$kohde_ins}',
-                        projekti = '{$projekti_ins}',
-                        tapvm    = '{$tapvm}',
-                        summa    = $tiliointisumma,
-                        vero     = 0,
-                        lukko    = '',
-                        selite   = 'Inventointi: ".t("Tuotteen")." {$tapahtumarow["tuoteno"]} $selite',
-                        laatija  = '$tapahtumarow[laatija]',
-                        laadittu = '$tapahtumarow[laadittu]'";
-              $result = pupe_query($query);
-
-              $query = "INSERT into tiliointi set
-                        yhtio    = '$kukarow[yhtio]',
-                        ltunnus  = '$laskuid',
-                        tilino   = '$varastonmuutos_tili',
-                        kustp    = '{$kustp_ins}',
-                        kohde    = '{$kohde_ins}',
-                        projekti = '{$projekti_ins}',
-                        tapvm    = '{$tapvm}',
-                        summa    = $tiliointisumma * -1,
-                        vero     = 0,
-                        lukko    = '',
-                        selite   = 'Inventointi: ".t("Tuotteen")." {$tapahtumarow["tuoteno"]} $selite',
-                        laatija  = '$tapahtumarow[laatija]',
-                        laadittu = '$tapahtumarow[laadittu]'";
-              $result = pupe_query($query);
-            }
-            
+    $query = "INSERT INTO tiliointi SET
+              yhtio    = '$yhtiorow[yhtio]',
+              ltunnus  = '$laskuid',
+              tilino   = '$varastonmuutos_tili',
+              kustp    = '{$kustp_ins}',
+              kohde    = '{$kohde_ins}',
+              projekti = '{$projekti_ins}',
+              tapvm    = '{$tapvm}',
+              summa    = $tiliointisumma * -1,
+              vero     = 0,
+              lukko    = '',
+              selite   = 'Inventointi: ".t("Tuotteen")." {$tapahtumarow["tuoteno"]} $selite',
+              laatija  = '$tapahtumarow[laatija]',
+              laadittu = '$tapahtumarow[laadittu]'";
+    $result = pupe_query($query);
+  }
+}

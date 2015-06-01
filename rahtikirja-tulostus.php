70241468a256157f710159d0890b15692c89ccd8
Sami Luukkonen <sami@devlab.fi>
2015-06-01 14:28:30 +0300

    Tuning

--- rahtikirja-tulostus.php
+++ rahtikirja-tulostus.php
@@ -670,7 +670,7 @@ if ($tee == 'tulosta') {
                     AND tulostettu = '0000-00-00 00:00:00'";
           $ures  = pupe_query($query);
 
-          if ($_onko_unifaun and isset($tulosta_rahtikirjat_nappulatsukka)) {
+          if ($_onko_unifaun and $tultiin == 'koonti_eratulostus_pakkaustiedot') {
             require_once "inc/unifaun_send.inc";
 
             $query = "SELECT unifaun_nimi

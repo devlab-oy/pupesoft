39505c31cb3d28b144f5269746cccfbd9f11aa4f
Johan Tötterman <juppe@devlab.fi>
2015-08-05 14:02:09 +0300

    Pari viilausta

--- tilauskasittely/tilaus_osto.php
+++ tilauskasittely/tilaus_osto.php
@@ -1472,7 +1472,7 @@ if ($tee != "" and $tee != "MUUOTAOSTIKKOA") {
             }
           }
 
-          if ($prow["sarjanumeroseuranta"] != "") {
+          if ($prow["sarjanumeroseuranta"] != "" and $prow["sarjanumeroseuranta"] != "T") {
             $query = "SELECT count(*) kpl
                       from sarjanumeroseuranta
                       where yhtio='$kukarow[yhtio]' and tuoteno='$prow[tuoteno]' and ostorivitunnus='$prow[tunnus]'";

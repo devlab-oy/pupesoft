51fbe3bac43e2aa06f227371a51f9d9bd4782f5d
Petri Riski <petriski@hotmail.com>
2015-08-21 19:40:03 +0300

    Revert "All() one record found bugfix"
    This reverts commit 700ac3bcedcaba096d6908b8f1194cf123fd2ca4.

--- rajapinnat/presta/presta_categories.php
+++ rajapinnat/presta/presta_categories.php
@@ -54,11 +54,15 @@ class PrestaCategories extends PrestaClient {
     //Nodes are in parent -> deepest level children order
     foreach ($ancestors as $node_nimi) {
       $filter['name'] = $node_nimi;
-      $categories = $this->all($display, $filter);
+      $category = $this->all($display, $filter);
 
       //@TODO if many categories with the name is found, we have a problem...
       //We simply take the first one
-      $category = $categories[0];
+      //Hackhack logic for indentifying this situation
+      //(count on categories.category wont work)
+      if (!isset($category['id'])) {
+        $category = $category[0];
+      }
 
       if (empty($parents)) {
         $parents[] = $category['id'];

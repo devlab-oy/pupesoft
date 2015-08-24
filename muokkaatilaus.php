2908e545d4ae5b782d05580820483e2cddf81621
satu <satu@devlab.fi>
2015-08-24 10:59:37 +0300

    SQL-error korjattu

--- muokkaatilaus.php
+++ muokkaatilaus.php
@@ -1886,7 +1886,7 @@ elseif ($toim == 'YLLAPITO') {
   }
 
   $query = "SELECT lasku.tunnus tilaus,
-            lasku.asiakkaan_tilausnumero 'asiak. tilno',
+            lasku.asiakkaan_tilausnumero 'asiak_tilno',
             $asiakasstring asiakas,
             lasku.luontiaika,
             if(kuka1.kuka != kuka2.kuka, concat_ws('<br>', kuka1.nimi, kuka2.nimi), kuka1.nimi) laatija,
@@ -1914,7 +1914,7 @@ elseif ($toim == 'YLLAPITO') {
             GROUP BY 1,2,3,4,5,6
             $mt_order_by
             $rajaus";
-
+            echo "1917: $query <br><br>";
   // haetaan tilausten arvo
   if ($kukarow['hinnat'] == 0) {
     $sumquery = "SELECT

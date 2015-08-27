c76e781f529ec83cb020beb3be2991e065958054
Hanna Hagberg <hanna@devlab.fi>
2015-08-27 09:51:25 +0300

    Viimeisen välisumman ja loppusumman sijainnin korjaus.

--- raportit/myyntiseuranta.php
+++ raportit/myyntiseuranta.php
@@ -3489,7 +3489,7 @@ else {
 
               if ($rivimaara <= $rivilimitti) echo "<td class='tumma' align='right'>{$vsum}</td>";
 
-              if (isset($worksheet)) {
+              if (isset($worksheet) and $vnim != "asiakkaittain" and $vnim != "tuotteittain") {
                 $worksheet->writeNumber($excelrivi, $excelsarake++, $vsum);
               }
             }
@@ -3544,7 +3544,7 @@ else {
 
             if ($rivimaara <= $rivilimitti) echo "<td class='tumma' align='right'>{$vsum}</td>";
 
-            if (isset($worksheet)) {
+            if (isset($worksheet) and $vnim != "asiakkaittain" and $vnim != "tuotteittain") {
               $worksheet->writeNumber($excelrivi, $excelsarake++, $vsum);
             }
           }

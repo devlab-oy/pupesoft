5150de0261867ebb20b78e4ec0fd38512b0bcadd
Mika Myllynen <giefsufu@gmail.com>
2015-09-10 16:47:11 +0300

    aktivoidaan tuotteet tuotesiirtoon jos hinnastoja on muutettu

--- rajapinnat/tuote_export.php
+++ rajapinnat/tuote_export.php
@@ -92,6 +92,124 @@ $datetime_checkpoint_row = mysql_fetch_assoc($datetime_checkpoint_res);
 $datetime_checkpoint = $datetime_checkpoint_row['selite']; // Mikä tilanne on jo käsitelty
 $datetime_checkpoint_uusi = date('Y-m-d H:i:s'); // Timestamp nyt
 
+if (!function_exists("tee_querylisa_resultista")) {
+  function tee_querylisa_resultista($tyyppi, array $tulokset) {
+
+    $poimitut = '';
+
+    # $tulokset = array(
+    #   [0] => array ("muuttuneet_tuotenumerot" => "'3','4'"),
+    #   [1] => array ("muuttuneet_tuotenumerot" => "'12','24'"´),
+    #   [2] => array ("muuttuneet_tuotenumerot" => "'5'" )
+    # );
+    # -> 3','4','12','24','5'
+
+    if (isset($tulokset[0][$tyyppi]) and !empty($tulokset[0][$tyyppi])) {
+      $poimitut = $tulokset[0][$tyyppi];
+    }
+    if (isset($tulokset[1][$tyyppi]) and !empty($tulokset[1][$tyyppi])) {
+      if (empty($poimitut)) {
+        $poimitut .= $tulokset[1][$tyyppi];
+      }
+      else {
+        $poimitut .= ",{$tulokset[1][$tyyppi]}";
+      }
+
+    }
+    if (isset($tulokset[2][$tyyppi]) and !empty($tulokset[2][$tyyppi])) {
+      if (empty($poimitut)) {
+        $poimitut = $tulokset[2][$tyyppi];
+      }
+      else {
+        $poimitut .= ",{$tulokset[2][$tyyppi]}";
+      }        
+    }
+
+    if (!empty($poimitut)) {
+     if ($tyyppi == 'muuttuneet_tuotenot') {
+       $result = " AND tuote.tuoteno IN ($poimitut) ";
+     }
+     elseif($tyyppi == 'muuttuneet_ryhmat') {
+       $result = " AND tuote.aleryhma IN ($poimitut) ";         
+     }
+     else {
+       $result = '';
+     }
+    }
+
+    return $result;
+  }
+}
+
+if (!function_exists("hae_hintamuutoksia_sisaltavat_tuotenumerot")) {
+  function hae_hintamuutoksia_sisaltavat_tuotenumerot() {
+    global $kukarow, $datetime_checkpoint;
+
+    $kaikki_arvot = array();
+
+    $query1 = "SELECT group_concat('\'',tuoteno,'\'') muuttuneet_tuotenot 
+               FROM asiakashinta 
+               WHERE yhtio = '{$kukarow['yhtio']}' 
+               AND muutospvm >= '{$datetime_checkpoint}' 
+               AND tuoteno !=''";
+    $result1 = pupe_query($query1);
+    $row1 = mysql_fetch_assoc($result1);
+    $kaikki_arvot[] = $row1;
+
+    $query2 = "SELECT group_concat('\'',tuoteno,'\'') muuttuneet_tuotenot 
+               FROM asiakasalennus 
+               WHERE yhtio = '{$kukarow['yhtio']}'
+               AND muutospvm >= '{$datetime_checkpoint}' 
+               AND tuoteno !=''";
+    $result2 = pupe_query($query2);
+    $row2 = mysql_fetch_assoc($result2);
+    $kaikki_arvot[] = $row2;
+
+    $result = tee_querylisa_resultista('muuttuneet_tuotenot', $kaikki_arvot);
+
+    return $result;
+  }
+}
+
+if (!function_exists("hae_hintamuutoksia_sisaltavat_tuoteryhmat")) {
+  function hae_hintamuutoksia_sisaltavat_tuoteryhmat() {
+    global $kukarow, $datetime_checkpoint;
+
+    $kaikki_arvot = array();
+
+    $query3 = "SELECT group_concat('\'',ryhma,'\'') muuttuneet_ryhmat
+               FROM asiakashinta 
+               WHERE yhtio = '{$kukarow['yhtio']}'
+               AND muutospvm >= '{$datetime_checkpoint}'
+               AND ryhma !=''";
+    $result3 = pupe_query($query3);
+    $row3 = mysql_fetch_assoc($result3);
+    $kaikki_arvot[] = $row3;
+
+    $query4 = "SELECT group_concat('\'',ryhma,'\'') muuttuneet_ryhmat 
+               FROM asiakasalennus 
+               WHERE yhtio = '{$kukarow['yhtio']}'
+               AND muutospvm >= '{$datetime_checkpoint}'
+               AND ryhma !=''";
+    $result4 = pupe_query($query4);
+    $row4 = mysql_fetch_assoc($result4);
+    $kaikki_arvot[] = $row4;
+
+    $query5 = "SELECT group_concat('\'',ryhma,'\'') muuttuneet_ryhmat
+               FROM perusalennus 
+               WHERE yhtio = '{$kukarow['yhtio']}'
+               AND muutospvm >= '{$datetime_checkpoint}'
+               AND ryhma !=''";
+    $result5 = pupe_query($query5);
+    $row5 = mysql_fetch_assoc($result5);
+    $kaikki_arvot[] = $row5;      
+
+    $result = tee_querylisa_resultista('muuttuneet_ryhmat', $kaikki_arvot);
+
+    return $result;
+  }
+}
+
 // alustetaan arrayt
 $dnstuote = $dnsryhma = $dnstuoteryhma = $dnstock = $dnsasiakas = $dnshinnasto = $dnslajitelma =
   $kaikki_tuotteet = $individual_tuotteet = array();
@@ -100,10 +218,20 @@ echo date("d.m.Y @ G:i:s")." - Aloitetaan tuote-export.\n";
 
 if (in_array('tuotteet', $magento_ajolista)) {
   if ($ajetaanko_kaikki == "NO") {
+
+    $muuttuneet_tuotenumerot = $muuttuneet_tuoteryhmat = '';
+
+    if (isset($magento_asiakaskohtaiset_tuotehinnat) and !empty($magento_asiakaskohtaiset_tuotehinnat)) {
+      $muuttuneet_tuotenumerot = hae_hintamuutoksia_sisaltavat_tuotenumerot();
+      $muuttuneet_tuoteryhmat = hae_hintamuutoksia_sisaltavat_tuoteryhmat();
+    }
+    
     $muutoslisa = "AND (tuote.muutospvm >= '{$datetime_checkpoint}'
               OR ta_nimitys_se.muutospvm >= '{$datetime_checkpoint}'
               OR ta_nimitys_en.muutospvm >= '{$datetime_checkpoint}'
-              )";
+              {$muuttuneet_tuotenumerot}
+              {$muuttuneet_tuoteryhmat}
+              )";                         
   }
   else {
     $muutoslisa = "";

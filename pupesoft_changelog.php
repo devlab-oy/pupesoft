63ddfd4be9dd1efecb9473c23a4e5481b2024ded
Johan Tötterman <juppe@devlab.fi>
2015-09-22 11:16:34 +0300

    Näytetään myös pupenext-päivitykset!

--- pupesoft_changelog.php
+++ pupesoft_changelog.php
@@ -1,5 +1,9 @@
 <?php
 
+#alter table git_pulkkarit add column repository varchar(20) not null default 'pupesoft' after id;
+#alter table git_paivitykset add column repository varchar(20) not null default 'pupesoft' after hash;
+#alter table git_paivitykset add column hash_pupenext varchar(50) not null default '' after hash;
+
 // Kutsutaanko CLI:st‰
 $php_cli = (php_sapi_name() == 'cli');
 
@@ -47,11 +51,14 @@ else {
       </script>";
 }
 
+$pupe_root_polku = dirname(__FILE__);
+
 echo "<font class='head'>".t("Uudet ominaisuudet")."</font><hr><br>";
 
-// Haetaan pulkkareita githubista
+// Github curl API
 function github_api($url) {
   $ch  = curl_init();
+
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
@@ -72,43 +79,13 @@ function github_api($url) {
   }
 }
 
-// Katostaan milloin ollaan viimeksi kutsuttu githubin apia
-$query  = "SELECT max(date) haettu
-           FROM git_paivitykset
-           WHERE hash = 'github_api_request' ";
-$apires = pupe_query($query);
-$apirow = mysql_fetch_assoc($apires);
-
-$haetaanpulkkarit = TRUE;
-
-if (!$php_cli and !empty($apirow['haettu']) and strtotime($apirow['haettu']) > strtotime("1 hour ago")) {
-  // Kutsutaan apia korkeintaan kerran tunnissa
-  $haetaanpulkkarit = FALSE;
-}
-elseif ($php_cli and !empty($apirow['haettu']) and strtotime($apirow['haettu']) > strtotime("5 minute ago")) {
-  // Kutsutaan apia korkeintaan kerran 5 minuutissa kun vedet‰‰n narusta
-  $haetaanpulkkarit = FALSE;
-}
-
-if ($haetaanpulkkarit) {
-
-  // Haetaan muuttuneet/uudet pulkkarit kantaan
-  $query  = "SELECT max(updated) updated
-             FROM git_pulkkarit";
-  $prres = pupe_query($query);
-  $prrow = mysql_fetch_assoc($prres);
-
-  if (!empty($prrow['updated'])) {
-    $updatedtime = strtotime($prrow['updated']);
-  }
-  else {
-    // Ekalla ajolla haetaan vaikka parin kuukauden takaa
-    $updatedtime = strtotime("2 month ago");
-  }
+// Haetaan pulkkareita githubista
+function hae_pulkkarit($updatedtime, $repo, $url) {
+  GLOBAL $kukarow;
 
   $page = 1;
 
-  while ($pulkkarit = github_api("https://api.github.com/repos/devlab-oy/pupesoft/pulls?state=closed&sort=updated&direction=desc&page=$page")) {
+  while ($pulkkarit = github_api($url."/pulls?state=closed&sort=updated&direction=desc&page=$page")) {
     $page++;
 
     if ($pulkkarit === FALSE) break;
@@ -116,6 +93,7 @@ if ($haetaanpulkkarit) {
     // T‰g‰t‰‰n apikutsun aikaleima
     $query  = "INSERT INTO git_paivitykset
                SET hash = 'github_api_request',
+               repository = '$repo',
                date = now()";
     pupe_query($query);
 
@@ -133,7 +111,7 @@ if ($haetaanpulkkarit) {
           $pulkkari_ser = mysql_real_escape_string(serialize($pulkkari));
 
           // Haetaan muuttuneet failit
-          $filet = github_api("https://api.github.com/repos/devlab-oy/pupesoft/pulls/$number/files");
+          $filet = github_api($url."/pulls/$number/files");
 
           $filetarr = array();
 
@@ -164,6 +142,7 @@ if ($haetaanpulkkarit) {
 
           $query  = "INSERT INTO git_pulkkarit
                      SET id       = $number,
+                     repository   = '$repo',
                      updated      = '$updated',
                      merged       = '$merged',
                      feature      = $newfeature,
@@ -188,6 +167,74 @@ if ($haetaanpulkkarit) {
   }
 }
 
+// Parsitaan git-logista kahden narustavedon v‰liset mergetykset
+function git_log($repo, $edveto_hash, $taveto_hash) {
+  GLOBAL $pupe_root_polku;
+
+  $pulkkarit = array();
+
+  if ($repo == "pupenext") {
+    $polku = $pupe_root_polku."/pupenext";
+  }
+  else {
+    $polku = $pupe_root_polku;
+  }
+
+  exec("cd $polku; git log --merges $edveto_hash..$taveto_hash |grep \"pull request\"", $pulkkarit);
+
+  $pull_ids = array();
+
+  foreach ($pulkkarit as $pulkkari) {
+    preg_match("/pull request #([0-9]*) from/", $pulkkari, $pulkkarinro);
+
+    $pull_ids[] = $repo."#".$pulkkarinro[1];
+  }
+
+  $pull_ids = implode(",", $pull_ids);
+
+  return $pull_ids;
+}
+
+// Katsotaan milloin ollaan viimeksi kutsuttu githubin apia
+// ja haetaan omaan kantaan githubin pulkkarien tiedot
+$query  = "SELECT max(date) haettu
+           FROM git_paivitykset
+           WHERE hash = 'github_api_request' ";
+$apires = pupe_query($query);
+$apirow = mysql_fetch_assoc($apires);
+
+$haetaanpulkkarit = TRUE;
+
+if (!$php_cli and !empty($apirow['haettu']) and strtotime($apirow['haettu']) > strtotime("1 hour ago")) {
+  // Kutsutaan apia korkeintaan kerran tunnissa
+  $haetaanpulkkarit = FALSE;
+}
+elseif ($php_cli and !empty($apirow['haettu']) and strtotime($apirow['haettu']) > strtotime("5 minute ago")) {
+  // Kutsutaan apia korkeintaan kerran 5 minuutissa kun vedet‰‰n narusta
+  $haetaanpulkkarit = FALSE;
+}
+
+if ($haetaanpulkkarit) {
+  // Haetaan muuttuneet/uudet pulkkarit kantaan
+  $query  = "SELECT max(updated) updated
+             FROM git_pulkkarit";
+  $prres = pupe_query($query);
+  $prrow = mysql_fetch_assoc($prres);
+
+  if (!empty($prrow['updated'])) {
+    $updatedtime = strtotime($prrow['updated']);
+  }
+  else {
+    // Ekalla ajolla haetaan vaikka parin kuukauden takaa
+    $updatedtime = strtotime("2 month ago");
+  }
+
+  $updatedtime = strtotime("2 month ago");
+
+  hae_pulkkarit($updatedtime, "pupesoft", "https://api.github.com/repos/devlab-oy/pupesoft");
+  hae_pulkkarit($updatedtime, "pupenext", "https://api.github.com/repos/devlab-oy/pupenext");
+}
+
 if ($php_cli) {
   $display_h = "";
   // Pit‰‰ hakea kaksi uusinta vetoa, jotta voidaan hakea niitten v‰liset muutokset logista
@@ -200,6 +247,7 @@ else {
 }
 
 // Haetaan uusimmat narustavedot kannasta
+// ja n‰ytet‰‰n ruudulla p‰ivityksess‰ tulleet uudet ominaisuudet
 $query  = "SELECT *
            FROM git_paivitykset
            WHERE hash != 'github_api_request'
@@ -211,6 +259,7 @@ if (mysql_num_rows($vetores)) {
 
   $vedot = array();
   $taveto_hash = "";
+  $taveto_hash_pupenext = "";
 
   while ($vetorow = mysql_fetch_assoc($vetores)) {
     $vedot[] = $vetorow;
@@ -224,19 +273,25 @@ if (mysql_num_rows($vetores)) {
   foreach ($vedot as $i => $veto) {
 
     if (!empty($veto["hash"])) $taveto_hash = $veto["hash"];
+    if (!empty($veto["hash_pupenext"])) $taveto_hash_pupenext = $veto["hash_pupenext"];
 
     if (isset($vedot[$i+1])) {
-      $edveto_hash = $vedot[$i+1]["hash"];
+      if (!empty($vedot[$i+1]["hash"])) {
+        $edveto_hash = $vedot[$i+1]["hash"];
+      }
+      if (!empty($vedot[$i+1]["hash_pupenext"])) {
+        $edveto_hash_pupenext = $vedot[$i+1]["hash_pupenext"];
+      }
     }
     else {
       continue;
     }
 
     if ($veto == "HEAD") {
-      $query  = "SELECT group_concat(id) idt
+      // Tulossa olevat ominaisuudet
+      $query  = "SELECT group_concat(concat_ws('#', repository, id) ORDER BY feature DESC, merged) idt
                  FROM git_pulkkarit
-                 WHERE merged > '{$vedot[$i+1]["date"]}'
-                 ORDER BY feature DESC, id";
+                 WHERE merged > '{$vedot[$i+1]["date"]}'";
       $pulres = pupe_query($query);
       $pulrow = mysql_fetch_assoc($pulres);
 
@@ -244,7 +299,7 @@ if (mysql_num_rows($vetores)) {
         continue;
       }
 
-      echo "<tr><th>";
+      echo "$query<tr><th>";
       if (!$php_cli) echo "<img style='float:left;' class='nayta_rivit' id='HEAD' src='{$palvelin2}pics/lullacons/switch.png' /> ";
       echo t("Tulossa olevat ominaisuudet").":</th></tr>";
       echo "<tr><td class='back' style='padding:0px;'><table id='table_HEAD'>";
@@ -252,19 +307,24 @@ if (mysql_num_rows($vetores)) {
       $pull_ids = $pulrow['idt'];
     }
     else {
+      $pull_ids = "";
 
-      $pulkkarit = array();
-      exec("git log --merges $edveto_hash..$taveto_hash |grep \"pull request\"", $pulkkarit);
-
-      $pull_ids = array();
-
-      foreach ($pulkkarit as $pulkkari) {
-        preg_match("/pull request #([0-9]*) from/", $pulkkari, $pulkkarinro);
-
-        $pull_ids[] = $pulkkarinro[1];
+      // Narustavedossa tulleet pupesoft-ominaisuudet
+      if (!empty($edveto_hash) and !empty($taveto_hash)) {
+        $pull_ids .= git_log("pupesoft", $edveto_hash, $taveto_hash);
       }
 
-      $pull_ids = implode(",", $pull_ids);
+      // Narustavedossa tulleet pupenext-ominaisuudet
+      if (!empty($edveto_hash_pupenext) and !empty($taveto_hash_pupenext)) {
+        $pull_ids_next = git_log("pupenext", $edveto_hash_pupenext, $taveto_hash_pupenext);
+
+        if (!empty($pull_ids) and !empty($pull_ids_next)) {
+          $pull_ids .= ",";
+        }
+        if (!empty($pull_ids_next)) {
+          $pull_ids .= $pull_ids_next;
+        }
+      }
 
       // jos ei ollut yht‰‰n pulkkaria, niin skipataan koko rivi
       if ($pull_ids == "") continue;
@@ -276,14 +336,15 @@ if (mysql_num_rows($vetores)) {
     }
 
     if ($pull_ids != "") {
+      foreach(explode(",",$pull_ids) as $pull) {
+        list($repo, $pullid) = explode("#", $pull);
 
-      $query  = "SELECT *
-                 FROM git_pulkkarit
-                 WHERE id in ($pull_ids)
-                 ORDER BY feature DESC, id";
-      $pulres = pupe_query($query);
-
-      while ($pulrow = mysql_fetch_assoc($pulres)) {
+        $query  = "SELECT *
+                   FROM git_pulkkarit
+                   WHERE repository = '$repo'
+                   AND id = $pullid";
+        $pulres = pupe_query($query);
+        $pulrow = mysql_fetch_assoc($pulres);
 
         $pulkkaridata = unserialize($pulrow["pull_request"]);
 
@@ -304,7 +365,7 @@ if (mysql_num_rows($vetores)) {
           $titlelisa = t("Pienkehitys");
         }
 
-        echo "<td class='$class' style='width: 100%;'><font class='message'>$titlelisa</font>: <font class='$fclass'>$title</font></td>";
+        echo "<td class='$class' style='width: 100%;'><font class='message'>$repo $titlelisa</font>: <font class='$fclass'>$title</font></td>";
         echo "<td class='$class' style='width: 100%;'><a target='pulkkari' href='https://github.com/devlab-oy/pupesoft/pull/$pulrow[id]'>$pulrow[id]</a></td>";
         echo "</tr>";
         echo "<tr><td colspan='3' style='width: 100%;'><pre style='white-space: pre-wrap;'>$body</pre>";
@@ -338,7 +399,6 @@ if (mysql_num_rows($vetores)) {
   }
 
   echo "</table>";
-
 }
 
 if ($php_cli) {

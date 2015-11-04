bdc3cd84c6e83f5201ffa43b299cc3684ffc2d99
Mika Myllynen <giefsufu@gmail.com>
2015-11-04 13:12:15 +0200

    lisätään ylläpito.js

--- yllapito.php
+++ yllapito.php
@@ -17,7 +17,6 @@ else {
 
 if (strpos($_SERVER['SCRIPT_NAME'], "yllapito.php")  !== FALSE) {
   require "inc/parametrit.inc";
-  echo "<script src='yllapito.js'></script>";
 }
 
 //Huom ennen t‰t‰ rivi‰ ei saa olla mit‰‰n echoja!

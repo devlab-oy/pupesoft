e3e1e89d6343c35816b9c4e427797dcaef29cace
tiere <tommi@devlab.fi>
2015-11-16 13:26:03 +0200

    Hanskataan myös päivitys

--- lue_data.php
+++ lue_data.php
@@ -2325,22 +2325,25 @@ if ($kasitellaan_tiedosto) {
             // Itse lue_datan p‰ivitysquery
             $iresult = pupe_query($query);
 
-            // Haetaan tunnus ja luodaan syncrow
+            // Haetaan tunnus, jos oli INSERT
+            if ($taulunrivit[$taulu][$eriviindex][$postoiminto] == 'LISAA') {
+              $tunnus  = mysql_insert_id($GLOBALS["masterlink"]);
+            }
+
+            generoi_hinnastot($tunnus);
+
+            // Synkronoidaan
+            if (stripos($yhtiorow["synkronoi"], $table_mysql) !== FALSE) {
             if ($taulunrivit[$taulu][$eriviindex][$postoiminto] == 'LISAA') {
               $syncrow = array();
-              $tunnus  = mysql_insert_id($GLOBALS["masterlink"]);
             }
             else {
               $syncrow = mysql_fetch_array($syncres);
               $tunnus  = $syncrow["tunnus"];
             }
 
-            // Synkronoidaan
-            if (stripos($yhtiorow["synkronoi"], $table_mysql) !== FALSE) {
               synkronoi($kukarow["yhtio"], $table_mysql, $tunnus, $syncrow, "");
             }
-
-            generoi_hinnastot($tunnus);
 
             // tehd‰‰n ep‰kunrattijutut
             if ($tee == "paalle" or $tee == "25paalle" or $tee == "puolipaalle" or $tee == "75paalle" or $tee == "pois" or $tee == "peru") {

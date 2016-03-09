be6f6e1aa14730ecda1bf2cedb88bc03fe875b8a
Joni Kanerva <joni@devlab.fi>
2016-03-09 11:30:32 +0200

    codestyleä ja korjauksia

--- rajapinnat/yrityspeli.php
+++ rajapinnat/yrityspeli.php
@@ -3,46 +3,34 @@
 // T‰ll‰ ohjelmalla voidaan generoida myyntitilauksia kuluvalle viikolle yrityksille joilla ei
 // viel‰ sellaisia ole
 
-// Kutsutaanko CLI:st‰
-$php_cli = FALSE;
+require "../inc/parametrit.inc";
+require "tilauskasittely/luo_myyntitilausotsikko.inc";
 
-if (php_sapi_name() == 'cli') {
-  $php_cli = TRUE;
-}
+if (!isset($tee)) $tee = '';
+if (!isset($painoarvo)) $painoarvo = 100;
+if (!isset($tilausmaara)) $tilausmaara = 3;
 
-$pupe_root_polku = dirname(dirname(__FILE__));
+$kauppakeskus_myyra = '003732419754';
 
-require "{$pupe_root_polku}/inc/connect.inc";
-require "{$pupe_root_polku}/inc/functions.inc";
-require_once "{$pupe_root_polku}/tilauskasittely/luo_myyntitilausotsikko.inc";
-
-if (!$php_cli) {                                  
-  require "{$pupe_root_polku}/inc/parametrit.inc";
-
-  if (!isset($tee)) $tee = '';
-  if (!isset($painoarvo)) $painoarvo = 100;
-  if (!isset($tilausmaara)) $tilausmaara = 3;
-
-  if ($tee == 'GENEROI') {
-    if (empty($valitut)) {
-      echo "<br><font class='error'>".t('Et valinnut yht‰‰n yrityst‰')."</font><br><br>";
-    }
-    else {
-      $tilaukset = generoi_myyntitilauksia($valitut, $painoarvo, $tilausmaara);
-      echo "<br>".t('Tilaukset luotu')."<br><br>";
-    }
-
-    $tee = '';
+if ($tee == 'GENEROI') {
+  if (empty($valitut)) {
+    echo "<br><font class='error'>".t('Et valinnut yht‰‰n yrityst‰')."</font><br><br>";
+  }
+  else {
+    $tilaukset = generoi_myyntitilauksia($valitut, $painoarvo, $tilausmaara, $kauppakeskus_myyra);
+    echo "<br>".t('Tilaukset luotu')."<br><br>";
   }
 
-  if (empty($tee)) {
-    echo_yrityspeli_kayttoliittyma();
-  }
-
-  require ("{$pupe_root_polku}/inc/footer.inc");
+  $tee = '';
 }
 
-function echo_yrityspeli_kayttoliittyma() {
+if (empty($tee)) {
+  echo_yrityspeli_kayttoliittyma($painoarvo, $tilausmaara);
+}
+
+require "inc/footer.inc";
+
+function echo_yrityspeli_kayttoliittyma($painoarvo, $tilausmaara) {
   global $yhtiorow, $kukarow;
 
   $tilauksettomat_yhtiot = hae_tilauksettomat_yhtiot();
@@ -53,18 +41,31 @@ function echo_yrityspeli_kayttoliittyma() {
   echo "<table>";
   echo "<tr>";
   echo "<th>".t('Tilausten painoarvo')."</th>";
-  echo "<td><font>Painoarvo vaikuttaa tilaussumman suuruuteen. <br> Esim. normaalitilauksen summa on n. 1000 euroa (100%),<br>
-                  painoarvo 80 antaa tilaussummaksi n. 800 euroa (80%)<br>
-                  ja painoarvo 150 antaa tilaussummaksi n. 1500 euroa (150%).</font><br>";
-  echo "<input type='text' name='painoarvo' size='3' value='100'/>%";
+
+  echo "<td>";
+  echo "<input type='text' name='painoarvo' size='3' value='{$painoarvo}'/>%";
   echo "</td>";
+
+  echo "<td class='back'>";
+  echo "Painoarvo vaikuttaa tilaussumman suuruuteen. <br>
+    Esim. normaalitilauksen summa on n. 1000 euroa (100%),<br>
+    painoarvo 80 antaa tilaussummaksi n. 800 euroa (80%)<br>
+    ja painoarvo 150 antaa tilaussummaksi n. 1500 euroa (150%).";
+  echo "</td>";
+
   echo "</tr>";
   echo "<tr>";
 
   echo "<th>".t('Tilausten lukum‰‰r‰')."</th>";
-  echo "<td><font>Montako automaattitilausta luodaan per yritys</font><br>";
-  echo "<input type='text' name='tilausmaara' size='2' value='3'/>";
+
+  echo "<td>";
+  echo "<input type='text' name='tilausmaara' size='2' value='${tilausmaara}'/>";
   echo "</td>";
+
+  echo "<td class='back'>";
+  echo "Montako automaattitilausta luodaan per yritys";
+  echo "</td>";
+
   echo "</tr>";
 
   echo "</table>";
@@ -92,7 +93,7 @@ function echo_yrityspeli_kayttoliittyma() {
 }
 
 function hae_tilauksettomat_yhtiot() {
-  global $yhtiorow;
+  global $kukarow, $yhtiorow;
 
   $tilauksettomat_yhtiot = array();
 
@@ -100,55 +101,62 @@ function hae_tilauksettomat_yhtiot() {
   $alkuaika = date("Y-m-d", strtotime('monday this week'));
   $loppuaika = date("Y-m-d", strtotime('sunday this week'));
 
-  $query = "SELECT * 
+  $query = "SELECT *
             FROM yhtio
-            WHERE yhtio NOT IN ('{$yhtiorow['yhtio']}')";
+            WHERE yhtio NOT IN ('{$kukarow['yhtio']}')";
   $result = pupe_query($query);
+
   while ($row = mysql_fetch_assoc($result)) {
-    $tilausquery = "SELECT count(distinct lasku.tunnus) avoimia_tilauksia 
+    $tilausquery = "SELECT count(distinct lasku.tunnus) avoimia_tilauksia
                     FROM lasku
-                    JOIN tilausrivi 
-                     ON (lasku.yhtio = tilausrivi.yhtio AND lasku.tunnus = tilausrivi.otunnus) 
-                    WHERE lasku.yhtio = '{$row['yhtio']}' 
+                    JOIN tilausrivi ON (lasku.yhtio = tilausrivi.yhtio
+                      AND lasku.tunnus = tilausrivi.otunnus)
+                    WHERE lasku.yhtio = '{$row['yhtio']}'
                     AND lasku.tila IN ('N','L')
                     AND lasku.luontiaika BETWEEN '$alkuaika' AND '$loppuaika'";
     $tilausresult = pupe_query($tilausquery);
-
     $tilausrow = mysql_fetch_assoc($tilausresult);
+
     if (empty($tilausrow['avoimia_tilauksia'])) {
-      $tilauksettomat_yhtiot[] = $tilausrow['yhtio'];
+      $tilauksettomat_yhtiot[] = $row['yhtio'];
     }
   }
 
   return $tilauksettomat_yhtiot;
 }
 
-function generoi_myyntitilauksia($yhtiot, $painoarvo, $tilausmaara) {
+function generoi_myyntitilauksia($yhtiot, $painoarvo, $tilausmaara, $kauppakeskus_myyra) {
   foreach ($yhtiot as $yhtio) {
-    $asiakas = hae_oletusasiakkuus($yhtio);
-    if (!empty($asiakas)) {
-      for ($i=0; $i < $tilausmaara; $i++) { 
-        luo_tilausotsikot_ja_tilausrivit($yhtio, $asiakas, $painoarvo);
-      }
+    $asiakas = hae_oletusasiakkuus($yhtio, $kauppakeskus_myyra);
+
+    if (empty($asiakas)) {
+      continue;
+    }
+
+    for ($i=0; $i < $tilausmaara; $i++) {
+      luo_tilausotsikot_ja_tilausrivit($yhtio, $asiakas, $painoarvo);
     }
   }
 }
 
-function hae_oletusasiakkuus($yhtio) {
-  $query = "SELECT * 
+function hae_oletusasiakkuus($yhtio, $kauppakeskus_myyra) {
+  $query = "SELECT *
             FROM asiakas
             WHERE yhtio = '{$yhtio}'
-            AND ovttunnus = '003732419754'
+            AND ovttunnus = '{$kauppakeskus_myyra}'
             LIMIT 1";
   $result = pupe_query($query);
+
   return mysql_fetch_assoc($result);
 }
 
 function luo_tilausotsikot_ja_tilausrivit($yhtio, $asiakas, $painoarvo) {
-  global $kukarow;
+  global $yhtiorow, $kukarow;
 
-  $painoarvokerroin = $painoarvo / 100;
-  $alkuperainen_yhtio = $kukarow['yhtio'];
+  $painoarvokerroin   = $painoarvo / 100;
+  $alkuperainen_yhtio = $yhtiorow;
+  $alkuperainen_kuka  = $kukarow;
+
   $yhtiorow = hae_yhtion_parametrit($yhtio);
   $kukarow['kesken'] = '';
   $kukarow['yhtio'] = $yhtio;
@@ -159,9 +167,9 @@ function luo_tilausotsikot_ja_tilausrivit($yhtio, $asiakas, $painoarvo) {
   $kukarow["kesken"] = $tilausnumero;
 
   // Haetaan avoimen tilauksen otsikko
-  $query    = "SELECT * 
-               FROM lasku 
-               WHERE yhtio='{$yhtio}' 
+  $query    = "SELECT *
+               FROM lasku
+               WHERE yhtio='{$yhtio}'
                AND tunnus='{$tilausnumero}'";
   $laskures = pupe_query($query);
   $laskurow = mysql_fetch_assoc($laskures);
@@ -173,6 +181,7 @@ function luo_tilausotsikot_ja_tilausrivit($yhtio, $asiakas, $painoarvo) {
   $kokonaiskustannus = 1000 * $painoarvokerroin;
 
   $hintacounter = 0;
+
   while ($hintacounter < $kokonaiskustannus) {
     $trow = tuotearvonta($yhtio);
     $hinta = rand(55, 122);
@@ -193,19 +202,23 @@ function luo_tilausotsikot_ja_tilausrivit($yhtio, $asiakas, $painoarvo) {
   }
 
   $pupe_root_polku = dirname(dirname(__FILE__));
+
   // Tilaus valmiiksi
-  require "{$pupe_root_polku}/tilauskasittely/tilaus-valmis.inc";
+  require "tilauskasittely/tilaus-valmis.inc";
+
+  $yhtiorow = $alkuperainen_yhtio;
+  $kukarow = $alkuperainen_kuka;
 
   $kukarow['kesken'] = '';
   $kukarow['yhtio'] = $alkuperainen_yhtio;
-  return;
 }
 
 function tuotearvonta($yhtio) {
-  $query = "SELECT * 
-            FROM tuote 
-            WHERE yhtio = '{$yhtio}' 
-            ORDER BY RAND() LIMIT 0,1";
+  $query = "SELECT *
+            FROM tuote
+            WHERE yhtio = '{$yhtio}'
+            ORDER BY RAND() LIMIT 0, 1";
   $result = pupe_query($query);
+
   return mysql_fetch_assoc($result);
 }

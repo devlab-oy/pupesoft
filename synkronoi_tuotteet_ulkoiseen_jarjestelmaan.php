8d18df960cc9b015f232328a9c6668e6da334f71
Sami Luukkonen <sami@devlab.fi>
2016-03-16 14:18:37 +0200

    Poistetaan turha kenttä selectistä

--- synkronoi_tuotteet_ulkoiseen_jarjestelmaan.php
+++ synkronoi_tuotteet_ulkoiseen_jarjestelmaan.php
@@ -45,7 +45,7 @@ else {
     $wherelisa = "";
   }
 
-  $query = "SELECT tuote.*, ta.selite AS synkronointi, ta.tunnus AS tuotteen_avainsanat_tunnus
+  $query = "SELECT tuote.*, ta.selite AS synkronointi
             FROM tuote
             LEFT JOIN tuotteen_avainsanat AS ta ON (ta.yhtio = tuote.yhtio AND ta.tuoteno = tuote.tuoteno AND ta.laji = 'synkronointi' AND ta.selite != '')
             WHERE tuote.yhtio    = '{$kukarow['yhtio']}'

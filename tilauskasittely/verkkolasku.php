94963d2766fc3d4ca7c3afd58d312f852aa70f6c
Johan Tötterman <juppe@devlab.fi>
2016-06-16 12:02:18 +0300

    Käytetään poista_rivin_yhteensakommentti()-funkkaria.

--- tilauskasittely/verkkolasku.php
+++ tilauskasittely/verkkolasku.php
@@ -2548,10 +2548,8 @@ else {
 
               // Otetaan yhteensäkommentti pois jos summataan rivejä
               if ($rivigrouppaus) {
-                $tilrow["kommentti"] = preg_replace("/ ".t("yhteensä", $kieli).": [0-9\.]* [A-Z]{3}\./", "", $tilrow["kommentti"]);
-                $tilrow["kommentti"] = preg_replace("/ ".t("yhteensä", $asiakas_apu_row["kieli"]).": [0-9\.]* [A-Z]{3}\./", "", $tilrow["kommentti"]);
-                $tilrow["kommentti"] = preg_replace("/ ".t("yhteensä").": [0-9\.]* [A-Z]{3}\./", "", $tilrow["kommentti"]);
-                $tilrow["kommentti"] = preg_replace("/ "."yhteensä".": [0-9\.]* [A-Z]{3}\./", "", $tilrow["kommentti"]);
+                // Trimmataan ja otetaan "yhteensäkommentti" pois
+                $tilrow["kommentti"] = trim(poista_rivin_yhteensakommentti($tilrow["kommentti"]));
               }
 
               // Laitetaan alennukset kommenttiin, koska laskulla on vain yksi alekenttä

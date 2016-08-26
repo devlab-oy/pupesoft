5c8f74055890be251aa01ce77d906d5feba3c894
Sami Luukkonen <sami@devlab.fi>
2016-08-26 13:17:29 +0300

    Otetaan kaikki tilausnumerot eikä vain ensimmäistä

--- rajapinnat/logmaster/tracking_code.php
+++ rajapinnat/logmaster/tracking_code.php
@@ -76,29 +76,32 @@ while (false !== ($file = readdir($handle))) {
       list($posten_lahetenumero, $tilausnumero, $seurantakoodi) = explode(';', $tietue);
     }
 
-    if (trim($seurantakoodi) == '') {
+    $seurantakoodi = trim(preg_replace("/\r\n|\r|\n/", '', $seurantakoodi));
+
+    if ($seurantakoodi == '') {
       pupesoft_log('logmaster_tracking_code', "Seurantakoodi puuttuu riviltä");
 
       continue;
     }
 
-    // Otetaan vain eka ilmentymä tilausnumerosta jos sattuu olemaan monta eroteltuna spacella
-    list($tilausnumero) = explode(' ', $tilausnumero);
+    // Tilausnumerot voi olla eroteltuna spacella
+    $tilausnumerot = explode(' ', $tilausnumero);
+    $tilausnumerot = array_unique($tilausnumerot);
 
-    $tilausnumero = (int) $tilausnumero;
-    $seurantakoodi = preg_replace("/\r\n|\r|\n/", '', $seurantakoodi);
+    foreach ($tilausnumerot as $tilausnumero) {
+      $tilausnumero = (int) $tilausnumero;
 
-    if ($tilausnumero == 0 or trim($seurantakoodi) == '') {
-      pupesoft_log('logmaster_tracking_code', "Tilausnumero puuttuu riviltä");
+      if ($tilausnumero == 0) {
+        pupesoft_log('logmaster_tracking_code', "Tilausnumero puuttuu riviltä");
 
-      continue;
+        continue;
+      }
+
+      $seurantakoodit[$tilausnumero][] = $seurantakoodi;
     }
-
-    $seurantakoodit[$tilausnumero][] = $seurantakoodi;
   }
 
   foreach ($seurantakoodit as $tilausnumero => $koodit) {
-
     $koodit        = array_unique($koodit);
     $seurantakoodi = implode(' ', $koodit);
 
@@ -106,6 +109,7 @@ while (false !== ($file = readdir($handle))) {
               rahtikirjanro  = trim(concat(rahtikirjanro, ' ', '{$seurantakoodi}')),
               tulostettu     = now()
               WHERE yhtio    = '{$kukarow['yhtio']}'
+              AND tulostettu = '0000-00-00 00:00:00'
               AND otsikkonro = '{$tilausnumero}'";
     pupe_query($query);
 

c805445725cdf177722164ea035b9c58615251cc
satu <satu@devlab.fi>
2016-08-30 15:57:25 +0300

    Parempi järjestys

--- lue_data.php
+++ lue_data.php
@@ -2454,6 +2454,7 @@ if ($kasitellaan_tiedosto) {
                 $syncrow = array();
               }
               else {
+                mysql_data_seek($syncres,0);
                 $syncrow = mysql_fetch_array($syncres);
                 $tunnus = $syncrow["tunnus"];
               }

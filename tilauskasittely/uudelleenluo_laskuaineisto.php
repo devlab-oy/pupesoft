69cc25133def1c4ec33a9580f2435f6107bf9490
Johan Tötterman <juppe@devlab.fi>
2016-09-30 14:40:25 +0300

    Tägätään talenomille menevät sähköpostilaskut

--- tilauskasittely/uudelleenluo_laskuaineisto.php
+++ tilauskasittely/uudelleenluo_laskuaineisto.php
@@ -405,6 +405,8 @@ if (isset($tee) and ($tee == "GENEROI" or $tee == "NAYTATILAUS") and $laskunumer
       $myyresult = pupe_query($mquery);
       $myyrow = mysql_fetch_assoc($myyresult);
 
+      $lasrow['chn_orig'] = $lasrow['chn'];
+
       //HUOM: Tässä kaikki sallitut verkkopuolen chn:ät
       if (!in_array($lasrow['chn'], array("100", "010", "001", "020", "111", "112"))) {
         //Paperi by default

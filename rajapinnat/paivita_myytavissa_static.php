baca4c8a3b9410ed24f97b65c57684e2cb60a065
Joni Kanerva <joni@devlab.fi>
2017-01-12 10:08:05 +0200

    lokitus ja aikaleima

--- rajapinnat/paivita_myytavissa_static.php
+++ rajapinnat/paivita_myytavissa_static.php
@@ -32,17 +32,16 @@ pupesoft_flock($lock_params);
 // Laitetaan unlimited execution time
 ini_set("max_execution_time", 0);
 
-if (trim($argv[1]) != '') {
-  $yhtio = mysql_real_escape_string($argv[1]);
-  $yhtiorow = hae_yhtion_parametrit($yhtio);
-  $kukarow = hae_kukarow('admin', $yhtio);
-
-  if ($kukarow === null) {
-    die ("\n");
-  }
-}
-else {
+if (empty($argv[1])) {
   die ("Et antanut yhtiˆt‰.\n");
+}
+
+$yhtio = mysql_real_escape_string($argv[1]);
+$yhtiorow = hae_yhtion_parametrit($yhtio);
+$kukarow = hae_kukarow('admin', $yhtio);
+
+if ($kukarow === null) {
+  die ("\n");
 }
 
 $ajetaanko_kaikki = (isset($argv[2]) and trim($argv[2]) != '') ? "YES" : "NO";
@@ -50,17 +49,24 @@ $ajetaanko_kaikki = (isset($argv[2]) and trim($argv[2]) != '') ? "YES" : "NO";
 if (!isset($verkkokauppa_saldo_varasto)) $verkkokauppa_saldo_varasto = array();
 
 if (count($verkkokauppa_saldo_varasto) == 0) {
-  echo "verkkokauppa_saldo_varasto pit‰‰ m‰‰ritell‰!\n";
+  pupesoft_log("paivita_static", "Verkkokauppa_saldo_varasto pit‰‰ m‰‰ritell‰!");
   exit;
 }
+
+// Haetaan aika nyt
+$query = "SELECT now() as aika";
+$result = pupe_query($query);
+$row = mysql_fetch_assoc($result);
+$aloitusaika = $row['aika'];
 
 // Haetaan aika jolloin t‰m‰ skripti on viimeksi ajettu
 $datetime_checkpoint = cron_aikaleima("MYY_STATIC_CRON");
 
-echo date("d.m.Y @ G:i:s")." - Aloitetaan myytavissa_static-p‰ivitys.\n";
-echo date("d.m.Y @ G:i:s")." - Haetaan saldot.\n";
+pupesoft_log("paivita_static", "Aloitetaan saldop‰ivitys {$aloitusaika}");
 
 if ($datetime_checkpoint != "" and $ajetaanko_kaikki == "NO") {
+  pupesoft_log("paivita_static", "Haetaan {$datetime_checkpoint} j‰lkeen muuttuneet");
+
   $muutoslisa1 = "AND tapahtuma.laadittu  >= '{$datetime_checkpoint}'";
   $muutoslisa2 = "AND tilausrivi.laadittu >= '{$datetime_checkpoint}'";
   $muutoslisa3 = "AND tuote.muutospvm     >= '{$datetime_checkpoint}'";
@@ -117,7 +123,7 @@ $result = pupe_query($query);
 
 while ($row = mysql_fetch_assoc($result)) {
   foreach ($verkkokauppa_saldo_varasto as $varasto) {
-    $query =  "SELECT hyllyalue, hyllynro, hyllyvali, hyllytaso, tunnus
+    $query =  "SELECT hyllyalue, hyllynro, hyllyvali, hyllytaso, tunnus, myytavissa_static
                FROM tuotepaikat
                WHERE yhtio = '{$kukarow["yhtio"]}'
                AND tuoteno = '{$row["tuoteno"]}'
@@ -131,9 +137,18 @@ while ($row = mysql_fetch_assoc($result)) {
                  SET myytavissa_static = '$myytavissa'
                  WHERE tunnus = '{$tprow["tunnus"]}'";
       pupe_query($query);
+
+      $log_message  = "Tuote {$row["tuoteno"]}, ";
+      $log_message .= "paikka {$tprow["hyllyalue"]}-{$tprow["hyllynro"]}-{$tprow["hyllyvali"]}-{$tprow["hyllytaso"]}, ";
+      $log_message .= "vanha saldo {$tprow["myytavissa_static"]}, ";
+      $log_message .= "uusi saldo {$myytavissa}";
+
+      pupesoft_log("paivita_static", $log_message);
     }
   }
 }
 
 // Kun kaikki onnistui, p‰ivitet‰‰n lopuksi timestamppi talteen
-cron_aikaleima("MYY_STATIC_CRON", date('Y-m-d H:i:s'));
+cron_aikaleima("MYY_STATIC_CRON", $aloitusaika);
+
+pupesoft_log("paivita_static", "Saldot p‰ivitetty");

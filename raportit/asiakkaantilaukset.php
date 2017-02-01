c2a3119ab3ec20e914d99a2f8a1e4bf20df3a335
Tony Selkamo <tony@devlab.fi>
2017-02-01 15:59:32 +0200

    Whitelistaan muutos
    Lisätään Rosa Heidin pyynnöstä tuohon listaan mukaan.

--- raportit/asiakkaantilaukset.php
+++ raportit/asiakkaantilaukset.php
@@ -801,7 +801,7 @@ if ($ytunnus != '') {
           <input type='submit' value='".t("Näytä tilaus")."'>
           </form></td>";
 
-      $poista_tilaus_whiteliset = array("heidi", "tarja", "admin");
+      $poista_tilaus_whiteliset = array("heidi", "tarja", "admin", "Rosa");
 
       if ($row["tila"] != "U" and in_array($kukarow['kuka'], $poista_tilaus_whiteliset)) {
         echo "<td class='back'>

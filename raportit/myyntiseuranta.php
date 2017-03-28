b117453b2dda8c62df3bf4766b5d389219c13dfe
satu <satu@devlab.fi>
2017-03-28 14:21:32 +0300

    Näytetään laskutuspäivä ja maksupäivä samassa formaatissa

--- raportit/myyntiseuranta.php
+++ raportit/myyntiseuranta.php
@@ -3135,6 +3135,10 @@ if ((isset($aja_raportti) or isset($valitse_asiakas)) and count($_REQUEST) > 0) 
                 $row[$ken_nimi] = tv1dateconv($kentta);
               }
 
+              if ($ken_nimi == "maksupvm") {
+                $row[$ken_nimi] = tv1dateconv($kentta);
+              }
+
               // jos kyseessa on asiakasosasto, haetaan sen nimi
               if ($ken_nimi == "asiakasosasto") {
                 $osre = t_avainsana("ASIAKASOSASTO", "", "and avainsana.selite  = '{$row[$ken_nimi]}'", $yhtio);

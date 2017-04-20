af79ad319df078dc1e811c0598342698e0b97e56
Johan Tötterman <juppe@devlab.fi>
2017-04-20 15:00:10 +0300

    Turhat pois

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -4729,19 +4729,13 @@ if ($tee == '') {
           // Poistetaan muokattava tilausrivi
           $query = "DELETE FROM tilausrivi
                     WHERE tunnus = '$rivitunnus'";
-          $result = pupe_query($query);
-          
+          pupe_query($query);
+
           // Jos laiterekisteri käytössä, poistetaan myös laitteen_sopimukset -rivi
           if ($yhtiorow['laiterekisteri_kaytossa'] != '') {
-            $laitesop = "SELECT tunnus
-                            FROM laitteen_sopimukset
-                            WHERE sopimusrivin_tunnus = '$rivitunnus'";
-            $laitesopres = pupe_query($laitesop);
-            if (mysql_num_rows($laitesopres) == 1) {
-              $query = "DELETE FROM laitteen_sopimukset
-                        WHERE sopimusrivin_tunnus = '$rivitunnus'";
-              $result = pupe_query($query);
-            }
+            $query = "DELETE FROM laitteen_sopimukset
+                      WHERE sopimusrivin_tunnus = '$rivitunnus'";
+            pupe_query($query);
           }
         }
 

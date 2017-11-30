029b069f2d28f636fb53af95c3e7a9b639f2f157
tomit <tomi@devlab.fi>
2017-11-30 16:07:39 +0200

    Aikarajaus suljettuihin

--- extranet_tyomaaraykset.php
+++ extranet_tyomaaraykset.php
@@ -279,6 +279,7 @@ function hae_kayttajan_tyomaaraykset() {
   $alatila = " AND lasku.alatila != 'X' ";
   if (!empty($request['nayta_poistetut'])) {
     $alatila = " AND lasku.alatila = 'X' ";
+    $aikarajaus = " AND lasku.luontiaika > '2016-01-01 00:00:00' ";
   }
 
   $query = "SELECT
@@ -330,6 +331,7 @@ function hae_kayttajan_tyomaaraykset() {
             WHERE lasku.yhtio      = '{$kukarow['yhtio']}'
             AND lasku.tila         in ('A','L','N','S','C')
             {$alatila}
+            {$aikarajaus}
             AND lasku.liitostunnus = '{$kukarow['oletus_asiakas']}'
             GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
             ORDER BY lasku.tunnus";

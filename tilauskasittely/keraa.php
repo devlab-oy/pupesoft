51aa2ba345e9d39e1fcbb06e446c887e5b68ce40
Johan Tötterman <juppe@devlab.fi>
2018-01-03 14:02:17 +0200

    Referenssiluku aina tilkpl:n mukaan

--- tilauskasittely/keraa.php
+++ tilauskasittely/keraa.php
@@ -2507,7 +2507,7 @@ if ($tee == 'P') {
             }
 
             $lavat[$lavanumero][$row['otunnus']] += round(($row['varattu']+$row['kpl'])/$row['myynti_era'], 2);
-            $lava_referenssiluku += (($row['varattu']+$row['kpl']) * $row['lavakoko']);
+            $lava_referenssiluku += ($row['tilkpl'] * $row['lavakoko']);
 
             $rivinumerot[$row["tunnus"]] = $kal;
             $kal++;

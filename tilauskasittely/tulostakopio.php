44c0b3dd76450bd3f8f73dfbffe607ac47b7c8d4
Tony Selkamo <tony@devlab.fi>
2018-04-17 14:49:00 +0300

    Keräyslista lavakeräys fix

--- tilauskasittely/tulostakopio.php
+++ tilauskasittely/tulostakopio.php
@@ -2408,6 +2408,7 @@ if ($tee == "TULOSTA" or $tee == 'NAYTATILAUS') {
                   tuote.eankoodi,
                   abs(tilausrivin_lisatiedot.asiakkaan_positio) asiakkaan_positio
                   FROM tilausrivi
+                  JOIN lasku ON tilausrivi.yhtio = lasku.yhtio and tilausrivi.otunnus = lasku.tunnus
                   LEFT JOIN tilausrivin_lisatiedot ON tilausrivi.yhtio = tilausrivin_lisatiedot.yhtio and tilausrivi.tunnus = tilausrivin_lisatiedot.tilausrivitunnus
                   JOIN tuote ON tilausrivi.yhtio = tuote.yhtio and tilausrivi.tuoteno = tuote.tuoteno
                   WHERE tilausrivi.otunnus  in ($tilausnumeroita)

7613cae32e75040691359511ed2d0213b8e413a9
Tero A <tero.airasjarvi@sprintit.fi>
2020-02-26 12:18:22 +0200

    Logmaster copied to Smarten

---
+++ rajapinnat/smarten/inbound_delivery.php
@@ -0,0 +1,253 @@
+<?php
+
+// Kutsutaanko CLI:st‰
+if (php_sapi_name() != 'cli') {
+
+  $_cli = false;
+
+  if (strpos($_SERVER['SCRIPT_NAME'], "inbound_delivery.php") !== false) {
+    require "../../inc/parametrit.inc";
+  }
+}
+else {
+
+  $_cli = true;
+
+  date_default_timezone_set('Europe/Helsinki');
+
+  if (trim($argv[1]) == '') {
+    die ("Et antanut l‰hett‰v‰‰ yhtiˆt‰!\n");
+  }
+
+  if (trim($argv[2]) == '') {
+    die ("Et antanut saapumisnumeroa!\n");
+  }
+
+  // lis‰t‰‰n includepathiin pupe-root
+  ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(dirname(dirname(__FILE__))));
+  ini_set("display_errors", 1);
+
+  error_reporting(E_ALL);
+
+  // otetaan tietokanta connect ja funktiot
+  require "inc/connect.inc";
+  require "inc/functions.inc";
+
+  // Logitetaan ajo
+  cron_log();
+
+  // Sallitaan vain yksi instanssi t‰st‰ skriptist‰ kerrallaan
+  pupesoft_flock();
+
+  $yhtio = mysql_escape_string(trim($argv[1]));
+  $yhtiorow = hae_yhtion_parametrit($yhtio);
+  $kukarow = hae_kukarow('admin', $yhtio);
+
+  if (!isset($kukarow)) {
+    exit("VIRHE: Admin k‰ytt‰j‰ ei lˆydy!\n");
+  }
+
+  $pupe_root_polku = dirname(__FILE__);
+
+  $saapumisnro = $argv[2];
+
+  if (!empty($argv[3])) {
+    $ordercode = $argv[3];
+  }
+}
+
+if (!in_array($yhtiorow['ulkoinen_jarjestelma'], array('', 'S'))) {
+  die ("Saapumisen l‰hett‰minen estetty yhtiˆtasolla!\n");
+}
+
+require "rajapinnat/smarten/smarten-functions.php";
+
+// Tarvitaan:
+// $saapumisnro
+// ordercode (vapaaehtoinen) (u = new, m = change, p = delete)
+
+$saapumisnro = (int) $saapumisnro;
+$ordercode = !isset($ordercode) ? 'U' : $ordercode;
+
+$query = "SELECT lasku.*, varastopaikat.ulkoisen_jarjestelman_tunnus
+          FROM lasku
+          INNER JOIN varastopaikat
+            ON (varastopaikat.yhtio = lasku.yhtio AND varastopaikat.tunnus = lasku.varasto)
+          WHERE yhtio = '{$kukarow['yhtio']}'
+          AND tila    = 'K'
+          AND alatila = ''
+          AND vanhatunnus = 0
+          AND tunnus  = '{$saapumisnro}'";
+$res = pupe_query($query);
+$row = mysql_fetch_assoc($res);
+
+if ($row['sisviesti3'] != '') {
+  pupesoft_log('smarten_inbound_delivery', "Saapuminen {$saapumisnro} on jo l‰hetetty");
+  exit;
+}
+
+// T‰m‰n saapumisen "p‰‰"-ostotilaus.
+$query = "SELECT otunnus, min(toimaika) toimaika, count(*) maara
+          FROM tilausrivi
+          WHERE yhtio     = '{$kukarow['yhtio']}'
+          AND tyyppi      = 'O'
+          AND kpl         = 0
+          AND varattu     > 0
+          AND uusiotunnus = '{$saapumisnro}'
+          GROUP BY otunnus
+          ORDER BY maara DESC
+          LIMIT 1";
+$tilasnumero_res = pupe_query($query);
+$tilasnumero_row = mysql_fetch_assoc($tilasnumero_res);
+
+// haetaan toimittajan tiedot
+$query = "SELECT *
+          FROM toimi
+          WHERE yhtio = '{$kukarow['yhtio']}'
+          AND tunnus  = '{$row['liitostunnus']}'";
+$toimires = pupe_query($query);
+$toimirow = mysql_fetch_assoc($toimires);
+
+// haetaan tilausrivit
+$query = "SELECT varastopaikat.*, tilausrivi.*
+          FROM tilausrivi
+          JOIN varastopaikat ON (
+            varastopaikat.yhtio   = tilausrivi.yhtio AND
+            varastopaikat.tunnus  = tilausrivi.varasto AND
+            varastopaikat.tyyppi != 'P' AND
+            varastopaikat.ulkoinen_jarjestelma IN ('L','P')
+          )
+          WHERE tilausrivi.yhtio     = '{$kukarow['yhtio']}'
+          AND tilausrivi.tyyppi      = 'O'
+          AND kpl         = 0
+          AND varattu     > 0
+          AND uusiotunnus = '{$saapumisnro}'";
+$rivit_res = pupe_query($query);
+$varasto_check = array();
+
+while ($rivit_row = mysql_fetch_array($rivit_res)) {
+  $varasto_check[$rivit_row['ulkoinen_jarjestelma']]++;
+}
+
+mysql_data_seek($rivit_res, 0);
+
+if ($varasto_check['L'] > 0 and $varasto_check['P'] == 0) {
+  $uj_nimi = "Velox";
+}
+elseif ($varasto_check['L'] == 0 and $varasto_check['P'] > 0) {
+  $uj_nimi = "Postnord";
+}
+else {
+  pupesoft_log('smarten_inbound_delivery', "Saapuminen {$row['laskunro']} sis‰lt‰‰ useamman ulkoisen varaston tuotteita. Ei saa sis‰lt‰‰ kuin yht‰.");
+  exit("Saapuminen {$row['laskunro']} sis‰lt‰‰ useamman ulkoisen varaston tuotteita. Ei saa sis‰lt‰‰ kuin yht‰.");
+}
+
+# Rakennetaan XML
+$xml = simplexml_load_string("<?xml version='1.0' encoding='UTF-8'?><Message></Message>");
+
+$header = $xml->addChild('MessageHeader');
+$header->addChild('MessageType', 'inboundDelivery');
+$header->addChild('Sender',      xml_cleanstring($yhtiorow['nimi']));
+$header->addChild('Receiver',    'smarten');
+
+$body = $xml->addChild('VendReceiptsList');
+$body->addChild('PurchId',          $row['laskunro']);
+$body->addChild('ReceiptsListId',   $tilasnumero_row['otunnus']);
+$body->addChild('OrderCode',        $ordercode);
+$body->addChild('OrderType',        'PO');
+$body->addChild('ReceiptsListDate', tv1dateconv($row['luontiaika']));
+$body->addChild('DeliveryDate',     tv1dateconv($tilasnumero_row['toimaika']));
+if ($uj_nimi == "Velox") {
+  $body->addChild('Comments',        xml_cleanstring($row['comments']));
+}
+$body->addChild('Warehouse',        xml_cleanstring($row['ulkoisen_jarjestelman_tunnus']));
+
+$vendor = $body->addChild('Vendor');
+$vendor->addChild('VendAccount',  xml_cleanstring($toimirow['toimittajanro']));
+$vendor->addChild('VendName',     xml_cleanstring($row['nimi']));
+$vendor->addChild('VendStreet',   xml_cleanstring($row['osoite']));
+$vendor->addChild('VendPostCode', xml_cleanstring($row['postino']));
+$vendor->addChild('VendCity',     xml_cleanstring($row['postitp']));
+$vendor->addChild('VendCountry',  xml_cleanstring($row['maa']));
+$vendor->addChild('VendInfo',     '');
+
+$purchaser = $body->addChild('Purchaser');
+$purchaser->addChild('PurcAccount',  xml_cleanstring($yhtiorow['ytunnus']));
+$purchaser->addChild('PurcName',     xml_cleanstring($yhtiorow['nimi']));
+$purchaser->addChild('PurcStreet',   xml_cleanstring($yhtiorow['osoite']));
+$purchaser->addChild('PurcPostCode', xml_cleanstring($yhtiorow['postino']));
+$purchaser->addChild('PurcCity',     xml_cleanstring($yhtiorow['postitp']));
+$purchaser->addChild('PurcCountry',  xml_cleanstring($yhtiorow['maa']));
+
+$i = 1;
+$ostotilaukset = array();
+
+while ($rivit_row = mysql_fetch_assoc($rivit_res)) {
+  $ostotilaukset[] = $rivit_row['otunnus'];
+
+  $lines = $body->addChild('Lines');
+  $line = $lines->addChild('Line');
+  $line->addAttribute('No', $i);
+  $line->addChild('TransId',         xml_cleanstring($rivit_row['tunnus']));
+  $line->addChild('ItemNumber',      xml_cleanstring($rivit_row['tuoteno'], 32));
+  $line->addChild('OrderedQuantity', xml_cleanstring($rivit_row['varattu']));
+  $line->addChild('Unit',            xml_cleanstring($rivit_row['yksikko']));
+  $line->addChild('Price',           xml_cleanstring($rivit_row['hinta']));
+  $line->addChild('CurrencyCode',    xml_cleanstring($row['valkoodi']));
+  $line->addChild('RowInfo',         xml_cleanstring($rivit_row['kommentti']));
+
+  $i++;
+}
+
+$xml_chk = (isset($xml->VendReceiptsList) and isset($xml->VendReceiptsList->Lines));
+
+if ($xml_chk) {
+  $ostotilaukset = array_unique($ostotilaukset);
+
+  $_name = substr("in_{$row['laskunro']}_".implode('_', $ostotilaukset), 0, 25);
+  $filename = $pupe_root_polku."/dataout/{$_name}.xml";
+
+  if (file_put_contents($filename, $xml->asXML())) {
+
+    $palautus = smarten_send_file($filename);
+
+    if ($palautus == 0) {
+      pupesoft_log('smarten_inbound_delivery', "Siirretiin saapuminen {$row['laskunro']}.");
+    }
+    else {
+      pupesoft_log('smarten_inbound_delivery', "Saapumisen {$row['laskunro']} siirt‰minen ep‰onnistui.");
+    }
+
+    pupesoft_log('smarten_inbound_delivery', "Saapumisen {$row['laskunro']} luonti onnistui.");
+
+    if ($_cli) {
+      echo "\n", t("Tiedoston luonti onnistui"), "\n";
+    }
+    else {
+      echo "<br /><font class='message'>", t("Tiedoston luonti onnistui"), "</font><br />";
+    }
+
+
+    $query = "UPDATE lasku SET
+              sisviesti3  = 'lahetetty_ulkoiseen_jarjestelmaan'
+              WHERE yhtio = '{$kukarow['yhtio']}'
+              AND tila    = 'K'
+              AND tunnus  = '{$saapumisnro}'";
+    $updres = pupe_query($query);
+
+    pupesoft_log('smarten_inbound_delivery', "Saapuminen {$row['laskunro']} l‰hetetty");
+  }
+  else {
+    pupesoft_log('smarten_inbound_delivery', "Saapumisen {$row['laskunro']} luonti ep‰onnistui");
+
+    if ($_cli) {
+      echo "\n", t("Tiedoston luonti ep‰onnistui"), "\n";
+    }
+    else {
+      echo "<br /><font class='error'>", t("Tiedoston luonti ep‰onnistui"), "</font><br />";
+    }
+  }
+}
+else {
+  pupesoft_log('smarten_inbound_delivery', "Saapumisen {$saapumisnro} XML ei luotu, koska yht‰‰n rivi‰ ei lˆytynyt.");
+}
---
+++ rajapinnat/smarten/stock_report.php
@@ -0,0 +1,275 @@
+<?php
+
+// Kutsutaanko CLI:st‰
+if (php_sapi_name() != 'cli') {
+  die ("T‰t‰ scripti‰ voi ajaa vain komentorivilt‰!\n");
+}
+
+date_default_timezone_set('Europe/Helsinki');
+
+if (trim($argv[1]) == '') {
+  die ("Et antanut l‰hett‰v‰‰ yhtiˆt‰!\n");
+}
+
+if (trim($argv[2]) == '') {
+  die ("Et antanut luettavien tiedostojen polkua!\n");
+}
+
+if (trim($argv[3]) == '') {
+  die ("Et antanut s‰hkˆpostiosoitetta!\n");
+}
+
+// lis‰t‰‰n includepathiin pupe-root
+ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(dirname(dirname(__FILE__))));
+ini_set("display_errors", 1);
+ini_set("memory_limit", "2G");
+
+error_reporting(E_ALL);
+
+// otetaan tietokanta connect ja funktiot
+require "inc/connect.inc";
+require "inc/functions.inc";
+require "rajapinnat/smarten/smarten-functions.php";
+
+// Logitetaan ajo
+cron_log();
+
+// Sallitaan vain yksi instanssi t‰st‰ skriptist‰ kerrallaan
+pupesoft_flock();
+
+$yhtio = mysql_real_escape_string(trim($argv[1]));
+$yhtiorow = hae_yhtion_parametrit($yhtio);
+$kukarow = hae_kukarow('admin', $yhtio);
+
+if (empty($kukarow)) {
+  exit("VIRHE: Admin k‰ytt‰j‰ ei lˆydy!\n");
+}
+
+$path = trim($argv[2]);
+$error_email = trim($argv[3]);
+$email_array = array();
+
+$path = rtrim($path, '/').'/';
+$handle = opendir($path);
+
+if ($handle === false) {
+  exit;
+}
+
+$query = "SELECT *
+              FROM avainsana
+              WHERE yhtio = '{$kukarow['yhtio']}'
+              AND laji    = 'INVEN_LAJI'";
+$result = pupe_query($query);
+$inventointi_selitteet = array();
+while ($inventointi_selite = mysql_fetch_assoc($result)) {
+  $inventointi_selitteet[] = $inventointi_selite;
+  echo "INVEN_LAJI: {$inventointi_selite}\n";
+}
+
+if (sizeof($inventointi_selitteet) == 1) {
+  $inventointi_seliteen_tunnus = $inventointi_selitteet[0];
+}
+
+if (!empty($inventointi_seliteen_tunnus)) {
+  $query = "SELECT *
+              FROM avainsana
+              WHERE yhtio = '{$kukarow['yhtio']}'
+              AND tunnus  = '{$inventointi_seliteen_tunnus}'";
+  $result = pupe_query($query);
+  $row = mysql_fetch_assoc($result);
+
+  $inven_laji = $row['selite'];
+}
+else {
+  $inven_laji = "";
+}
+
+while (false !== ($file = readdir($handle))) {
+  $full_filepath = $path.$file;
+  $message_type = smarten_message_type($full_filepath);
+
+  if ($message_type != 'StockReport') {
+    continue;
+  }
+
+  $xml = simplexml_load_file($full_filepath);
+
+  pupesoft_log('smarten_stock_report', "K‰sitell‰‰n sanoma {$file}");
+
+  $luontiaika = $xml->InvCounting->TransDate;
+
+  unset($xml->InvCounting->TransDate);
+
+  $saldoeroja = array();
+
+  foreach ($xml->InvCounting->Line as $line) {
+    $item_number = $line->ItemNumber;
+    $item_number = str_replace(chr(194) . chr(160), " ", $item_number);
+
+    $kpl                       = (float) $line->Quantity;
+    $smarten_itemnumberfield = smarten_field('ItemNumber');
+
+    $query = "SELECT tuoteno, nimitys
+              FROM tuote
+              WHERE yhtio  = '{$kukarow['yhtio']}'
+              AND {$smarten_itemnumberfield} = '{$item_number}'";
+    $tuoteres = pupe_query($query);
+    $tuoterow = mysql_fetch_assoc($tuoteres);
+
+    if (mysql_num_rows($tuoteres) == 0) {
+      $saldoeroja[] = array(
+        "varasto"   => "",
+        "item"      => $item_number,
+        "smarten" => $kpl,
+        "nimitys"   => "",
+        "pupe"      => "",
+        "tuoteno"   => "",
+        "lisatieto" => "Tuotetta ei lˆydy",
+      );
+
+      continue;
+    }
+
+    $tuoteno = $tuoterow["tuoteno"];
+
+    $query = "SELECT *
+              FROM varastopaikat
+              WHERE yhtio = '{$kukarow['yhtio']}'
+                AND ulkoinen_jarjestelma = 'P'
+                AND ulkoisen_jarjestelman_tunnus = '" . $line->Stock . "'";
+    $varastores = pupe_query($query);
+    if (mysql_num_rows($varastores) == 1) {
+      $varastorow = mysql_fetch_assoc($varastores);
+    }
+    else {
+      $saldoeroja[] = array(
+        "varasto"   => "",
+        "item"      => $item_number,
+        "smarten" => $kpl,
+        "nimitys"   => $tuoterow['nimitys'],
+        "pupe"      => "",
+        "tuoteno"   => $tuoteno,
+        "lisatieto" => t("Ulkoisen j‰rjestelm‰n varastotunnusta ei lˆydy") . ": " . $line->Stock,
+      );
+
+      continue;
+    }
+
+    list($saldo, $hyllyssa, $myytavissa, $devnull) = saldo_myytavissa($tuoterow["tuoteno"], "KAIKKI", $varastorow['tunnus']);
+
+    // Etuk‰teen maksetut tilaukset, jotka ovat ker‰‰m‰tt‰ mutta tilaus jo laskutettu
+    // Lasketaan ne mukaan Pupen hyllyss‰ m‰‰r‰‰n, koska saldo_myytavissa ei huomioi niit‰
+    $query = "SELECT ifnull(sum(tilausrivi.kpl), 0) AS keraamatta
+              FROM tilausrivi
+              INNER JOIN lasku on (lasku.yhtio = tilausrivi.yhtio
+                AND lasku.tunnus          = tilausrivi.otunnus
+                AND lasku.mapvm          != '0000-00-00'
+                AND lasku.chn             = '999')
+              WHERE tilausrivi.yhtio      = '{$kukarow['yhtio']}'
+              AND tilausrivi.tyyppi       = 'L'
+              AND tilausrivi.var         != 'P'
+              AND tilausrivi.keratty      = ''
+              AND tilausrivi.kerattyaika  = '0000-00-00 00:00:00'
+              AND tilausrivi.tuoteno      = '{$tuoterow['tuoteno']}'";
+    $ker_result = pupe_query($query);
+    $ker_rivi = mysql_fetch_assoc($ker_result);
+
+    $hyllyssa += $ker_rivi['keraamatta'];
+
+    // Vertailukonversio
+    $a = (int) $kpl * 10000;
+    $b = (int) $hyllyssa * 10000;
+
+    if ($a != $b) {
+      $lisatieto = "";
+
+      if (isset($varastorow)) {
+        $query = "SELECT *, concat_ws('-', hyllyalue, hyllynro, hyllyvali, hyllytaso) AS tuotepaikka
+                  FROM tuotepaikat
+                  WHERE yhtio = '{$kukarow['yhtio']}'
+                    AND varasto = '{$varastorow['tunnus']}'
+                    AND tuoteno = '{$tuoteno}'
+                  ORDER BY oletus DESC, prio, hyllyalue, hyllynro, hyllytaso, hyllypaikka";
+        $tuotepaikat = pupe_query($query);
+
+        if (mysql_num_rows($tuotepaikat) > 0) {
+          while ($tuotepaikka_row = mysql_fetch_array($tuotepaikat)) {
+            $tuotepaikka = $tuotepaikka_row['tuotepaikka'];
+
+            if ($lisatieto == "") {
+              $uusi_maara = $kpl;
+              $lisatieto = t("Inventoitu tuotepaikalle") . " {$tuotepaikka}";
+            }
+            else {
+              $uusi_maara = 0;
+              $lisatieto .= ", " . t("paikan") . " {$tuotepaikka} " . t("saldo nollattu");
+            }
+
+            $hylly = array($tuoteno, $tuotepaikka_row['hyllyalue'], $tuotepaikka_row['hyllynro'], $tuotepaikka_row['hyllyvali'], $tuotepaikka_row['hyllytaso']);
+            $hash = implode('###', $hylly);
+            $tuote = array($tuotepaikka_row['tunnus'] => $hash);
+            $maara = array($tuotepaikka_row['tunnus'] => $uusi_maara);
+            $tee = 'VALMIS';
+            $lisaselite = t("smarten");
+            $mobiili = 'YES';
+
+            require 'inventoi.php';
+          }
+        }
+        else {
+          $lisatieto = t("Ei inventoitu") . ": '" . $varastorow['nimitys'] . "' " . t("varastosta ei lˆydy tuotepaikkaa tuotteelle") . " '{$tuoteno}'";
+        }
+      }
+      else {
+        $lisatieto = t("Ei inventoitu") . ": '" . $line->Stock . "' ". t("varastoa ei lˆydy tuotteelle") . " '{$tuoteno}'";
+      }
+
+      $saldoeroja[] = array(
+        "varasto"   => $varastorow['tunnus'] . " " . $varastorow['nimitys'],
+        "item"      => $item_number,
+        "smarten" => $kpl,
+        "nimitys"   => $tuoterow['nimitys'],
+        "pupe"      => $hyllyssa,
+        "tuoteno"   => $tuoteno,
+        "lisatieto" => $lisatieto,
+      );
+    }
+  }
+
+  if (count($saldoeroja) > 0) {
+
+    $email_array[] = t("Seuraavien tuotteiden saldovertailuissa on havaittu eroja").":";
+    $email_array[] = t("Varasto").";".t("smarten-tuoteno").";".t("Tuoteno").";".t("Nimitys").";".t("smarten-kpl").";".t("Pupesoft-kpl").";".t("Lis‰tieto");
+
+    foreach ($saldoeroja as $ero) {
+      $email_array[] = "{$ero['varasto']};{$ero['item']};{$ero['tuoteno']};{$ero['nimitys']};{$ero['smarten']};{$ero['pupe']};{$ero['lisatieto']}";
+    }
+
+    pupesoft_log('smarten_stock_report', "Sanoman {$file} saldovertailussa oli eroja.");
+  }
+  else {
+    pupesoft_log('smarten_stock_report', "Sanoman {$file} saldovertailussa ei ollut eroja.");
+  }
+
+  $params = array(
+    'email' => $error_email,
+    'email_array' => $email_array,
+    'log_name' => 'smarten_stock_report',
+  );
+
+  // smarten_send_email($params);
+
+  echo "\n";
+  foreach($email_array as $line) {
+    echo "{$line}\n";
+  }
+  echo "\n";
+
+  // siirret‰‰n tiedosto done-kansioon
+  rename($full_filepath, $path.'done/'.$file);
+
+  pupesoft_log('smarten_stock_report', "Sanoman {$file} saldovertailu k‰sitelty");
+}
+
+closedir($handle);

46f837ffa197dcdc25b41f4c95621cec18337ffa
Johan Tötterman <johan.totterman@sprintit.fi>
2020-09-30 15:55:15 +0300

    Osataan valita oikea soppari myös silloin kun sopimuksia on vain yksi

--- nordeafactoring.php
+++ nordeafactoring.php
@@ -70,7 +70,7 @@ elseif (mysql_num_rows($factoring_result) == 1) {
   // meillä on vaan yksi, ei tarvitse valita
   $vrow = mysql_fetch_assoc($factoring_result);
   $factoring_id = $vrow['tunnus'];
-  $tee = isset($tee) ? $tee : 'TOIMINNOT';
+  $tee = !empty($tee) ? $tee : 'TOIMINNOT';
 }
 
 if ($tee == '') {

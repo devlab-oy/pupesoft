3eb643f594c0e128d83b575c9331510c80e847cf
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2021-05-19 15:01:40 +0300

    Maksutavat / statukset

--- rajapinnat/mycashflow/mycf_tilaukset.php
+++ rajapinnat/mycashflow/mycf_tilaukset.php
@@ -79,10 +79,10 @@ class MyCashflowTilaukset {
     $this->mycf_kaupat = $value;
   }
 
-  public function get_asiakastiedot($asiakasnro, $etsi_asiakas) {
+  public function get_asiakastiedot($kauppaversio, $etsi_asiakas) {
 
     // vain version 6 asiakkaan tiedot haetaan
-    if(!in_array($asiakasnro, array(99999))) {
+    if(!in_array($kauppaversio, array(6))) {
       return;
     }
     
@@ -162,7 +162,7 @@ class MyCashflowTilaukset {
     $tilaus = array();
 
     foreach ($xml->Order as $order) {
-
+      
       // Ohitetaan duplikaatit
       $query = "SELECT asiakkaan_tilausnumero
                 FROM lasku
@@ -253,15 +253,18 @@ class MyCashflowTilaukset {
       $tilaus['items'] = array();
 
       // Yritetään hakea erikoisasiakastiedot ja muut tiedot
-      if($asiakastiedot = $this->get_asiakastiedot($options['asiakasnro'], $order->CustomerInformation)) {
+      if($asiakastiedot = $this->get_asiakastiedot($kauppaversio, $order->CustomerInformation)) {
         $options['asiakasnro'] = $asiakastiedot['asiakasnro'];
         $kurssi_kerroin = $asiakastiedot['kurssi_kerroin'];
+        $custom_payment_method = (array) $order->PaymentMethod;
+        $tilaus['payment']['method'] = $custom_payment_method[0];
+        $tilaus['status'] = "";
       }
 
       foreach ($order->Products->Product as $product) {
 
         // Maksutavan hinta
-        if (!empty($product->ProductID->attributes()->PaymentID)) {
+        if (!isset($custom_payment_method) and !empty($product->ProductID->attributes()->PaymentID)) {
           $tilaus['payment']['method'] = "";
           continue;
         }

07a1e16dcd4fdf463e2b0903ea48c0f98310fb86
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2021-06-02 12:50:39 +0300

    Update fitek_laskut_cron.php

--- fitek_laskut_cron.php
+++ fitek_laskut_cron.php
@@ -48,14 +48,22 @@ $fitek_xml_cron_tiedot = array(
   "toimi" => $fitek_xml_cron_toimi
 );
 
+// kansiot pit‰‰ luoda etuk‰teen ja groupina "apache" group
 $verkkolaskut_in     = $fitek_xml_cron_dirname;
 $verkkolaskut_ok     = $fitek_xml_cron_dirname."/ok";
 $verkkolaskut_orig   = $fitek_xml_cron_dirname."/orig";
 $verkkolaskut_error  = $fitek_xml_cron_dirname."/error";
 $verkkolaskut_reject = $fitek_xml_cron_dirname."/reject";
 
-$ftp_exclude_files = array_diff(scandir($verkkolaskut_orig), array('..', '.'));
+$ftphost = $ftphost_fitek_cron;
+$ftpuser = $ftpuser_fitek_cron;
+$ftppass = $ftppass_fitek_cron;
+$ftpport = $ftpport_fitek_cron;
+$ftppath = $ftppath_fitek_cron;
+$ftpdest = $ftpdest_fitek_cron;
 
+// jo olevia olemassa tiedostoja kansiossa orig ei saa k‰sitell‰ uudestaan
+$ftp_exclude_files = array_diff(scandir($verkkolaskut_orig), array('..', '.'));
 require 'sftp-get.php';
 
 include_once("verkkolasku-in.php");

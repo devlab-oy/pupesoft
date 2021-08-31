0d478dd596e8250408c2f91e84de814b03d4710c
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2021-08-31 12:57:12 +0300

    fix 2

--- tilauskasittely/tuote_selaus_haku.php
+++ tilauskasittely/tuote_selaus_haku.php
@@ -1697,16 +1697,16 @@ if ($submit_button != '' and ($lisa != '' or $lisa_parametri != '')) {
                                           AND toim_tuoteno in ('".$vertaile_tuoteno."');";
         $tarkista_tehdas_saldot = pupe_query($tarkista_tehdas_saldot_query);
         while ($_tarkista_tehdas = mysql_fetch_assoc($tarkista_tehdas_saldot)) {
+          if($_tarkista_tehdas['tehdas_saldo'] <= 0) {
+            continue;
+          }
           $hae_toim_nimi = "SELECT nimi  
             FROM toimi
             WHERE yhtio = '$kukarow[yhtio]'
              AND tunnus = '$_tarkista_tehdas[liitostunnus]';";
           $hae_toim_nimi = pupe_query($hae_toim_nimi);
           $hae_toim_nimi = mysql_fetch_assoc($hae_toim_nimi);
-          $_tarkista_tehdas['toim_nimitys'] = $hae_toim_nimi['nimi'];
-          if($_tarkista_tehdas['tehdas_saldo'] <= 0) {
-            continue;
-          }
+          $_tarkista_tehdas['toim_nimitys'] = $hae_toim_nimi[];
           if (!$suoratoimits_ominaisuus) {
             $suoratoimits_ominaisuus = array();
           }

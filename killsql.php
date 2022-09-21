ce8b215bfc0923a0c81a673e597b0b3d7a2a67a3
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2022-09-21 10:44:14 +0300

    show all processes too

--- killsql.php
+++ killsql.php
@@ -14,7 +14,9 @@ $result = pupe_query("SHOW FULL PROCESSLIST");
 <select name="id">
 <option checked><?php echo t('Valitse kysely'); ?></option>
 <?php 
+$kaikki_prosessit = array();
 while ($row = mysql_fetch_array($result)) {
+  $kaikki_prosessit[] = $row;
   if ($row["Time"] >= 10 and $row["Info"] !== NULL) {
     if($id and $row["Id"] == $id) {
       $result = pupe_query("KILL $id");
@@ -30,11 +32,20 @@ while ($row = mysql_fetch_array($result)) {
 }
 ?>
 </select>
+
 <div>
 <div style="margin-top: 5px; margin-bottom: 5px; padding: 15px; background-color: red; color: #ffffff; display: inline-block;">
 <?php echo t('OLE TOSI TARKKA ENNEN KUIN PAINAT "LOPETA" -NAPPIA!'); ?>
 </div></div>
 <input type="submit" value="<?php echo t('Lopeta'); ?>">
+<h3><?php echo t("Kaikki prosessit"); ?></h3>
+<?php
+foreach ($kaikki_prosessit as $prosessi) {
+  if ($prosessi["Time"] >= 0 and $prosessi["Info"] !== NULL) {
+    echo substr($prosessi["Info"], 0, 100)."...<hr>";
+  }
+}
+?>
 </form>
 <?php 
 

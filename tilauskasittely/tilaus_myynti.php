ea81630c37c4d79182e7b0e16683e0ea340b5180
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2022-09-29 19:12:01 +0300

    Button color

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -9135,7 +9135,7 @@ if ($tee == '') {
             <input type='hidden' name='maara' value='".$row['tilkpl']."'>
             <input type='hidden' name='hinta' value='".$row['hinta']."'>
             <input type='hidden' name='palaute_lisaa' value='1'>
-            <input type='submit' style='line-height: 20px; height: 35px;' value='".t("Palaute")." &#128232;'>
+            <input type='submit' class='tuotepalaute_btn' style='line-height: 20px; height: 35px;' value='".t("Palaute")." &#128232;'>
             </form>";
           }
         }

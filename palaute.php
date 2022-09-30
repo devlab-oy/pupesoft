c5418d24728d44c3822a5b0149052302a649970b
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2022-09-30 12:49:53 +0300

    Fixes and cleaning

--- palaute.php
+++ palaute.php
@@ -1,23 +1,29 @@
 <?php
 ob_start();
 require "inc/parametrit.inc";
+
+if(!isset($palaute_dl_tiedosto) or !$palaute_dl_tiedosto) {
+  $palaute_dl_tiedosto = getcwd()."/datain/palaute_dl.csv";
+}
+
 if (!isset($palaute_dl)) $palaute_dl = false;
-if($palaute_dl and $file = getcwd().'/datain/palaute_dl.csv' and file_exists($file)) {
+if($palaute_dl and file_exists($palaute_dl_tiedosto)) {
   ob_end_clean();
   header('Content-Description: File Transfer');
   header('Content-Type: application/octet-stream');
   header("Cache-Control: no-cache, must-revalidate");
   header("Expires: 0");
-  header('Content-Disposition: attachment; filename="'.date("d_m_Y-H_i_s", filemtime($file))."-".basename($file).'"');
-  header('Content-Length: ' . filesize($file));
+  header('Content-Disposition: attachment; filename="'.date("d_m_Y-H_i_s", filemtime($palaute_dl_tiedosto))."-".basename($palaute_dl_tiedosto).'"');
+  header('Content-Length: ' . filesize($palaute_dl_tiedosto));
   header('Pragma: public');
   flush();
-  readfile($file);
+  readfile($palaute_dl_tiedosto);
   exit;
 }
 
 if (!isset($palaute_mail)) $palaute_mail = false;
-if($palaute_mail and $file = getcwd().'/datain/palaute_dl.csv' and file_exists($file)) {
+
+if($palaute_mail and file_exists($palaute_dl_tiedosto)) {
   $viikko_obj = new DateTime();
   $viikko = $viikko_obj->format("W");
   ob_end_clean();
@@ -28,8 +34,8 @@ if($palaute_mail and $file = getcwd().'/datain/palaute_dl.csv' and file_exists($
     "body"       => t('Tuotepalautteet, viikko ').$viikko,
     "attachements"   => array(
       array(
-        "filename"     => $file,
-        "newfilename"   => date("d_m_Y-H_i_s", filemtime($file))."-".basename($file),
+        "filename"     => $palaute_dl_tiedosto,
+        "newfilename"   => date("d_m_Y-H_i_s", filemtime($palaute_dl_tiedosto))."-".basename($palaute_dl_tiedosto),
         "ctype"       => "csv"
       )
     )
@@ -53,16 +59,23 @@ if($palaute_lisaa and $maara and $hinta and $tuoteno and $palaute_kuka) {
     $palaute_kuka,
     date("d.m.Y H:i:s")
   );
-  $tiedosto = fopen(getcwd()."/datain/palaute_dl.csv", "a");
-  fputcsv($tiedosto, $lisataan);
-  fclose($handle); 
-  echo '<div class="vastaus">';
-  echo t('Palaute lähetetty &#10003;');
-  echo '</div>';
+  if($tiedosto = fopen($palaute_dl_tiedosto, "a")) {
+    fputcsv($tiedosto, $lisataan);
+    fclose($handle); 
+    echo '<div class="vastaus">';
+    echo t('Palaute lähetetty &#10003;');
+    echo '</div>';
+  } else {
+    echo '<div class="vastaus">';
+    echo t('VIRHE!');
+    echo '</div>';
+  }
+
   exit;
 }
 
 if ($oikeurow['paivitys'] === '') {
+  ob_end_clean();
   echo "<table width='100%' height='70%'>";
   echo "<tr>";
   echo "<td style='text-align:center;font-size:9pt;font-family:Lucida,Verdana,Helvetica,Arial; color: #666;'>";
@@ -76,7 +89,7 @@ if ($oikeurow['paivitys'] === '') {
 
 echo "<font class='head'>";
 echo t("Tuotepalautteet"), "</font><hr>";
-if($file = getcwd().'/datain/palaute_dl.csv' and file_exists($file)) {
+if(file_exists($palaute_dl_tiedosto)) {
 ?>
 <p><?php echo t('Alla on tiedosto, joka sisältää kaikki palautteet.'); ?>
 <div><a class="message warning" target="_blank" href="?palaute_dl=lataa"><?php echo t('Lataa CSV'); ?></a></div>
